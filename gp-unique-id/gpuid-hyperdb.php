f6f54611f2d93965aada8cde5f935c63ea2f405f
Clay Griffiths <clay@claygriffiths.com>
2020-09-01 20:53:51 -0500

    Added gpuid-hyperdb.php
    Original Gist: https://gist.github.com/spivurno/b25723b0f17c1bd28ab6c5e9100fc506

---
+++ gp-unique-id/gpuid-hyperdb.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Gravity Perks // GP Unique ID // HyperDB Support for Sequential IDs
+ * http://gravitywiz.com/documentation/gravity-forms-unique-id/
+ */
+add_filter( 'gpui_sequential_unique_id_pre_insert', function ( $uid, $form_id, $field_id, $starting_number ) {
+	global $wpdb;
+
+	$wpdb->query( 'START TRANSACTION' );
+
+	$sql = $wpdb->prepare(
+		'INSERT INTO ' . $wpdb->prefix . 'gpui_sequence ( form_id, field_id, current ) VALUES ( %d, %d, ( @next := 1 ) ) ON DUPLICATE KEY UPDATE current = ( @next := current + 1 )',
+		$form_id,
+		$field_id
+	);
+
+	$wpdb->query( $sql );
+	$uid = $wpdb->get_var( $wpdb->prepare( 'SELECT `current` from ' . $wpdb->prefix . 'gpui_sequence where form_id = %d and field_id = %d', $form_id, $field_id ) );
+
+	$wpdb->query( 'COMMIT' );
+
+	return $uid;
+}, 10, 5 );
