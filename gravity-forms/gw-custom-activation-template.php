621c05dd62910b998ad97e53c3fe8d3fb602db4b
Eihab Ibrahim <eihab@gravitywiz.com>
2020-09-08 21:02:55 -0400

    ~ PHPCS: Code cleanup

--- gravity-forms/gw-custom-activation-template.php
+++ gravity-forms/gw-custom-activation-template.php
@@ -3,16 +3,17 @@
 * Gravity Forms Custom Activation Template
 * http://gravitywiz.com/customizing-gravity-forms-user-registration-activation-page
 */
-add_action('wp', 'custom_maybe_activate_user', 9);
+add_action( 'wp', 'custom_maybe_activate_user', 9 );
 function custom_maybe_activate_user() {
 
-    $template_path = STYLESHEETPATH . '/gfur-activate-template/activate.php';
-    $is_activate_page = isset( $_GET['page'] ) && $_GET['page'] == 'gf_activation';
-    
-    if( ! file_exists( $template_path ) || ! $is_activate_page  )
-        return;
-    
-    require_once( $template_path );
-    
-    exit();
+	$template_path    = STYLESHEETPATH . '/gfur-activate-template/activate.php';
+	$is_activate_page = isset( $_GET['page'] ) && $_GET['page'] === 'gf_activation';
+
+	if ( ! file_exists( $template_path ) || ! $is_activate_page ) {
+		return;
+	}
+
+	require_once( $template_path );
+
+	exit();
 }
