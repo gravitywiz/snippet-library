5b1cfa9d00b9417c643f4b7d4c5d70cb0d9a15fb
Eihab Ibrahim <eihab@gravitywiz.com>
2020-09-08 21:04:18 -0400

    Fixed a compatibility issue with WP 5.5 and newer versions of GFUR.

--- gravity-forms/gw-custom-activation-template.php
+++ gravity-forms/gw-custom-activation-template.php
@@ -8,6 +8,7 @@ function custom_maybe_activate_user() {
 
 	$template_path    = STYLESHEETPATH . '/gfur-activate-template/activate.php';
 	$is_activate_page = isset( $_GET['page'] ) && $_GET['page'] === 'gf_activation';
+	$is_activate_page = $is_activate_page || isset( $_GET['gfur_activation'] ); // WP 5.5 Compatibility
 
 	if ( ! file_exists( $template_path ) || ! $is_activate_page ) {
 		return;
