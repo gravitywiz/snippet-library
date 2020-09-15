3fd3d5c887ced00af7df711a399455e4c8a208a6
Scott Buchmann <scott@gravitywiz.com>
2020-09-15 11:22:44 -0400

    Added gppr-validation-messages.php Original Gist: https://gist.github.com/spivurno/0101cef16768da629ff24b8e1ab07891

---
+++ gp-price-range/gppr-validation-messages.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Gravity Perks // GP Price Range // Modify Validation Messages
+ * http://gravitywiz.com/documentation/gp-price-range/
+ */
+add_filter( 'gppr_validation_messages', function( $messages, $min, $max, $form_id, $field_id ) {
+    
+    $min = GFCommon::to_money( $min );
+    $max = GFCommon::to_money( $max );
+    
+    $messages['min_and_max'] = sprintf( 'Please enter an amount between %s and %s.', $min, $max );
+    $messages['min'] = sprintf( 'Please enter a price greater than or equal to %s.', $min );
+    $messages['max'] = sprintf( 'Please enter a price less than or equal to %s.', $max );
+    
+    return $messages;
+}, 10, 5 );
