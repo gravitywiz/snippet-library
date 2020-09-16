61505f4bea9ba7e18f3cf5763ea9a1808cbca0f2
David Smith <david@gravitywiz.com>
2020-09-15 20:03:41 -0400

    New snippet! Populate All Results; include all results as a comma-delimited list instead of only the first found result.

---
+++ gp-populate-anything/gppa-populate-all-results.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Gravity Perks // Populate Anything // Populate All Results
+ * https://gravitywiz.com/documentation/gravity-forms-populate-anything/
+ *
+ * By default, when populating a field's value, Populate Anything will only populated the first found result. This
+ * snippet adds support for populating all found results. Just add the "gppa-populate-all" CSS class to the field's
+ * CSS Class Name setting.
+ */
+add_filter( 'gppa_process_template_value', function( $template_value, $field, $template_name, $populate, $object, $object_type, $objects, $template ) {
+	if ( strpos( $field->cssClass, 'gppa-populate-all' ) !== false ) {
+		$values = array();
+		foreach ( $objects as $_object ) {
+			$values[] = $object_type->get_object_prop_value( $_object, $template );
+		}
+		$template_value = implode( ', ', $values );
+	}
+	return $template_value;
+}, 10, 8 );
