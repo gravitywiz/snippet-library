a3ab16cae7b6a50708d520be61cbee663ef5a3dd
Scott Buchmann <scott@gravitywiz.com>
2020-09-18 13:22:27 -0400

    Added gpecf-consolidate-discounts.php and gpecf-custom-total.php
    Original Gists: https://gist.github.com/spivurno/18c9a470db892ecaa12cf2340fbba6aa
    		https://gist.github.com/spivurno/b015d2576ee3270e6f1b86eb26eae4b2

---
+++ gp-ecommerce-fields/gpecf-consolidate-discounts.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Gravity Perks // eCommerce Fields // Consolidate Separate Discount Line Items into a Single Discounts Line Item
+ * http://gravitywiz.com/documentation/gravity-forms-ecommerce-fields/
+ *
+ * Default:    https://gwiz.io/2IiPALf
+ * w/ Snippet: https://gwiz.io/2Itfyfo
+ */
+// Update "123" to your form ID - or - remove "_123" to apply to all forms.
+add_filter( 'gpecf_order_summary_123', function( $summary ) {
+
+	if( empty( $summary['discounts'] ) ) {
+		return $summary;
+	}
+
+	$consolidated_discount = $summary['discounts'][0];
+	$consolidated_discount['name'] = 'Discounts';
+	$consolidated_discount['price'] = 0;
+
+	foreach( $summary['discounts'] as $discount ) {
+		$consolidated_discount['price'] += $discount['price'];
+	}
+
+	$summary['discounts'] = array( $consolidated_discount );
+
+	return $summary;
+} );
