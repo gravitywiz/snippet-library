c84111c8b9d5fee5db4c128a5ea1c36ab0c1ff00
Samuel Bassah <sbassah@gmail.com>
2020-10-16 14:42:15 +0000

    Support to set the coupon limit using a field value

--- gravity-forms/gw-create-coupon.php
+++ gravity-forms/gw-create-coupon.php
@@ -56,6 +56,11 @@ class GW_Create_Coupon {
 			$coupon_name = ($coupon_name == ''? $coupon_code : $coupon_name);
 		}
 
+		$limit_value = rgar($entry, $this->_args['meta']['coupon_limit']);
+		if ($limit_value != null){
+			 $this->_args['meta']['coupon_limit'] = $limit_value;
+		}
+
 		$amount      = $this->_args['amount'];
 		$type        = $this->_args['type'];
 
