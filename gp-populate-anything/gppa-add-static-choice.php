8a07ccceb22f0a4aba9e9e0bea4b8293ee5a9bfa
David Smith <david@gravitywiz.com>
2020-11-13 16:03:47 -0500

    Added new snippet for Populate Anything: Add a Static Choice.

---
+++ gp-populate-anything/gppa-add-static-choice.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Gravity Perks // Populate Anything // Add a Static Choice
+ * https://gravitywiz.com/documentation/gravity-forms-populate-anything/
+ */
+add_filter( 'gppa_input_choices_123_4', function( $choices, $field, $objects ) {
+
+	array_unshift( $choices, array(
+		'text' => 'My Static Choice',
+		'value' => 'static',
+	) );
+
+	return $choices;
+}, 10, 3 );
