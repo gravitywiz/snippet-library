35428bc675d796d6421977a5c534955e0fbc5922
David Smith <david@gravitywiz.com>
2020-11-26 14:57:49 -0500

    gpls-show-poll-results.php: PHPCS formatting.

--- gp-limit-submissions/gpls-show-poll-results.php
+++ gp-limit-submissions/gpls-show-poll-results.php
@@ -5,11 +5,11 @@
  */
 add_action( 'gform_get_form_filter', function( $markup, $form ) {
 
-	if( ! is_callable( 'gf_polls' ) || ! gf_polls()->get_form_setting( $form, 'displayResults' ) ) {
+	if ( ! is_callable( 'gf_polls' ) || ! gf_polls()->get_form_setting( $form, 'displayResults' ) ) {
 		return $markup;
 	}
 
-	if( is_callable( 'gp_limit_submissions' ) && property_exists( gp_limit_submissions(), 'enforce' ) && gp_limit_submissions()->enforce->is_limit_reached() ) {
+	if ( is_callable( 'gp_limit_submissions' ) && property_exists( gp_limit_submissions(), 'enforce' ) && gp_limit_submissions()->enforce->is_limit_reached() ) {
 		add_filter( "gform_get_form_filter_{$form['id']}", function( $markup, $form ) {
 			$results = gf_polls()->gpoll_get_results( $form['id'] );
 			$markup .= $results['summary'];
