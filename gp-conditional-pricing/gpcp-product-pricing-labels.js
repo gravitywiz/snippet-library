6deea2a2ffdec91ef6d78a64e84c7ad7ab6c2221
scyt <scott@gravitywiz.com>
2020-12-07 10:07:24 -0500

    Added gpcp-product-pricing-labels.js and gpcp-product-pricing-labels.php (#18)
    * Added gpcp-product-pricing-labels.js and gpcp-product-pricing-labels.php
    
    Original gist: https://gist.github.com/spivurno/cc461abccceaf9aec531210cb0846476
    
    * Added var in front of variables and replaced EOL commas with semicolons

---
+++ gp-conditional-pricing/gpcp-product-pricing-labels.js
@@ -0,0 +1,18 @@
+/**
+ * Gravity Perks // GP Conditional Pricing // Display Price Labels (JS)
+ * http://gravitywiz.com/documentation/gravity-forms-conditional-pricing/
+ *
+ * This snippet requires the PHP counterpart gpcp-product-pricing-labels.php
+ */
+( function( $ ) {
+	function update_price_labels() {
+		$( 'label[data-gpcp-template], option[data-gpcp-template]' ).each( function() {
+			var $priceElem = $( this ).is( 'option' ) ? $( this ) : $( this ).siblings( 'input' );
+			var price = gformFormatMoney( $priceElem.val().split( '|' )[1] );
+			var template = $( this ).attr( 'data-gpcp-template' );
+			$( this ).html( template.replace( '{price}', price ) );
+		} );
+	}
+	gform.addAction( 'gpcp_after_update_pricing', update_price_labels);
+	update_price_labels();
+} )( jQuery );
