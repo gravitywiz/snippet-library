1bdc3ef7c117365cad6da157c0a277136d185ab5
Eihab Ibrahim <eihab@gravitywiz.com>
2021-01-04 15:07:06 -0500

    Replaced GPNF save and continue snippet with a comment indicating its migration.

--- gp-nested-forms/gpnf-clear-session-after-save-and-continue.php
+++ gp-nested-forms/gpnf-clear-session-after-save-and-continue.php
@@ -1,11 +1,4 @@
 <?php
 /**
- * Gravity Perks // Nested Forms // Clear Session after Save & Continue
- * http://gravitywiz.com/documentation/gravity-forms-nested-forms/
+ * This snippet's functionality has been integrated into GP Nested Forms as of 1.0-beta-9.2
  */
-add_action( 'gform_post_process', function( $form ) {
-	if ( rgpost( 'gform_save' ) && class_exists( 'GPNF_Session' ) ) {
-		$session = new GPNF_Session( $form['id'] );
-		$session->delete_cookie();
-	}
-} );
