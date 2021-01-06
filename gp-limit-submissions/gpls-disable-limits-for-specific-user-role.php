171425793363dc606ab9ab3ec7984fc5c9b6c5c6
Samuel Bassah <sbassah@gmail.com>
2021-01-06 21:58:31 +0000

    Tweaks for Usability and Clarity

--- gp-limit-submissions/gpls-disable-limits-for-specific-user-role.php
+++ gp-limit-submissions/gpls-disable-limits-for-specific-user-role.php
@@ -1,12 +1,13 @@
 <?php
 /**
- * Gravity Perks // GP Limit Submissions // Disable Rule for a Specific User Role
+ * Gravity Perks // GP Limit Submissions // Disable Limits for a Specific User Role
  * http://gravitywiz.com/documentaiton/gravity-forms-limit-submissions/
  */
-// Change '123' to the ID of the Form.
+// Update '123' to the ID of the Form.
 add_filter( 'gpls_rule_groups_123', function( $rule_groups, $form_id ) {
+	// Update to the slug of the role, i.e. administrator, editor, author, contributor, subscriber.
+	$role_to_check = 'administrator';
 	$user          = wp_get_current_user();
-	$role_to_check = 'administrator'; //Change to the slug of the role, i.e. administrator, editor, author, contributor, subscriber.
 	if ( in_array( $role_to_check, (array) $user->roles ) ) {
 		$rule_groups = array();
 	}
