08058b815002f2d95afa3f29da98dfac170d6302
David Smith <david@gravitywiz.com>
2021-02-22 15:09:37 -0500

    Added new snippet to include child entry products in the parent entry order summary.

---
+++ gp-nested-forms/gpnf-child-products-in-parent-order-summary.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Gravity Perks // Nested Forms // Include Child Products Directly in Parent Form Order Summary
+ * https://gravitywiz.com/documentation/gravity-forms-nested-forms/
+ *
+ * 1. Add a Calculated Product to your parent form.
+ * 2. Add your Nested Form field with the :total modifier.
+ * 3. Copy and paste this snippet into your theme's functions.php file.
+ *
+ * Now the Calculated Product field on your parent form will be replaced with the products from each child entry.
+ */
+add_filter( 'gform_product_info', function( $product_info, $form, $entry ) {
+
+	foreach ( $form['fields'] as $field ) {
+
+		if ( ! is_a( $field, 'GF_Field_Calculation' ) ) {
+			continue;
+		}
+
+		$child_products = array();
+
+		preg_match_all( '/{[^{]*?:([0-9]+):(sum|total|count)=?([0-9]*)}/', $field->calculationFormula, $matches, PREG_SET_ORDER );
+		foreach ( $matches as $match ) {
+
+			list( ,$nested_form_field_id,, ) = $match;
+
+			$nested_form_field = GFFormsModel::get_field( $form, $nested_form_field_id );
+			if ( ! $nested_form_field ) {
+				continue;
+			}
+
+			$child_form    = gp_nested_forms()->get_nested_form( $field->gpnfForm );
+			$_entry        = new GPNF_Entry( $entry );
+			$child_entries = $_entry->get_child_entries( $nested_form_field_id );
+
+			foreach ( $child_entries as $child_entry ) {
+				$child_product_info = GFCommon::get_product_fields( $child_form, $child_entry );
+				$_child_products    = array();
+				foreach ( $child_product_info['products'] as $child_field_id => $child_product ) {
+					$child_product['name'] = "{$product_info['products'][ $field->id ]['name']} — {$child_product['name']}";
+					$_child_products[ "{$nested_form_field_id}.{$child_entry['id']}.{$child_field_id}" ] = $child_product;
+				}
+				$child_products = array_merge( $child_products, $_child_products );
+			}
+		}
+
+		if ( empty( $child_products ) ) {
+			continue;
+		}
+
+		array_splice( $product_info['products'], array_search( $field->id, $product_info['products'] ), 1, $child_products );
+
+	}
+
+	return $product_info;
+}, 10, 3 );
