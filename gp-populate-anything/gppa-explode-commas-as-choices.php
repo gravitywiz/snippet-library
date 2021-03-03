204067fecb8455e8089c5c1a6566dadafbf44e4e
David Smith <david@gravitywiz.com>
2021-03-03 15:37:44 -0500

    Added new snippet to allow populating array-like values as choices with Populate Anything.

--- gp-populate-anything/gppa-explode-commas-as-choices.php
+++ gp-populate-anything/gppa-explode-commas-as-choices.php
@@ -1,1 +1,34 @@
 <?php
+/**
+ * Gravity Perks // Populate Anything // Explode Commas into Choices
+ * https://gravitywiz.com/documentation/gravity-forms-populate-anything/
+ *
+ * If you have an array-like value mapped to a choice-based field, Populate Anything does not yet know when it should be
+ * converted to choices or imploded into a string so it defaults to the latter.
+ *
+ * This snippet adds the `gppa-explode-commas-into-choices` designator to all you to specify when array-like values
+ * should be converted into choices.
+ *
+ * Usage:
+ *
+ * 1. Install this code as a plugin or as a snippet.
+ * 2. Add the `gppa-explode-commas-into-choices` designator to your field's CSS Class Name setting.
+ */
+add_filter( 'gppa_input_choices', function ( $choices, $field, $objects ) {
+	if ( strpos( $field->cssClass, 'gppa-explode-commas-into-choices' ) === false ) {
+		return $choices;
+	}
+	$new_choices = array();
+	foreach ( $choices as $choice ) {
+		$values = explode( ',', $choice['value'] );
+		foreach ( $values as $value ) {
+			$new_choices[] = array(
+				'text'       => $value,
+				'value'      => $value,
+				'isSelected' => false,
+			);
+		}
+	}
+
+	return $new_choices;
+}, 10, 3 );
