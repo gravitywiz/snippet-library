b409e18821b5ef1ae779215e4027a34ae8a0741a
Eihab Ibrahim <eihab@gravitywiz.com>
2021-03-15 10:28:14 -0400

    ~ Fixed comment placement.

--- gravity-forms/gw-gravity-forms-rename-uploaded-files-usage.php
+++ gravity-forms/gw-gravity-forms-rename-uploaded-files-usage.php
@@ -4,8 +4,7 @@
 new GW_Rename_Uploaded_Files( array(
 	'form_id'  => 628,
 	'field_id' => 3,
-	'template' => '{Name (First):1.3}-{Name (Last):1.6}-{filename}',
-	// most merge tags are supported, original file extension is preserved
+	'template' => '{Name (First):1.3}-{Name (Last):1.6}-{filename}', // most merge tags are supported, original file extension is preserved
 ) );
 
 # Form Title Merge Tag
