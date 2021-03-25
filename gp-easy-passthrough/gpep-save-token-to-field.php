8c7a308d98e0ef4061c26a9c84f64e5c96d8389b
Samuel Bassah <sbassah@gmail.com>
2021-03-25 14:12:40 +0000

    Gravity Perks // Easy Passthrough // Save EP Token to Field
    Original Gist: https://gist.github.com/spivurno/f30988968900333c631e81a9c873358a

---
+++ gp-easy-passthrough/gpep-save-token-to-field.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Gravity Perks // Easy Passthrough // Save EP Token to Field
+ * http://gravitywiz.com/documentation/gravity-forms-easy-passthrough/
+ *
+ * This snippet allows you to populate the EP token into a field so it can be mapped in Gravity Forms feeds. The filter
+ * fires before feeds are processed so the token is available in time.
+ */
+// Update "123" to your form ID.
+add_filter( 'gform_entry_post_save_123', function( $entry, $form ) {
+
+	// Update "4" to the ID of the field you woule like to populate with the EP token.
+	$target_field_id = 4;
+
+	if( is_callable( 'gp_easy_passthrough' ) ) {
+		$token = gp_easy_passthrough()->get_entry_token( $entry );
+		GFAPI::update_entry_field( $entry['id'], $target_field_id, $token );
+		$entry[ $target_field_id ] = $token;
+	}
+
+	return $entry;
+}, 5, 2 );
