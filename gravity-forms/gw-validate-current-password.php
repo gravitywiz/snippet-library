fa4b4270df05389d7f73c5cd83b8453e110ee59e
Samuel Bassah <sbassah@gmail.com>
2021-03-25 17:33:06 +0000

    Checking if $user returns true

--- gravity-forms/gw-validate-current-password.php
+++ gravity-forms/gw-validate-current-password.php
@@ -6,9 +6,11 @@
 // update "123" to your form ID, update "4" to your Password field ID
 add_filter( 'gform_field_validation_123_4', function( $result, $value ) {
 	$user = wp_get_current_user();
-	if ( ! wp_check_password( $value, $user->data->user_pass, $user->ID ) ) {
-		$result['is_valid'] = false;
-		$result['message'] = 'Invalid current password. Please try again.';
+	if ($user){
+		if ( ! wp_check_password( $value, $user->data->user_pass, $user->ID ) ) {
+			$result['is_valid'] = false;
+			$result['message'] = 'Invalid current password. Please try again.';
+		}
 	}
 	return $result;
 }, 10, 2 );
