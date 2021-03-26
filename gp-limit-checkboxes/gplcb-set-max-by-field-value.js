4b5beb0ee249de0c0ef4b78ebff13bc4486a473c
Samuel Bassah <sbassah@gmail.com>
2021-03-26 12:15:12 +0000

    Set Max Limit by Field Value
    Original Gist: https://gist.github.com/spivurno/09aee6a8ec9ceb684cf5bf0d9c51e19f

---
+++ gp-limit-checkboxes/gplcb-set-max-by-field-value.js
@@ -0,0 +1,23 @@
+gform.addFilter( 'gplc_group', function( group, fieldId, $elem, gplc ) {
+
+	var formId = 123,
+		  checkboxFieldId = 4,
+		  maxFieldId = 5;
+
+	var $maxField = $( '#input_{0}_{1}'.format( formId, maxFieldId ) );
+
+	if ( $elem.parents( '.gfield' ).attr( 'id' ) === 'field_{0}_{1}'.format( formId, checkboxFieldId ) ) {
+		group.max = $maxField.val() ? $maxField.val() : 0;
+		// Only bind our event listener once.
+		if ( ! $maxField.data( 'gplcIsBound' ) ) {
+			// If our max field value changes, reset the checkboxes and reinitialize GPLC.
+			$maxField.on( 'change', function() {
+				$( '#field_{0}_{1}'.format( formId, checkboxFieldId ) ).find( 'input' ).prop( 'checked', false );
+				gplc.handleCheckboxClick( $elem );
+			} );
+			$maxField.data( 'gplcIsBound', true );
+		}
+	}
+
+	return group;
+} );
