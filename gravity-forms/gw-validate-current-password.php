74b43e329f885ac1fe2ffedf90299b8fcdc4e05b
Samuel Bassah <sbassah@gmail.com>
2021-03-26 17:34:54 +0000

    Gravity Wiz // Gravity Forms // Validate Current Password

---
+++ gravity-forms/gw-validate-current-password.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Gravity Wiz // Gravity Forms // Validate Current Password
+ * http://gravitywiz.com/
+ */
+// Update "123" to your form ID and "4" to your Password field ID.
+add_filter( 'gform_field_validation_123_4', function( $result, $value ) {
+	$user = wp_get_current_user();
+	if ( $user && ! wp_check_password( $value, $user->data->user_pass, $user->ID ) ) { 
+			$result['is_valid'] = false;
+			$result['message'] = 'Invalid current password. Please try again.';
+	}
+	return $result;
+}, 10, 2 );
