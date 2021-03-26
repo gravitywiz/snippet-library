569962680555b67d34b2f0585441e3e105c52c6c
Samuel Bassah <sbassah@gmail.com>
2021-03-26 12:04:40 +0000

    Updated the check into one line.

--- gravity-forms/gw-validate-current-password.php
+++ gravity-forms/gw-validate-current-password.php
@@ -6,11 +6,9 @@
 // Update "123" to your form ID and "4" to your Password field ID.
 add_filter( 'gform_field_validation_123_4', function( $result, $value ) {
 	$user = wp_get_current_user();
-	if ($user){
-		if ( ! wp_check_password( $value, $user->data->user_pass, $user->ID ) ) {
+	if ( ! $user || ! wp_check_password( $value, $user->data->user_pass, $user->ID ) ) { 
 			$result['is_valid'] = false;
 			$result['message'] = 'Invalid current password. Please try again.';
-		}
 	}
 	return $result;
 }, 10, 2 );
