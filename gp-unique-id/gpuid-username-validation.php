e4cd30564770bb33e941478c9ea039d246968018
Samuel Bassah <sbassah@gmail.com>
2021-03-29 14:45:44 +0000

    Updated the Snippet Header information.

--- gp-unique-id/gpuid-username-validation.php
+++ gp-unique-id/gpuid-username-validation.php
@@ -5,6 +5,12 @@
  * The Unique ID field is populated on submission, after validation. This snippet runs post-validation to allow User Registration feeds with a Unique ID
  * field mapped as the username to still pass validation.
  *
+ * Plugin Name:  GP Unique ID — Unique ID as Username or Site Address
+ * Plugin URI:   https://gravitywiz.com/documentation/gravity-forms-unique-id/
+ * Description:  This snippet runs post-validation to allow User Registration feeds with a Unique ID field mapped as the username to still pass validation.
+ * Author:       Gravity Wiz
+ * Version:      0.1
+ * Author URI:   https://gravitywiz.com
  */
 add_filter( 'gform_user_registration_validation', 'gp_unique_id_for_username_validation', 11, 2 );
 function gp_unique_id_for_username_validation( $form, $feed ) {
