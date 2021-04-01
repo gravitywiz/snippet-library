336730615ca07beadcb029a27d37ce67062d8f52
Samuel Bassah <sbassah@gmail.com>
2021-04-01 12:57:51 +0000

    Display Child Entries in Table Format for All Fields

---
+++ gp-nested-forms/gpnf-display-child-entries-table-format.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Gravity Perks // GP Nested Forms // Display Table Format for All Fields
+ * https://gravitywiz.com/documentation/gravity-forms-nested-forms/
+ *
+ * This snippet displays the child entries in a table format when using the {all_fields} merge tag with the gpnf_table modifier. 
+ * Add "gpnf_table" as a modifier to the {all_fields} merge tag to enable this functionality.
+ * Examples:
+ *
+ * {all_fields:gpnf_table}
+ * {all_fields:nohidden,gpnf_table}
+ *
+ * Plugin Name:  GP Nested Forms - Display Table Format for All Fields
+ * Plugin URI:   https://gravitywiz.com/documentation/gravity-forms-nested-forms/
+ * Description:  This snippet displays the child entries in a table format when using the {all_fields} merge tag with the gpnf_table modifier.
+ * Author:       Gravity Wiz
+ * Version:      0.1
+ * Author URI:   https://gravitywiz.com
+ */
+add_filter( 'gform_merge_tag_filter', function ( $value, $merge_tag, $modifiers, $field, $raw_value ) {
+
+	if ( ! is_callable( 'gp_nested_forms' ) || $field->type != 'form' || $value === false || strpos( $modifiers, 'gpnf_table' ) === false ) {
+		return $value;
+	}
+
+	// Adds support for :filter modifier on Nested Form field merge tags but not {all_fields}.
+	$nested_field_ids = $field->gpnfFields;
+	if ( function_exists( 'gw_all_fields_template' ) ) {
+		$_modifiers = gw_all_fields_template()->parse_modifiers( $modifiers );
+		if( $_modifiers['filter'] ) {
+			$nested_field_ids = is_array( $_modifiers['filter'] ) ? $_modifiers['filter'] : array( $_modifiers['filter'] );
+		}
+	}
+
+	$template    = new GP_Template( gp_nested_forms() );
+	$nested_form = GFAPI::get_form( rgar( $field, 'gpnfForm' ) );
+	$args        = array(
+		'template'         => 'nested-entries-detail',
+		'field'            => $field,
+		'nested_form'      => GFAPI::get_form( rgar( $field, 'gpnfForm' ) ),
+		'modifiers'        => $modifiers,
+		'nested_fields'    => gp_nested_forms()->get_fields_by_ids( $nested_field_ids, $nested_form ),
+		'entries'          => gp_nested_forms()->get_entries( $raw_value ),
+		'actions'          => array(),
+		'nested_field_ids' => $nested_field_ids,
+		'labels'           => array( 'view_entry' => '' )
+	);
+
+	$value = $template->parse_template(
+		array(
+			sprintf( '%s-%s-%s.php', $args['template'], $nested_form['id'], $field->id ),
+			sprintf( '%s-%s.php', $args['template'], $nested_form['id'] ),
+			sprintf( '%s.php', $args['template'] ),
+		), true, false, $args
+	);
+
+	return $value;
+}, 12, 5 );
