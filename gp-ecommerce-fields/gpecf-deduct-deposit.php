483218fad5248d311c400e79624a7bb5d5735282
Samuel Bassah <sbassah@gmail.com>
2021-04-05 17:25:25 +0000

    Added a snippet description and updated comments

--- gp-ecommerce-fields/gpecf-deduct-deposit.php
+++ gp-ecommerce-fields/gpecf-deduct-deposit.php
@@ -3,7 +3,10 @@
  * Gravity Perks // GP eCommerce Fields // Deduct Deposit from Order Summary
  * https://gravitywiz.com/documentation/gravity-forms-ecommerce-fields/
  *
- * Plugin Name:   GP eCommerce Fields - Deduct Deposit from Order Summary
+ * This snippet uses a Produdct field to create a deposit field and deducts the deposit from Order Summary.
+ * To use the snippet, you'll have to update the Form ID and the deposit field ID within the snippet.
+ *
+ * Plugin Name:  GP eCommerce Fields - Deduct Deposit from Order Summary
  * Plugin URI:   https://gravitywiz.com/documentation/gravity-forms-ecommerce-fields/
  * Description:  This snippet uses a Produdct field to create a deposit field and deducts the deposit from Order Summary
  * Author:       Gravity Wiz
@@ -19,12 +22,12 @@ add_action( 'wp_loaded', function() {
 	remove_action( 'gform_product_info', array( gp_ecommerce_fields(), 'add_ecommerce_fields_to_order' ), 9, 3 );
 	add_action( 'gform_product_info', function( $order, $form, $entry ) {
 
-		// CHANGE: Update "123" to the ID of your form.
+		// Update "123" to the ID of your form.
 		if( $form['id'] != 123 ) {
 			return gp_ecommerce_fields()->add_ecommerce_fields_to_order( $order, $form, $entry );
 		}
 
-		// CHANGE: Update the "2" to your deposit field ID.
+		// Update the "2" to your deposit field ID.
 		$deposit =& $order['products'][2];
 
 		// Run this first so calculations are reprocessed before we convert deposit to a negative number.
