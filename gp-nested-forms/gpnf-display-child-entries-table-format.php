cf1a6af35c3ba5599864b781ba9ed6f632b5f340
Samuel Bassah <sbassah@gmail.com>
2021-04-12 15:16:37 +0000

    Added support to show all fields in table format

--- gp-nested-forms/gpnf-display-child-entries-table-format.php
+++ gp-nested-forms/gpnf-display-child-entries-table-format.php
@@ -5,10 +5,13 @@
  *
  * This snippet displays the child entries in a table format when using the {all_fields} merge tag with the gpnf_table modifier. 
  * Add "gpnf_table" as a modifier to the {all_fields} merge tag to enable this functionality.
- * Examples:
+ * Add "gpnf_all_fields" as an additional modfier too show all the child fields when displaying in the table format.
  *
+ * Examples:
+ * Show Nested Form fields in the table format in the {all_fields} merge tag.
  * {all_fields:gpnf_table}
- * {all_fields:nohidden,gpnf_table}
+ * Show all child fields when displaying in the table format.
+ * {all_fields:gpnf_table,gpnf_all_fields}
  *
  * Plugin Name:  GP Nested Forms - Display Table Format for All Fields
  * Plugin URI:   https://gravitywiz.com/documentation/gravity-forms-nested-forms/
@@ -22,9 +25,9 @@ add_filter( 'gform_merge_tag_filter', function ( $value, $merge_tag, $modifiers,
 	if ( ! is_callable( 'gp_nested_forms' ) || $field->type != 'form' || $value === false || strpos( $modifiers, 'gpnf_table' ) === false ) {
 		return $value;
 	}
-
+	$nested_form = GFAPI::get_form( rgar( $field, 'gpnfForm' ) );
 	// Adds support for :filter modifier on Nested Form field merge tags but not {all_fields}.
-	$nested_field_ids = $field->gpnfFields;
+	$nested_field_ids = strpos( $modifiers, 'gpnf_all_fields' ) !== false ? wp_list_pluck( $nested_form['fields'], 'id' ) : $field->gpnfFields;
 	if ( function_exists( 'gw_all_fields_template' ) ) {
 		$_modifiers = gw_all_fields_template()->parse_modifiers( $modifiers );
 		if( $_modifiers['filter'] ) {
