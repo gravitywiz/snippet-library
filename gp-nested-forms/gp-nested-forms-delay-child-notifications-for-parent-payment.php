71e5cc155ed963b1dd03b5d460d2ae34d32e1872
Eihab Ibrahim <eihab@gravitywiz.com>
2021-04-22 18:05:12 -0400

    ~ Using snippet template format and more direct filter removal.

--- gp-nested-forms/gp-nested-forms-delay-child-notifications-for-parent-payment.php
+++ gp-nested-forms/gp-nested-forms-delay-child-notifications-for-parent-payment.php
@@ -1,44 +1,59 @@
 <?php
 /**
- * Gravity Perks // Nested Forms // Delay Child Notifications for Parent Payment
- * http://gravitywiz.com/documentation/gravity-forms-nested-forms/
+ * Gravity Wiz // Nested Forms // Delay Child Notifications for Parent Payment
+ * https://github.com/gravitywiz/snippet-library/blob/master/gw-snippet-template.php
+ *
+ * Delay GP Nested Forms child form notification until payment processing is completed.
+ *
+ * Plugin Name:  Delay Child Notifications for Parent Payment
+ * Plugin URI:   http://gravitywiz.com/documentation/gravity-forms-nested-forms/
+ * Description:  Delay GP Nested Forms child form notification until payment processing is completed.
+ * Author:       Gravity Wiz
+ * Version:      1.1
+ * Author URI:   http://gravitywiz.com
  */
 class GW_GPNF_Delay_Child_Notifications {
-	public $form_ids;
 
-	public function __construct( $args ) {
-		$this->form_ids = $args['form_ids'];
+	public function __construct( $args = array() ) {
 
-		add_filter( 'gpnf_should_send_notification', function( $should_send_notification, $notification, $context, $parent_form, $nested_form_field, $entry, $child_form ) {
+		// set our default arguments, parse against the provided arguments, and store for use throughout the class
+		$this->_args = wp_parse_args( $args, array(
+			'form_ids' => false,
+		) );
 
-			if ( $context === 'parent' ) {
-				$parent_entry             = GFAPI::get_entry( rgar( $entry, 'gpnf_entry_parent' ) );
-				$should_send_notification = in_array( rgar( $parent_entry, 'payment_status' ), array( 'Paid', 'Active' ), true );
-			}
+		// do version check in the init to make sure if GF is going to be loaded, it is already loaded
+		add_action( 'init', array( $this, 'init' ) );
 
-			return $should_send_notification;
-		}, 10, 7 );
+	}
 
-		add_action( 'gform_post_payment_completed', function( $entry ) {
-			if ( is_callable( 'gpnf_notification_processing' ) ) {
-				gpnf_notification_processing()->maybe_send_child_notifications( $entry, GFAPI::get_form( $entry['form_id'] ) );
-			}
-		} );
+	public function init() {
 
+		add_filter( 'gpnf_should_send_notification', array( $this, 'gpnf_should_send_notification' ), 10, 7 );
+		add_action( 'gform_post_payment_completed', array( $this, 'gform_post_payment_completed' ) );
 		remove_filter( 'gform_entry_post_save', array( gpnf_notification_processing(), 'maybe_send_child_notifications' ), 11 );
-		add_filter( 'gform_entry_post_save', array( $this, 'gform_entry_post_save' ), 11, 2 );
+
 	}
 
-	public function gform_entry_post_save( $entry, $form ) {
-		/**
-		 * Do not send notification for form ID 100 on the normal entry save hook. This form is expected to send
-		 * notifications using the gform_post_payment_completed action.
-		 */
-		if ( in_array( rgar( $form, 'id' ), $this->form_ids, true ) ) {
-				return $entry;
+	public function gpnf_should_send_notification( $should_send_notification, $notification, $context, $parent_form, $nested_form_field, $entry, $child_form ) {
+
+		if ( $context === 'parent' ) {
+			$parent_entry             = GFAPI::get_entry( rgar( $entry, 'gpnf_entry_parent' ) );
+			$should_send_notification = in_array( rgar( $parent_entry, 'payment_status' ), array( 'Paid', 'Active' ), true );
 		}
-		return gpnf_notification_processing()->maybe_send_child_notifications( $entry, $form );
+		// Prevent double notifications for non-delayed payment gateways.
+		remove_filter( 'gform_post_payment_completed', array( $this, 'gpnf_should_send_notification' ) );
+		return $should_send_notification;
+
 	}
+
+	public function gform_post_payment_completed( $entry ) {
+
+		if ( is_callable( 'gpnf_notification_processing' ) ) {
+			gpnf_notification_processing()->maybe_send_child_notifications( $entry, GFAPI::get_form( $entry['form_id'] ) );
+		}
+
+	}
+
 }
 
 // Configuration
