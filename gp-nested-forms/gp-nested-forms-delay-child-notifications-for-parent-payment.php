525a45b44e9c39e0db9c42544058db086c7e57c1
Eihab Ibrahim <eihab@gravitywiz.com>
2021-04-23 15:59:49 -0400

    ~ Added form targeting to snippet

--- gp-nested-forms/gp-nested-forms-delay-child-notifications-for-parent-payment.php
+++ gp-nested-forms/gp-nested-forms-delay-child-notifications-for-parent-payment.php
@@ -18,7 +18,7 @@ class GW_GPNF_Delay_Child_Notifications {
 
 		// set our default arguments, parse against the provided arguments, and store for use throughout the class
 		$this->_args = wp_parse_args( $args, array(
-			'form_ids' => false,
+			'form_id' => false,
 		) );
 
 		// do version check in the init to make sure if GF is going to be loaded, it is already loaded
@@ -30,32 +30,39 @@ class GW_GPNF_Delay_Child_Notifications {
 
 		add_filter( 'gpnf_should_send_notification', array( $this, 'gpnf_should_send_notification' ), 10, 7 );
 		add_action( 'gform_post_payment_completed', array( $this, 'gform_post_payment_completed' ) );
+		remove_filter( 'gform_entry_post_save', array( gpnf_notification_processing(), 'maybe_send_child_notifications' ), 11 );
 
 	}
 
 	public function gpnf_should_send_notification( $should_send_notification, $notification, $context, $parent_form, $nested_form_field, $entry, $child_form ) {
-
-		if ( $context === 'parent' ) {
+		if ( $context === 'parent' && $this->is_applicable_form( $parent_form['id'] ) ) {
 			$parent_entry             = GFAPI::get_entry( rgar( $entry, 'gpnf_entry_parent' ) );
 			$should_send_notification = in_array( rgar( $parent_entry, 'payment_status' ), array( 'Paid', 'Active' ), true );
 			// Prevent double notifications for non-delayed payment gateways.
 			remove_filter( 'gform_post_payment_completed', array( $this, 'gpnf_should_send_notification' ) );
 		}
+
 		return $should_send_notification;
 
 	}
 
 	public function gform_post_payment_completed( $entry ) {
-
-		if ( is_callable( 'gpnf_notification_processing' ) ) {
+		if ( is_callable( 'gpnf_notification_processing' ) && $this->is_applicable_form( $entry['form_id'] ) ) {
 			gpnf_notification_processing()->maybe_send_child_notifications( $entry, GFAPI::get_form( $entry['form_id'] ) );
 		}
 
+	}
+
+	public function is_applicable_form( $form ) {
+
+		$form_id = isset( $form['id'] ) ? $form['id'] : $form;
+
+		return empty( $this->_args['form_id'] ) || (int) $form_id === (int) $this->_args['form_id'];
 	}
 
 }
 
 // Configuration
 new GW_GPNF_Delay_Child_Notifications( array(
-	'form_ids' => array( 3 ), // Add all parent form IDs to this array
+	'form_id' => 3, // Set this to the parent form ID
 ) );
