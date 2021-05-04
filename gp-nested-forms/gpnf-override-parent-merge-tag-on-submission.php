ee49a6be3e22870d4c6185e888cac6ba9874bbff
David Smith <david@gravitywiz.com>
2021-05-04 16:37:20 -0400

    ~ PHPCBF.

--- gp-nested-forms/gpnf-override-parent-merge-tag-on-submission.php
+++ gp-nested-forms/gpnf-override-parent-merge-tag-on-submission.php
@@ -14,36 +14,35 @@ function gpnf_override_parent_merge_tags( $entry, $form = false ) {
 		$form = GFAPI::get_form( $entry['form_id'] );
 	}
 
-	foreach( $form['fields'] as $field ) {
-		if( $field->get_input_type() == 'form' ) {
+	foreach ( $form['fields'] as $field ) {
+		if ( $field->get_input_type() === 'form' ) {
 			$child_form_id = $field->gpnfForm;
-			$child_form = GFAPI::get_form( $child_form_id );
-			foreach( $child_form['fields'] as $child_field ) {
+			$child_form    = GFAPI::get_form( $child_form_id );
+			foreach ( $child_form['fields'] as $child_field ) {
 				if ( $child_field->get_entry_inputs() ) {
-					foreach( $child_field->get_entry_inputs() as $input ) {
+					foreach ( $child_field->get_entry_inputs() as $input ) {
 						preg_match( '/{Parent:(.+)}/i', rgar( $input, 'defaultValue' ), $match );
-						if( $match ) {
-							$value = rgar( $entry, $match[1] );
+						if ( $match ) {
+							$value           = rgar( $entry, $match[1] );
 							$child_entry_ids = explode( ',', rgar( $entry, $field->id ) );
-							foreach( $child_entry_ids as $child_entry_id ) {
+							foreach ( $child_entry_ids as $child_entry_id ) {
 								GFAPI::update_entry_field( $child_entry_id, $input['id'], $value );
 							}
 						}
 					}
 				} else {
 					preg_match( '/{Parent:(.+)}/i', $child_field->defaultValue, $match );
-					if( $match ) {
-						$value = rgar( $entry, $match[1] );
+					if ( $match ) {
+						$value           = rgar( $entry, $match[1] );
 						$child_entry_ids = explode( ',', rgar( $entry, $field->id ) );
-						foreach( $child_entry_ids as $child_entry_id ) {
+						foreach ( $child_entry_ids as $child_entry_id ) {
 							GFAPI::update_entry_field( $child_entry_id, $child_field->id, $value );
 						}
 					}
 				}
-
 			}
 		}
 	}
 
 	return $entry;
-}, 11, 2 );
+}
