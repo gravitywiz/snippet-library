8b914f497db479fc0c7d3fa9046f409eceac590c
David Smith <david@gravitywiz.com>
2021-05-26 16:22:18 -0400

    Create gprf-reload-globally.js

---
+++ experimental/gprf-reload-globally.js
@@ -0,0 +1,8 @@
+/**
+ * By default, GPRF scopes it's replacement to a static container. If the user is including multiple instances of the same form 
+ * on the same page (which GF does not support by default), all instances of the form will be submitted but only the submitted
+ * instance will be reloaded. Use this snippet to reload all forms of the same ID.
+ */
+gform.addFilter( 'gprf_replacing_elem', function( $replacingElem, formId ) { 
+    return jQuery( '#gform_confirmation_wrapper_' + formId + ', .gform_confirmation_message_' + formId + ', #gform_wrapper_' + formId );
+} );
