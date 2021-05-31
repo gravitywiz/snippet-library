f55f1b42e63f551a56233802a54b560b1b719c37
David Smith <david@gravitywiz.com>
2021-05-31 10:25:35 -0400

    Update gpuid-custom-format-ABC12345.php

--- experimental/gpuid-custom-format-ABC12345.php
+++ experimental/gpuid-custom-format-ABC12345.php
@@ -4,9 +4,12 @@
  * 
  * Generate a Unique ID in the following format `ABC12345` where each letter can be any letter between A and Z and each 
  * digit can be any number between 1 and 9.
+ * 
+ * Configuration instructions are inline.
  */
 add_filter( 'gpui_unique_id', function( $unique, $form_id, $field_id ) {
-	if ( $form_id == 300 && $field_id == 1 ) {
+	// Update the "123" to your form ID and the "4" to your Unique ID field's ID.
+	if ( $form_id == 123 && $field_id == 4 ) {
 		$unique = '';
 		foreach( range( 0, 2 ) as $index ) {
 			$unique .= chr( rand( 65, 90 ) );
