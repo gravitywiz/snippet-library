fd967dd85b33f8bb9ec05989625fdceac2991795
David Smith <david@gravitywiz.com>
2021-05-31 12:18:43 -0400

    Create gpbua-enable-block-editor.php

---
+++ experimental/gpbua-enable-block-editor.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Gravity Perks // Better User Activation // Enable Block Editor for Activation Page
+ * https://gravitywiz.com/documentation/gravity-forms-better-user-activation/
+ */
+add_action( 'init', function() {
+	remove_filter( 'use_block_editor_for_post', array( gp_better_user_activation(), 'disable_block_editor_for_activation_page' ), 101 );
+}, 16 );
