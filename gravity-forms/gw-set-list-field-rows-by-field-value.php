25d9bd157c00796a5c10066bb7e59a8beeb60590
Eihab <eihab@cluecode.com>
2021-06-03 14:18:57 -0400

    Fixed an issue with GF2.5 where rows were not counted correctly in `gw-set-list-field-rows-by-field-value`.

--- gravity-forms/gw-set-list-field-rows-by-field-value.php
+++ gravity-forms/gw-set-list-field-rows-by-field-value.php
@@ -23,7 +23,7 @@ class GWAutoListFieldRows {
 		$this->_args = wp_parse_args( $args, array(
 			'form_id'       => false,
 			'input_html_id' => false,
-			'list_field_id' => false
+			'list_field_id' => false,
 		) );
 
 		// do version check in the init to make sure if GF is going to be loaded, it is already loaded
@@ -34,7 +34,7 @@ class GWAutoListFieldRows {
 	public function init() {
 
 		// make sure we're running the required minimum version of Gravity Forms
-		if( ! property_exists( 'GFCommon', 'version' ) || ! version_compare( GFCommon::$version, '1.8', '>=' ) ) {
+		if ( ! property_exists( 'GFCommon', 'version' ) || ! version_compare( GFCommon::$version, '1.8', '>=' ) ) {
 			return;
 		}
 
@@ -59,60 +59,60 @@ class GWAutoListFieldRows {
 
 		<script type="text/javascript">
 
-            window.gwalfr;
+			window.gwalfr;
 
-            (function($){
+			(function($){
+				gwalfr = function( args ) {
 
-                gwalfr = function( args ) {
+					this.formId      = args.formId,
+						this.listFieldId = args.listFieldId,
+						this.inputHtmlId = args.inputHtmlId;
 
-                    this.formId      = args.formId,
-                        this.listFieldId = args.listFieldId,
-                        this.inputHtmlId = args.inputHtmlId;
+					this.init = function() {
 
-                    this.init = function() {
+						var gwalfr = this,
+							triggerInput = $( this.inputHtmlId );
 
-                        var gwalfr = this,
-                            triggerInput = $( this.inputHtmlId );
+						// update rows on page load
+						this.updateListItems( triggerInput, this.listFieldId, this.formId );
 
-                        // update rows on page load
-                        this.updateListItems( triggerInput, this.listFieldId, this.formId );
+						// update rows when field value changes
+						triggerInput.change(function(){
+							gwalfr.updateListItems( $(this), gwalfr.listFieldId, gwalfr.formId );
+						});
 
-                        // update rows when field value changes
-                        triggerInput.change(function(){
-                            gwalfr.updateListItems( $(this), gwalfr.listFieldId, gwalfr.formId );
-                        });
+					}
 
-                    }
+					this.updateListItems = function( elem, listFieldId, formId ) {
 
-                    this.updateListItems = function( elem, listFieldId, formId ) {
+						var listField = $( '#field_' + formId + '_' + listFieldId ),
+							count = parseInt( elem.val() );
+						// `gfield_list_group` represents the rows in GF2.4 and 2.5. Use that instead of table markup.
+						rowCount = listField.find('.gfield_list_group').length;
+							diff = count - rowCount;
 
-                        var listField = $( '#field_' + formId + '_' + listFieldId ),
-                            count = parseInt( elem.val() );
-                        rowCount = listField.find( 'table.gfield_list tbody tr' ).length,
-                            diff = count - rowCount;
+						if( diff > 0 ) {
+							for( var i = 0; i < diff; i++ ) {
+								listField.find( '.add_list_item:last' ).click();
+							}
+						} else {
 
-                        if( diff > 0 ) {
-                            for( var i = 0; i < diff; i++ ) {
-                                listField.find( '.add_list_item:last' ).click();
-                            }
-                        } else {
+							// make sure we never delete all rows
+							if( rowCount + diff == 0 )
+								diff++;
 
-                            // make sure we never delete all rows
-                            if( rowCount + diff == 0 )
-                                diff++;
+							for( var i = diff; i < 0; i++ ) {
+								listField.find( '.delete_list_item:last' ).click();
+							}
 
-                            for( var i = diff; i < 0; i++ ) {
-                                listField.find( '.delete_list_item:last' ).click();
-                            }
+						}
+					}
 
-                        }
-                    }
+					this.init();
 
-                    this.init();
+				}
 
-                }
-
-            })(jQuery);
+			})(jQuery);
 
 		</script>
 
@@ -128,13 +128,13 @@ class GWAutoListFieldRows {
 		$args = array(
 			'formId'      => $this->_args['form_id'],
 			'listFieldId' => $this->_args['list_field_id'],
-			'inputHtmlId' => $this->_args['input_html_id']
+			'inputHtmlId' => $this->_args['input_html_id'],
 		);
 
-		$script = "new gwalfr(" . json_encode( $args ) . ");";
-		$key = implode( '_', $args );
+		$script = 'new gwalfr(' . json_encode( $args ) . ');';
+		$key    = implode( '_', $args );
 
-		GFFormDisplay::add_init_script( $form['id'], 'gwalfr_' . $key , GFFormDisplay::ON_PAGE_RENDER, $script );
+		GFFormDisplay::add_init_script( $form['id'], 'gwalfr_' . $key, GFFormDisplay::ON_PAGE_RENDER, $script );
 	}
 
 	public function is_applicable_form( $form ) {
@@ -148,15 +148,15 @@ class GWAutoListFieldRows {
 
 // EXAMPLE #1: Number field for the "input_html_id"
 new GWAutoListFieldRows( array(
-	'form_id' => 240,
+	'form_id'       => 240,
 	'list_field_id' => 3,
-	'input_html_id' => '#input_240_4'
+	'input_html_id' => '#input_240_4',
 ) );
 
 // EXAMPLE #2: Single Product Field's Quantity input as the "input_html_id"
 // Note: input_html_id has a format of "#ginput_quantity_240_5" if using Gravity Forms <2.5 or if Legacy Markup is enabled
 new GWAutoListFieldRows( array(
-	'form_id' => 240,
+	'form_id'       => 240,
 	'list_field_id' => 6,
 	'input_html_id' => '#input_240_5_1'
 ) );
