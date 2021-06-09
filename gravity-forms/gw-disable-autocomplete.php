ead90f8436fef5df5ea63b7c46f293f518ea785f
scyt <scott@gravitywiz.com>
2021-06-09 16:37:19 -0400

    Add gw-disable-autocomplete.php to snippet library. Added name attrib… (#104)
    * Add gw-disable-autocomplete.php to snippet library. Added name attribute to target Safari.
    
    * Removed Safari hack because it broke form submission.

---
+++ gravity-forms/gw-disable-autocomplete.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Gravity Wiz // Gravity Forms // Disable Auto-complete
+ *
+ * Disable browser auto-complete.
+ *
+ * @version 0.2
+ * @author  Scott Buchmann <scott@gravitywiz.com>
+ * @license GPL-2.0+
+ * @link    http://gravitywiz.com
+ */
+// Disable auto-complete on form.
+add_filter( 'gform_form_tag', function( $form_tag ) {
+	return str_replace( '>', ' autocomplete="password">', $form_tag );
+}, 11 );
+
+// Diable auto-complete on each field.
+add_filter( 'gform_field_content', function( $input ) {
+	return preg_replace( '/<(input|textarea)/', '<${1} autocomplete="password" ', $input );
+}, 11 );
