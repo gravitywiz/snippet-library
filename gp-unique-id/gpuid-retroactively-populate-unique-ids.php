a63ef548731106175fc9c317ff25660b62ab7b92
Eihab Ibrahim <eihab@gravitywiz.com>
2021-06-16 12:55:30 -0400

    Fixed an issue where `gpuid-retroactively-populate-unique-ids` may cause a fatal error.

--- gp-unique-id/gpuid-retroactively-populate-unique-ids.php
+++ gp-unique-id/gpuid-retroactively-populate-unique-ids.php
@@ -17,67 +17,69 @@
  *
  * NOTE: This snippet has a hard-coded limit of 999 entries.
  */
-if ( isset( $_GET['gpui_retro_pop'] ) && class_exists( 'GFAPI' ) ) {
+add_action( 'init', function() {
+	if ( isset( $_GET['gpui_retro_pop'] ) && class_exists( 'GFAPI' ) ) {
 
-	$form_id = rgget( 'form_id' );
-	$form    = GFAPI::get_form( $form_id );
-	if ( ! $form ) {
-		echo 'Please provide a valid form ID.';
-		exit;
-	}
+		$form_id = rgget( 'form_id' );
+		$form    = GFAPI::get_form( $form_id );
+		if ( ! $form ) {
+			echo 'Please provide a valid form ID.';
+			exit;
+		}
 
-	$unique_id_fields = GFCommon::get_fields_by_type( $form, array( 'uid' ) );
-	if ( empty( $unique_id_fields ) ) {
-		echo 'There are no Unique ID fields on this form.';
-		exit;
-	}
+		$unique_id_fields = GFCommon::get_fields_by_type( $form, array( 'uid' ) );
+		if ( empty( $unique_id_fields ) ) {
+			echo 'There are no Unique ID fields on this form.';
+			exit;
+		}
 
-	$filters = array();
-	foreach ( $unique_id_fields as $unique_id_field ) {
-		$filters[] = array(
-			'key'   => $unique_id_field->id,
-			'value' => null,
-		);
-	}
-	$filters['mode'] = 'any';
-
-	$entries = GFAPI::get_entries(
-		$form['id'],
-		array(
-			'field_filters' => $filters,
-		),
-		null,
-		array(
-			'offset'    => 0,
-			'page_size' => 999,
-		)
-	);
-
-	if ( empty( $entries ) ) {
-		echo 'There are no entries requiring retroactive population.';
-		exit;
-	}
-
-	$count = 0;
-
-	foreach ( $entries as $entry ) {
-
-		$has_update = false;
-
+		$filters = array();
 		foreach ( $unique_id_fields as $unique_id_field ) {
-			if ( empty( $entry[ $unique_id_field->id ] ) ) {
-				$entry[ $unique_id_field->id ] = gp_unique_id()->get_unique( $form['id'], $unique_id_field );
-				$has_update                    = true;
+			$filters[] = array(
+				'key'   => $unique_id_field->id,
+				'value' => null,
+			);
+		}
+		$filters['mode'] = 'any';
+
+		$entries = GFAPI::get_entries(
+			$form['id'],
+			array(
+				'field_filters' => $filters,
+			),
+			null,
+			array(
+				'offset'    => 0,
+				'page_size' => 999,
+			)
+		);
+
+		if ( empty( $entries ) ) {
+			echo 'There are no entries requiring retroactive population.';
+			exit;
+		}
+
+		$count = 0;
+
+		foreach ( $entries as $entry ) {
+
+			$has_update = false;
+
+			foreach ( $unique_id_fields as $unique_id_field ) {
+				if ( empty( $entry[ $unique_id_field->id ] ) ) {
+					$entry[ $unique_id_field->id ] = gp_unique_id()->get_unique( $form['id'], $unique_id_field );
+					$has_update                    = true;
+				}
+			}
+
+			if ( $has_update ) {
+				GFAPI::update_entry( $entry );
+				$count ++;
 			}
 		}
 
-		if ( $has_update ) {
-			GFAPI::update_entry( $entry );
-			$count ++;
-		}
+		printf( '%d entries updated.', $count );
+
+		exit;
 	}
-
-	printf( '%d entries updated.', $count );
-
-	exit;
-}
+} );
