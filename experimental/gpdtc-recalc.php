f0d2d5cf6092ac479190f32f6ad7f51c80f928f8
Eihab <eihab@cluecode.com>
2021-06-18 15:39:22 -0400

    Update gpdtc-recalc.php

--- experimental/gpdtc-recalc.php
+++ experimental/gpdtc-recalc.php
@@ -3,7 +3,7 @@
  * Re-calculate a GPDTC calculated field dynamically every time it's viewed.
  */
 add_action( 'wp_loaded', function() {
-	$form_id = 413;  // Change this to the form's ID
+	$form_id = 13;  // Change this to the form's ID
 	$field_id = 132; // Change this to the Age's field ID
 	if ( class_exists( 'GP_Date_Time_Calculator' ) ) {
 		$values = array();
