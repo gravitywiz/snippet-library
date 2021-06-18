ce154f02394bb8430c2012e47408e4552aecf767
David Smith <david@gravitywiz.com>
2021-06-18 11:04:12 -0700

    Create gw-field-ids-in-editor-labels.php

---
+++ experimental/gw-field-ids-in-editor-labels.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Gravity Wiz // Gravity Forms // Display Field IDs Next to Field Labels in the Editor
+ * https://gravitywiz.com/
+ */
+add_filter( 'gform_field_content', function( $content, $field ) {
+
+	if ( ! GFCommon::is_form_editor() ) {
+		return $content;
+	}
+
+	static $_gw_inline_field_id_style;
+	if ( ! $_gw_inline_field_id_style ) {
+		$content .= '
+			<style>
+				.gw-inline-field-id {
+				    background-color: #ecedf8;
+				    border: 1px solid #d5d7e9;
+				    border-radius: 40px;
+				    font-size: 0.6875rem;
+				    font-weight: 600;
+				    padding: 0.1125rem 0.4625rem;
+				    margin-bottom: 0.5rem;
+                    display: inline-block;
+                    vertical-align: middle;
+				}
+			</style>';
+		$_gw_inline_field_id_style = true;
+	}
+
+	$search  = '<\/label>|<\/legend>';
+	$replace = sprintf( '\0 <span class="gw-inline-field-id">ID: %d</span>', $field->id );
+	$content = preg_replace( "/$search/", $replace, $content, 1 );
+
+	return $content;
+}, 10, 2 );
