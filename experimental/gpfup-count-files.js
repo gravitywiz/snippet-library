caf9da377afe992b3b1717cddc44605ea10ec8c5
David Smith <david@gravitywiz.com>
2021-06-21 16:45:15 -0700

    Create gpfup-count-files.js

---
+++ experimental/gpfup-count-files.js
@@ -0,0 +1,17 @@
+jQuery(function() {
+  // Update "123" to your form ID and "4" to your File Upload field ID.
+	if (typeof window['GPFUP_123_4'] === 'undefined') {
+		return;
+	}
+	
+  // Update "123" to your form ID and "4" to your File Upload field ID.
+	var store = window.GPFUP_123_4.$store;
+	
+	store.subscribe(function(mutation, state) {
+		if (mutation.type !== 'SET_FILES') {
+			return;
+		}
+		// Update "5" to your Single Product field ID - or - update `#input_123_5_1` to your desired input's HTML ID.
+		jQuery( '#input_123_5_1' ).val( state.files.length ).change();
+	});
+});
