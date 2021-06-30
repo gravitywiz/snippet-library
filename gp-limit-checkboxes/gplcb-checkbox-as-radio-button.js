37ce8e7a154e8a4073c860df8ae533db582272a8
David Smith <david@gravitywiz.com>
2021-06-29 14:04:36 -1000

    Updated activation class name to "gplcb-checkbox-as-radio".

--- gp-limit-checkboxes/gplcb-checkbox-as-radio-button.js
+++ gp-limit-checkboxes/gplcb-checkbox-as-radio-button.js
@@ -1,4 +1,4 @@
-$( '.gplcb-cb-as-radio' ).find( 'input[type="checkbox"]' ).change( function() {
+$( '.gplcb-checkbox-as-radio' ).find( 'input[type="checkbox"]' ).change( function() {
 	$( this ).parents( '.gfield' ).find( 'input[type="checkbox"]' ).prop( 'checked', false );
 	$( this ).prop( 'checked', true );
 } );
