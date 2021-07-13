30483b546c62c80e126a042e4e3602e7dcb0bf00
scyt <scott@gravitywiz.com>
2021-07-13 15:21:44 -0400

    Update gw-auto-complete to check user agent and adjust attribute automatically (#123)
    * Update gw-disable-autocomplete with @spivurno's fix to adjust autocomplete attribute based on user agent.
    
    * Version bump and attribution update.
    
    * Update gw-disable-autocomplete.php
    
    * Removed @author from header.
    * Wrapped `gw_get_browser_name()` function in a conditional to make sure it does not already exist.
    * Updated function bound to `gform_form_tag` to also use user-agent based replacement.
    
    * Added missing closing partenthesis to gw_get_browser_name if statement
    
    Co-authored-by: David Smith <david@gravitywiz.com>

--- gravity-forms/gw-disable-autocomplete.php
+++ gravity-forms/gw-disable-autocomplete.php
@@ -4,17 +4,38 @@
  *
  * Disable browser auto-complete.
  *
- * @version 0.2
- * @author  Scott Buchmann <scott@gravitywiz.com>
+ * @version 0.3
  * @license GPL-2.0+
  * @link    http://gravitywiz.com
  */
 // Disable auto-complete on form.
 add_filter( 'gform_form_tag', function( $form_tag ) {
-	return str_replace( '>', ' autocomplete="password">', $form_tag );
+	$autocomplete = gw_get_browser_name( $_SERVER['HTTP_USER_AGENT'] ) === 'Chrome' ? 'password' : 'off';
+	return str_replace( '>', ' autocomplete="' . $autocomplete . '">', $form_tag );
 }, 11 );
 
 // Diable auto-complete on each field.
 add_filter( 'gform_field_content', function( $input ) {
-	return preg_replace( '/<(input|textarea)/', '<${1} autocomplete="password" ', $input );
+	$autocomplete = gw_get_browser_name( $_SERVER['HTTP_USER_AGENT'] ) === 'Chrome' ? 'password' : 'off';
+	return preg_replace( '/<(input|textarea)/', '<${1} autocomplete="' . $autocomplete . '" ', $input );
 }, 11 );
+
+if ( ! function_exists( 'gw_get_browser_name' ) ) {
+	function gw_get_browser_name( $user_agent ) {
+		if ( strpos( $user_agent, 'Opera' ) || strpos( $user_agent, 'OPR/' ) ) {
+			return 'Opera';
+		} elseif ( strpos( $user_agent, 'Edge' ) ) {
+			return 'Edge';
+		} elseif ( strpos( $user_agent, 'Chrome' ) ) {
+			return 'Chrome';
+		} elseif ( strpos( $user_agent, 'Safari' ) ) {
+			return 'Safari';
+		} elseif ( strpos( $user_agent, 'Firefox' ) ) {
+			return 'Firefox';
+		} elseif ( strpos( $user_agent, 'MSIE' ) || strpos( $user_agent, 'Trident/7' ) ) {
+			return 'Internet Explorer';
+		}
+
+		return 'Other';
+	}
+}
