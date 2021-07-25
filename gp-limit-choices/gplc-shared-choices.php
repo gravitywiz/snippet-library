ef0523bb1fe2c5a822523cde8ac24121f3f3028e
David Smith <david@gravitywiz.com>
2021-07-25 13:04:58 -0400

    ~ PHPCS formatting.

--- gp-limit-choices/gplc-shared-choices.php
+++ gp-limit-choices/gplc-shared-choices.php
@@ -1,22 +1,22 @@
 <?php
 /**
- * Gravity Perks // Limit Choices // Shared Limits Field
+ * Gravity Perks // Limit Choices // Shared Choices
  *
- * Share limits across choices of the same field.
+ * Share limits across choices in the same field.
  *
  * @version  1.0
  * @author   David Smith <david@gravitywiz.com>
  * @license  GPL-2.0+
  * @link     http://gravitywiz.com/documentation/gravity-forms-limit-choices/
  *
- * Plugin Name:  GP Limit Choices — Shared Limits Field
+ * Plugin Name:  GP Limit Choices — Shared Choices
  * Plugin URI:   http://gravitywiz.com/documentation/gravity-forms-limit-choices/
- * Description:  Share limits across choices of the same field.
+ * Description:  Share limits across choices in the same field.
  * Author:       Gravity Wiz
  * Version:      1.0
  * Author URI:   http://gravitywiz.com
  */
-class GPLC_Shared_Limits_Field {
+class GPLC_Shared_Choices {
 
 	public $_args = array();
 
@@ -25,7 +25,7 @@ class GPLC_Shared_Limits_Field {
 		// set our default arguments, parse against the provided arguments, and store for use throughout the class
 		$this->_args = wp_parse_args( $args, array(
 			'form_id'  => false,
-			'field_id' => false
+			'field_id' => false,
 		) );
 
 		// do version check in the init to make sure if GF is going to be loaded, it is already loaded
@@ -42,27 +42,27 @@ class GPLC_Shared_Limits_Field {
 	public function is_applicable_field( $field ) {
 		$form_id = isset( $field->formId ) ? $field->formId : null;
 
-		return $form_id == $this->_args['form_id'] && isset($field->id) && $field->id == $this->_args['field_id'];
+		return (int) $form_id === (int) $this->_args['form_id'] && isset( $field->id ) && (int) $field->id === (int) $this->_args['field_id'];
 	}
 
-	public function modify_gplc_choice_counts( $counts, $form_id, $field )
-	{
-		if( ! $this->is_applicable_field( $field ) ) {
+	public function modify_gplc_choice_counts( $counts, $form_id, $field ) {
+
+		if ( ! $this->is_applicable_field( $field ) ) {
 			return $counts;
 		}
 
 		$shared_count = 0;
 
-		if (is_array($counts)) {
-			foreach ($counts as $count) {
+		if ( is_array( $counts ) ) {
+			foreach ( $counts as $count ) {
 				$shared_count += $count;
 			}
 		}
 
-		if (is_array($field->choices)) {
-			foreach ($field->choices as $choice) {
-				if (isset($choice['limit']) && isset($choice['value'])) {
-					$counts[$choice['value']] = $shared_count;
+		if ( is_array( $field->choices ) ) {
+			foreach ( $field->choices as $choice ) {
+				if ( isset( $choice['limit'] ) && isset( $choice['value'] ) ) {
+					$counts[ $choice['value'] ] = $shared_count;
 				}
 			}
 		}
@@ -73,7 +73,7 @@ class GPLC_Shared_Limits_Field {
 
 # Configuration
 
-new GPLC_Shared_Limits_Field( array(
-	'form_id' => 916,
-	'field_id' => 1
+new GPLC_Shared_Choices( array(
+	'form_id'  => 123,
+	'field_id' => 4,
 ) );
