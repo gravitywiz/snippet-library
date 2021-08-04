addd2ced8541246e78f08dbcd0307b4d4e183068
Eihab Ibrahim <eihab@gravitywiz.com>
2021-08-04 14:17:33 -0400

    ~ Updated gplc-multiselect.js to support Enhanced-UI.

--- experimental/gplc-multiselect.js
+++ experimental/gplc-multiselect.js
@@ -1,8 +1,20 @@
-    var formId      = 560; // Change this to the form ID
-    var fieldId     = 2;   // Change this to the field ID
-    var maxSelected = 2;   // Number to limit maximum selected options to
-    $('#input_' + formId + '_' + fieldId + ' option').on('click change keyup blur', function () {
-        var $select = $(this).parent();
-        var disable = ($select.find('option:checked').length === maxSelected) ? true : false;
-        $select.find('option:not(:checked)').prop( 'disabled', disable );
-    });
+( function( $ ) {
+	var formId      = 560; // Change this to the form ID
+	var fieldId     = 2;   // Change this to the field ID
+	var maxSelected = 2;   // Number to limit maximum selected options to
+
+	var selector                  = '#input_' + formId + '_' + fieldId;
+	var triggerMultiSelectOptions = function( $select ) {
+		var disable = $select.find( 'option:checked' ).length === maxSelected;
+		$select.find( 'option:not(:checked)' ).prop( 'disabled', disable );
+		$select.trigger( 'chosen:updated' );
+	};
+	// Standard multi-select
+	$( selector + ' option' ).on('click change keyup blur', function () {
+		triggerMultiSelectOptions( $( this ).parent() );
+	});
+	// Enhanced-UI multi-select
+	$( selector ).on('change', function () {
+		triggerMultiSelectOptions( $( this ) );
+	});
+})( jQuery );
