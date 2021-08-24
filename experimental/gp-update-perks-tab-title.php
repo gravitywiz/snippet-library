cc8cc282bd34b9e08796901831c5a70218a84b92
David Smith <david@gravitywiz.com>
2021-08-24 09:41:44 -0400

    Update gp-update-perks-tab-title.php

--- experimental/gp-update-perks-tab-title.php
+++ experimental/gp-update-perks-tab-title.php
@@ -1,1 +1,13 @@
-
+/**
+ * Gravity Perks // Update Perks Tab Field Settings Title
+ * https://gravitywiz.com/
+ */
+add_action( 'gform_field_settings_tabs', function( $tabs ) {
+	foreach ( $tabs as &$tab ) {
+		if ( $tab['title'] === __( 'Perks', 'gravityperks' ) ) {
+			// Update "Bonus Features" to whatever you want to call the perks tab.
+			$tab['title'] = 'Bonus Features';
+		}
+	}
+	return $tabs;
+}, 99 );
