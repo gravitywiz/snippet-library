9bdf8608acb4848df8998c3456008503b46de5b8
David Smith <david@gravitywiz.com>
2021-08-25 11:23:57 -0400

    Create gpecf-remove-field-label-from-option-label.php

---
+++ gp-ecommerce-fields/gpecf-remove-field-label-from-option-label.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Gravity Perks // eCommerce Fields // Remove Field Label from Option Label in Order Summary
+ * https://gravitywiz.com/documentation/gravity-forms-ecommerce-fields/
+ * 
+ * By default, Gravity Forms displays options in order summary like so:
+ * Field Label: Option Label
+ * 
+ * With this snippet in place, options will be displayed as:
+ * Option Label
+ */
+add_filter( 'gform_product_info', function( $product_info ) {
+	foreach ( $product_info['products'] as &$product ) {
+		if ( ! empty( $product['options'] ) ) {
+			foreach ( $product['options'] as &$option ) {
+				$option['option_label'] = $option['option_name'];
+			}
+		}
+	}
+	return $product_info;
+} );
