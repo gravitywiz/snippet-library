1345534b96be7de694f79d0ba6f612c367a3875d
David Smith <david@gravitywiz.com>
2021-09-01 09:04:41 -0400

    Update gpuid-input-masks.php
    Whitespace

--- gp-unique-id/gpuid-input-masks.php
+++ gp-unique-id/gpuid-input-masks.php
@@ -6,12 +6,12 @@
 add_filter( 'gpui_unique_id', function( $unique, $form_id, $field_id ) {
 	// Update "123" to your form ID and "4" to your Unique ID's field ID.
 	if ( $form_id == 123 && $field_id == 4 ) {
-  	/* 
-     * Define your input mask. 
-     * - "a" will be any lowercase letter a through z.
-     * - "A" will be any uppercase letter A through Z.
-     * - "9" will be any number 0 - 9.
-     */
+	  	/* 
+		 * Define your input mask. 
+		 * - "a" will be any lowercase letter a through z.
+		 * - "A" will be any uppercase letter A through Z.
+		 * - "9" will be any number 0 - 9.
+		 */
 		$mask = 'aa 999 999';
 		$unique = gpui_generate_unique_id_from_mask( $mask );
 	}
