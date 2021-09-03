b17091518183cf8819bbd3710d0ebfc10e9e436b
David Smith <david@gravitywiz.com>
2021-09-03 14:25:35 -0400

    Added support for filtering the displayed states based on the allowed states.

--- gravity-forms/gw-restrict-states-in-address-fields.php
+++ gravity-forms/gw-restrict-states-in-address-fields.php
@@ -5,13 +5,14 @@
  * Restrict the states that can be selected for Address fields. Either restrict specific fields or restrict all Address
  * fields on the site.
  *
- * See https://gravitywiz.com/documentation/how-do-i-install-a-snippet/ for details on how to install this snippet. 
+ * See https://gravitywiz.com/documentation/how-do-i-install-a-snippet/ for details on how to install this snippet.
  *
  * @version 1.0
  * @license GPL-2.0+
  * @link    http://gravitywiz.com
  */
 class GW_Restrict_States_In_Address_Field {
+
 	public function __construct( $args = array() ) {
 		// set our default arguments, parse against the provided arguments, and store for use throughout the class
 		$this->_args = wp_parse_args(
@@ -29,6 +30,9 @@ class GW_Restrict_States_In_Address_Field {
 		}
 
 		add_filter( 'gform_validation', array( $this, 'validate' ) );
+		add_filter( 'gform_field_input', array( $this, 'register_us_states_filter' ), 10, 2 );
+		add_filter( 'gform_field_content', array( $this, 'unregister_us_states_filter' ) );
+
 	}
 
 
@@ -40,17 +44,17 @@ class GW_Restrict_States_In_Address_Field {
 
 		// Do not validate the states unless the form ID matches or if no form ID was supplied which means we validate
 		// for all forms.
-		if ( $this->_args['form_id'] && $form['id'] !== $this->_args['form_id'] ) {
+		if ( ! $this->is_applicable_form( $form ) ) {
 			return $result;
 		}
 
 		foreach ( $form['fields'] as &$field ) {
-			$field_value = GFFormsModel::get_field_value( $field );
 
-			if ( $field->type !== 'address' ) {
+			if ( ! $this->is_applicable_field( $field ) ) {
 				continue;
 			}
 
+			$field_value    = GFFormsModel::get_field_value( $field );
 			$selected_state = rgar( $field_value, "{$field->id}.4" );
 
 			if ( ! $selected_state ) {
@@ -72,6 +76,60 @@ class GW_Restrict_States_In_Address_Field {
 
 		return $result;
 	}
+
+	/**
+	 * Register our states filter immediately before the field's input markup is generated.
+	 *
+	 * @param $return
+	 * @param $field
+	 *
+	 * @return mixed
+	 */
+	public function register_us_states_filter( $return, $field ) {
+		if ( $this->is_applicable_field( $field ) ) {
+			add_filter( 'gform_us_states', array( $this, 'filter_states' ) );
+		}
+		return $return;
+	}
+
+	/**
+	 * Unregister our states filter after the field's input markup has been generated.
+	 *
+	 * @param $return
+	 *
+	 * @return mixed
+	 */
+	public function unregister_us_states_filter( $return ) {
+		remove_filter( 'gform_us_states', array( $this, 'filter_states' ) );
+		return $return;
+	}
+
+	public function filter_states( $states ) {
+		return array_intersect( $this->_args['allowed_states'], $states );
+	}
+
+	public function is_applicable_form( $form ) {
+
+		$form_id = isset( $form['id'] ) ? $form['id'] : $form;
+
+		return empty( $this->_args['form_id'] ) || (int) $form_id === (int) $this->_args['form_id'];
+	}
+
+	public function is_applicable_field( $field ) {
+
+		if ( ! $this->is_applicable_form( $field->formId ) ) {
+			return false;
+		}
+
+		// Check if this is our specified field if set.
+		if ( isset( $this->_args['field_id'] ) && ! empty( $this->_args['field_id'] ) ) {
+			return (int) $field->id === (int) $this->_args['field_id'];
+		}
+
+		// Otherwise, all Address fields are applicable.
+		return $field->get_input_type() === 'address';
+	}
+
 }
 
 // Restrict states for all Address field.
@@ -79,15 +137,15 @@ class GW_Restrict_States_In_Address_Field {
 //	'allowed_states' => array(
 //		'California',
 //		'Iowa',
-//	)
+//	),
 //) );
 
 // Restrict states for specific Address field.
 new GW_Restrict_States_In_Address_Field( array(
-	'form_id'        => 1,
-	'field_id'       => 2,
+	'form_id'        => 123,
+	'field_id'       => 4,
 	'allowed_states' => array(
 		'California',
 		'Iowa',
-	)
+	),
 ) );
