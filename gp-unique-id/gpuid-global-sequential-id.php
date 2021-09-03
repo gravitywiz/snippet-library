a9750f265a79b0ced476709b8cfc555675a30641
Samuel Bassah <sbassah@gmail.com>
2021-09-03 17:41:46 +0000

    Updated Snippet Header

--- gp-unique-id/gpuid-global-sequential-id.php
+++ gp-unique-id/gpuid-global-sequential-id.php
@@ -1,15 +1,16 @@
 <?php
 /**
- * Gravity Wiz // GP Unique ID // Share a global sequential unique ID across all forms
+ * Gravity Perks // Unique ID // Share a global sequential unique ID across all forms
  * https://gravitywiz.com/documentation/gravity-forms-unique-id/
  *
- * Sets a globally sequential unique ID across all forms on the website. 
+ * Sets a global sequential unique ID across all forms on the website. 
+ *
  * To use this snippet, add Unique ID fields on all the forms you'd like to generate Unique IDs, 
  * and update the $atts variable within the snippet to target any of the forms and its Unique ID field. 
  *
- * Plugin Name:  GP Unique ID — Share a global sequential unique ID across all forms
+ * Plugin Name:  GP Unique ID — Global Sequential ID
  * Plugin URI:   https://gravitywiz.com/documentation/gravity-forms-unique-id/
- * Description:  Sets a globally sequential unique ID across all forms on the website. 
+ * Description:  Sets a global sequential unique ID across all forms on the website. 
  * Author:       Gravity Wiz
  * Version:      0.1
  * Author URI:   https://gravitywiz.com/
