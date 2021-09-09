54db2931aae79da667b5936d1428c27da2d5e41f
Samuel Bassah <sbassah@gmail.com>
2021-09-09 03:23:21 +0000

    Set Post Taxonomy Terms

---
+++ gp-populate-anything/gppa-set-post-taxonomy-terms.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Gravity Perks // Populate Anything // Set Post Taxonomy Terms
+ * https://gravitywiz.com/documentation/gravity-forms-populate-anything/
+ * 
+ * When the Term ID is set as the value of a dynamically populated choice-based, 
+ * this snippet allows you update the Post that is created with the name of the Taxonomy instead the Term ID. 
+ * This applies to forms that have Post Fields.
+ */
+add_filter( 'gform_after_create_post', function( $post_id, $entry, $form ) {
+
+	foreach( $form['fields'] as &$field ) {
+		if( is_callable( 'gp_populate_anything' ) && $field->{'gppa-choices-enabled'} && $field->{'gppa-choices-object-type'} == 'term' ) {
+			$value = gp_populate_anything()->get_field_value( $form, $entry, $field->id );
+			$term_ids = is_array( $value ) ? $value : explode( ',', $value );
+			$term = get_term( $term_ids[0] );
+			wp_set_post_terms( $post_id, $term_ids, $term->taxonomy );
+		}
+	}
+
+}, 10, 3 );
