ad3ceb13a1b9d0c1078f678b10e90b0f2b41ab06
Samuel Bassah <sbassah@gmail.com>
2021-09-09 17:34:12 +0000

    Made Changes requested
    Updated the Snippet Description.
    Updated the Instruction to use our Custom Javascript plugin
    Removed the <script> tag within the snippet

--- gp-copy-cat/gpcc-copy-product-value.js
+++ gp-copy-cat/gpcc-copy-product-value.js
@@ -1,16 +1,17 @@
 /**
-* Gravity Perks // GP Copy Cat // Copy Product Value Without the Price
+* Gravity Perks // Copy Cat // Copy Product Value Without the Price
 * https://gravitywiz.com/documentation/gravity-forms-copy-cat/
 *
-* Copy the value of a choice based product field without the price.
+* By default, choice-based Product fields will copy a raw value that includes 
+* both the selected value and the price of the selection option(s). 
+* Use this snippet to copy only the value and exclude the price.
 *
 * Instructions:
-* 1. Add an HTML field to your form.
-* 2. Copy and paste the entire content of this snippet into the "Content" field setting.
+* 1. Install our free Custom Javascript for Gravity Forms plugin. 
+* Download the plugin here: https://gravitywiz.com/gravity-forms-custom-javascript/
+* 2. Copy and paste the snippet into the editor of the Custom Javascript for Gravity Forms plugin.
 * 3. Update the target field ID within the snippet.
 */
-<script type="text/javascript">
-
 gform.addFilter( 'gppc_copied_value', function( value, $elem, data ) {
   // Update "1" to the ID of the field being copied to.
 	if( data.target == 1 && value ) {
@@ -18,5 +19,3 @@ gform.addFilter( 'gppc_copied_value', function( value, $elem, data ) {
 	}
 	return value;
 } );
- 
-</script>
