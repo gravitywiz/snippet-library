cbb6827755b41111f7e92bf254e6365d1d0eb035
Samuel Bassah <sbassah@gmail.com>
2021-09-09 15:57:00 +0000

    Updated the snippet to use anonymous function.

--- gravity-forms/gw-currency-symbol-right-to-left.php
+++ gravity-forms/gw-currency-symbol-right-to-left.php
@@ -3,9 +3,7 @@
  * Graivty Wiz // Gravity Forms // Move Currency Symbol from the Right to the Left (i.e. "0,00 €" to "€ 0,00")
  * http://gravitywiz.com/how-do-i-move-the-currency-symbol-from-the-right-to-the-left-for-gravity-forms/
  */
-add_filter( 'gform_currencies', 'gw_modify_currencies' );
-function gw_modify_currencies( $currencies ) {
-
+add_filter( 'gform_currencies', function ($currencies) {
     $currencies['EUR'] = array(
       'name'               => esc_html__( 'Euro', 'gravityforms' ),
       'symbol_left'        => '&#8364;',
@@ -15,6 +13,5 @@ function gw_modify_currencies( $currencies ) {
       'decimal_separator'  => ',',
       'decimals'           => 2
     );
-
     return $currencies;
-}
+});
