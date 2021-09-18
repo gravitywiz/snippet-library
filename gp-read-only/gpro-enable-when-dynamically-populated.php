708ba21aedc20a654b5533ec7beb8f01a9b8d7e8
David Smith <david@gravitywiz.com>
2021-09-17 20:38:34 -0400

    Update gpro-enable-when-dynamically-populated.php

--- gp-read-only/gpro-enable-when-dynamically-populated.php
+++ gp-read-only/gpro-enable-when-dynamically-populated.php
@@ -5,14 +5,14 @@
  *
  * Use this snippet to make fields readonly if they are dynamically populated. You must enable the "Read-only" field
  * setting for this functionality to apply.
- * 
+ *
  * See video: https://www.loom.com/share/7c42fc8c0aef42d990ae600675546774
  */
-add_filter( 'gform_pre_render', function( $form, $ajax, $field_values ) {
+add_filter( 'gform_pre_render', function ( $form, $ajax, $field_values ) {
 
 	foreach ( $form['fields'] as &$field ) {
 		if ( $field->gwreadonly_enable ) {
-			$value = GFFormsModel::get_field_value( $field, $field_values, false );
+			$value = GFFormsModel::get_field_value( $field, $field_values, false ) || $field->gppa_hydrated_value;
 			if ( ! $value ) {
 				$field->gwreadonly_enable = false;
 			}
