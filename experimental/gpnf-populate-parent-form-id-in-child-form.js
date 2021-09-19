7b46bae78f7f5fd2ce089141685a48b586d31610
David Smith <david@gravitywiz.com>
2021-09-19 00:31:18 -0400

    Create gpnf-populate-parent-form-id-in-child-form.js

---
+++ experimental/gpnf-populate-parent-form-id-in-child-form.js
@@ -0,0 +1,13 @@
+/**
+ * Gravity Perks // Nested Forms // Populate Parent Form ID in Child Form
+ * https://gravitywiz.com/documentation/gravity-forms-nested-forms/
+ */
+gform.addAction( 'gpnf_init_nested_form', function( childFormId, gpnf ) {
+	// Update "123" to your the ID of your child form.
+	var targetChildFormId  = 485; 
+	// Update "4" to the ID of the child field in which the parent form ID will be populated.
+	var targetChildFieldId = 2;
+	if ( childFormId == targetChildFormId ) {
+		$( '#input_' + targetChildFormId + '_' + targetChildFieldId ).val( gpnf.formId ).change();
+	}
+} );
