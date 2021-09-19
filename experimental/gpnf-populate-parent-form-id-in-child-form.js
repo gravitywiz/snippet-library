470f0275b1fef35cb3c88d41a083326652fa2313
David Smith <david@gravitywiz.com>
2021-09-19 00:48:20 -0400

    Update gpnf-populate-parent-form-id-in-child-form.js

--- experimental/gpnf-populate-parent-form-id-in-child-form.js
+++ experimental/gpnf-populate-parent-form-id-in-child-form.js
@@ -4,9 +4,9 @@
  */
 gform.addAction( 'gpnf_init_nested_form', function( childFormId, gpnf ) {
 	// Update "123" to your the ID of your child form.
-	var targetChildFormId  = 485; 
+	var targetChildFormId  = 123; 
 	// Update "4" to the ID of the child field in which the parent form ID will be populated.
-	var targetChildFieldId = 2;
+	var targetChildFieldId = 4;
 	if ( childFormId == targetChildFormId ) {
 		$( '#input_' + targetChildFormId + '_' + targetChildFieldId ).val( gpnf.formId ).change();
 	}
