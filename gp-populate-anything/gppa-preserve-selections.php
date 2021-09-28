6b15e565728ce8c9cc72080d23e659fc908ac8b9
David Smith <david@gravitywiz.com>
2021-09-28 13:56:20 -0400

    Update gppa-preserve-selections.php
    I wasn't able to recreate this issue but in some cases it seems the value may not always be an array? Let's typecast it to be safe.
    
    See: https://secure.helpscout.net/conversation/1612210784/26937/#thread-4793893526

--- gp-populate-anything/gppa-preserve-selections.php
+++ gp-populate-anything/gppa-preserve-selections.php
@@ -5,7 +5,7 @@
  */
 // Update "123" to your form ID and "4" to your field ID. 
 add_filter( 'gppa_input_choices_123_4', function( $choices, $field, $objects ) {
-	$selected_values = rgar( gp_populate_anything()->get_field_values_from_request(), $field->id );
+	$selected_values = (array) rgar( gp_populate_anything()->get_field_values_from_request(), $field->id );
 	foreach ( $choices as &$choice ) {
 		if ( in_array( $choice['value'], $selected_values, true ) ) {
 			$choice['isSelected'] = true;
