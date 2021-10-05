01457bba3e16f7019b15835e049b893ee5eca9d6
David Smith <david@gravitywiz.com>
2021-10-05 17:47:29 -0400

    Create gw-none-of-the-above-checkbox.php

---
+++ gravity-forms/gw-none-of-the-above-checkbox.php
@@ -0,0 +1,31 @@
+/**
+ * Gravity Wiz // Gravity Forms // "None of the Above" Checkbox
+ * https://gravitywiz.com/
+ *
+ * Use this snippet to enable a proper "None of the Above" option in your Checkbox fields. If any 
+ * other option is checked, the "None of the Above" option will be disabled. If the "None of the Above"
+ * option is checked, all other options will be disabled.
+ */
+// Update "1" to your Checkbox field ID. 
+var $field  = $( '#field_GFFORMID_1' );
+var $last   = $field.find( '.gchoice:last-child input' );
+var $others = $field.find( 'input' ).not( $last );
+
+$last.on( 'click', function() {
+	var $checkboxes = $field.find( 'input' ).not( $( this ) )
+	if ( $( this ).is( ':checked' ) ) {
+		$checkboxes
+			.prop( 'checked', false )
+			.prop( 'disabled', true );
+	} else {
+		$checkboxes.prop( 'disabled', false );
+	}
+} );
+
+$others.on( 'click', function() {
+	if ( $others.filter( ':checked' ).length ) {
+		$last.prop( 'disabled', true );
+	} else {
+		$last.prop( 'disabled', false );
+	}
+} );
