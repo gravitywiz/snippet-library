63e8453f019d51df12bd46874900b5d18a03bdd0
Eihab Ibrahim <eihab@gravitywiz.com>
2021-10-06 13:00:58 -0400

    ~ PHPCS: Auto format.

--- gp-nested-forms/gp-nested-forms-auto-attach-child-files.php
+++ gp-nested-forms/gp-nested-forms-auto-attach-child-files.php
@@ -5,19 +5,18 @@
  */
 add_filter( 'gform_notification', function( $notification, $form, $entry ) {
 
-	if( ! class_exists( 'GPNF_Entry' ) ) {
+	if ( ! class_exists( 'GPNF_Entry' ) ) {
 		return $notification;
 	}
 
 	$upload_fields = GFCommon::get_fields_by_type( $form, array( 'fileupload' ) );
 
 	// If parent form has upload fields, rely on the notification's Attachments setting.
-	if( ! empty( $upload_fields ) ) {
+	if ( ! empty( $upload_fields ) ) {
 		if ( ! rgar( $notification, 'enableAttachments', false ) ) {
 			return $notification;
 		}
-	}
-	// Otherwise, rely on a manually defined array of notification IDs.
+	} // Otherwise, rely on a manually defined array of notification IDs.
 	else {
 		$notification_ids = array( '5daaedb49dc32', '5dbce25cc21c2' );
 		if ( ! in_array( $notification['id'], $notification_ids ) ) {
@@ -25,12 +24,12 @@ add_filter( 'gform_notification', function( $notification, $form, $entry ) {
 		}
 	}
 
-	$attachments =& $notification['attachments'];
+	$attachments  =& $notification['attachments'];
 	$parent_entry = new GPNF_Entry( $entry );
 
-	foreach( $form['fields'] as $field ) {
+	foreach ( $form['fields'] as $field ) {
 
-		if( $field->get_input_type() !== 'form' ) {
+		if ( $field->get_input_type() !== 'form' ) {
 			continue;
 		}
 
@@ -38,7 +37,7 @@ add_filter( 'gform_notification', function( $notification, $form, $entry ) {
 		$upload_fields = GFCommon::get_fields_by_type( GFAPI::get_form( $field->gpnfForm ), array( 'fileupload' ) );
 		$child_entries = $parent_entry->get_child_entries( $field->id );
 
-		foreach( $child_entries as $child_entry ) {
+		foreach ( $child_entries as $child_entry ) {
 			foreach ( $upload_fields as $upload_field ) {
 
 				$attachment_urls = rgar( $child_entry, $upload_field->id );
