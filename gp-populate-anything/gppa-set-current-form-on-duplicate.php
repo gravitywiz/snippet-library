f1550fa76842f78b6c27e5a6bddd3efc51ed44aa
Samuel Bassah <sbassah@gmail.com>
2021-10-07 08:48:51 +0000

    Added Plugin Header

--- gp-populate-anything/gppa-set-current-form-on-duplicate.php
+++ gp-populate-anything/gppa-set-current-form-on-duplicate.php
@@ -7,6 +7,13 @@
  * form ID to the new form ID when duplicating an existing form. 
  *
  * To enable on any field, just set the "gppa-set-current-form" class on the CSS Class Name setting.
+ * 
+ * Plugin Name:  GP Populate Anything - Auto-update Form ID on Form Duplication
+ * Plugin URI:   https://gravitywiz.com/documentation/gravity-forms-populate-anything/
+ * Description:  Auto-update form ID to the new form ID when duplicating an existing form. 
+ * Author:       Gravity Wiz
+ * Version:      0.1
+ * Author URI:   https://gravitywiz.com/
  */
 add_action( 'gform_post_form_duplicated', function( $form_id, $new_id ) {
 	$form = GFAPI::get_form( $new_id );
