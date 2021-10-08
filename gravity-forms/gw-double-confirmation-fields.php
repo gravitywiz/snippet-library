67bcd91499982752980ecbdeda91a57e4d7945b7
Samuel Bassah <sbassah@gmail.com>
2021-10-08 13:04:54 +0000

    Fixed Code Formatting to Standard

--- gravity-forms/gw-double-confirmation-fields.php
+++ gravity-forms/gw-double-confirmation-fields.php
@@ -12,65 +12,71 @@
  * Version: 0.3
  * Author URI: https://gravitywiz.com/
  */
-add_filter('gform_validation', 'gfcf_validation');
-function gfcf_validation($validation_result) {
+add_filter( 'gform_validation', 'gfcf_validation' );
+function gfcf_validation( $validation_result ) {
 	global $gfcf_fields;
 
-	$form = $validation_result['form'];
+	$form          = $validation_result['form'];
 	$confirm_error = false;
 
-	if(!isset($gfcf_fields[$form['id']]))
+	if ( ! isset( $gfcf_fields[ $form['id'] ] ) ) {
 		return $validation_result;
+	}
 
-	foreach($gfcf_fields[$form['id']] as $confirm_fields) {
+	foreach ( $gfcf_fields[ $form['id'] ] as $confirm_fields ) {
 
 		$values = array();
 
 		// loop through form fields and gather all field values for current set of confirm fields
-		foreach($form['fields'] as $confirm_field) {
-			if(!in_array($confirm_field['id'], $confirm_fields))
+		foreach ( $form['fields'] as $confirm_field ) {
+			if ( ! in_array( $confirm_field['id'], $confirm_fields ) ) {
 				continue;
+			}
 
-			$values[] = rgpost("input_{$confirm_field['id']}");
+			$values[] = rgpost( "input_{$confirm_field['id']}" );
 
 		}
 
 		// filter out unique values, if greater than 1, a value was different
-		if(count(array_unique($values)) <= 1)
+		if ( count( array_unique( $values ) ) <= 1 ) {
 			continue;
+		}
 
 		$confirm_error = true;
 
-		foreach($form['fields'] as &$field) {
-			if(!in_array($field['id'], $confirm_fields))
+		foreach ( $form['fields'] as &$field ) {
+			if ( ! in_array( $field['id'], $confirm_fields ) ) {
 				continue;
+			}
 
 			// fix to remove phone format instruction
-			if(RGFormsModel::get_input_type($field) == 'phone')
+			if ( RGFormsModel::get_input_type( $field ) == 'phone' ) {
 				$field['phoneFormat'] = '';
+			}
 
-			$field['failed_validation'] = true;
+			$field['failed_validation']  = true;
 			$field['validation_message'] = 'Your values do not match.';
 		}
-
 	}
 
-	$validation_result['form'] = $form;
-	$validation_result['is_valid'] = !$validation_result['is_valid'] ? false : !$confirm_error;
+	$validation_result['form']     = $form;
+	$validation_result['is_valid'] = ! $validation_result['is_valid'] ? false : ! $confirm_error;
 
 	return $validation_result;
 }
 
-function register_confirmation_fields($form_id, $fields) {
+function register_confirmation_fields( $form_id, $fields ) {
 	global $gfcf_fields;
 
-	if(!$gfcf_fields)
+	if ( ! $gfcf_fields ) {
 		$gfcf_fields = array();
+	}
 
-	if(!isset($gfcf_fields[$form_id]))
-		$gfcf_fields[$form_id] = array();
+	if ( ! isset( $gfcf_fields[ $form_id ] ) ) {
+		$gfcf_fields[ $form_id ] = array();
+	}
 
-	$gfcf_fields[$form_id][] = $fields;
+	$gfcf_fields[ $form_id ][] = $fields;
 }
 
 register_confirmation_fields( 1, array( 2, 3 ) );
