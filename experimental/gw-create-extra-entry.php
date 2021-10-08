fc0e13120681a9d2ddcb0fdd4fb12be0bd12ecc2
David Smith <david@gravitywiz.com>
2021-10-08 10:01:46 -0400

    Update gw-create-extra-entry.php

--- experimental/gw-create-extra-entry.php
+++ experimental/gw-create-extra-entry.php
@@ -5,7 +5,7 @@
  * 
  * Create an additional entry identical to the first if a specified checkbox is checked.
  * 
- * @see 
+ * @see https://stackoverflow.com/a/69497108/227711
  */
 // Update "123" to your form ID.
 add_filter( 'gform_after_submission_123', function( $entry ) {
