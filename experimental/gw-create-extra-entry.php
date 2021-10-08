41a3b56daa3147585b9329454a018daeb374dfe3
David Smith <david@gravitywiz.com>
2021-10-08 10:00:07 -0400

    Create gw-create-extra-entry.php

---
+++ experimental/gw-create-extra-entry.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Gravity Wiz // Gravity Forms // Create an Extra Entry if Checkbox is Checked
+ * https://gravitywiz.com/
+ * 
+ * Create an additional entry identical to the first if a specified checkbox is checked.
+ * 
+ * @see 
+ */
+// Update "123" to your form ID.
+add_filter( 'gform_after_submission_123', function( $entry ) {
+	// Update "4" to your Checkbox field ID. Assuming this is a single checkbox field, leave the "1" as it will target the first checkbox.
+	if ( $entry['4.1'] ) {
+		GFAPI::add_entry( $entry );
+	}
+}, 10 );
