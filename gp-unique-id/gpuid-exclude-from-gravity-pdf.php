46b6c4d226c5a4d979bdc735fdb31adb6d2a7f4d
Samuel Bassah <sbassah@gmail.com>
2021-10-12 16:55:08 +0000

    Added a Plugin Header

--- gp-unique-id/gpuid-exclude-from-gravity-pdf.php
+++ gp-unique-id/gpuid-exclude-from-gravity-pdf.php
@@ -2,6 +2,15 @@
 /**
  * Gravity Perks // Unique ID // Exclude Unique ID Fields from Gravity PDF
  * https://gravitywiz.com/documentation/gravity-forms-unique-id/
+ *
+ * Exclude Unique ID Fields, when using Gravity PDF together with GP Unique ID Perk.
+ *
+ * Plugin Name:  GP Unique ID - Exclude Unique ID Fields from Gravity PDF
+ * Plugin URI:   https://gravitywiz.com/documentation/gravity-forms-unique-id/
+ * Description:  Exclude Unique ID Fields, when using Gravity PDF together with GP Unique ID Perk.
+ * Author:       Gravity Wiz
+ * Version:      0.1
+ * Author URI:   https://gravitywiz.com/
  */
 add_filter( 'gform_form_post_get_meta', function( $form ) {
 	foreach ( $form['fields'] as &$field ) {
