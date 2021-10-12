581ab2866407d2c77b0eed9f2ab2b34b1d73228a
Samuel Bassah <sbassah@gmail.com>
2021-10-12 16:01:54 +0000

    Updated Snippet URL and IDs

--- gp-copy-cat/gpcc-set-copy-cat-fields.php
+++ gp-copy-cat/gpcc-set-copy-cat-fields.php
@@ -1,17 +1,18 @@
 <?php
 /**
  * Gravity Perks // Copy Cat // Filter Copy Cat Fields
- * https://gravityperks.com/
+ * https://gravitywiz.com/documentation/gravity-forms-copy-cat/
  *
  * Add copy cat class to fields programmatically.
  */
-add_filter( 'gpcc_copy_cat_fields_126', function( $fields, $form ) {
+// Update 123 to your form ID
+add_filter( 'gpcc_copy_cat_fields_123', function( $fields, $form ) {
 
-	// Update 44 to the ID of the field that would have the Copy Cat Class
-	$fields[44] = array(
+	// Update 1 to the ID of the field that would have the Copy Cat Class
+	$fields[1] = array(
 		array(
-			'source'       => 44,  // Update 44 to the ID of the Source Field
-			'target'       => 45,  // Update 45 to the ID of the Target Field.
+			'source'       => 1,  // Update 1 to the ID of the source field
+			'target'       => 2,  // Update 2 to the ID of the target field.
 			'sourceFormId' => $form['id'],
 			'targetFormId' => $form['id'],
 		),
@@ -19,4 +20,3 @@ add_filter( 'gpcc_copy_cat_fields_126', function( $fields, $form ) {
 
 	return $fields;
 }, 10, 2 );
-
