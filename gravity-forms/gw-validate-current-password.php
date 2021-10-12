1d2dad52b5afc1f6e14b5a6853d495e6ed3624b1
Samuel Bassah <sbassah@gmail.com>
2021-10-12 15:50:13 +0000

    Updated the Snippet Header

--- gravity-forms/gw-validate-current-password.php
+++ gravity-forms/gw-validate-current-password.php
@@ -4,9 +4,8 @@
  * https://gravitywiz.com/
  *
  * Require users to confirm their current password before changing their password.
- *
- * Update "123" to your form ID and "4" to your Password field ID.
  */
+// Update "123" to your form ID and "4" to your Password field ID.
 add_filter( 'gform_field_validation_123_4', function( $result, $value ) {
 	$user = wp_get_current_user();
 	if ( $user && ! wp_check_password( $value, $user->data->user_pass, $user->ID ) ) {
