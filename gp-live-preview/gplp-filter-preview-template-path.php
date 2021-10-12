c8bd0cfd71276b8a143b612f8d848d9313adee2a
Samuel Bassah <sbassah@gmail.com>
2021-10-12 16:08:19 +0000

    Updated Snippet Header

--- gp-live-preview/gplp-filter-preview-template-path.php
+++ gp-live-preview/gplp-filter-preview-template-path.php
@@ -1,9 +1,9 @@
 <?php
 /**
  * Gravity Perks // Live Preview // Filter Preview Template Path
- * https://gravityperks.com/
+ * https://gravitywiz.com/documentation/gravity-forms-live-preview/
  *
- * Change the template used to render the Live Preview.
+ * Change the page template used to render the live preview.
  */
 add_filter( 'gplp_preview_template', function( $template ) {
     return get_stylesheet_directory() . '/custom-template.php';
