6dfdd5a1c18fc672d5011c91bd36f0b9e9251871
David Smith <david@gravitywiz.com>
2021-10-13 10:40:38 -0400

    Update gpcc-set-copy-cat-fields.php

--- gp-copy-cat/gpcc-set-copy-cat-fields.php
+++ gp-copy-cat/gpcc-set-copy-cat-fields.php
@@ -5,14 +5,14 @@
  *
  * Add copy cat class to fields programmatically.
  */
-// Update 123 to your form ID
+// Update 123 to your form ID.
 add_filter( 'gpcc_copy_cat_fields_123', function( $fields, $form ) {
 
-	// Update 1 to the ID of the field that would have the Copy Cat Class
-	$fields[1] = array(
+	// Update 4 to the ID of the field that would have the Copy Cat Class
+	$fields[4] = array(
 		array(
-			'source'       => 1,  // Update 1 to the ID of the source field
-			'target'       => 2,  // Update 2 to the ID of the target field.
+			'source'       => 4,  // Update 4 to the ID of the source field.
+			'target'       => 5,  // Update 5 to the ID of the target field.
 			'sourceFormId' => $form['id'],
 			'targetFormId' => $form['id'],
 		),
