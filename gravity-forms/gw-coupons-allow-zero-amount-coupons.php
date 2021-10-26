a5bfc8de84415fc548fe0812ceeeefb4b0e4dd8c
Samuel Bassah <sbassah@gmail.com>
2021-10-26 11:54:17 +0000

    Added a plugin header

--- gravity-forms/gw-coupons-allow-zero-amount-coupons.php
+++ gravity-forms/gw-coupons-allow-zero-amount-coupons.php
@@ -2,6 +2,16 @@
 /**
  * Gravity Wiz // Gravity Forms Coupons // Allow Zero Amount Coupons
  * https://gravitywiz.com/
+ *
+ * By default it's not possible to create a coupn with the amount of zero.
+ * This snippet allows you to create coupons with the amount of zero.
+ *
+ * Plugin Name:  Gravity Forms Coupons - Allow Zero Amount Coupons
+ * Plugin URI:   https://gravitywiz.com/
+ * Description:  Create Coupons with the amount of zero.
+ * Author:       Gravity Wiz
+ * Version:      0.1
+ * Author URI:   https://gravitywiz.com/
  */
 add_filter( 'gform_gravityformscoupons_feed_settings_fields', function( $settings ) {
 
