83ec350c3b70315e64626d6af6ee10f67395511d
David Smith <david@gravitywiz.com>
2021-10-25 23:19:23 -0400

    Update gw-wcgfpa-display-choice-labels.php

--- experimental/gw-wcgfpa-display-choice-labels.php
+++ experimental/gw-wcgfpa-display-choice-labels.php
@@ -3,15 +3,13 @@
  * Gravity Wiz // WC GF Product Add-ons // Display Choice Labels in Cart
  * https://gravitywiz.com/
  */
-add_filter( 'woocommerce_gforms_order_meta_value', function( $value, $field ) {
-
+add_filter( 'woocommerce_gforms_get_item_data', function( $data, $field, $entry, $form ) {
 	if ( ! empty( $field->choices ) ) {
 		foreach ( $field->choices as $choice ) {
-			if ( $choice['value'] == $value ) {
-				$value = $choice['text'];
+			if ( $choice['value'] == $data['display'] ) {
+				$data['display'] = $choice['text'];
 			}
 		}
 	}
-	
-	return $value;
-}, 10, 2 );
+	return $data;
+}, 10, 4 );
