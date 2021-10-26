25fca4d4dd0dd47abd7248244753c46b90d78b3f
David Smith <david@gravitywiz.com>
2021-10-26 11:34:42 -0400

    Update gppa-populate-multi-file-upload-as-separate-choices.php

--- gp-populate-anything/gppa-populate-multi-file-upload-as-separate-choices.php
+++ gp-populate-anything/gppa-populate-multi-file-upload-as-separate-choices.php
@@ -4,13 +4,6 @@
  * https://gravitywiz.com/documentation/gravity-forms-populate-anything/
  *
  * Populate a preview of each image uploaded into a Multi-file Upload field as a separate choice in a Radio or Checkbox field.
- *
- * Plugin Name:  GP Populate Anything - Populate Multi-file Upload Images as Separate Choices
- * Plugin URI:   https://gravitywiz.com/documentation/gravity-forms-populate-anything/
- * Description:  Populate a preview of each image uploaded into a Multi-file Upload field as a separate choice in a Radio or Checkbox field.
- * Author:       Gravity Wiz
- * Version:      0.1
- * Author URI:   https://gravitywiz.com/
  */
 // Update "123" to your form ID; update "4" to the ID the field you're populating via Populate Anything.
 add_filter( 'gppa_input_choices_123_4', function( $choices, $field, $objects ) {
