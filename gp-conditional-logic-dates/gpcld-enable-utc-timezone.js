84ab2ad2a4d7ea8260bbfe1fdb5d82575fa3b570
David Smith <david@gravitywiz.com>
2021-10-26 11:25:21 -0400

    Update gpcld-enable-utc-timezone.js

--- gp-conditional-logic-dates/gpcld-enable-utc-timezone.js
+++ gp-conditional-logic-dates/gpcld-enable-utc-timezone.js
@@ -4,7 +4,7 @@
  *
  * Instructions:
  * 1. Install our free Custom Javascript for Gravity Forms plugin. 
- * Download the plugin here: https://gravitywiz.com/gravity-forms-custom-javascript/
+ *    Download the plugin here: https://gravitywiz.com/gravity-forms-custom-javascript/
  * 2. Copy and paste the snippet into the editor of the Custom Javascript for Gravity Forms plugin.
  */
 gform.addFilter( 'gpcld_enable_utc_timezone', function( enable ) {
