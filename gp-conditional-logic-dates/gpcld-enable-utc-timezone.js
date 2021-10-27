5032dd9d864682f202e1244a157adbd6d24b478b
Samuel Bassah <sbassah@gmail.com>
2021-10-27 16:20:45 +0000

    Adjust User's Local Time to UTC
    * Adjust User's Local Time to UTC
    
    * Converted the snippet to Javascript
    
    * Update gpcld-enable-utc-timezone.js
    
    * Update gpcld-enable-utc-timezone.js
    
    Co-authored-by: David Smith <david@gravitywiz.com>

---
+++ gp-conditional-logic-dates/gpcld-enable-utc-timezone.js
@@ -0,0 +1,12 @@
+/**
+ * Gravity Perks // Conditional Logic Dates // Adjust User's Local Time to UTC
+ * https://gravitywiz.com/documentation/gravity-forms-conditional-logic-dates/
+ *
+ * Instructions:
+ * 1. Install our free Custom Javascript for Gravity Forms plugin. 
+ *    Download the plugin here: https://gravitywiz.com/gravity-forms-custom-javascript/
+ * 2. Copy and paste the snippet into the editor of the Custom Javascript for Gravity Forms plugin.
+ */
+gform.addFilter( 'gpcld_enable_utc_timezone', function( enable ) {
+	return true;
+} );
