6a09c50fecdb4789bffdc6c13ccf6e2e2271678b
David Smith <david@gravitywiz.com>
2021-10-30 10:23:29 -0400

    Update gppa-populate-from-multiple-forms.php

--- gp-populate-anything/gppa-populate-from-multiple-forms.php
+++ gp-populate-anything/gppa-populate-from-multiple-forms.php
@@ -2,12 +2,16 @@
 /**
  * Gravity Perks // Populate Anything // Populate Entries from Multiple Forms
  * https://gravitywiz.com/documentation/gravity-forms-populate-anything/
+ *
+ * Filter and populate desired entry IDs by using the Populate Anything Database object type. Populate the choice label
+ * with the "entry_id" column. Configure the snippet below to fetch the desired input values from each entry depending
+ * on the form it belongs to.
  */
 // Update "123" to your form ID and "4" to your field ID.
 add_filter( 'gppa_input_choices_123_4', function ( $choices, $field, $objects ) {
 
-  // Each case below represents a form ID (e.g. 121, 122) from which you are pulling entries for population.
-  // You must target the field (or input IDs) that you want to populate from.
+	// Each case below represents a form ID (e.g. 121, 122) from which you are pulling entries for population.
+	// You must target the field (or input IDs) that you want to populate from.
 	foreach ( $choices as &$choice ) {
 		$entry = GFAPI::get_entry( $choice['object']['entry_id'] );
 		switch ( $entry['form_id'] ) {
