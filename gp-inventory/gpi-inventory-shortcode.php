c833aa81331bcd4b2a06ce5035717ab0d704dd88
David Smith <david@gravitywiz.com>
2021-11-02 11:38:47 -0400

    Added support for passing content to the shortcode to use in place of the default template.

--- gp-inventory/gpi-inventory-shortcode.php
+++ gp-inventory/gpi-inventory-shortcode.php
@@ -9,7 +9,7 @@
  * @todo
  * - Add support for excluding field parameter and showing a consolidated list of all inventories.
  */
-add_filter( 'gform_shortcode_inventory', function ( $output, $atts ) {
+add_filter( 'gform_shortcode_inventory', function ( $output, $atts, $content ) {
 
 	$atts = shortcode_atts( array(
 		'id'    => false,
@@ -18,14 +18,36 @@ add_filter( 'gform_shortcode_inventory', function ( $output, $atts ) {
 
 	$form  = GFAPI::get_form( $atts['id'] );
 	$field = GFAPI::get_field( $form, $atts['field'] );
+	if ( ! $field ) {
+		return $content;
+	}
 
 	$items = array();
 
 	if ( is_array( $field->choices ) ) {
 		$field->gpiShowAvailableInventory = true;
-		$choices                          = gp_inventory_type_choices()->apply_choice_limits( $field->choices, $field, $form );
-		foreach ( $choices as $choice ) {
-			$items[] = $choice['text'];
+		if ( $content ) {
+			$counts = gp_inventory_type_choices()->get_choice_counts( $form['id'], $field );
+			foreach ( $field->choices as $choice ) {
+
+				$limit     = (int) $choice['inventory_limit'];
+				$count     = (int) rgar( $counts, $choice['value'] );
+				$available = (int) $limit - $count;
+
+				$items[] = gpis_get_item_markup( $content, array(
+					'limit'     => $limit,
+					'count'     => $count,
+					'available' => $available,
+					'label'     => $choice['text'],
+					'value'     => $choice['value'],
+				) );
+
+			}
+		} else {
+			$choices = gp_inventory_type_choices()->apply_choice_limits( $field->choices, $field, $form );
+			foreach ( $choices as $choice ) {
+				$items[] = $choice['text'];
+			}
 		}
 		$output = sprintf(
 			'<ul class="gpi-inventory-list gpi-inventory-list-%d-%d"><li>%s</li></ul>',
@@ -33,8 +55,34 @@ add_filter( 'gform_shortcode_inventory', function ( $output, $atts ) {
 		);
 	} else {
 		$available = gp_inventory_type_simple()->get_available_stock( $field );
-		$output   .= $field->get_field_label( false, '' ) . ': ' . $available;
+		$label     = $field->get_field_label( false, '' );
+		if ( $content ) {
+			$limit   = gp_inventory_type_simple()->get_stock_quantity( $field );
+			$count   = gp_inventory_type_simple()->get_claimed_inventory( $field );
+			$output .= gpis_get_item_markup( $content, array(
+				'limit'     => $limit,
+				'count'     => $count,
+				'available' => $available,
+				'label'     => $label,
+				'value'     => '',
+			) );
+		} else {
+			$output .= $label . ': ' . $available;
+		}
 	}
 
 	return $output;
-}, 10, 2 );
+}, 10, 3 );
+
+function gpis_get_item_markup( $template, $args ) {
+
+	$markup = $template;
+
+	$markup = str_replace( '{limit}', $args['limit'], $markup );
+	$markup = str_replace( '{count}', $args['count'], $markup );
+	$markup = str_replace( '{available}', $args['available'], $markup );
+	$markup = str_replace( '{label}', $args['label'], $markup );
+	$markup = str_replace( '{value}', $args['value'], $markup );
+
+	return $markup;
+}
