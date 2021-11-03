e1bd0bc8942fa4022d8330947426b26264162020
Samuel Bassah <sbassah@gmail.com>
2021-11-03 20:03:43 +0000

    Removed the plugin header
    Removed the plugin header as requested by Scott.

--- gp-populate-anything/gppa-display-post-featured-image.php
+++ gp-populate-anything/gppa-display-post-featured-image.php
@@ -10,13 +10,6 @@
  * 1. Populate the value of a field with a post ID.
  * 2. Insert the live merge tag for that field into the content setting of an HTML field.
  * 3. The live merge tag should have the `:featured_image` modifer like so: `@{Field Label:4:featured_image}` (where 4 is the ID of the field).
- *
- * Plugin Name:  GP Populate Anything - Display Post Featured Image
- * Plugin URI:   https://gravitywiz.com/documentation/gravity-forms-populate-anything/
- * Description:  Populate post featured image into an HTML field.
- * Author:       Gravity Wiz
- * Version:      0.1
- * Author URI:   https://gravitywiz.com/
  */
 // Update 123 to your Form ID
 add_filter( 'gppa_live_merge_tag_value_123', function( $merge_tag_match_value, $merge_tag, $form, $field_id, $entry_values ) {
