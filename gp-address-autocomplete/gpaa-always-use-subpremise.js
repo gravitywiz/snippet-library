de84b165167935dab3f318cf4eb426064bf3cecf
Clay Griffiths <clay@claygriffiths.com>
2021-11-09 11:26:06 -0600

    Improve support for AU addresses in gpaa-always-use-subpremise.js

--- gp-address-autocomplete/gpaa-always-use-subpremise.js
+++ gp-address-autocomplete/gpaa-always-use-subpremise.js
@@ -1,10 +1,10 @@
 /**
  * Gravity Perks // GP Address Autocomplete // Always Use Subpremise/Unit Numbers
- * 
+ *
  * For some countries including Australia and Scotland, when entering an address with a subpremise,
  * the place result may sometimes truncate it. This snippet will prepend the subpremise if it is not
  * included in the result.
- * 
+ *
  * http://gravitywiz.com/documentation/gravity-forms-address-autocomplete
  *
  * Instructions:
@@ -13,19 +13,24 @@
  *     2. Copy and paste the snippet into the editor of the Custom Javascript for Gravity Forms plugin.
  */
 window.gform.addFilter('gpaa_values', function(values, place, instance) {
+    if (instance.inputs.address1.value.indexOf('/') === -1) {
+        return values;
+    }
+
     for (var i = 0; i < place.address_components.length; i++) {
         var component = place.address_components[i];
         var componentType = component.types[0];
 
-        if (componentType != 'route') {
+        if (componentType !== 'route') {
             continue;
         }
 
         var subPremisePattern = RegExp('^(.*)' + component.long_name.split(' ', 1)[0]);
         var results = subPremisePattern.exec(instance.inputs.address1.value);
+        var streetNumber = results[1].trim().split('/')[1];
 
         if (results && values.address1.indexOf(results[1]) === -1) {
-            values.address1 = results[1] + values.address1;
+            values.address1 = results[1].trim() + ' ' + values.address1.replace(streetNumber, '').trim();
         }
     }
 
