b1c25d0638d2c322cece5a1071adfa458f543eff
David Smith <david@gravitywiz.com>
2021-11-23 12:25:33 -0500

    Update gpcc-copy-label.js

--- gp-copy-cat/gpcc-copy-label.js
+++ gp-copy-cat/gpcc-copy-label.js
@@ -7,7 +7,7 @@
 *
 * Instructions:
 * 1. Install our free Custom Javascript for Gravity Forms plugin. 
-* Download the plugin here: https://gravitywiz.com/gravity-forms-custom-javascript/
+*    Download the plugin here: https://gravitywiz.com/gravity-forms-custom-javascript/
 * 2. Copy and paste the snippet into the editor of the Custom Javascript for Gravity Forms plugin.
 */
 gform.addFilter( 'gppc_copied_value', function( value, $elem, data ) {
