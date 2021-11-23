c683de85c2d7e0473e4a1a063f7e80a125620467
Clay Griffiths <clay@claygriffiths.com>
2021-11-23 16:48:03 -0600

    Updated `gpnf-hourly-cron.php` to fix issue with scheduling and also turn into class to make it more configurable.

--- gp-nested-forms/gpnf-hourly-cron.php
+++ gp-nested-forms/gpnf-hourly-cron.php
@@ -1,24 +1,54 @@
 <?php
+
 /**
- * Gravity Perks // Nested Forms // Hourly Cron & Orphaned Entry Expiration
+ * Gravity Perks // Nested Forms // Custom Cron & Orphaned Entry Expiration
  * https://gravitywiz.com/documentation/gravity-forms-nested-forms/
  */
-function gpnf_custom_hourly_cron() {
+class GPNF_Custom_Orphaned_Entry_Expiration {
 
-	if ( ! is_callable( 'gp_nested_forms' ) ) {
-		return;
+	public $expiration;
+	public $cron_interval;
+
+	public function __construct( $args = array() ) {
+		$args = wp_parse_args( $args, array(
+			'expiration'    => HOUR_IN_SECONDS,
+			'cron_interval' => 'hourly',
+		) );
+
+		$this->expiration    = $args['expiration'];
+		$this->cron_interval = $args['cron_interval'];
+
+		$this->schedule();
+
+		add_action( 'gpnf_custom_cron', array( $this, 'cron' ) );
+		add_filter( 'gpnf_expiration_modifier', array( $this, 'expiration_modifier' ) );
 	}
 
-	if ( ! wp_next_scheduled( 'gpnf_hourly_cron' ) ) {
-		wp_schedule_event( time(), 'hourly', 'gpnf_hourly_cron' );
+	public function schedule() {
+		if ( ! wp_next_scheduled( 'gpnf_custom_cron' ) ) {
+			wp_schedule_event( time(), $this->cron_interval, 'gpnf_custom_cron' );
+		}
 	}
 
-	add_action( 'gpnf_hourly_cron', array( gp_nested_forms(), 'daily_cron' ) );
+	public function cron() {
+		if ( ! is_callable( 'gp_nested_forms' ) ) {
+			return;
+		}
+
+		gp_nested_forms()->daily_cron();
+	}
+
+	public function expiration_modifier() {
+		return $this->expiration;
+	}
 
 }
 
-gpnf_custom_hourly_cron();
+// Configure with default interval of hourly with expiration of 1 hour
+new GPNF_Custom_Orphaned_Entry_Expiration();
 
-add_filter( 'gpnf_expiration_modifier', function() {
-	return HOUR_IN_SECONDS;
-} );
+// Configure with custom expiration and cron interval
+//new GPNF_Custom_Orphaned_Entry_Expiration( array(
+//	'expiration'    => 60 * 60 * 4, // 4 Hours
+//	'cron_interval' => 'twicedaily',
+//) );
