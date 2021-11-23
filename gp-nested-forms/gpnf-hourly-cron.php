dfcb139730a741fc1f5d66df441c8aef55e3b098
Clay Griffiths <clay@claygriffiths.com>
2021-11-23 17:01:45 -0600

    Added additional configuration examples to `gpnf-hourly-cron.php`

--- gp-nested-forms/gpnf-hourly-cron.php
+++ gp-nested-forms/gpnf-hourly-cron.php
@@ -3,6 +3,10 @@
 /**
  * Gravity Perks // Nested Forms // Custom Cron & Orphaned Entry Expiration
  * https://gravitywiz.com/documentation/gravity-forms-nested-forms/
+ *
+ * Instructions:
+ *   1. Choose from one of the configurations below.
+ *   2. Ensure that whichever configurations you're not using are commented out.
  */
 class GPNF_Custom_Orphaned_Entry_Expiration {
 
@@ -44,11 +48,39 @@ class GPNF_Custom_Orphaned_Entry_Expiration {
 
 }
 
-// Configure with default interval of hourly with expiration of 1 hour
+/*
+ * Default configuration: configure with interval of hourly with expiration of 1 hour
+ */
 new GPNF_Custom_Orphaned_Entry_Expiration();
 
-// Configure with custom expiration and cron interval
+/*
+ * Example configuration: configure with custom expiration and built-in cron interval (twice daily)
+ *
+ * See https://developer.wordpress.org/reference/functions/wp_get_schedules/ for built-in cron intervals/schedules.
+ */
+
+// Configure with custom expiration and built-in cron interval (twice daily)
 //new GPNF_Custom_Orphaned_Entry_Expiration( array(
-//	'expiration'    => 60 * 60 * 4, // 4 Hours
-//	'cron_interval' => 'twicedaily',
+//	'expiration' => 60 * 60 * 4, // 4 hours
+//	'interval'   => 'twicedaily',
+//) );
+
+/*
+ * Example configuration: configure with custom expiration and custom cron interval
+ */
+
+// Add custom cron schedule of every 5 minutes so it can be used by the GPNF_Custom_Orphaned_Entry_Expiration class.
+//add_filter( 'cron_schedules', function ( $schedules ) {
+//	// Adds every five minutes to the existing schedules.
+//	$schedules['everyfiveminutes'] = array(
+//		'interval' => 60 * 5,
+//		'display'  => 'Every Five Minutes',
+//	);
+//
+//	return $schedules;
+//} );
+//
+//new GPNF_Custom_Orphaned_Entry_Expiration( array(
+//	'expiration'    => 60 * 5, // 5 Minutes
+//	'cron_interval' => 'everyfiveminutes',
 //) );
