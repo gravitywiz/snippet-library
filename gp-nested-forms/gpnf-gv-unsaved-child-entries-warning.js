a0138181704bd3595e875a9738e8cb3abd82bfaf
David Smith <david@gravitywiz.com>
2021-12-06 21:58:53 -0500

    Create gpnf-gv-unsaved-child-entries-warning.js

---
+++ gp-nested-forms/gpnf-gv-unsaved-child-entries-warning.js
@@ -0,0 +1,28 @@
+/**
+	 * Gravity Perks // Nested Forms // Unsaved Child Entries Warning for Gravity View
+	 * https://gravitywiz.com/documentation/gravity-forms-nested-forms/
+	 *
+	 * If you add child entries to a Nested Form field when editing a parent entry in GravityView, these new child entries
+	 * will not be saved to the parent entry unless the user updates the parent entry.
+	 *
+	 * This snippet will show a warning that there are unsaved changes if the user attempts to exit the page without
+	 * updating the parent entry.
+	 */
+	$( document ).ready( function() {
+
+		// Update "123" to your parent form ID and "4" to your Nested Form field ID.
+		var $field        = $( '#input_711_1' );
+		var origValue     = $field.val();
+		var unloadWarning = true;
+
+		window.onbeforeunload = function ( e) {
+			if ( unloadWarning && $field.val() !== origValue ) {
+				return true;
+			}
+		};
+
+		$( '.gv-button-update, .gv-button-delete' ).on( 'click', function() {
+			unloadWarning = false;
+		} );
+
+	} );
