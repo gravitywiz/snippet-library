863e0c3d7d59228c2ae88f3fe4ab7e26d72304db
David Smith <david@gravitywiz.com>
2021-12-06 21:23:50 -0500

    Update gppa-force-dyn-pop-on-gv-edit.php

--- gp-populate-anything/gppa-force-dyn-pop-on-gv-edit.php
+++ gp-populate-anything/gppa-force-dyn-pop-on-gv-edit.php
@@ -13,3 +13,18 @@ add_filter( 'gravityview/edit_entry/field_value', function( $field_value, $field
 	}
 	return $field_value;
 }, 10, 2 );
+
+/**
+ * When editing a child entry in a Nested Form field via Gravity View, use this variation.
+ * Remove this section if you are not using Nested Forms.
+ */
+// Update "124" to your parent form ID and "5" to your Nested Form field ID.
+add_filter( 'gpnf_populated_entry_124_5', function( $entry, $form ) {
+	if ( is_callable( 'gravityview_get_context' ) && gravityview_get_context() === 'edit' && isset( $GLOBALS['gppa-field-values'] ) ) {
+		// Update "6" to the field ID you wish to force dynamic population for in the nested form.
+		$field               = GFAPI::get_field( $form, 6 );
+		$hydrated_field      = gp_populate_anything()->hydrate_field( $field, $form, array(), null, false );
+		$entry[ $field->id ] = $hydrated_field['field_value'];
+	}
+	return $entry;
+}, 10, 2 );
