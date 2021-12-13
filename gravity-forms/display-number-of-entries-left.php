72a9803875f5d70dea51d5184c1293b9e8517f42
Clay Griffiths <clay@claygriffiths.com>
2021-12-13 16:09:39 -0600

    Use GFAPI methods in display-number-of-entries-left.php (resolves PHP warnings/notices)

--- gravity-forms/display-number-of-entries-left.php
+++ gravity-forms/display-number-of-entries-left.php
@@ -1,11 +1,12 @@
 <?php
 /**
- * Entries Left Shortcode
- * http://gravitywiz.com/2012/09/19/shortcode-display-number-of-entries-left/
+ * Gravity Forms // Entries Left Shortcode
+ * https://gravitywiz.com/shortcode-display-number-of-entries-left/
+ * 
+ * Installation instructions: https://gravitywiz.com/documentation/how-do-i-install-a-snippet/
  */
 add_filter( 'gform_shortcode_entries_left', 'gwiz_entries_left_shortcode', 10, 2 );
 function gwiz_entries_left_shortcode( $output, $atts ) {
-
 	extract( shortcode_atts( array(
 		'id'     => false,
 		'format' => false // should be 'comma', 'decimal'
@@ -15,12 +16,15 @@ function gwiz_entries_left_shortcode( $output, $atts ) {
 		return '';
 	}
 
-	$form = RGFormsModel::get_form_meta( $id );
+	$form = GFAPI::get_form( $id );
 	if ( ! rgar( $form, 'limitEntries' ) || ! rgar( $form, 'limitEntriesCount' ) ) {
 		return '';
 	}
 
-	$entry_count  = RGFormsModel::get_lead_count( $form['id'], '', null, null, null, null, 'active' );
+	$entry_count  = GFAPI::count_entries( $form['id'], array( 
+		'status' => 'active' 
+	) );
+	
 	$entries_left = rgar( $form, 'limitEntriesCount' ) - $entry_count;
 	$output       = $entries_left;
 
