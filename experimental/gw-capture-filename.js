98f339acf6b5d3b82af15c75b8bb09a6829a59b8
David Smith <david@gravitywiz.com>
2021-12-21 16:35:14 -0500

    Create gw-capture-filename.js

---
+++ experimental/gw-capture-filename.js
@@ -0,0 +1,15 @@
+/**
+ * Gravity Wiz // Gravity Forms // Capture Filename from Single File Upload Field
+ * https://gravitywiz.com/
+ */
+var uploadFieldId = 4;
+var targetFieldId = 5;
+var template = 'Uploaded File: {filename}';
+
+var $uploadField = $( '#input_GFFORMID_{0}'.format( uploadFieldId ) );
+var $targetField = $( '#input_GFFORMID_{0}'.format( targetFieldId ) );
+
+$uploadField.on( 'change', function() {
+	var filename = $( this ).val().split("\\").pop();
+	$targetField.val( template.replace( '{filename}', filename ) );
+} );
