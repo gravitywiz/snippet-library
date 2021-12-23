901cd988f107895de1f1bfc63df81194c4e6a6d8
David Smith <david@gravitywiz.com>
2021-12-23 16:44:20 -0500

    Update gw-validate-current-password.php

--- gravity-forms/gw-validate-current-password.php
+++ gravity-forms/gw-validate-current-password.php
@@ -2,15 +2,10 @@
 /**
  * Gravity Wiz // Gravity Forms // Validate Current Password
  * https://gravitywiz.com/
+ * 
+ * Instruction Video: https://www.loom.com/share/118994cfff0f4441a519116bbc3ed0c7
  *
- * Require users to confirm their current password before changing their password.
- *
- *
- * Video
- * https://www.loom.com/share/118994cfff0f4441a519116bbc3ed0c7
- *
- *
- *
+ * Require users to confirm their current password before changing their password. 
  */
 // Update "123" to your form ID and "4" to your Password field ID.
 add_filter( 'gform_field_validation_123_4', function( $result, $value ) {
