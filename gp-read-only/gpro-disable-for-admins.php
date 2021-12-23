c92d23f5758fac97eb747db325c72891a0053e7b
Samuel Bassah <sbassah@gmail.com>
2021-12-23 11:41:26 +0000

    Added Usage to Snippet Header

--- gp-read-only/gpro-disable-for-admins.php
+++ gp-read-only/gpro-disable-for-admins.php
@@ -6,6 +6,11 @@
  * Disable the Read Only property on fields for Admin users. 
  * This is useful when wanting to allow administrative users to edit fields that non-admins should not be able to edit.
  *
+ * Usage:
+ *
+ * 1. Install this code as a plugin or as a snippet.
+ * 2. Add the `gp-read-only-except-admin` CSS Class Name to field's Custom CSS Class setting.
+ *
  * Plugin Name:  GP Read Only — Disable Read Only for Admins
  * Plugin URI:   https://gravitywiz.com/documentation/gravity-forms-read-only/
  * Description:  This snippet allows you to disbale the Read Only property on fields for Admin users.
