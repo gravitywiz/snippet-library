40b812cc554a08da95ccc0e7726988d089ac3508
dariospace <30393370+dariospace@users.noreply.github.com>
2021-12-28 11:27:30 -0300

    Adopted new format for adding the video link

--- gp-populate-anything/gppa-populate-all-results.php
+++ gp-populate-anything/gppa-populate-all-results.php
@@ -3,13 +3,13 @@
  * Gravity Perks // Populate Anything // Populate All Results
  * https://gravitywiz.com/documentation/gravity-forms-populate-anything/
  *
+ * Instruction Video: https://www.loom.com/share/f324b8477a1140d7808b49b6b553258f
+ *
  * By default, when populating a field's value, Populate Anything will only populated the first found result. This
  * snippet adds support for populating all found results. Just add the "gppa-populate-all" CSS class to the field's
  * CSS Class Name setting.
  *
- * Video
  *
- * https://www.loom.com/share/f324b8477a1140d7808b49b6b553258f
  *
  *
  *
