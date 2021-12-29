34db697027644c0eb8421a6e03ebfbdf9c827fda
dariospace <30393370+dariospace@users.noreply.github.com>
2021-12-29 13:06:05 -0300

    fixed typo

--- gravity-forms/gw-populate-current-quarter.php
+++ gravity-forms/gw-populate-current-quarter.php
@@ -3,7 +3,7 @@
  * Gravity Wiz // Gravity Forms // Populate the Current Quarter
  * https://gravitywiz.com/snippet-library/
  *
- * Instruction video: https://www.loom.com/share/78e7add08c7042a4ad02b2682a3a8ee9
+ * Instruction Video: https://www.loom.com/share/78e7add08c7042a4ad02b2682a3a8ee9
  */
 add_filter( 'gform_field_value_current_quarter', function() {
 
