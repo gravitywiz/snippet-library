7ef702dd00db396db271302158cfa21d037f6607
David Smith <david@gravitywiz.com>
2021-12-29 10:24:53 -0500

    Update gw-populate-current-quarter.php

--- gravity-forms/gw-populate-current-quarter.php
+++ gravity-forms/gw-populate-current-quarter.php
@@ -4,8 +4,6 @@
  * https://gravitywiz.com/snippet-library/
  *
  * Instruction video: https://www.loom.com/share/78e7add08c7042a4ad02b2682a3a8ee9
- *
- *
  */
 add_filter( 'gform_field_value_current_quarter', function() {
 
