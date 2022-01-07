e811cd26ee04815033eb834e9c4bdde51ee569ea
David Smith <david@gravitywiz.com>
2022-01-07 09:52:46 -0500

    Create gpaa-search-by-place-name.php

---
+++ gp-address-autocomplete/gpaa-search-by-place-name.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Gravity Perks // Address Autocomplete // Search by Place Name
+ * https://gravitywiz.com/documentation/gravity-forms-address-autocomplete/
+ *
+ * Find addresses by their name. For example, "Mount Trashmore" in Virginia Beach would resolve to 
+ * an address of "310 Edwin Drive, Virginia Beach, VA 23462".
+ */
+add_action( 'gform_preview_header', 'gpaa_search_by_place_name' );
+add_action( 'wp_head', 'gpaa_search_by_place_name' );
+function gpaa_search_by_place_name() {
+	?>
+	<script>
+		gform.addFilter( 'gpaa_autocomplete_options', function( options ) {
+			options.types = [ 'geocode', 'establishment' ];
+			return options;
+		} );
+	</script>
+	<?php
+}
