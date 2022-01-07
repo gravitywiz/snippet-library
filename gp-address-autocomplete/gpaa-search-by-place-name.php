be5b7ee1cd2125f4b75be264e631a6768cda2468
David Smith <david@gravitywiz.com>
2022-01-07 09:53:57 -0500

    Update gpaa-search-by-place-name.php

--- gp-address-autocomplete/gpaa-search-by-place-name.php
+++ gp-address-autocomplete/gpaa-search-by-place-name.php
@@ -5,6 +5,10 @@
  *
  * Find addresses by their name. For example, "Mount Trashmore" in Virginia Beach would resolve to 
  * an address of "310 Edwin Drive, Virginia Beach, VA 23462".
+ *
+ * Developer's Note: The JS in this snippet cannot be included via the Custom Javascript plugin due 
+ * to an order-of-events issue with GPAA calling the options filter before the Custom Javascript plugin 
+ * has initialized.
  */
 add_action( 'gform_preview_header', 'gpaa_search_by_place_name' );
 add_action( 'wp_head', 'gpaa_search_by_place_name' );
