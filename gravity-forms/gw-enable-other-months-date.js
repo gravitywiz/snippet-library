470660409955c096743f309b6a70c86d695a7fab
dariospace <30393370+dariospace@users.noreply.github.com>
2022-01-11 16:40:40 -0300

    added Instruction Video gw-enable-other-months-dates (#232)

--- gravity-forms/gw-enable-other-months-date.js
+++ gravity-forms/gw-enable-other-months-date.js
@@ -2,14 +2,17 @@
 * Gravity Wiz // Gravity Forms // Enable Other Months Dates
 * https://gravitywiz.com/
 *
+*
+* Instruction Video: https://www.loom.com/share/4c37d9312cc74fd69195972d36c3d971
+*
 * By default, when viewing the datepicker, any visible dates in the previous or next months are disabled.
-* This snippet will enable these dates. 
+* This snippet will enable these dates.
 *
 * Works well with [GP Limit Dates](https://gravitywiz.com/documentation/gravity-forms-limit-dates/)!
 *
 * Instructions:
 *
-* 1. Install our free Custom Javascript for Gravity Forms plugin. 
+* 1. Install our free Custom Javascript for Gravity Forms plugin.
 *    Download the plugin here: https://gravitywiz.com/gravity-forms-custom-javascript/
 * 2. Copy and paste the snippet into the editor of the Custom Javascript for Gravity Forms plugin.
 */
