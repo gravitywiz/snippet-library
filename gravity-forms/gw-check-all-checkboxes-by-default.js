04e01da5bf2bfedb275372b8a885332b98e9eaf6
dariospace <30393370+dariospace@users.noreply.github.com>
2022-01-20 12:49:20 -0300

    added Instruction Video gw-check-all-checkboxes-by-default

--- gravity-forms/gw-check-all-checkboxes-by-default.js
+++ gravity-forms/gw-check-all-checkboxes-by-default.js
@@ -2,6 +2,13 @@
  * Gravity Wiz // Gravity Forms // Check All Checkboxes by Default
  * https://gravitywiz.com/
  *
+ * Instruction Video: https://www.loom.com/share/69c4a8905b934a2f92681c751f171a25
+ *
+ * * Instructions:
+ *     1. Install our free Custom Javascript for Gravity Forms plugin.
+ *        Download the plugin here: https://gravitywiz.com/gravity-forms-custom-javascript/
+ *     2. Copy and paste the snippet into the editor of the Custom Javascript for Gravity Forms plugin.
+ *
  * @see Form Export: https://gwiz.io/3DyGUrR
  */
 // Update "4" and "5" to Checkbox field IDs which should be selected by default.
@@ -9,5 +16,5 @@ $.each( [ 4, 5 ], function( index, fieldId ) {
 	$checkboxes = $( '#field_GFFORMID_{0}'.format( fieldId ) ).find( 'input' );
 	if ( $checkboxes.filter( ':checked' ).length === 0 ) {
 		$checkboxes.prop( 'checked', true );
-	}	
+	}
 } );
