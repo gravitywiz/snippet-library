dac5d35c72cd6028980b0e82a9d1c13c1d8d24ee
dariospace <30393370+dariospace@users.noreply.github.com>
2022-01-21 12:58:55 -0300

    added Instruction Video for populating current quarter of the year (#231)
    * added Instruction Video for populating current quarter of the year in a field
    
    * Update gw-populate-current-quarter.php
    
    * fixed typo
    
    * video redo
    
    Co-authored-by: David Smith <david@gravitywiz.com>

--- gravity-forms/gw-populate-current-quarter.php
+++ gravity-forms/gw-populate-current-quarter.php
@@ -2,6 +2,8 @@
 /**
  * Gravity Wiz // Gravity Forms // Populate the Current Quarter
  * https://gravitywiz.com/snippet-library/
+ *
+ * Instruction Video: https://www.loom.com/share/a8355c7ab85c42e88130a5eda11a1f81
  */
 add_filter( 'gform_field_value_current_quarter', function() {
 
