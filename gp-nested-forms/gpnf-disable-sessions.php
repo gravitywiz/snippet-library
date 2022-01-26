9bd05280f89e40d0b9deeeda289dd72210f066ea
Samuel Bassah <sbassah@gmail.com>
2022-01-26 19:21:50 +0000

    Formatted the snippet

--- gp-nested-forms/gpnf-disable-sessions.php
+++ gp-nested-forms/gpnf-disable-sessions.php
@@ -4,9 +4,9 @@
  * https://gravitywiz.com/documentation/gravity-forms-nested-forms/
  */
 add_filter( 'gform_pre_render', function( $form ) {
-	if ( empty( $_POST ) && class_exists( 'GPNF_Session' ) ) {
-		$session = new GPNF_Session( $form['id'] );
-		$session->delete_cookie();
-	}
-	return $form;
+    if ( empty( $_POST ) && class_exists( 'GPNF_Session' ) ) {
+        $session = new GPNF_Session( $form['id'] );
+        $session->delete_cookie();
+    }
+    return $form;
 } );
