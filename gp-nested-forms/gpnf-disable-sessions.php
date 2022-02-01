3478a6a721563e741424baaade8d0b9cceb6dccd
dariospace <30393370+dariospace@users.noreply.github.com>
2022-02-01 16:06:03 -0300

    added video for gpnf disable sessions

--- gp-nested-forms/gpnf-disable-sessions.php
+++ gp-nested-forms/gpnf-disable-sessions.php
@@ -2,6 +2,9 @@
 /**
  * Gravity Perks // Nested Forms // Disable Session Storage
  * https://gravitywiz.com/documentation/gravity-forms-nested-forms/
+ *
+ * Instruction Video: https://www.loom.com/share/348e9e70999a43eaaf73cf0c820161aa
+ *
  */
 add_filter( 'gform_pre_render', function( $form ) {
     if ( empty( $_POST ) && class_exists( 'GPNF_Session' ) ) {
