9a960b8b13b1ffd3f7db0b310aa95192f0658038
David Smith <david@gravitywiz.com>
2022-02-02 11:16:00 -0500

    Update gpep-delete-passthrough-entry-after-submission.php
    Fixed issue where fatal error was generated when token's entry did not exist.

--- gp-easy-passthrough/gpep-delete-passthrough-entry-after-submission.php
+++ gp-easy-passthrough/gpep-delete-passthrough-entry-after-submission.php
@@ -8,6 +8,8 @@
 add_filter( 'gform_after_submission', function() {
 	if ( rgget( 'ep_token' ) && is_callable( 'gp_easy_passthrough' ) ) {
 		$entry = gp_easy_passthrough()->get_entry_for_token( rgget( 'ep_token' ) );
-		GFAPI::delete_entry( $entry['id'] );
+		if ( $entry ) {
+			GFAPI::delete_entry( $entry['id'] );
+		}
 	}
 } );
