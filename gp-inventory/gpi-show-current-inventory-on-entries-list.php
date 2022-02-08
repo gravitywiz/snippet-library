8c9dcb6df5fbb41ab716cd2645629fc5fa9b30d9
Samuel Bassah <sbassah@gmail.com>
2022-02-08 21:00:53 +0000

    Added Support for Choice Based fields
    Added support to display inventory for choice-based fields on the entry page.

--- gp-inventory/gpi-show-current-inventory-on-entries-list.php
+++ gp-inventory/gpi-show-current-inventory-on-entries-list.php
@@ -1,8 +1,9 @@
 <?php
 /**
  * Gravity Perks // GP Inventory // Display Simple Inventory Type Current/Remaining Inventory
- * http://gravitywiz.com/documentation/gravity-forms-inventory/
+ * https://gravitywiz.com/documentation/gravity-forms-inventory/
  */
+
 add_action( 'gform_pre_entry_list', function ( $form_id ) {
 	$form = GFAPI::get_form( $form_id );
 
@@ -10,16 +11,34 @@ add_action( 'gform_pre_entry_list', function ( $form_id ) {
 		return;
 	}
 
-	if ( ! gp_inventory_type_simple()->is_applicable_form( $form ) ) {
+	if ( ! gp_inventory_type_simple()->is_applicable_form( $form, true ) ) {
 		return;
 	}
 
 	$message_content = '<strong>' . __( 'Current Inventory:', 'gp-inventory' ) . '</strong><ul style="margin-top: .5rem;padding: 0 0 0 2rem;">';
 
-	foreach ( gp_inventory_type_simple()->get_applicable_fields( $form ) as $inventory_field ) {
-		$available = gp_inventory_type_simple()->get_available_stock( $inventory_field );
+	foreach ( gp_inventory_type_simple()->get_applicable_fields( $form, true ) as $inventory_field ) {
 
-		$message_content .= '<li style="list-style: disc;">' . $inventory_field->get_field_label( false, '' ) . ': ' . $available . '</li>';
+		if ( isset( $inventory_field->choices ) && is_array( $inventory_field->choices ) ) {
+			$message_content .= '<li style="list-style: disc;">' . $inventory_field->get_field_label( false, '' );
+			$message_content .= '<ul style="margin-top: .5rem;padding: 0 0 0 2rem;">';
+			$counts          = gp_inventory_type_choices()->get_choice_counts( $form['id'], $inventory_field );
+
+			foreach ( $inventory_field->choices as $choice ) {
+
+				$limit     = (int) $choice['inventory_limit'];
+				$count     = (int) rgar( $counts, $choice['value'] );
+				$available = (int) $limit - $count;
+
+				$message_content .= '<li style="list-style: circle;">' . $choice['text'] . ': ' . $available . '</li>';
+			}
+
+			$message_content .= '</ul></li>';
+		} else {
+			$available = gp_inventory_type_simple()->get_available_stock( $inventory_field );
+
+			$message_content .= '<li style="list-style: disc;">' . $inventory_field->get_field_label( false, '' ) . ': ' . $available . '</li>';
+		}
 	}
 
 	$message_content .= '</ul>';
