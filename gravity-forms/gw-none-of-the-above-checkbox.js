724c3c88425c2f36d998ebe39434cc7969734e8d
dariospace <30393370+dariospace@users.noreply.github.com>
2022-02-10 12:50:19 -0300

    added Instruction Video none of the above

--- gravity-forms/gw-none-of-the-above-checkbox.js
+++ gravity-forms/gw-none-of-the-above-checkbox.js
@@ -2,11 +2,13 @@
  * Gravity Wiz // Gravity Forms // "None of the Above" Checkbox
  * https://gravitywiz.com/
  *
- * Use this snippet to enable a proper "None of the Above" option in your Checkbox fields. If any 
+ * Instruction Video: https://www.loom.com/share/0f8de708790b4afd879bf0632efd7eae
+ *
+ * Use this snippet to enable a proper "None of the Above" option in your Checkbox fields. If any
  * other option is checked, the "None of the Above" option will be disabled. If the "None of the Above"
  * option is checked, all other options will be disabled.
  */
-// Update "1" to your Checkbox field ID. 
+// Update "1" to your Checkbox field ID.
 var $field  = $( '#field_GFFORMID_1' );
 var $last   = $field.find( '.gchoice:last-child input' );
 var $others = $field.find( 'input' ).not( $last );
