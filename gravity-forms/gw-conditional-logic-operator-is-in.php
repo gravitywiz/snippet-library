392681a84c35310bd83ac00ca9b1e414902195e4
David Smith <david@gravitywiz.com>
2022-02-10 22:22:18 -0500

    Create gw-conditional-logic-operator-is-in.php

---
+++ gravity-forms/gw-conditional-logic-operator-is-in.php
@@ -0,0 +1,170 @@
+<?php
+/**
+ * Gravity Wiz // Gravity Forms // Conditional Logic Operator: "Is In"
+ *
+ * Check if a source value is in a comma-delimited list of values with the new "is in" conditional logic operator!
+ *
+ * Plugin Name:  GF Conditional Logic Operator: "Is In"
+ * Plugin URI:   https://gravitywiz.com/
+ * Description:  Check if a source value is in a comma-delimited list of values.
+ * Author:       Gravity Wiz
+ * Version:      0.9
+ * Author URI:   https://gravitywiz.com
+ */
+class GF_CLO_Is_In {
+
+	public function __construct() {
+
+		add_action( 'init', array( $this, 'init' ) );
+
+	}
+
+	public function init() {
+
+		add_filter( 'gform_admin_pre_render', array( $this, 'enqueue_inline_admin_script' ) );
+		add_filter( 'gform_is_valid_conditional_logic_operator', array( $this, 'whitelist_operator' ), 10, 2 );
+
+		add_filter( 'gform_pre_render', array( $this, 'load_form_script' ), 10, 2 );
+		add_filter( 'gform_register_init_scripts', array( $this, 'add_init_script' ), 10, 2 );
+		add_filter( 'gform_is_value_match', array( $this, 'evaluate_operator' ), 10, 6 );
+
+	}
+
+	public function enqueue_inline_admin_script( $return ) {
+		add_filter( 'admin_footer', array( $this, 'output_admin_inline_script' ) );
+		return $return;
+	}
+
+	public function output_admin_inline_script() {
+		?>
+
+		<script>
+
+			gform.addFilter( 'gform_conditional_logic_operators', function( operators ) {
+				operators.is_in = 'is in';
+				return operators;
+			} );
+
+		</script>
+
+		<?php
+	}
+
+	public function load_form_script( $form, $is_ajax_enabled ) {
+
+		if ( $this->is_applicable_form( $form ) && ! has_action( 'wp_footer', array( $this, 'output_script' ) ) ) {
+			add_action( 'wp_footer', array( $this, 'output_script' ) );
+			add_action( 'gform_preview_footer', array( $this, 'output_script' ) );
+		}
+
+		return $form;
+	}
+
+	public function output_script() {
+		?>
+
+		<script type="text/javascript">
+
+			( function( $ ) {
+
+				window.GWCLOIsIn = function( args ) {
+
+					var self = this;
+
+					// copy all args to current object: (list expected props)
+					for( var prop in args ) {
+						if( args.hasOwnProperty( prop ) ) {
+							self[ prop ] = args[ prop ];
+						}
+					}
+
+					self.init = function() {
+
+						gform.addFilter( 'gform_is_value_match', function( isMatch, formId, rule ) {
+
+							if ( rule.operator !== 'is_in' ) {
+								return isMatch;
+							}
+
+							rule.value.split( ',' ).every( function( currentValue, index, arr ) {
+								var tempRule = {
+									fieldId:  rule.fieldId,
+									operator: 'is',
+									value:    currentValue.trim()
+								}
+								if ( gf_is_match( formId, tempRule ) ) {
+									isMatch = true;
+									return false;
+								}
+								return true;
+							} );
+
+							return isMatch;
+						} );
+
+					};
+
+					self.init();
+
+				}
+
+			} )( jQuery );
+
+		</script>
+
+		<?php
+	}
+
+	public function add_init_script( $form ) {
+
+		if ( ! $this->is_applicable_form( $form ) ) {
+			return;
+		}
+
+		$script = 'new GWCLOIsIn();';
+		$slug   = implode( '_', array( 'gwclo_isin', $form['id'] ) );
+
+		GFFormDisplay::add_init_script( $form['id'], $slug, GFFormDisplay::ON_PAGE_RENDER, $script );
+
+	}
+
+	public function whitelist_operator( $is_valid, $operator ) {
+		if ( $operator === 'is_in' ) {
+			$is_valid = true;
+		}
+		return $is_valid;
+	}
+
+	public function evaluate_operator( $is_match, $field_value, $target_value, $operation, $source_field, $rule ) {
+
+		if ( $rule['operator'] !== 'is_in' || $rule['gwcloinEvaluatingOperator'] ) {
+			return $is_match;
+		}
+
+		$rule['gwcloinEvaluatingOperator'] = true;
+
+		$values = explode( ',', $rule['value'] );
+
+		foreach ( $values as $value ) {
+			if ( GFFormsModel::is_value_match( $field_value, trim( $value ), 'is', $source_field, $rule ) ) {
+				$is_match = true;
+				break;
+			}
+		}
+
+		$rule['gwcloinEvaluatingOperator'] = false;
+
+		return $is_match;
+
+	}
+
+	public function is_applicable_form( $form ) {
+		// @todo we will need to recursively search conditional logic for "is in" operator (see GPCLD).
+		return GFFormDisplay::has_conditional_logic( $form );
+	}
+
+}
+
+# Configuration
+
+new GF_CLO_Is_In();
