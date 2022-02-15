ba5b88f00fb7e207df6f766c28dfcd029ca42166
David Smith <david@gravitywiz.com>
2022-02-14 21:19:02 -0500

    Update gw-tiny-mailing-list.php

--- gravity-forms/gw-tiny-mailing-list.php
+++ gravity-forms/gw-tiny-mailing-list.php
@@ -10,7 +10,7 @@
  * modifier with the merge tag if you are populating the email address as the value rather than the label.
  *
  * Note: This *should not* be used to large numbers of notifications.
- * 
+ *
  * Plugin Name:  Gravity Forms Tiny Mailing List
  * Plugin URI:   http://gravitywiz.com/
  * Description:  Send a notification to multiple recipients individually.
@@ -29,6 +29,8 @@ class GW_Tiny_Mailing_List {
 	public function init() {
 
 		add_filter( 'gform_notification', array( $this, 'send_individual_notifications' ), 10, 3 );
+		add_filter( 'gform_is_valid_notification_to', array( $this, 'allow_multiple_modifiers_in_notification_to_merge_tags' ), 10, 4 );
+		add_filter( 'gform_pre_replace_merge_tags', array( $this, 'remove_tinyml_modifier' ) );
 
 	}
 
@@ -36,6 +38,11 @@ class GW_Tiny_Mailing_List {
 		global $_gwtml_sending_notifications;
 
 		if ( $_gwtml_sending_notifications ) {
+			return $notification;
+		}
+
+		// Only execute if our custom modifier is specified on any merge tag in the "to" address.
+		if ( strpos( $notification['to'], 'tinyml' ) === false ) {
 			return $notification;
 		}
 
@@ -56,6 +63,43 @@ class GW_Tiny_Mailing_List {
 		$notification['to'] = false;
 
 		return $notification;
+	}
+
+	/**
+	 * Gravity Forms doesn't support multiple modifiers in the "Send to Email" setting with their official
+	 * comma-delimited format. Bypass validation if our "tinyml" modifier is present.
+	 *
+	 * @param $is_valid
+	 * @param $to_type
+	 * @param $value
+	 * @param $to_field
+	 *
+	 * @return bool|mixed
+	 */
+	public function allow_multiple_modifiers_in_notification_to_merge_tags( $is_valid, $to_type, $value, $to_field ) {
+
+		// Bypass validation if our modifier is present.
+		// @todo Beef this up to actually validate that all emails and merge tags are valid.
+		if ( $to_type === 'email' && strpos( $value, 'tinyml' ) ) {
+			$is_valid = true;
+		}
+
+		return $is_valid;
+	}
+
+	/**
+	 * Gravity Forms doesn't support multiple modifiers on choice-based field types. Replace our modifier so it doesn't
+	 * conflict if there is another parameter present.
+	 *
+	 * For example, if we had ":value,tinyml" modifiers, the :value modifier would not be recognized when Gravity Forms
+	 * determined what to return for the field value.
+	 *
+	 * @param $text
+	 *
+	 * @return array|string|string[]|null
+	 */
+	public function remove_tinyml_modifier( $text ) {
+		return preg_replace( '/,?tinyml,?/', '', $text );
 	}
 
 }
