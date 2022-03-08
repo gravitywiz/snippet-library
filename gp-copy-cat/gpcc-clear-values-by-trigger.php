101419c51ee70015a8f5bc342f6188cad2efe25f
David Smith <david@gravitywiz.com>
2022-03-08 15:01:59 -0500

    Create gpcc-clear-values-by-trigger.php

---
+++ gp-copy-cat/gpcc-clear-values-by-trigger.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Gravity Perks // Copy Cat // Clear Values for Specific Value
+ * https://gravitywiz.com/documentation/gravity-forms-copy-cat/
+ * 
+ * Clear values in the target field when a specific option in a choice-based trigger field is
+ * selected. 
+ * 
+ * For example, if copying a Name field to another Name field when a "Copy" option is 
+ * selected in a Radio Button field, use this snippet to clear the copied name when a "Clear"
+ * option is selected in the same Radio Button field.
+ */
+// Update "123" to the ID of your form.
+add_action( 'gform_pre_enqueue_scripts_123', function() {
+	?>
+	<script>
+    gform.addFilter( 'gpcc_copied_value', function( value, $targetElem, field ) {
+      // Update "4" to the field ID of your choice-based trigger field.
+      var triggerFieldId = 4;
+      if ( triggerFieldId == field.trigger && $( '#choice_GFFORMID_{0}_1'.format( triggerFieldId ) ).is( ':checked' ) ) {
+        value = '';
+      }
+      return value;
+    } );
+	</script>
+	<?php
+} );
