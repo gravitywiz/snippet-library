0f6d0b03ef7dfe9c63b7b694da79e8445a7b7e4c
dariospace <30393370+dariospace@users.noreply.github.com>
2022-03-15 14:55:00 -0300

    added video for gpep delete entry after submission

--- gp-easy-passthrough/gpep-delete-passthrough-entry-after-submission.php
+++ gp-easy-passthrough/gpep-delete-passthrough-entry-after-submission.php
@@ -3,6 +3,8 @@
  * Gravity Perks // Easy Passthrough // Delete Passthrough Entry After Submission
  * https://gravitywiz.com/documentation/gravity-forms-easy-passthrough/
  *
+ * Instruction Video: https://www.loom.com/share/e7f4f525a2d0438ea9cd45df959c87c9
+ *
  * Delete the entry passed through via the EP token after the target form has been submitted.
  */
 add_filter( 'gform_after_submission', function() {
