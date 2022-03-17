2b682e2360bf713088b7b13bce18e8b34aca3aa3
David Smith <david@gravitywiz.com>
2022-03-16 23:33:59 -0400

    Create gw-add-custom-footer-button.php

---
+++ experimental/gw-add-custom-footer-button.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Gravity Wiz // Gravity Forms // Add Custom Footer Button after Save & Continue
+ * https://gravitywiz.com/
+ */
+// Update "123" to your form ID.
+add_filter( 'gform_savecontinue_link_123', function( $markup, $form ) {
+	return $markup . GFFormDisplay::get_form_button( $form['id'], "my_custom_button_{$form['id']}", array( 'type' => 'button' ), 'My Custom Button', 'gform_previous_button', 'My Custom Button', false );
+}, 10, 2 );
