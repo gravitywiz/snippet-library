ab42765697e113af86119cd093d3631591737fb6
Clay Griffiths <clay@claygriffiths.com>
2022-03-30 16:14:17 -0500

    Create gpaa-show-all-geocode-results.js

---
+++ gp-address-autocomplete/gpaa-show-all-geocode-results.js
@@ -0,0 +1,14 @@
+/**
+ * Gravity Perks // GP Address Autocomplete // Show All Geocode Results (Including Postal Codes)
+ * https://gravitywiz.com/documentation/gravity-forms-address-autocomplete
+ *
+ * Instructions:
+ *     1. Install our free Custom JavaScript for Gravity Forms plugin.
+ *	     Download the plugin here: https://gravitywiz.com/gravity-forms-custom-javascript/
+ *     2. Copy and paste the snippet into the editor of the Custom Javascript for Gravity Forms plugin.
+ */
+window.gform.addFilter( 'gpaa_autocomplete_options', function( options ) {
+	options.types = ['geocode'];
+
+	return options;
+} );
