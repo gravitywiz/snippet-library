9f296db995150469dc8895e6e7139b26bd94e29e
David Smith <david@gravitywiz.com>
2022-03-31 10:39:22 -0400

    Update gpns-trigger-function-by-scheduled-notification.php
    Fixed PHPCS issues.

--- gp-notification-scheduler/gpns-trigger-function-by-scheduled-notification.php
+++ gp-notification-scheduler/gpns-trigger-function-by-scheduled-notification.php
@@ -3,8 +3,8 @@
  * Gravity Perks // Notification Scheduler // Trigger Function on Send
  * https://gravitywiz.com/documentation/gravity-forms-notification-scheduler/
  *
- * This snippet demonstrates how to trigger a custom function using a scheduled notification. The notification 
- * is suppressed by default but can also be triggered by removing the specified line below. The trigger 
+ * This snippet demonstrates how to trigger a custom function using a scheduled notification. The notification
+ * is suppressed by default but can also be triggered by removing the specified line below. The trigger
  * notification is identified by its subject line.
  */
 add_filter( 'gform_pre_send_email', function( $email, $message_format, $notification, $entry ) {
@@ -14,15 +14,15 @@ add_filter( 'gform_pre_send_email', function( $email, $message_format, $notifica
 		return $email;
 	}
 
-  // Update "Trigger: My Custom Function" to your trigger notification's subject line.
+	// Update "Trigger: My Custom Function" to your trigger notification's subject line.
 	if ( $notification['subject'] !== 'Trigger: My Custom Function' ) {
 		return $email;
 	}
 
-  // Delete this line if you want the notification to still be sent.
+	// Delete this line if you want the notification to still be sent.
 	$email['abort_email'] = true;
 
-  // Update this to your own custom function name.
+	// Update this to your own custom function name.
 	my_custom_function();
 
 	return $email;
