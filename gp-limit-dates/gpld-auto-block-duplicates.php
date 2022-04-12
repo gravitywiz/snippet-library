b6626560566f9e93e906043d32aa921fccd55d8e
dariospace <30393370+dariospace@users.noreply.github.com>
2022-04-12 19:48:14 -0300

    added instruction video (#295)

--- gp-limit-dates/gpld-auto-block-duplicates.php
+++ gp-limit-dates/gpld-auto-block-duplicates.php
@@ -2,6 +2,8 @@
 /**
  * Gravity Perks // GP Limit Dates // Auto-block Selected Dates when No Duplicates Enabled
  * http://gravitywiz.com/documentation/gp-limit-dates/
+ *
+ * Instruction Video: https://www.loom.com/share/4c7aead03d3b4083b31e488013d5b4b3
  */
 // Update "123" in the filter name to your form ID and "1" to your field ID.
 add_filter( 'gpld_limit_dates_options_123_1', function( $options, $form, $field ) {
