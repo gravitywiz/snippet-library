d18c5707d66ea2a73e3074af7ff292079a3e2c5d
dariospace <30393370+dariospace@users.noreply.github.com>
2022-04-14 16:11:34 -0300

    Updated linting

--- gp-copy-cat/gpcc-copy-label.js
+++ gp-copy-cat/gpcc-copy-label.js
@@ -1,21 +1,21 @@
 /**
- * Gravity Perks // Copy Cat // Copy Label (instead of Value)
- * https://gravitywiz.com/documentation/gravity-forms-copy-cat/
- *
- * Instruction Video: https://www.loom.com/share/00f1287e4a23495aa9aa02ba95f882a5
- *
- * By default, the value of a choice-based field is copied.
- * Use this snippet to copy the label instead of the value.
- *
- * Instructions:
- * 1. Install our free Custom Javascript for Gravity Forms plugin.
- *    Download the plugin here: https://gravitywiz.com/gravity-forms-custom-javascript/
- * 2. Copy and paste the snippet into the editor of the Custom Javascript for Gravity Forms plugin.
- */
-gform.addFilter("gppc_copied_value", function (value, $elem, data) {
-  $source = jQuery("#input_" + data.sourceFormId + "_" + data.source);
-  if ($source.is("select")) {
-    value = $source.find("option:selected").text();
-  }
-  return value;
-});
+* Gravity Perks // Copy Cat // Copy Label (instead of Value)
+* https://gravitywiz.com/documentation/gravity-forms-copy-cat/
+*
+* Instruction Video: https://www.loom.com/share/00f1287e4a23495aa9aa02ba95f882a5
+*
+* By default, the value of a choice-based field is copied.
+* Use this snippet to copy the label instead of the value.
+*
+* Instructions:
+* 1. Install our free Custom Javascript for Gravity Forms plugin. 
+*    Download the plugin here: https://gravitywiz.com/gravity-forms-custom-javascript/
+* 2. Copy and paste the snippet into the editor of the Custom Javascript for Gravity Forms plugin.
+*/
+gform.addFilter( 'gppc_copied_value', function( value, $elem, data ) {
+    $source = jQuery( '#input_' + data.sourceFormId + '_' + data.source );
+    if( $source.is( 'select' ) ) {
+        value = $source.find( 'option:selected' ).text();
+    }
+    return value;
+} );
