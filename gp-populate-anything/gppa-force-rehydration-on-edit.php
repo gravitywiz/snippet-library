fe19c360ca70c4e390184faf30042c98ffc45cbf
David Smith <david@gravitywiz.com>
2022-04-15 16:37:14 -0400

    Fixed issue where functionality was applying to fields that were not populated by Populate Anything.

--- gp-populate-anything/gppa-force-rehydration-on-edit.php
+++ gp-populate-anything/gppa-force-rehydration-on-edit.php
@@ -5,7 +5,7 @@
  */
 add_filter( 'gform_get_field_value', function( $value, $entry, $field ) {
 	static $_gppa_forcing_hydration;
-	if ( GFForms::get_page() === 'entry_detail_edit' && ! $_gppa_forcing_hydration ) {
+	if ( GFForms::get_page() === 'entry_detail_edit' && ! $_gppa_forcing_hydration && gp_populate_anything()->is_field_dynamically_populated( $field ) ) {
 		$_gppa_forcing_hydration = true;
 		$hydrated_field          = gp_populate_anything()->hydrate_field( $field, GFAPI::get_form( $field->formId ), $entry, $entry );
 		$value                   = $hydrated_field['field_value'];
