2e99ae03a1f302f60bd0add72b43821279404ee0
dariospace <30393370+dariospace@users.noreply.github.com>
2022-04-27 09:47:42 -0300

    ~ added instruction video

--- gp-multi-page-navigation/gpmpn-enable-back-to-last-page.js
+++ gp-multi-page-navigation/gpmpn-enable-back-to-last-page.js
@@ -2,10 +2,12 @@
  * Gravity Perks // Multi-page Navigation // Enable "Back to Last Page" Button for Progress Bar Navigation
  * https://gravitywiz.com/documentation/gravity-forms-multi-page-navigation/
  *
- * This snippet is designed to be used with the Gravity Forms Custom Javascript plugin. 
+ * Instruction Video: https://www.loom.com/share/877d576a3e3d46329d132dd634c1ecca
+ *
+ * This snippet is designed to be used with the Gravity Forms Custom Javascript plugin.
  * https://gravitywiz.com/gravity-forms-custom-javascript/
- * 
- * The "Back to Last Page" button relies on the "Steps" Progress Indicator. Use this snippet to enable it for 
+ *
+ * The "Back to Last Page" button relies on the "Steps" Progress Indicator. Use this snippet to enable it for
  * the "Progress Bar" or "None" Progress Indicator.
  */
 var myGPMPN = window['gpmpn_GFFORMID'];
