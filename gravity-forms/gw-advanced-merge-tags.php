3b56638d5549bba8d68ea948d972d68be9d82115
David Smith <david@gravitywiz.com>
2022-05-05 15:10:21 -0400

    Added support for `:wordcount` modifier to retrieve the number of words in a given text-based field.

--- gravity-forms/gw-advanced-merge-tags.php
+++ gravity-forms/gw-advanced-merge-tags.php
@@ -18,7 +18,7 @@
  *   + HTML fields
  *      {HTML:3}
  *      {all_fields:allowHtmlFields}
- * 
+ *
  * Coming soon...
  *   + {Address:1}
  *        Output values from all Address inputs.
@@ -50,53 +50,56 @@
  */
 class GW_Advanced_Merge_Tags {
 
-    /**
-    * @TODO:
-    *   - add support for validating based on the merge tag (to prevent values from being changed)
-    *   - add support for merge tags in dynamic population parameters
-    *   - add merge tag builder
-    */
+	/**
+	 * @TODO:
+	 *   - add support for validating based on the merge tag (to prevent values from being changed)
+	 *   - add support for merge tags in dynamic population parameters
+	 *   - add merge tag builder
+	 */
 
-    private $_args = null;
+	private $_args = null;
 
-    public static $instance = null;
+	public static $instance = null;
 
-    public static function get_instance( $args ) {
+	public static function get_instance( $args ) {
 
-        if( null == self::$instance )
-            self::$instance = new self( $args );
+		if ( null == self::$instance ) {
+			self::$instance = new self( $args );
+		}
 
-        return self::$instance;
-    }
+		return self::$instance;
+	}
 
-    private function __construct( $args ) {
+	private function __construct( $args ) {
 
-        if( ! class_exists( 'GFForms' ) )
-            return;
+		if ( ! class_exists( 'GFForms' ) ) {
+			return;
+		}
 
-        $this->_args = wp_parse_args( $args, array(
-            'save_source_post_id' => false
-        ) );
+		$this->_args = wp_parse_args( $args, array(
+			'save_source_post_id' => false
+		) );
 
-        add_action( 'gform_pre_render', array( $this, 'support_default_value_and_html_content_merge_tags' ) );
-        add_action( 'gform_pre_render', array( $this, 'support_dynamic_population_merge_tags' ) );
+		add_action( 'gform_pre_render', array( $this, 'support_default_value_and_html_content_merge_tags' ) );
+		add_action( 'gform_pre_render', array( $this, 'support_dynamic_population_merge_tags' ) );
 
-        add_action( 'gform_merge_tag_filter',       array( $this, 'support_html_field_merge_tags' ), 10, 4 );
-        add_action( 'gform_replace_merge_tags',     array( $this, 'replace_merge_tags' ), 10, 3 );
-        add_action( 'gform_pre_replace_merge_tags', array( $this, 'replace_get_variables' ), 10, 5 );
+		add_action( 'gform_merge_tag_filter', array( $this, 'support_html_field_merge_tags' ), 10, 4 );
+		add_action( 'gform_replace_merge_tags', array( $this, 'replace_merge_tags' ), 10, 3 );
+		add_action( 'gform_pre_replace_merge_tags', array( $this, 'replace_get_variables' ), 10, 5 );
+		add_action( 'gform_merge_tag_filter', array( $this, 'handle_field_modifiers' ), 10, 6 );
 
-        if( $this->_args['save_source_post_id'] ) {
-            add_filter( 'gform_entry_created', array( $this, 'save_source_post_id' ), 10, 2 );
-        }
+		if ( $this->_args['save_source_post_id'] ) {
+			add_filter( 'gform_entry_created', array( $this, 'save_source_post_id' ), 10, 2 );
+		}
 
-    }
+	}
 
-    public function support_default_value_and_html_content_merge_tags( $form ) {
+	public function support_default_value_and_html_content_merge_tags( $form ) {
 
-        $current_page = max( 1, (int) rgars( GFFormDisplay::$submission, "{$form['id']}/page_number" ) );
-        $fields = array();
+		$current_page = max( 1, (int) rgars( GFFormDisplay::$submission, "{$form['id']}/page_number" ) );
+		$fields       = array();
 
-        foreach( $form['fields'] as &$field ) {
+		foreach ( $form['fields'] as &$field ) {
 
 //            $default_value = rgar( $field, 'defaultValue' );
 //            preg_match_all( '/{.+}/', $default_value, $matches, PREG_SET_ORDER );
@@ -108,7 +111,7 @@ class GW_Advanced_Merge_Tags {
 //                }
 //            }
 
-            // only run 'content' filter for fields on the current page
+			// only run 'content' filter for fields on the current page
 //            if( rgar( $field, 'pageNumber' ) != $current_page )
 //                continue;
 //
@@ -117,256 +120,268 @@ class GW_Advanced_Merge_Tags {
 //            if( ! empty( $matches ) )
 //                $field['content'] = $this->replace_merge_tags( $html_content, $form, null );
 
-        }
+		}
+
+		return $form;
+	}
+
+	public function support_dynamic_population_merge_tags( $form ) {
+
+		$filter_names = array();
 
-        return $form;
-    }
+		foreach ( $form['fields'] as &$field ) {
+
+			if ( ! rgar( $field, 'allowsPrepopulate' ) ) {
+				continue;
+			}
+
+			// complex fields store inputName in the "name" property of the inputs array
+			if ( is_array( rgar( $field, 'inputs' ) ) && $field['type'] != 'checkbox' ) {
+				foreach ( $field['inputs'] as $input ) {
+					if ( rgar( $input, 'name' ) ) {
+						$filter_names[] = array( 'type' => $field['type'], 'name' => rgar( $input, 'name' ) );
+					}
+				}
+			} else {
+				$filter_names[] = array( 'type' => $field['type'], 'name' => rgar( $field, 'inputName' ) );
+			}
 
-    public function support_dynamic_population_merge_tags( $form ) {
+		}
 
-        $filter_names = array();
+		foreach ( $filter_names as $filter_name ) {
 
-        foreach($form['fields'] as &$field) {
+			// do standard GF prepop replace first...
+			$filtered_name = GFCommon::replace_variables_prepopulate( $filter_name['name'] );
 
-            if(!rgar($field, 'allowsPrepopulate'))
-                continue;
+			// if default prepop doesn't find anything, do our advanced replace
+			if ( $filter_name['name'] == $filtered_name ) {
+				$filtered_name = $this->replace_merge_tags( $filter_name['name'], $form, null );
+			}
+
+			if ( $filter_name['name'] == $filtered_name ) {
+				continue;
+			}
 
-            // complex fields store inputName in the "name" property of the inputs array
-            if(is_array(rgar($field, 'inputs')) && $field['type'] != 'checkbox') {
-                foreach($field['inputs'] as $input) {
-                    if(rgar($input, 'name'))
-                        $filter_names[] = array('type' => $field['type'], 'name' => rgar($input, 'name'));
-                }
-            } else {
-                $filter_names[] = array('type' => $field['type'], 'name' => rgar($field, 'inputName'));
-            }
+			add_filter( "gform_field_value_{$filter_name['name']}", create_function( "", "return '$filtered_name';" ) );
+		}
 
-        }
+		return $form;
+	}
 
-        foreach($filter_names as $filter_name) {
+	public function replace_merge_tags( $text, $form, $entry ) {
 
-            // do standard GF prepop replace first...
-            $filtered_name = GFCommon::replace_variables_prepopulate($filter_name['name']);
+		// at some point GF started passing a pre-submission generated entry, it will have a null ID
+		if ( rgar( $entry, 'id' ) == null ) {
+			$entry = null;
+		}
 
-            // if default prepop doesn't find anything, do our advanced replace
-            if( $filter_name['name'] == $filtered_name )
-                $filtered_name = $this->replace_merge_tags( $filter_name['name'], $form, null );
+		// matches {Label:#fieldId#}
+		//         {Label:#fieldId#:#options#}
+		//         {Custom:#options#}
+		while ( preg_match_all( '/{(\w+)(:([\w&,=)(\-]+)){1,2}}/mi', $text, $matches, PREG_SET_ORDER ) ) {
 
-            if( $filter_name['name'] == $filtered_name )
-                continue;
+			foreach ( $matches as $match ) {
 
-            add_filter("gform_field_value_{$filter_name['name']}", create_function("", "return '$filtered_name';"));
-        }
+				list( $tag, $type, $args_match, $args_str ) = array_pad( $match, 4, false );
+				parse_str( $args_str, $args );
 
-        return $form;
-    }
+				$args  = array_map( array( $this, 'check_for_value_modifiers' ), $args );
+				$value = '';
 
-    public function replace_merge_tags( $text, $form, $entry ) {
+				switch ( $type ) {
+					case 'post':
+						$value = $this->get_post_merge_tag_value( $args );
+						break;
+					case 'post_meta':
+					case 'custom_field':
+						$value = $this->get_post_meta_merge_tag_value( $args );
+						break;
+					case 'entry':
+						$args['entry'] = $entry;
+						$value         = $this->get_entry_merge_tag_value( $args );
+						break;
+					case 'entry_meta':
+						$args['entry'] = $entry;
+						$value         = $this->get_entry_meta_merge_tag_value( $args );
+						break;
+					case 'callback':
+						$args['callback'] = array_shift( array_keys( $args ) );
+						unset( $args[ $args['callback'] ] );
+						$args['entry'] = $entry;
+						$value         = $this->get_callback_merge_tag_value( $args );
+						break;
+				}
 
-	    // at some point GF started passing a pre-submission generated entry, it will have a null ID
-        if( rgar( $entry, 'id' ) == null ) {
-            $entry = null;
-        }
+				// @todo: figure out if/how to support values that are not strings
+				if ( is_array( $value ) || is_object( $value ) ) {
+					$value = '';
+				}
 
-	    // matches {Label:#fieldId#}
-	    //         {Label:#fieldId#:#options#}
-	    //         {Custom:#options#}
-        while( preg_match_all( '/{(\w+)(:([\w&,=)(\-]+)){1,2}}/mi', $text, $matches, PREG_SET_ORDER ) ) {
+				$text = str_replace( $tag, $value, $text );
 
-            foreach( $matches as $match ) {
+			}
 
-                list( $tag, $type, $args_match, $args_str ) = array_pad( $match, 4, false );
-                parse_str( $args_str, $args );
+		}
 
-                $args = array_map( array( $this, 'check_for_value_modifiers' ), $args );
-                $value = '';
+		return $text;
+	}
 
-                switch( $type ) {
-                    case 'post':
-                        $value = $this->get_post_merge_tag_value( $args );
-                        break;
-                    case 'post_meta':
-                    case 'custom_field':
-                        $value = $this->get_post_meta_merge_tag_value( $args );
-                        break;
-                    case 'entry':
-                        $args['entry'] = $entry;
-                        $value = $this->get_entry_merge_tag_value( $args );
-                        break;
-                    case 'entry_meta':
-                        $args['entry'] = $entry;
-                        $value = $this->get_entry_meta_merge_tag_value( $args );
-                        break;
-                    case 'callback':
-                        $args['callback'] = array_shift( array_keys( $args ) );
-                        unset( $args[$args['callback']] );
-                        $args['entry'] = $entry;
-                        $value = $this->get_callback_merge_tag_value( $args );
-                        break;
-                }
+	public function save_source_post_id( $entry, $form ) {
 
-                // @todo: figure out if/how to support values that are not strings
-                if( is_array( $value ) || is_object( $value ) )
-                    $value = '';
+		if ( is_singular() && ! rgget( 'gf_page' ) ) {
+			$post_id = get_queried_object_id();
+			gform_update_meta( $entry['id'], 'source_post_id', $post_id );
+		}
 
-                $text = str_replace( $tag, $value, $text );
+	}
 
-            }
+	public function check_for_value_modifiers( $text ) {
 
-        }
+		// modifier regex (i.e. "get(value)")
+		preg_match_all( '/([a-z]+)\(([a-z_\-]+)\)/mi', $text, $matches, PREG_SET_ORDER );
+		if ( empty( $matches ) ) {
+			return $text;
+		}
 
-        return $text;
-    }
+		foreach ( $matches as $match ) {
 
-    public function save_source_post_id( $entry, $form ) {
+			list( $tag, $type, $arg ) = array_pad( $match, 3, false );
+			$value = '';
 
-        if( is_singular() && ! rgget( 'gf_page' ) ) {
-            $post_id = get_queried_object_id();
-            gform_update_meta( $entry['id'], 'source_post_id', $post_id );
-        }
+			switch ( $type ) {
+				case 'get':
+					$value = rgget( $arg );
+					break;
+				case 'post':
+					$value = rgpost( $arg );
+					break;
+			}
 
-    }
+			$text = str_replace( $tag, $value, $text );
 
-    public function check_for_value_modifiers( $text ) {
+		}
 
-        // modifier regex (i.e. "get(value)")
-        preg_match_all( '/([a-z]+)\(([a-z_\-]+)\)/mi', $text, $matches, PREG_SET_ORDER );
-        if( empty( $matches ) )
-            return $text;
+		return $text;
+	}
 
-        foreach( $matches as $match ) {
+	public function get_post_merge_tag_value( $args ) {
 
-            list( $tag, $type, $arg ) = array_pad( $match, 3, false );
-            $value = '';
+		extract( wp_parse_args( $args, array(
+			'id'   => false,
+			'prop' => false
+		) ) );
 
-            switch( $type ) {
-            case 'get':
-                $value = rgget( $arg );
-                break;
-            case 'post':
-                $value = rgpost( $arg );
-                break;
-            }
+		if ( ! $id || ! $prop ) {
+			return '';
+		}
 
-            $text = str_replace( $tag, $value, $text );
+		$post = get_post( $id );
+		if ( ! $post ) {
+			return '';
+		}
 
-        }
+		return isset( $post->$prop ) ? $post->$prop : '';
+	}
 
-        return $text;
-    }
+	public function get_post_meta_merge_tag_value( $args ) {
 
-    public function get_post_merge_tag_value( $args ) {
+		extract( wp_parse_args( $args, array(
+			'id'       => false,
+			'meta_key' => false
+		) ) );
 
-        extract( wp_parse_args( $args, array(
-            'id' => false,
-            'prop' => false
-        ) ) );
+		if ( ! $id || ! $meta_key ) {
+			return '';
+		}
 
-        if( !$id || !$prop )
-            return '';
+		$value = get_post_meta( $id, $meta_key, true );
 
-        $post = get_post( $id );
-        if( !$post )
-            return '';
+		return $value;
+	}
 
-        return isset( $post->$prop ) ? $post->$prop : '';
-    }
+	public function get_entry_merge_tag_value( $args ) {
 
-    public function get_post_meta_merge_tag_value( $args ) {
+		extract( wp_parse_args( $args, array(
+			'id'    => false,
+			'prop'  => false,
+			'entry' => false
+		) ) );
 
-        extract( wp_parse_args( $args, array(
-            'id' => false,
-            'meta_key' => false
-            ) ) );
+		if ( ! $entry ) {
 
-        if( !$id || !$meta_key )
-            return '';
+			if ( ! $id ) {
+				$id = rgget( 'eid' );
+			}
 
-        $value = get_post_meta( $id, $meta_key, true );
+			if ( is_callable( 'gw_post_content_merge_tags' ) ) {
+				$id = gw_post_content_merge_tags()->maybe_decrypt_entry_id( $id );
+			}
 
-        return $value;
-    }
+			$entry = GFAPI::get_entry( $id );
 
-    public function get_entry_merge_tag_value( $args ) {
+		}
 
-        extract( wp_parse_args( $args, array(
-            'id' => false,
-            'prop' => false,
-            'entry' => false
-        ) ) );
+		if ( ! $prop ) {
+			$prop = key( $args );
+		}
 
-	    if( ! $entry ) {
+		if ( ! $entry || is_wp_error( $entry ) || ! $prop ) {
+			return '';
+		}
 
-		    if( ! $id ) {
-			    $id = rgget( 'eid' );
-		    }
+		$value = rgar( $entry, $prop );
 
-		    if( is_callable( 'gw_post_content_merge_tags' ) ) {
-			    $id = gw_post_content_merge_tags()->maybe_decrypt_entry_id( $id );
-		    }
+		return $value;
+	}
 
-		    $entry = GFAPI::get_entry( $id );
+	public function get_entry_meta_merge_tag_value( $args ) {
 
-	    }
+		extract( wp_parse_args( $args, array(
+			'id'       => false,
+			'meta_key' => false,
+			'entry'    => false
+		) ) );
 
-        if( ! $prop ) {
-	        $prop = key( $args );
-        }
+		if ( ! $id ) {
+			if ( rgget( 'eid' ) ) {
+				$id = rgget( 'eid' );
+			} else if ( isset( $entry['id'] ) ) {
+				$id = $entry['id'];
+			}
+		}
 
-        if( ! $entry || is_wp_error( $entry ) || ! $prop ) {
-	        return '';
-        }
+		if ( ! $meta_key ) {
+			$meta_key = key( $args );
+		}
 
-        $value = rgar( $entry, $prop );
+		if ( ! $id || ! $meta_key ) {
+			return '';
+		}
 
-        return $value;
-    }
+		if ( is_callable( 'gw_post_content_merge_tags' ) ) {
+			$id = gw_post_content_merge_tags()->maybe_decrypt_entry_id( $id );
+		}
 
-    public function get_entry_meta_merge_tag_value( $args ) {
+		$value = gform_get_meta( $id, $meta_key );
 
-        extract( wp_parse_args( $args, array(
-            'id' => false,
-            'meta_key' => false,
-            'entry' => false
-            ) ) );
+		return $value;
+	}
 
-        if( ! $id ) {
-            if( rgget( 'eid' ) ) {
-                $id = rgget( 'eid' );
-            } else if( isset( $entry['id'] ) ) {
-                $id = $entry['id'];
-            }
-        }
+	public function get_callback_merge_tag_value( $args ) {
 
-        if( ! $meta_key )
-            $meta_key = key( $args );
+		$callback = $args['callback'];
+		unset( $args['callback'] );
 
-        if( ! $id || ! $meta_key )
-            return '';
+		extract( wp_parse_args( $args, array(
+			'entry' => false
+		) ) );
 
-        if( is_callable( 'gw_post_content_merge_tags' ) ) {
-            $id = gw_post_content_merge_tags()->maybe_decrypt_entry_id( $id );
-        }
+		if ( ! is_callable( $callback ) ) {
+			return '';
+		}
 
-        $value = gform_get_meta( $id, $meta_key );
-
-        return $value;
-    }
-
-    public function get_callback_merge_tag_value( $args ) {
-
-        $callback = $args['callback'];
-        unset( $args['callback'] );
-
-        extract( wp_parse_args( $args, array(
-            'entry' => false
-        ) ) );
-
-        if( ! is_callable( $callback ) )
-            return '';
-
-        return call_user_func( $callback, $args );
-    }
+		return call_user_func( $callback, $args );
+	}
 
 	/**
 	 * Replace {get:xxx} merge tags. Thanks, Gravity View!
@@ -374,18 +389,18 @@ class GW_Advanced_Merge_Tags {
 	 * @param       $text
 	 * @param array $form
 	 * @param array $entry
-	 * @param bool  $url_encode
+	 * @param bool $url_encode
 	 *
 	 * @return mixed
 	 */
-    public function replace_get_variables( $text, $form, $entry, $url_encode, $esc_html, $get = null ) {
+	public function replace_get_variables( $text, $form, $entry, $url_encode, $esc_html, $get = null ) {
 
-    	if( $get === null ) {
-    		$get = $_GET;
-	    }
+		if ( $get === null ) {
+			$get = $_GET;
+		}
 
 		preg_match_all( "/{get:(.*?)}/ism", $text, $matches, PREG_SET_ORDER );
-		if( empty( $matches ) ) {
+		if ( empty( $matches ) ) {
 			return $text;
 		}
 
@@ -411,18 +426,47 @@ class GW_Advanced_Merge_Tags {
 	}
 
 	public function support_html_field_merge_tags( $value, $tag, $modifiers, $field ) {
-    	if( $field->type == 'html' && ( $tag != 'all_fields' || in_array( 'allowHtmlFields', explode( ',', $modifiers ) ) ) ) {
-    		$value = $field->content;
-	    }
+		if ( $field->type == 'html' && ( $tag != 'all_fields' || in_array( 'allowHtmlFields', explode( ',', $modifiers ) ) ) ) {
+			$value = $field->content;
+		}
+
+		return $value;
+	}
+
+	/**
+	 * @param $value
+	 * @param $input_id
+	 * @param $modifier
+	 * @param \GF_Field $field
+	 * @param $raw_value
+	 * @param $format
+	 *
+	 * @return mixed|void
+	 */
+	public function handle_field_modifiers( $value, $input_id, $modifier, $field, $raw_value, $format ) {
+
+		$modifiers = $field->get_modifiers();
+		if ( empty( $modifiers ) ) {
+			return $value;
+		}
+
+		foreach ( $modifiers as $modifier ) {
+			switch ( $modifier ) {
+				case 'wordcount':
+					return str_word_count( $value );
+					break;
+			}
+		}
+
 		return $value;
 	}
 
 }
 
 function gw_advanced_merge_tags( $args = array() ) {
-    return GW_Advanced_Merge_Tags::get_instance( $args );
+	return GW_Advanced_Merge_Tags::get_instance( $args );
 }
 
 gw_advanced_merge_tags( array(
-    'save_source_post_id' => true
+	'save_source_post_id' => false
 ) );
