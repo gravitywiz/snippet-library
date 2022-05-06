3f6cc58e0940de23837ed399e7b6ec4f7995b9b2
scyt <scott@gravitywiz.com>
2022-05-06 11:32:31 -0400

    Update gw-unrequire-required-fields.php

--- gravity-forms/gw-unrequire-required-fields.php
+++ gravity-forms/gw-unrequire-required-fields.php
@@ -14,33 +14,33 @@
 
 class GWUnrequire {
 
- var $_args = null;
+	 var $_args = null;
 
- public function __construct( $args = array() ) {
+	 public function __construct( $args = array() ) {
 
- $this->_args = wp_parse_args( $args, array(
- 'admins_only' => true,
- 'require_query_param' => true
- ) );
+	 $this->_args = wp_parse_args( $args, array(
+		'admins_only' => true,
+ 		'require_query_param' => true
+	 ) );
 
- add_filter( 'gform_pre_validation', array( $this, 'unrequire_fields' ) );
+	 add_filter( 'gform_pre_validation', array( $this, 'unrequire_fields' ) );
 
- }
+	 }
 
- function unrequire_fields( $form ) {
+	 function unrequire_fields( $form ) {
 
- if( $this->_args['admins_only'] && ! current_user_can( 'activate_plugins' ) )
- return $form;
+		if( $this->_args['admins_only'] && ! current_user_can( 'activate_plugins' ) )
+			return $form;
 
- if( $this->_args['require_query_param'] && ! isset( $_GET['gwunrequire'] ) )
- return $form;
+		if( $this->_args['require_query_param'] && ! isset( $_GET['gwunrequire'] ) )
+			return $form;
 
- foreach( $form['fields'] as &$field ) {
- $field['isRequired'] = false;
- }
+		foreach( $form['fields'] as &$field ) {
+			$field['isRequired'] = false;
+		}
 
- return $form;
- }
+		return $form;
+	}
 
 }
 
