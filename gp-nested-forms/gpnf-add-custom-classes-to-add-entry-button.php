1190656c410b25c99194179d9ca6b7eec2fa1ed7
dariospace <30393370+dariospace@users.noreply.github.com>
2022-05-12 11:50:58 -0300

    Added instruction video

--- gp-nested-forms/gpnf-add-custom-classes-to-add-entry-button.php
+++ gp-nested-forms/gpnf-add-custom-classes-to-add-entry-button.php
@@ -2,6 +2,9 @@
 /**
  * Gravity Perks // Nested Forms // Add Custom Classes to Add Entry Button
  * https://gravitywiz.com/documentation/gravity-forms-nested-forms/
+ *
+ * Instruction Video: https://www.loom.com/share/bfa19506533a42f98fb22cf5b951dc3a
+ *
  */
 add_filter( 'gpnf_template_args', function( $args ) {
 	if ( $args['template'] === 'nested-entries' ) {
