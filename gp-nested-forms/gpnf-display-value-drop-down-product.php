aa03f8575bb5f391f328199b0a92f9f666addaff
dariospace <30393370+dariospace@users.noreply.github.com>
2022-05-19 13:53:54 -0300

    Added instruction Video (#329)

--- gp-nested-forms/gpnf-display-value-drop-down-product.php
+++ gp-nested-forms/gpnf-display-value-drop-down-product.php
@@ -3,6 +3,8 @@
  * Gravity Perks // GP Nested Forms // Hide Price in Nested Form Field Display Value for Drop Down Product Fields
  * https://gravitywiz.com/documentation/gravity-forms-nested-forms/
  *
+ * Instruction Video: https://www.loom.com/share/fbd30ef61b234c38a0a19140457e7e8f
+ *
  * "My Product Choice ($10.00)" => "My Product Choice"
  */
 add_filter( 'gpnf_select_display_value', function( $value, $field ) {
