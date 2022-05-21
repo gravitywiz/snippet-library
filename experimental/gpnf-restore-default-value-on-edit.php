08e1e8cfcaf1d2f16ef41ee241cf0159a3385850
David Smith <david@gravitywiz.com>
2022-05-20 22:17:14 -0400

    Update gpnf-restore-default-value-on-edit.php

--- experimental/gpnf-restore-default-value-on-edit.php
+++ experimental/gpnf-restore-default-value-on-edit.php
@@ -5,14 +5,14 @@
  *
  * Use this snippet to restore the default value of a given field when it is edited via a Nested Form field.
  */
-// Update "123" to your form ID.
-add_filter( 'gpnf_populated_entry_123', function( $entry, $form ) {
+// Update "123" to your parent form ID and "4" to your Nested Form field ID.
+add_filter( 'gpnf_populated_entry_123_4', function( $entry, $form ) {
 
-	// Update "4" to your child field ID that should have its default value restored.
-	$child_field_id = 4;
+	// Update "5" to your child field ID that should have its default value restored.
+	$child_field_id = 5;
 
 	$field                    = GFAPI::get_field( $form, $child_field_id );
 	$entry[ $child_field_id ] = $field->get_value_default();
-	
+
 	return $entry;
 }, 10, 3 );
