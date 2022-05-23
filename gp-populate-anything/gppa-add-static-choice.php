13026e6900c0d2a241b60d43db3cd831f3e37c0f
dariospace <30393370+dariospace@users.noreply.github.com>
2022-05-23 13:38:35 -0300

    Updated video URL

--- gp-populate-anything/gppa-add-static-choice.php
+++ gp-populate-anything/gppa-add-static-choice.php
@@ -3,7 +3,7 @@
  * Gravity Perks // Populate Anything // Add a Static Choice
  * https://gravitywiz.com/documentation/gravity-forms-populate-anything/
  *
- * Instruction Video: https://www.loom.com/share/27e2a0f006e04b25b2391ff8bc55d286
+ * Instruction Video: https://www.loom.com/share/e425398f584148f58fdfea3d6b6f969b
  *
  */
 add_filter( 'gppa_input_choices_123_4', function( $choices, $field, $objects ) {
