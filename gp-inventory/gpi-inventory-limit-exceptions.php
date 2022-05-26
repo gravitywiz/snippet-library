7272daeea2b1721c4dff4795d74c4a28c0a2df46
David Smith <david@gravitywiz.com>
2022-05-26 17:30:05 -0400

    Update gpi-inventory-limit-exceptions.php

--- gp-inventory/gpi-inventory-limit-exceptions.php
+++ gp-inventory/gpi-inventory-limit-exceptions.php
@@ -3,9 +3,11 @@
  * Gravity Perks // Inventory // Limit by Scoped Value
  * https://gravitywiz.com/documentation/gravity-forms-inventory/
  */
-add_filter( 'gpi_inventory_limit_advanced_156_3', function( $inventory_limit, $field ) {
-	$date = rgpost( 'input_1' );
-	switch ( $date ) {
+// Update "123" to your form ID and "4" to your Inventory-enabled field ID.
+add_filter( 'gpi_inventory_limit_advanced_123_4', function( $inventory_limit, $field ) {
+	// Update "5" to the value of the scoped field whose value should impact the inventory limit.
+	$value = rgpost( 'input_5' );
+	switch ( $value ) {
 		case '05/02/2022';
 			$inventory_limit = 2;
 			break;
