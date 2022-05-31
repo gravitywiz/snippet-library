78b4e39e0b95b9ffb5a48f51b69b7c71206162d2
Clay Griffiths <clay@claygriffiths.com>
2022-05-30 19:33:11 -0500

    gpls-redirect-on-limit.php: Improved compatibility with GPNF

--- gp-limit-submissions/gpls-redirect-on-limit.php
+++ gp-limit-submissions/gpls-redirect-on-limit.php
@@ -6,7 +6,7 @@
  * Plugin URI:   https://gravitywiz.com/documentation/gravity-forms-limit-submissions/
  * Description:  Redirect to a specified URL when the limit is reached (rather than displaying a limit message).
  * Author:       Gravity Wiz
- * Version:      0.1
+ * Version:      0.2
  * Author URI:   https://gravitywiz.com
  *
  * Instructions
@@ -16,13 +16,20 @@
  *
  * That's it! This plugin will now handle redirecting to that URL rather than displaying the limit message.
  */
-add_filter( 'gform_get_form_filter', function( $form_string ) {
-	if ( is_callable( 'gp_limit_submissions' ) && gp_limit_submissions()->enforce->is_limit_reached() ) {
+add_filter( 'gform_get_form_filter', function ( $form_string, $form ) {
+	if ( ! is_callable( 'gp_limit_submissions' ) ) {
+		return $form_string;
+	}
+
+	gp_limit_submissions()->enforce->set_form_id( $form['id'] );
+
+	if ( gp_limit_submissions()->enforce->is_limit_reached() ) {
 		$url = gp_limit_submissions()->enforce->test()->failed_rule_group->get_message();
 		if ( GFCommon::is_valid_url( $url ) ) {
 			echo "<script>window.location = '{$url}';</script>";
 			exit;
 		}
 	}
+
 	return $form_string;
-} );
+}, 10, 2 );
