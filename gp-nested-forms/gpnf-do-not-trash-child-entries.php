ed34cabedb61c293fa28037438b7744d6d423bf7
dariospace <30393370+dariospace@users.noreply.github.com>
2022-06-06 11:28:28 -0300

    added instruction video

--- gp-nested-forms/gpnf-do-not-trash-child-entries.php
+++ gp-nested-forms/gpnf-do-not-trash-child-entries.php
@@ -3,6 +3,8 @@
  * Gravity Perks // Nested Forms // Don't Trash Child Entries
  * https://gravitywiz.com/documentation/gravity-forms-nested-forms/
  *
+ * Instruction Video: https://www.loom.com/share/d35a8611240c4be085019b8e4c5bbac7
+ *
  * By default, Nested Forms will trash child entries when the parent entry is trashed. Use this snippet to prevent this
  * default behavior.
  */
