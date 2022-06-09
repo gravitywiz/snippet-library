a11b90a74c942a5d0228c65e6f9690e2998c5621
Clay Griffiths <clay@claygriffiths.com>
2022-06-09 18:15:59 -0500

    Create gppa-use-choice-labels-when-populating-from-another-field.php

---
+++ gp-populate-anything/gppa-use-choice-labels-when-populating-from-another-field.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Gravity Perks // Populate Anything // Populate choices using choice labels rather than values
+ * http://gravitywiz.com/documentation/gravity-forms-populate-anything/
+ *
+ * Installation instructions:
+ *   1. https://gravitywiz.com/documentation/how-do-i-install-a-snippet/
+ *   2. Update variables below accordingly.
+ */
+add_filter( 'gppa_process_template', function ( $template_value, $field, $template_name, $populate, $object, $object_type, $objects, $template ) {
+	/* Variables to customize */
+	$populated_form_id  = 3;
+	$populated_field_id = 3;
+	/* End variables to customize */
+
+	if ( $template_name !== 'label' || $field->id !== $populated_field_id || $field->formId !== $populated_form_id ) {
+		return $template_value;
+	}
+
+	$entry = ( array ) $object;
+
+	$populating_from_field_id = str_replace( 'gf_field_', '', $template );
+	$populating_from_field    = GFAPI::get_field( $entry['form_id'], $populating_from_field_id );
+
+	// For dynamically populated choices
+	$choice_text = gp_populate_anything()->get_submitted_choice_label( $template_value, $populating_from_field, $entry['id'] );
+
+	// Get label for static choices.
+	if ( $choice_text === $template_value ) {
+		$choice_text = RGFormsModel::get_choice_text( $populating_from_field, $template_value );
+	}
+
+	return $choice_text;
+}, 10, 8 );
