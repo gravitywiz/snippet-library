1012d720028236b3457d5a64eeb94fda585885df
David Smith <david@gravitywiz.com>
2022-06-10 15:22:49 -0400

    Update gw-gravity-forms-filter-out-zero-dollar-products.php
    PHPCS fixes.

--- gravity-forms/gw-gravity-forms-filter-out-zero-dollar-products.php
+++ gravity-forms/gw-gravity-forms-filter-out-zero-dollar-products.php
@@ -2,17 +2,17 @@
 /**
  * Gravity Wiz // Gravity Forms // Filter Out $0.00 Products
  * http://gravitywiz.com/
- * 
+ *
  * Use this snippet to hide Products that have $0.00 Products
- * 
+ *
  */
 add_filter( 'gform_product_info', 'gw_remove_empty_products', 10, 3 );
 function gw_remove_empty_products( $product_info, $form, $lead ) {
 
 	$products = array();
-	
-	foreach( $product_info['products'] as $field_id => $product ) {
-		if( GFCommon::to_number( $product['price'] ) != 0 ) {
+
+	foreach ( $product_info['products'] as $field_id => $product ) {
+		if ( GFCommon::to_number( $product['price'] ) != 0 ) {
 			$products[ $field_id ] = $product;
 		}
 	}
