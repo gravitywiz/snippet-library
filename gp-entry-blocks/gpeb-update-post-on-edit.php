0e11f42fa0a78ae0fbddbb51a5fbccf4a059ee71
David Smith <david@gravitywiz.com>
2022-06-24 12:25:10 -0400

    Update gpeb-update-post-on-edit.php
    Updated to preserve the original post status on update.

--- gp-entry-blocks/gpeb-update-post-on-edit.php
+++ gp-entry-blocks/gpeb-update-post-on-edit.php
@@ -12,6 +12,8 @@ add_filter( 'gform_post_data', function( $post_data, $form, $entry ) {
 	$post_id = $entry['post_id'];
 	if ( $post_id ) {
 		$post_data['ID'] = $post_id;
+		$post = get_post( $post_id );
+		$post_data['post_status'] = $post['post_status'];
 	}
 	return $post_data;
 }, 10, 3 );
