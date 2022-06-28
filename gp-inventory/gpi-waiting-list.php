38efdca99b796e41b46ee2ea26d8925bf4660e1b
David Smith <david@gravitywiz.com>
2022-06-28 11:13:57 -0400

    Added support for showing waitlist message without enabling available inventory message.
    Added support for showing waitlist message in confirmation and notifications.

--- gp-inventory/gpi-waiting-list.php
+++ gp-inventory/gpi-waiting-list.php
@@ -19,8 +19,30 @@ add_filter( 'gpi_pre_render_choice', function( $choice, $exceeded_limit, $field,
 	if ( $how_many_left <= 0 ) {
 		$message         = '(waiting list)';
 		$default_message = gp_inventory_type_choices()->replace_choice_available_inventory_merge_tags( gp_inventory_type_choices()->get_inventory_available_message( $field ), $field, $form, $choice, $how_many_left );
-		$choice['text']  = str_replace( $default_message, $message, $choice['text'] );
+		if ( strpos( $choice['text'], $default_message ) === false ) {
+			$choice['text'] .= ' ' . $message;
+		} else {
+			$choice['text'] = str_replace( $default_message, $message, $choice['text'] );
+		}
+
 	}
 
 	return $choice;
 }, 10, 5 );
+
+add_filter( 'gform_pre_submission_filter', function( $form ) {
+	foreach ( $form['fields'] as &$field ) {
+		if ( ! gp_inventory_type_choices()->is_applicable_field( $field ) ) {
+			continue;
+		}
+		$choice_counts = gp_inventory_type_choices()->get_choice_counts( $form['id'], $field );
+		$choices       = $field['choices'];
+		foreach( $choices as &$choice ) {
+			$value        = $field->sanitize_entry_value( $choice['value'], $form['id'] );
+			$choice_count = intval( rgar( $choice_counts, $value ) );
+			$choice       = gf_apply_filters( array( 'gpi_pre_render_choice', $form['id'], $field->id ), $choice, null, $field, $form, $choice_count );
+		}
+		$field['choices'] = $choices;
+	}
+	return $form;
+} );
