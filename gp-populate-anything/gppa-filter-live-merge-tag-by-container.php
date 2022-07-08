7aa7daf02a3f6c1be5ec09cb3a08cf229a090d70
David Smith <david@gravitywiz.com>
2022-07-08 11:22:14 -0400

    Create gppa-filter-live-merge-tag-by-container.php

---
+++ gp-populate-anything/gppa-filter-live-merge-tag-by-container.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Gravity Perks // Populate Anything // Filter Live Merge Tag Value by Container Field
+ * https://gravitywiz.com/documentation/gravity-forms-populate-anything/
+ *
+ * The `gppa_live_merge_tag_value` provides a powerful method for filtering the value of a live merge tag but it does not
+ * allow you to modify the value based on the field in which the live merge tag is being displayed (e.g. the "container" field).
+ *
+ * Use this combination of filters to limit your live merge tag value filter to a specific container field.
+ */
+// Update "123" to your form ID and "4" to the field that contains the live merge tag.
+add_filter( 'gppa_hydrated_field_123_4', function( $return ) {
+	$func = function( $merge_tag_match_value, $merge_tag, $form, $field_id, $entry_values ) use ( &$func ) {
+		// Update "5" to the ID of the field targeted by the live merge tag.
+		remove_filter( "gppa_live_merge_tag_value_{$form['id']}_5", $func );
+		return $merge_tag_match_value . ' FILTERED!';
+	};
+	add_filter( 'gppa_live_merge_tag_value', $func, 10, 5 );
+	return $return;
+} );
