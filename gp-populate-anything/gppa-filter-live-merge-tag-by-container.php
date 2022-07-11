8eecce7d5646a73320398648d009818ca2173e0f
David Smith <david@gravitywiz.com>
2022-07-11 11:07:20 -0400

    Update gppa-filter-live-merge-tag-by-container.php

--- gp-populate-anything/gppa-filter-live-merge-tag-by-container.php
+++ gp-populate-anything/gppa-filter-live-merge-tag-by-container.php
@@ -9,12 +9,13 @@
  * Use this combination of filters to limit your live merge tag value filter to a specific container field.
  */
 // Update "123" to your form ID and "4" to the field that contains the live merge tag.
-add_filter( 'gppa_hydrated_field_123_4', function( $return ) {
-	$func = function( $merge_tag_match_value, $merge_tag, $form, $field_id, $entry_values ) use ( &$func ) {
-		// Update "5" to the ID of the field targeted by the live merge tag.
-		remove_filter( "gppa_live_merge_tag_value_{$form['id']}_5", $func );
+add_filter( 'gppa_hydrated_field_123_4', function( $field ) {
+	// Update "5" to the ID of the field targeted by the live merge tag.
+	$live_merge_tag_field_id = 5;
+	$func = function( $merge_tag_match_value, $merge_tag, $form, $field_id, $entry_values ) use ( &$func, $live_merge_tag_field_id ) {
+		remove_filter( "gppa_live_merge_tag_value_{$form['id']}_{$live_merge_tag_field_id}", $func );
 		return $merge_tag_match_value . ' FILTERED!';
 	};
-	add_filter( 'gppa_live_merge_tag_value', $func, 10, 5 );
-	return $return;
+	add_filter( "gppa_live_merge_tag_value_{$field->formId}_{$live_merge_tag_field_id}", $func, 10, 5 );
+	return $field;
 } );
