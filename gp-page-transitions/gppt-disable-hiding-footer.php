e68d34812988e2708bc8e102a6c9906c2fe321bd
Samuel Bassah <sbassah@gmail.com>
2022-07-13 20:10:09 +0000

    Disable Hiding the Form Footer

---
+++ gp-page-transitions/gppt-disable-hiding-footer.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Gravity Perks // Page Transitions // Disable Hiding the Form Footer
+ * https://gravitywiz.com/documentation/gravity-forms-page-transitions/
+ *
+ * Disable hiding the form footer.
+ */
+add_filter( 'gppt_hide_footer', '__return_false', 10, 2 );
