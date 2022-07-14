56288bd1cf8c702c956eabe93da94f986770480f
David Smith <david@gravitywiz.com>
2022-07-14 09:12:08 -0400

    Update and rename gppa_lmt_whitelist.php to gppa-lmt-whitelist.php

--- gp-populate-anything/gppa-lmt-whitelist.php
+++ gp-populate-anything/gppa-lmt-whitelist.php
@@ -3,12 +3,11 @@
  * Gravity Perks // Populate Anything // Add Live Merge Tag to Whitelist
  * https://gravitywiz.com/documentation/gravity-forms-populate-anything/
  */
-function add_ip_lmt_to_whitelist ($whitelist, $form) {
+add_filter( 'gppa_lmt_whitelist', function() {
 
     $merge_tag = '{ip}';
     
     $whitelist[ $merge_tag ] = wp_create_nonce( 'gppa-lmt-' . $form['id'] . '-' . $merge_tag );
     
     return $whitelist;
-}
-add_filter( 'gppa_lmt_whitelist', 'add_ip_lmt_to_whitelist', 10, 2 );
+}, 10, 2 );
