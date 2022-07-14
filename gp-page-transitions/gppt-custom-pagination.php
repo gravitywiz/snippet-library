6e85f314fb5edeff3604e0b41fbc8cb0d2818976
David Smith <david@gravitywiz.com>
2022-07-14 09:14:33 -0400

    Update gppt-custom-pagination.php

--- gp-page-transitions/gppt-custom-pagination.php
+++ gp-page-transitions/gppt-custom-pagination.php
@@ -2,7 +2,5 @@
 /**
  * Gravity Perks // Page Transitions // Enable Custom Pagination Support
  * https://gravitywiz.com/documentation/gravity-forms-page-transitions/
- *
- * Enable Custom Pagination Support.
  */
 add_filter( 'gppt_is_custom_pagination', '__return_true' );
