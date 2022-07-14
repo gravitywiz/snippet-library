b3f86aa097bc044c235088602e79e1803fe7ddec
dariospace <30393370+dariospace@users.noreply.github.com>
2022-07-14 11:53:05 -0300

    Create gppa_lmt_whitelist.php (#353)
    * Create gppa_lmt_whitelist.php
    
    Added hook to the library
    
    * Update gppa_lmt_whitelist.php
    
    Fixed linting and updated URL
    
    * Update and rename gppa_lmt_whitelist.php to gppa-lmt-whitelist.php
    
    * Update gppa-lmt-whitelist.php
    
    Co-authored-by: David Smith <david@gravitywiz.com>

---
+++ gp-populate-anything/gppa-lmt-whitelist.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Gravity Perks // Populate Anything // Add Live Merge Tag to Whitelist
+ * https://gravitywiz.com/documentation/gravity-forms-populate-anything/
+ */
+add_filter( 'gppa_lmt_whitelist', function() {
+
+    $merge_tag = '{ip}';
+
+    $whitelist[ $merge_tag ] = wp_create_nonce( 'gppa-lmt-' . $form['id'] . '-' . $merge_tag );
+
+    return $whitelist;
+}, 10, 2 );
