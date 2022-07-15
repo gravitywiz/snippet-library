197f54587fff34f42405f3ad99c6c4f27916e84b
David Smith <david@gravitywiz.com>
2022-07-15 15:16:59 -0400

    Update gpls-field-validation-message.php

--- gp-limit-submissions/gpls-field-validation-message.php
+++ gp-limit-submissions/gpls-field-validation-message.php
@@ -1,9 +1,7 @@
 <?php
 /**
- * Gravity Perks // Limit Submission // Change Validation Message for a specific field
+ * Gravity Perks // Limit Submission // Change Validation Message for a Specific Field
  * https://gravitywiz.com/documentation/gravity-forms-limit-submissions/
- *
- * The following example changes the GP Limit Submission validation message on field #3 in form #5.
  */
 add_filter( 'gpls_field_failed_validation_123_4', function( $message ) { 
     return 'Example validation message.'; 
