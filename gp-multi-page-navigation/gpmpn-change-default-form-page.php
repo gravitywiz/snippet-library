4090b8d08b8c31a5c6710c6a53ccc6353d845a17
David Smith <david@gravitywiz.com>
2022-07-15 11:20:31 -0400

    Update gpmpn-change-default-form-page.php

--- gp-multi-page-navigation/gpmpn-change-default-form-page.php
+++ gp-multi-page-navigation/gpmpn-change-default-form-page.php
@@ -2,9 +2,8 @@
 /**
  * Gravity Perks // Multi-Page Navigation // Change Default Form Page
  * https://gravitywiz.com/documentation/gravity-forms-multi-page-navigation/
- *
- * This snippet sets the default form page to the second page.
  */
 add_filter( 'gpmpn_default_page', function( $page ) {
+	// Update "2" to the page at which the form should start. "2" would start the form on the second page.
 	return 2;
 } );
