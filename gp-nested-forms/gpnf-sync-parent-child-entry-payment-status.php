6d49fb74c2316164fb3d7f5a6f3f7ac122a907e5
David Smith <david@gravitywiz.com>
2022-07-15 14:32:49 -0400

    Updated bulk action to appear for any form with a Nested Form field and at least one product field.
    Lots of miscellaenous tweaks.

--- gp-nested-forms/gpnf-sync-parent-child-entry-payment-status.php
+++ gp-nested-forms/gpnf-sync-parent-child-entry-payment-status.php
@@ -1,69 +1,91 @@
 <?php
 /**
- * Gravity Perks // Nested Forms // Sync Parent/Child Entry Payment Info
+ * Gravity Perks // Nested Forms // Sync Child Entry Payment Details w/ Parent
  * https://gravitywiz.com/documentation/gravity-forms-nested-forms/
  *
- * Sync the payment info of child entries with their parent's. This is useful with GP Limit Choices and GP Inventory
+ * Sync the payment details of child entries with their parent's. This is useful with GP Limit Choices and GP Inventory
  * to ensure that limits/inventories applied to child field's will not include child entries where payment has not been
  * collected for their parent entry.
  *
- * Plugin Name:  GP Nested Forms — Sync Parent/Child Entry Payment Info
+ * Plugin Name:  GP Nested Forms — Sync Parent/Child Entry Payment Details
  * Plugin URI:   https://gravitywiz.com/documentation/gravity-forms-nested-forms/
- * Description:  Sync the payment info of child entries with their parent's.
+ * Description:  Sync the payment details of child entries with their parent's.
  * Author:       Gravity Wiz
- * Version:      0.1
+ * Version:      0.2
  * Author URI:   https://gravitywiz.com
  */
 add_action( 'gform_after_submission', function( $entry, $form ) {
-	gpnf_update_child_entries_payment_status( $entry );
+	gpnf_sync_child_entries_payment_details( $entry );
 }, 10, 2 );
 
 add_action( 'gform_post_update_entry', function( $entry, $original_entry ) {
-		gpnf_update_child_entries_payment_status( $entry );
+	gpnf_sync_child_entries_payment_details( $entry );
 }, 10, 2 );
 
 add_action( 'gform_after_update_entry', function( $entry_id, $original_entry ) {
 	$entry = GFAPI::get_entry( $entry_id );
-		gpnf_update_child_entries_payment_status( $entry );
+	gpnf_sync_child_entries_payment_details( $entry );
 }, 10, 2 );
 
-
 /**
- * Bulk Sync Parent/Child Entry Payment Info
+ * Bulk Sync Parent/Child Entry Payment Details
  * https://docs.gravityforms.com/gform_entry_list_bulk_actions/
  * https://docs.gravityforms.com/gform_entry_list_action/
  */
 
-add_filter( 'gform_entry_list_bulk_actions_32', 'add_payment_action', 10, 2 );	//	Add bulk syncing entries via "Bulk Actions" for form 32
-function add_payment_action( $actions, $form_id ) {
-    $actions['copy_payment_info'] = 'Copy Payment Info to Child Entries';
-    return $actions;
-}
-
-add_action( 'gform_entry_list_action', 'perform_payment_action', 10, 3 );
-function perform_payment_action( $action, $entries, $form_id ) {
-    if ( $action == 'copy_payment_info' ) {
-        foreach ( $entries as $entry_id ) {
-            $entry = GFAPI::get_entry( $entry_id );
-			gpnf_update_child_entries_payment_status( $entry );
-        }
-    }
-}
-
-function gpnf_update_child_entries_payment_status( $parent_entry ) {
-
-	$parent_entry = new GPNF_Entry( $parent_entry );
-	if ( ! $parent_entry->payment_status ) {
-		return;
+add_filter( 'gform_entry_list_bulk_actions', function( $actions, $form_id ) {
+	$form = GFAPI::get_form( $form_id );
+	if ( is_callable( 'gp_nested_forms' )
+	     && gp_nested_forms()->has_nested_form_field( $form )
+	     && gpnf_has_product_field( $form )
+	) {
+		$actions['gpnf_sync_child_entries'] = 'Sync Child Entry Payment Details';
 	}
+	return $actions;
+}, 10, 2 );
 
-	$child_entries = $parent_entry->get_child_entries();
-	foreach ( $child_entries as $child_entry ) {
-		if($parent_entry->payment_status !== $child_entry['payment_status'] || $parent_entry->payment_date !== $child_entry['payment_date'] || $parent_entry->transaction_id !== $child_entry['transaction_id']){
-			GFAPI::update_entry_property( $child_entry['id'], 'payment_status', $parent_entry->payment_status );	// Deliberately not syncing payment_amount because the payment_amount from the parent entry will be a total which isn't relevant at the child entry level
-			GFAPI::update_entry_property( $child_entry['id'], 'payment_date', $parent_entry->payment_date );
-			GFAPI::update_entry_property( $child_entry['id'], 'transaction_id', $parent_entry->transaction_id );
+add_action( 'gform_entry_list_action', function( $action, $entries, $form_id ) {
+	if ( $action === 'gpnf_sync_child_entries' ) {
+		foreach ( $entries as $entry_id ) {
+			$entry = GFAPI::get_entry( $entry_id );
+			gpnf_sync_child_entries_payment_details( $entry );
 		}
 	}
+}, 10, 3 );
 
+if ( ! function_exists( 'gpnf_sync_child_entries_payment_details' ) ) {
+	function gpnf_sync_child_entries_payment_details( $parent_entry ) {
+
+		$parent_entry = new GPNF_Entry( $parent_entry );
+		if ( ! $parent_entry->payment_status ) {
+			return;
+		}
+
+		$child_entries = $parent_entry->get_child_entries();
+
+		// "payment_amount" is excluded as the parents total is not relevant on the entry level.
+		$sync_props = array( 'payment_status', 'payment_date', 'transaction_id' );
+
+		foreach ( $child_entries as $child_entry ) {
+			foreach ( $sync_props as $sync_prop ) {
+				if ( $parent_entry->$sync_prop !== $child_entry[ $sync_prop ] ) {
+					GFAPI::update_entry_property( $child_entry['id'], $sync_prop, $parent_entry->$$sync_prop );
+				}
+			}
+		}
+
+	}
+}
+
+if ( ! function_exists( 'gpnf_has_product_field' ) ) {
+	function gpnf_has_product_field( $form ) {
+
+		foreach ( $form['fields'] as $field ) {
+			if ( GFCommon::is_product_field( $field['type'] ) ) {
+				return true;
+			}
+		}
+
+		return false;
+	}
 }
