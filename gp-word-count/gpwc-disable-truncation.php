bf2600a231320f4586c1404f49f050bab9c92930
Samuel Bassah <sbassah@gmail.com>
2022-07-15 14:23:42 +0000

    Disable truncation

---
+++ gp-word-count/gpwc-disable-truncation.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Gravity Perks // Word Count // Disable truncation
+ * https://gravitywiz.com/documentation/gravity-forms-word-count/
+ *
+ * This will prevent the words in a specific field from being truncated when the limit is reached.
+ */
+add_filter( 'gpwc_script_args', function( $args, $field ) {
+	// Update "123" to your form ID and "4" to your Word-Count-enabled field.
+	if ( $field->formId == 123 && $field->id == 4 ) {
+		$args['truncate'] = false;
+	}
+	return $args;
+}, 10, 2 );
