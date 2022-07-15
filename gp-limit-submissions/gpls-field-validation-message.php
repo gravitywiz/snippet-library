c68a85838cc8404f6dad2ef89703f2e24202b2dd
David Smith <david@gravitywiz.com>
2022-07-15 15:17:19 -0400

    Update gpls-field-validation-message.php

--- gp-limit-submissions/gpls-field-validation-message.php
+++ gp-limit-submissions/gpls-field-validation-message.php
@@ -3,6 +3,7 @@
  * Gravity Perks // Limit Submission // Change Validation Message for a Specific Field
  * https://gravitywiz.com/documentation/gravity-forms-limit-submissions/
  */
+// Update "123" to your form ID and "4" to your field ID.
 add_filter( 'gpls_field_failed_validation_123_4', function( $message ) { 
     return 'Example validation message.'; 
 } );
