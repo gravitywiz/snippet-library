0fa9b6efc041736183f9c4e0f073805e9032c713
dariospace <30393370+dariospace@users.noreply.github.com>
2022-07-18 14:01:05 -0300

    Create gpnf-child-entries-separator.php
    added hook

---
+++ gp-nested-forms/gpnf-child-entries-separator.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Gravity Perks // Nested Forms // Set Custom Separator
+ * https://gravitywiz.com/documentation/gravity-forms-nested-forms/
+ *
+ * Use “—” to separate child entries rather than the default <hr>.
+ */
+add_filter( 'gpnf_child_entries_separator', function() {
+    return '---';
+} );
