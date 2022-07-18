c8e270691b6ba5bb7c9185f97dcd6f0e17cb4bab
David Smith <david@gravitywiz.com>
2022-07-18 17:26:08 -0400

    Update gpnf-child-entries-separator.php

--- gp-nested-forms/gpnf-child-entries-separator.php
+++ gp-nested-forms/gpnf-child-entries-separator.php
@@ -3,8 +3,10 @@
  * Gravity Perks // Nested Forms // Set Custom Separator
  * https://gravitywiz.com/documentation/gravity-forms-nested-forms/
  *
- * Use “—” to separate child entries rather than the default <hr>.
+ * By default, an <hr> is used to separate child entries rendered via the {all_fields} merge tag 
+ * and the Nested Form field merge tag. Use this snippet to customize the separator.
  */
 add_filter( 'gpnf_child_entries_separator', function() {
+	// Update this value to any characters or HTML you would like to use as a separator.
 	return '---';
 } );
