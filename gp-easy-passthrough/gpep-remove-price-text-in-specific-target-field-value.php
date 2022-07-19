d635c06d2143d3ada163baba34f857fce094b1af
David Smith <david@gravitywiz.com>
2022-07-19 09:11:35 -0400

    Update gpep-remove-price-text-in-specific-target-field-value.php

--- gp-easy-passthrough/gpep-remove-price-text-in-specific-target-field-value.php
+++ gp-easy-passthrough/gpep-remove-price-text-in-specific-target-field-value.php
@@ -4,6 +4,6 @@
  * https://gravitywiz.com/documentation/gravity-forms-easy-passthrough/
  */
  // Update '123' to the Tartget Form ID and '4' to the Target field ID.
-add_filter('gpep_target_field_value_123_4', function( $field_value ) {
+add_filter( 'gpep_target_field_value_123_4', function( $field_value ) {
 	return preg_replace( '/\s?\(.*?\)$/', '', $field_value );
-});
+} );
