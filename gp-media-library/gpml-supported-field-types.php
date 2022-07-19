dbb4c77dffb41c8a8b758bfb2bef9ab5c01b81e5
David Smith <david@gravitywiz.com>
2022-07-19 09:15:47 -0400

    Update gpml-supported-field-types.php

--- gp-media-library/gpml-supported-field-types.php
+++ gp-media-library/gpml-supported-field-types.php
@@ -3,9 +3,9 @@
  * Gravity Perks // GP Media Library // Filter the supported field types by GP Media Library.
  * https://gravitywiz.com/documentation/gravity-forms-media-library/
  */
- add_filter( 'gpml_supported_field_types', function( $supported_field_types ) {
+add_filter( 'gpml_supported_field_types', function( $supported_field_types ) {
 
-    $supported_field_types[] = 'example_field_type';
+	$supported_field_types[] = 'example_field_type';
 
-    return $supported_field_types;
+	return $supported_field_types;
 } );
