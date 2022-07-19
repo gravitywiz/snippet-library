37c7203a24377caf5b38ec6cbfc6b4c3984f6269
David Smith <david@gravitywiz.com>
2022-07-19 08:43:16 -0400

    Update gpeu-increase-number-of-concurrent-connections.php

--- gp-email-users/gpeu-increase-number-of-concurrent-connections.php
+++ gp-email-users/gpeu-increase-number-of-concurrent-connections.php
@@ -1,11 +1,12 @@
 <?php
 /**
- * Gravity Perks // Email Users // Increase the Number of Concurrent Connections to 10.
+ * Gravity Perks // Email Users // Increase the Number of Concurrent Connections
  * https://gravitywiz.com/documentation/gravity-forms-email-users/
  *
- * The default number of concurrent connection is 5. 
- * You can use this snippet to increase it to 10.
+ * The default number of concurrent connections is `5`. Use this filter to increase the that number. 
+ * Emails will be sent faster but it will create an additional load on your server.
  */
 add_filter('gpeu_connection_threshold', function( $conn_threshold ) {
+	// Update "10" the desired number of concurrent connections.
 	return 10;
-}, 10, 1 );
+} );
