b466335016fbb81937ea6077845a341d0ecd970d
Clay Griffiths <clay@claygriffiths.com>
2022-07-19 15:10:23 -0500

    Added gpdec-delay-for-gravity-pdf-bg-processing.php

---
+++ experimental/gpdec-delay-for-gravity-pdf-bg-processing.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Gravity Perks // Disable Entry Creation // Delay Deletion for Gravity PDF Background Processing
+ * http://gravitywiz.com/documentation/gravity-forms-disable-entry-creation/
+ *
+ * Prevent the deletion of entries until PDFs are generated and attached to notifications. This is necessary if
+ * using Gravity PDF's background processing as the entry will be deleted prior to the PDF being generated since
+ * background processing uses subsequent requests rather than form submission to generate the PDF.
+ *
+ * Installation instructions:
+ *   1. https://gravitywiz.com/documentation/how-do-i-install-a-snippet/
+ *   2. Update FORMID and $form_id accordingly.
+ */
+add_filter( 'gpbua_should_delete_entry_FORMID', '__return_false' );
+
+add_action( 'gfpdf_post_generate_and_save_pdf_notification', function ( $form, $entry, $settings, $notifications ) {
+	$form_id = 8;
+
+	if ( ! function_exists( 'gp_disable_entry_creation' ) ) {
+		return;
+	}
+
+	if ( $form['id'] !== $form_id ) {
+		return;
+	}
+
+	gp_disable_entry_creation()->delete_form_entry( $entry );
+}, 50, 4 );
