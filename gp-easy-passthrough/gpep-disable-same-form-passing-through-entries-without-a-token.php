67b07db59e0aa80efee041c3bda984931dc07608
Samuel Bassah <sbassah@gmail.com>
2022-07-19 15:09:25 +0000

    Disable Same Form Passthrough Without a Token
    * Disable Same Form Passthrough Without a Token
    
    * Update gpep-disable-same-form-passing-through-entries-without-a-token.php
    
    Co-authored-by: David Smith <david@gravitywiz.com>

---
+++ gp-easy-passthrough/gpep-disable-same-form-passing-through-entries-without-a-token.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Gravity Perks // Easy Passthrough // Disable Same Form Passthrough Without a Token
+ * https://gravitywiz.com/documentation/gravity-forms-easy-passthrough/
+ *
+ * This example will prevent GPEP from continuously passing through entries if the `ep_token` is
+ * not present in the URL and the source and target forms are the same.
+ */
+add_filter('gpep_disable_same_form_passthrough', function( $disable ) {
+	if ( ! rgget( 'ep_token' ) ) {
+		$disable = true;
+	}
+	return $disable;
+});
