6acd772feb3d9d1331e222e4d5b770115d33a4de
dariospace <30393370+dariospace@users.noreply.github.com>
2022-07-19 11:44:17 -0300

    Create gpnf-fetch-form-html-after-add.js
    added hook

---
+++ gp-nested-forms/gpnf-fetch-form-html-after-add.js
@@ -0,0 +1,15 @@
+?php
+/**
+ * Gravity Perks // Nested Forms // Disable automatically loading child form HTML after child entries are added
+ * https://gravitywiz.com/documentation/gravity-forms-nested-forms/
+ *
+ * Instructions:
+ *     1. Install our free Custom Javascript for Gravity Forms plugin.
+ *        Download the plugin here: https://gravitywiz.com/gravity-forms-custom-javascript/
+ *     2. Copy and paste the snippet into the editor of the Custom Javascript for Gravity Forms plugin.
+ */
+ <script>
+    window.gform.addFilter('gpnf_fetch_form_html_after_add', function () {
+        return false;
+    });
+</script>
