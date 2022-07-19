74c979bd9d3dc965a6a2166c7f213bd08cf2d70c
David Smith <david@gravitywiz.com>
2022-07-19 13:50:38 -0400

    Update gpfup-scale-based-on-dimension.js

--- gp-file-upload-pro/gpfup-scale-based-on-dimension.js
+++ gp-file-upload-pro/gpfup-scale-based-on-dimension.js
@@ -4,10 +4,10 @@
  *
  * Scale uploaded images instead of cropping, based on dimensions set in the cropping settings.
  *
- * * Instructions:
- *     1. Install our free Custom Javascript for Gravity Forms plugin.
- *        Download the plugin here: https://gravitywiz.com/gravity-forms-custom-javascript/
- *     2. Copy and paste the snippet into the editor of the Custom Javascript for Gravity Forms plugin.
+ * Instructions:
+ *   1. Install our free Custom Javascript for Gravity Forms plugin.
+ *      Download the plugin here: https://gravitywiz.com/gravity-forms-custom-javascript/
+ *   2. Copy and paste the snippet into the editor of the Custom Javascript for Gravity Forms plugin.
  */
 gform.addFilter( 'gpfup_image_loader_options', function( options ) {
 	options.crop = false;
