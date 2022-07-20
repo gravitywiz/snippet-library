d1ef8f351de02e7bd2397f108ea89d1f061b4384
David Smith <david@gravitywiz.com>
2022-07-20 11:44:01 -0400

    Update gpls-field-failed-validation.php

--- gp-limit-submissions/gpls-field-failed-validation.php
+++ gp-limit-submissions/gpls-field-failed-validation.php
@@ -3,4 +3,5 @@
  * Gravity Perks // Limit Submission // Prevent GP Limit Submission Validation Errors From Showing On Fields
  * https://gravitywiz.com/documentation/gravity-forms-limit-submissions/
  */
+// Update "123" to your form ID.
 add_filter( 'gpls_field_failed_validation_123', '__return_false' );
