3288f3776442b2b55ed49389f26954ade1eea539
dariospace <30393370+dariospace@users.noreply.github.com>
2022-07-20 12:29:18 -0300

    Update gpnf-jquery-ui-dependencies.php

--- gp-nested-forms/gpnf-jquery-ui-dependencies.php
+++ gp-nested-forms/gpnf-jquery-ui-dependencies.php
@@ -1,11 +1,10 @@
 /**
  * Gravity Perks // Nested Forms // Populate Parent Form ID in Child Form
  * https://gravitywiz.com/documentation/gravity-forms-nested-forms/
- *
  */
  add_filter( 'gpnf_jquery_ui_dependencies', function( $deps ) {
-    
+
     $deps[] = 'my-custom-script-handle';
-    
+
     return $deps;
 } );
