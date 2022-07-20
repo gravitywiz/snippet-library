2d48f424519fdc83ba06818f51889597a9c80444
David Smith <david@gravitywiz.com>
2022-07-20 09:11:45 -0400

    Update gpnf-export-child-field-header.php

--- gp-nested-forms/gpnf-export-child-field-header.php
+++ gp-nested-forms/gpnf-export-child-field-header.php
@@ -8,11 +8,8 @@ add_filter( 'gpnf_export_child_field_header', function( $header, $form, $field, 
 	$parent_label = $field->get_field_label( false, null );
 
 	if ( empty( $parent_label ) ) {
-	
 		$header = $child_field->get_field_label( false, null );
-
 	} else {
-
 		$header = sprintf( '%s / %s', $parent_label, $child_field->get_field_label( false, null ) );
 	}
 
