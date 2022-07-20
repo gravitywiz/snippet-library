0d6981008557dcbf15f02504f66873a90ab8475e
dariospace <30393370+dariospace@users.noreply.github.com>
2022-07-20 12:30:09 -0300

    Create gpnf-export-child-field-header.php (#384)
    * Create gpnf-export-child-field-header.php
    
    added hook
    
    * Update gpnf-export-child-field-header.php
    
    * Update gpnf-export-child-field-header.php
    
    Co-authored-by: David Smith <david@gravitywiz.com>

---
+++ gp-nested-forms/gpnf-export-child-field-header.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Gravity Perks // Nested Forms // Only show child field labels when Nested Form field has no label
+ * https://gravitywiz.com/documentation/gravity-forms-nested-forms/
+ */
+add_filter( 'gpnf_export_child_field_header', function( $header, $form, $field, $child_field ) {
+
+	$parent_label = $field->get_field_label( false, null );
+
+	if ( empty( $parent_label ) ) {
+		$header = $child_field->get_field_label( false, null );
+	} else {
+		$header = sprintf( '%s / %s', $parent_label, $child_field->get_field_label( false, null ) );
+	}
+
+	return $header;
+}, 10, 4 );
