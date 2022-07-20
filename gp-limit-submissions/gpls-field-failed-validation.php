2828581f21605bb39ffc434c626f831bba089222
dariospace <30393370+dariospace@users.noreply.github.com>
2022-07-20 14:00:05 -0300

    Create gpls-field-failed-validation (#361)
    * Create gpls-field-failed-validation
    
    added hook
    
    * Update gpls-field-failed-validation
    
    added php opening tag
    
    * Rename gpls-field-failed-validation to gpls-field-failed-validation.php
    
    modified file extension
    
    * Update gpls-field-failed-validation.php
    
    * Update gpls-field-failed-validation.php
    
    * Update gpls-field-failed-validation.php
    
    Co-authored-by: David Smith <david@gravitywiz.com>

---
+++ gp-limit-submissions/gpls-field-failed-validation.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Gravity Perks // Limit Submission // Prevent Validation Errors From Showing On Fields
+ * https://gravitywiz.com/documentation/gravity-forms-limit-submissions/
+ */
+// Update "123" to your form ID.
+add_filter( 'gpls_field_failed_validation_123', '__return_false' );
