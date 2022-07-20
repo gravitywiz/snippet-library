b61aee378de8204c896b3c3654af62fa923c1f8d
David Smith <david@gravitywiz.com>
2022-07-20 09:14:52 -0400

    Update gpfup-set-jpeg-quality-for-uplodaed-images.js

--- gp-file-upload-pro/gpfup-set-jpeg-quality-for-uplodaed-images.js
+++ gp-file-upload-pro/gpfup-set-jpeg-quality-for-uplodaed-images.js
@@ -7,7 +7,7 @@
  *      Download the plugin here: https://gravitywiz.com/gravity-forms-custom-javascript/
  *   2. Copy and paste the snippet into the editor of the Custom Javascript for Gravity Forms plugin.
  */
- window.gform.addFilter('gpfup_jpeg_quality', function () {
-   // Update '1' to the desired quality. 1 represents 100%.
-        return 1;
-    });
+window.gform.addFilter( 'gpfup_jpeg_quality', function () {
+	// Update '1' to the desired quality. 1 represents 100%.
+	return 1;
+} );
