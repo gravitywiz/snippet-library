c72f87ed4ee4ae185a74422247fd2965da625faf
David Smith <david@gravitywiz.com>
2022-07-20 11:45:09 -0400

    Update gpnf-child-entry-max.php

--- gp-nested-forms/gpnf-child-entry-max.php
+++ gp-nested-forms/gpnf-child-entry-max.php
@@ -3,6 +3,6 @@
  * Gravity Perks // Nested Forms // Double The Default Child Entry Max
  * https://gravitywiz.com/documentation/gravity-forms-nested-forms/
  */
-add_filter( 'gpnf_child_entry_max', function( $child_entry_max ) { 
-    return 200;
+add_filter( 'gpnf_child_entry_max', function( $child_entry_max ) {
+	return 200;
 } );
