69bf9b41a300f156f0adc12b5a2bdeda3dc32ab4
Clay Griffiths <clay@claygriffiths.com>
2022-07-21 09:20:03 -0500

    Intentional PHPCS error

--- experimental/gpbua-enable-block-editor.php
+++ experimental/gpbua-enable-block-editor.php
@@ -5,6 +5,6 @@
  */
 add_action( 'init', function() {
 	if ( is_callable( 'gp_better_user_activation' ) ) {
-		remove_filter( 'use_block_editor_for_post', array( gp_better_user_activation(), 'disable_block_editor_for_activation_page' ), 101 );
+		remove_filter(  'use_block_editor_for_post', array( gp_better_user_activation(), 'disable_block_editor_for_activation_page' ), 101 );
 	}
 }, 16 );
