343706770b6792bcacbca09fd1554f64ae496f50
David Smith <david@gravitywiz.com>
2022-07-21 08:54:31 -0400

    Update gpi-set-inventory-limit-by-custom-field.php

--- gp-inventory/gpi-set-inventory-limit-by-custom-field.php
+++ gp-inventory/gpi-set-inventory-limit-by-custom-field.php
@@ -3,8 +3,8 @@
  * Gravity Perks // Inventory // Set Inventory Limit by Custom Field
  * https://gravitywiz.com/documentation/gravity-forms-inventory/
  *
- * This filter sets the inventory of an Advanced resource dynamically
- * based on the inventory custom field of the post/page on which the form is embedded.
+ * This filter sets the inventory of an Advanced resource dynamically based on the
+ * inventory custom field of the post/page on which the form is embedded.
  */
 add_filter( 'gpi_inventory_limit_advanced', function() {
 	// Update 'inventory' to the custom field meta key.
