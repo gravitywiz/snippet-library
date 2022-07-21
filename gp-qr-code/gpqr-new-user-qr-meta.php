86adadc3987dffe4ce2674ea71eef455d7362595
David Smith <david@gravitywiz.com>
2022-07-21 08:56:23 -0400

    Update gpqr-new-user-qr-meta.php
    ~ PHPCS

--- gp-qr-code/gpqr-new-user-qr-meta.php
+++ gp-qr-code/gpqr-new-user-qr-meta.php
@@ -6,6 +6,6 @@
 // Update "user_register" to "gform_user_registered" to limit QR generation only to users created via a Gravity Form.
 add_action( 'user_register', function( $user_id ) {
 	if ( is_callable( 'gp_qr_code' ) ) {
-		add_user_meta( $user_id,'qr_code', gp_qr_code()->generator->generate( (string) $user_id ) );
+		add_user_meta( $user_id, 'qr_code', gp_qr_code()->generator->generate( (string) $user_id ) );
 	}
 } );
