157d0b4e3f2f57914f52ba1713e36d509aebb943
dariospace <30393370+dariospace@users.noreply.github.com>
2022-07-22 12:52:46 -0300

    Create gppr-price-range-min.php
    added hook

---
+++ gp-price-range/gppr-price-range-min.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Gravity Perks // GP Price Range // Modify The Minimum Price Range Based On A Field Value
+ * http://gravitywiz.com/documentation/gp-price-range/
+ */
+// Update "123" to your form ID and "4" to your field ID.
+add_filter( 'gppr_price_range_min_123_4', function( $min ) {
+
+    // Update "5" to the field ID whose value should be used as the minimum price range.
+	
+    $source_field_id = 5;
+    
+    return $_POST[ 'input_' . $source_field_id ];
+} );
