9a8aad41437f8253099a1689018880c80ee9496d
dariospace <30393370+dariospace@users.noreply.github.com>
2022-07-22 12:42:58 -0300

    Create gppr-price-range-min-max.php
    added hook

---
+++ gp-price-range/gppr-price-range-min-max.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Gravity Perks // GP Price Range // Set a Variable min/max Range Based On A Field Value
+ * http://gravitywiz.com/documentation/gp-price-range/
+ *
+ * Take a base value and calculate a range based on that value
+ */
+// Update "123" to your form ID and "4" to your field ID.
+add_filter( 'gppr_price_range_min_123_4', 'gw_variable_price_range' );
+add_filter( 'gppr_price_range_max_123_4', 'gw_variable_price_range' );
+function gw_variable_price_range( $value ) {
+	// Update "5" to the field ID whose value should be used as the minimum price range.
+	$source_field_id = 5;
+	$base_value = (int) rgpost( 'input_' . $source_field_id );
+	$is_min = strpos( current_filter(),'min' ) !== false;
+	$range  = 50;
+
+	if ( $is_min ) {
+		$value = $base_value - $range;
+	} else {
+		$value = $base_value + $range;
+	}
+
+	return $value;
+}
