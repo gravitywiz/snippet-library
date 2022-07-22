22ddc007795dc2bcc44147d3a39b0b9bfa237d3d
Samuel Bassah <sbassah@gmail.com>
2022-07-22 16:01:19 +0000

    Share limit between two different choices on the same field
    * Share Limit Between two Different Choices on the same field
    
    * Update gplc-share-limit-between-two-different-choices.php
    
    Co-authored-by: David Smith <david@gravitywiz.com>

---
+++ gp-limit-choices/gplc-share-limit-between-two-different-choices.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Gravity Perks // Limit Choices // Share Limit Between Two Different Choices on the Same Field
+ * https://gravitywiz.com/documentation/gravity-forms-limit-choices/
+ */
+// Update the "123" to your form ID and "4" to your field ID.
+add_filter( 'gplc_choice_counts_123_4', function( $counts, $form, $field ) {
+	// Update "First Choice" and "Second Choice" to the values of the two different choices.
+	$counts['First Choice'] += $counts['Second Choice'];
+	$counts['Second Choice'] = $counts['First Choice'];
+	return $counts;
+}, 10, 3 );
