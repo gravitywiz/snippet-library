0c450ce8176528f82b12eb652240db3458410cca
David Smith <david@gravitywiz.com>
2022-07-22 09:25:43 -0400

    Update gplc-override-limit-of-specific-choice.php

--- gp-limit-choices/gplc-override-limit-of-specific-choice.php
+++ gp-limit-choices/gplc-override-limit-of-specific-choice.php
@@ -3,7 +3,7 @@
  * Gravity Perks // Limit Choices // Override Limit of Specific Choice
  * https://gravitywiz.com/documentation/gravity-forms-limit-choices/
  */
-// Update '123' to the form ID and '4' to the field ID.
+// Update "123" to the form ID and "4" to the field ID.
 add_filter( 'gplc_choice_limit_123_4', function( $limit, $choice ) {
 	if ( $choice['value'] === 'First Choice' ) {
 		// Update "1" to the desired limit.
