7aa649f0d214300102a69aefd3497e006c6d5fc9
dariospace <30393370+dariospace@users.noreply.github.com>
2022-07-26 11:41:00 -0300

    Update gppr-price-range-min-max.php

--- gp-price-range/gppr-price-range-min-max.php
+++ gp-price-range/gppr-price-range-min-max.php
@@ -12,7 +12,7 @@ function gw_variable_price_range( $value ) {
 	// Update "5" to the field ID whose value should be used as the minimum price range.
 	$source_field_id = 5;
 	$base_value = (int) rgpost( 'input_' . $source_field_id );
-	$is_min = strpos( current_filter(),'min' ) !== false;
+	$is_min = strpos( current_filter(), 'min' ) !== false;
 	$range  = 50;
 
 	if ( $is_min ) {
