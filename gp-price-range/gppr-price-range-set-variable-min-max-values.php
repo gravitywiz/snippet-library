c8a3b631ccc4a54b0361a0b225516c03a4600fad
David Smith <david@gravitywiz.com>
2022-07-26 13:39:47 -0400

    Update gppr-price-range-set-variable-min-max-values.php

--- gp-price-range/gppr-price-range-set-variable-min-max-values.php
+++ gp-price-range/gppr-price-range-set-variable-min-max-values.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * Gravity Perks // GP Price Range // Set a Variable min/max Range Based On A Field Value
- * http://gravitywiz.com/documentation/gp-price-range/
+ * Gravity Perks // GP Price Range // Set a Variable Min/Max Range Based On A Field Value
+ * https://gravitywiz.com/documentation/gravity-forms-price-range/
  *
- * Take a base value and calculate a range based on that value
+ * Take a base value and calculate a minimum and maximum range based on that value.
  */
 // Update "123" to your form ID and "4" to your field ID.
 add_filter( 'gppr_price_range_min_123_4', 'gw_variable_price_range' );
