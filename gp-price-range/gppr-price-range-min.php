9c1d6425697ae100e73dbdcfe76e99fa1e0e3139
dariospace <30393370+dariospace@users.noreply.github.com>
2022-07-26 11:40:26 -0300

    Update gppr-price-range-min.php
    fixed phpcs errors

--- gp-price-range/gppr-price-range-min.php
+++ gp-price-range/gppr-price-range-min.php
@@ -6,9 +6,9 @@
 // Update "123" to your form ID and "4" to your field ID.
 add_filter( 'gppr_price_range_min_123_4', function( $min ) {
 
-    // Update "5" to the field ID whose value should be used as the minimum price range.
-	
-    $source_field_id = 5;
-    
-    return $_POST[ 'input_' . $source_field_id ];
+	// Update "5" to the field ID whose value should be used as the minimum price range.
+
+	$source_field_id = 5;
+
+	return $_POST[ 'input_' . $source_field_id ];
 } );
