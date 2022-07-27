2f5a5b24e92026057f803c770fd6d9ce7b3925b8
Samuel Bassah <sbassah@gmail.com>
2022-07-27 20:28:11 +0000

    Automatically Show Datepicker for Linked Date Field (Minimum Date)
    * Automatically Show Datepicker for Linked Date Field (Minimum Date)
    
    * Update gpld-automatically-show-datepicker-for-linked-date-field-min.js
    
    * Update gpld-automatically-show-datepicker-for-linked-date-field-min.js
    
    * Update gpld-automatically-show-datepicker-for-linked-date-field-min.js
    
    Co-authored-by: David Smith <david@gravitywiz.com>

---
+++ gp-limit-dates/gpld-automatically-show-datepicker-for-linked-date-field-min.js
@@ -0,0 +1,16 @@
+/**
+ * Gravity Perks // Limit Dates // Automatically Show Datepicker for Linked Date Field (Minimum Date)
+ * https://gravitywiz.com/documentation/gravity-forms-limit-dates/
+ *
+ * When a date selected in Field A modifies the minimum date in Field B,
+ * this snippet will automatically open the datepicker in Field B after
+ * the date has been selected in Field A.
+ *
+ * Instructions:
+ * 
+ * 1. Install this snippet with our free Custom JavaScript plugin.
+ *    https://gravitywiz.com/gravity-forms-custom-javascript/
+ */
+gform.addAction( 'gpld_after_set_min_date', function( $input, date ) { 
+	$input.datepicker( 'show' );
+} );
