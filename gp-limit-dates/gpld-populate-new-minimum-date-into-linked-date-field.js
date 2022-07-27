796921211a4be9caddff02ab1ece7c5297e0be0d
Samuel Bassah <sbassah@gmail.com>
2022-07-27 20:31:52 +0000

    Populate New Minimum Date into Linked Date Field
    * Populate New Minimum Date into Linked Date Field
    
    * Update gpld-populate-new-minimum-date-into-linked-date-field.js
    
    * Update gpld-populate-new-minimum-date-into-linked-date-field.js

---
+++ gp-limit-dates/gpld-populate-new-minimum-date-into-linked-date-field.js
@@ -0,0 +1,15 @@
+/**
+ * Gravity Perks // Limit Dates // Populate the New Minimum Date into Linked Date Field
+ * https://gravitywiz.com/documentation/gravity-forms-limit-dates/
+ *
+ * When Field B's minimum date is dependent on the selected date in Field A,
+ * automatically populate the minimum date into Field B.
+ *
+ * Instructions:
+ *
+ * 1. Install this snippet with our free Custom JavaScript plugin.
+ *    https://gravitywiz.com/gravity-forms-custom-javascript/
+ */
+ gform.addAction( 'gpld_after_set_min_date', function( $input, date ) {
+ 	$input.datepicker( 'setDate', date );
+} );
