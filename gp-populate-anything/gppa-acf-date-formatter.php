ccad0658c2a1d8c359c8f5055e81a90dc0b1a18b
David Smith <david@gravitywiz.com>
2022-07-28 13:14:17 -0400

    Update gppa-acf-date-formatter.php

--- gp-populate-anything/gppa-acf-date-formatter.php
+++ gp-populate-anything/gppa-acf-date-formatter.php
@@ -2,6 +2,12 @@
 /**
  * Gravity Perks // Populate Anything // Convert ACF Date Into Gravity Forms Date
  * https://gravitywiz.com/documentation/gravity-forms-populate-anything/
+ * 
+ * Instructions:
+ *
+ * 1. Download and install as a plugin.
+ * 2. Go to the field settings for the Gravity Forms Date field you are populating from the ACF Date field.
+ * 3. Add "gppa-format-acf-date" to the "Custom CSS Class" setting (on the Appearance tab).
  *
  * Plugin Name:  GPPA ACF Date to GF Date
  * Plugin URI:   http:///gravitywiz.com.com/documentation/gravity-forms-populate-anything/
@@ -9,14 +15,12 @@
  * Author:       Gravity Wiz
  * Version:      0.1
  * Author URI:   http://gravitywiz.com
- *
- * Add "gppa-format-acf-date" to the "Custom CSS Class" setting (on the Appearance tab).
  */
-// Update "123" to your form ID.
-add_filter( 'gppa_process_template_value_123', function( $template_value, $field, $template_name, $populate, $object, $object_type, $objects ) {
+add_filter( 'gppa_process_template_value', function( $template_value, $field, $template_name, $populate, $object, $object_type, $objects ) {
 
     if ( strpos( $field->cssClass, 'gppa-format-acf-date' ) === false ) {
         return $template_value;
     }
+    
     return date( 'd/m/Y', strtotime( $template_value ) );
 }, 10, 7 );
