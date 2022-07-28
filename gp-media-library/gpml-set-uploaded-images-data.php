408b62b466d92c56d5c5a8cee236ba0e81ff98b6
Samuel Bassah <sbassah@gmail.com>
2022-07-28 11:54:25 +0000

    Set Uploaded Image Data

---
+++ gp-media-library/gpml-set-uploaded-images-data.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Gravity Perks // GP Media Library // Set Uploaded Image Data
+ * https://gravitywiz.com/documentation/gravity-forms-media-library/
+ */
+add_filter( 'gpml_media_data', function( $media, $field, $entry ) {
+
+	$media['post_data']['post_title']                            = 'Boom!';
+	$media['post_data']['post_content']                          = 'This is the description.';
+	$media['post_data']['post_excerpt']                          = 'This is the caption.';
+	$media['post_data']['post_meta']['_wp_attachment_image_alt'] = 'This is the alt text.';
+
+	return $media;
+}, 10, 3 );
