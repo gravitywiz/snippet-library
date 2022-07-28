8215a27a6d11f2fad534bee473b33ccdbaaa7d88
Samuel Bassah <sbassah@gmail.com>
2022-07-28 12:12:47 +0000

    Update gpml-set-custom-class-for-linked-image.php

--- gp-media-library/gpml-set-custom-class-for-linked-image.php
+++ gp-media-library/gpml-set-custom-class-for-linked-image.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Gravity Perks // GP Media Library // Set a Custom Class For the Linked Image
+ * Gravity Perks // Media Library // Set a Custom Class For the Linked Image
  * https://gravitywiz.com/documentation/gravity-forms-media-library/
  */
 add_filter( 'gpml_image_merge_tag_link_atts', function( $link_atts ) {
