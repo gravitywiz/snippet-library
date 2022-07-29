4c5e5e74b806fbf2bc5c6282bc0e1fdb845ed935
David Smith <david@gravitywiz.com>
2022-07-29 16:46:06 -0400

    Update gppa-modify-template-value.php

--- gp-populate-anything/gppa-modify-template-value.php
+++ gp-populate-anything/gppa-modify-template-value.php
@@ -2,7 +2,7 @@
 /**
  * Gravity Perks // GP Populate Anything // Modify Template Value
  * https://gravitywiz.com/documentation/gravity-forms-populate-anything/
- *
+ */
 add_filter( 'gppa_process_template_value', function( $template_value, $field, $template_name, $populate, $object, $object_type, $objects ) {
 
 	// Update "123" to your form ID and "4" to your field ID.
