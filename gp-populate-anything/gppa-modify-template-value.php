e1b14999e5e197af190e9111c8fd3031262196b1
David Smith <david@gravitywiz.com>
2022-07-29 16:47:51 -0400

    Update gppa-modify-template-value.php

--- gp-populate-anything/gppa-modify-template-value.php
+++ gp-populate-anything/gppa-modify-template-value.php
@@ -6,7 +6,7 @@
 add_filter( 'gppa_process_template_value', function( $template_value, $field, $template_name, $populate, $object, $object_type, $objects ) {
 
 	// Update "123" to your form ID and "4" to your field ID.
-	if( $field->formId == 123 and $field->id == 4 ) {
+	if ( $field->formId == 123 and $field->id == 4 ) {
 		$template_value = 'My modified value.';
 	}
 
