0019b332f88e3c3d7b7a1fd29b4470d64d571462
David Smith <david@gravitywiz.com>
2022-07-29 09:26:17 -0400

    Update gpml-set-custom-class-for-linked-image.php

--- gp-media-library/gpml-set-custom-class-for-linked-image.php
+++ gp-media-library/gpml-set-custom-class-for-linked-image.php
@@ -4,8 +4,7 @@
  * https://gravitywiz.com/documentation/gravity-forms-media-library/
  */
 add_filter( 'gpml_image_merge_tag_link_atts', function( $link_atts ) {
-	//Update "custom-class" to your preferred class name.
+	// Update "custom-class" to your preferred class name.
 	$link_atts['class'] = 'custom-class';
-
 	return $link_atts;
 }, 10, 3);
