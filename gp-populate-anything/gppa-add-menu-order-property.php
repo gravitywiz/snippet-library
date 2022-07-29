dad27328fe080d71c7b1f9073299f158b2e14216
David Smith <david@gravitywiz.com>
2022-07-29 16:45:38 -0400

    Update gppa-add-menu-order-property.php

--- gp-populate-anything/gppa-add-menu-order-property.php
+++ gp-populate-anything/gppa-add-menu-order-property.php
@@ -2,15 +2,15 @@
 /**
  * Gravity Perks // GP Populate Anything // Add Menu Order Property for Post Object
  * https://gravitywiz.com/documentation/gravity-forms-populate-anything/
- *
+ */
 add_filter( 'gppa_object_type_properties_post', function( $props ) {
 	global $wpdb;
-		$props['menu_order'] = array(
-			'label'    => esc_html__( 'Menu Order', 'gp-populate-anything' ),
-			'value'    => 'menu_order',
-			'callable' => array( gp_populate_anything()->get_object_type( 'post' ), 'get_col_rows' ),
-			'args'     => array( $wpdb->posts, 'menu_order' ),
-			'orderby'  => true,
-		);
+	$props['menu_order'] = array(
+		'label'    => esc_html__( 'Menu Order', 'gp-populate-anything' ),
+		'value'    => 'menu_order',
+		'callable' => array( gp_populate_anything()->get_object_type( 'post' ), 'get_col_rows' ),
+		'args'     => array( $wpdb->posts, 'menu_order' ),
+		'orderby'  => true,
+	);
 	return $props;
 } );
