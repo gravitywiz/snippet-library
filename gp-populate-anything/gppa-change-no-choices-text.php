e0d374cc0c4a231a6c46911eca681e50b51b2279
David Smith <david@gravitywiz.com>
2022-08-01 12:06:26 -0400

    Update and rename gppa-gppa-change-no-choices-text.php to gppa-change-no-choices-text.php

--- gp-populate-anything/gppa-change-no-choices-text.php
+++ gp-populate-anything/gppa-change-no-choices-text.php
@@ -3,7 +3,7 @@
  * Gravity Perks // GP Populate Anything // Change No Choices Text
  * https://gravitywiz.com/documentation/gravity-forms-populate-anything/
  */
-// Replace 123 with your Form Id and 4 with your Field Id
+// Replace "123" with your form ID and "4" with your field ID.
 add_filter( 'gppa_no_choices_text', function ( $label, $field ) {
 	if ( $field->formId == 123 and $field->id == 4 ) {
 		$label = 'No Post(s) Found';
