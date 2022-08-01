0c24e5de465d088111aff1a483c0c4e395c56f79
dariospace <30393370+dariospace@users.noreply.github.com>
2022-08-01 13:08:36 -0300

    Update gppa-include-all-results-as-list.php

--- gp-populate-anything/gppa-include-all-results-as-list.php
+++ gp-populate-anything/gppa-include-all-results-as-list.php
@@ -3,7 +3,8 @@
  * Gravity Perks // Populate Anything // Include All Query Results In Value As Comma-delimited List
  * https://gravitywiz.com/documentation/gravity-forms-populate-anything/
  */
-add_filter( 'gppa_get_input_values_FORMID_FIELDID', function ( $value, $field, $template, $objects ) {
+// Replace 123 with your form ID and 4 with your field ID
+add_filter( 'gppa_get_input_values_123_4', function ( $value, $field, $template, $objects ) {
 	$processed_values = array();
 	foreach ( $objects as $object ) {
 		$processed_values[] = gp_populate_anything()->process_template( $field, $template, $object, 'values', $objects );
