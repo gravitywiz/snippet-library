c2dd2e2f3e718750a418690490e1f2c9c8f93b2b
Samuel Bassah <sbassah@gmail.com>
2022-08-01 15:44:11 +0000

    Added support to check for Form ID

--- gp-date-time-calculator/gpdtc-change-max-delta.php
+++ gp-date-time-calculator/gpdtc-change-max-delta.php
@@ -7,8 +7,8 @@
  * and the backend calculation result before triggering a warning e-mail and/or note.
  */
 add_filter( 'gpdtc_warning_max_delta', function( $delta, $result, $formula, $field, $form, $entry ) {
-	// Update "4" to the ID of the Number Field with the Date Time Calculations.
-	if ( $field->id != 4 ) {
+	// Update "123" to the Form ID and "4" to the ID of the Number Field with the Date Time Calculations.
+	if ( $field->formId !== 123 || $field->id != 4 ) {
 		return $delta;
 	}
 	// Update "0.0002" to your preferred max delta value.
