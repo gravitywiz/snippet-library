fd451f3ed6bfafd2ec5d6084e488441390a5afed
Samuel Bassah <sbassah@gmail.com>
2022-08-01 13:56:03 +0000

    Upload the Original Image and the Thumbnail

---
+++ gp-media-library/gpml-upload-the-original-image-and-thumbnail.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Gravity Perks // Media Library // Upload the Original Image and the Thumbnail
+ * https://gravitywiz.com/documentation/gravity-forms-media-library/
+ */
+add_filter( 'gpml_image_sizes', function ( $sizes ) {
+	//Removing the “thumbnail” will only upload the original image.
+	return array( 'thumbnail' );
+} );
