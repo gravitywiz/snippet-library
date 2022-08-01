7196ea2510414c2da5901d7f08d8d3158f9614c7
dariospace <30393370+dariospace@users.noreply.github.com>
2022-08-01 13:10:46 -0300

    Update gpui-set-minimum-numeric-length.php
    fixed phpcs erros

--- gp-unique-id/gpui-set-minimum-numeric-length.php
+++ gp-unique-id/gpui-set-minimum-numeric-length.php
@@ -3,7 +3,6 @@
  * Gravity Perks // Unique ID // Set Minimum Numeric Unique ID Length
  * https://gravitywiz.com/documentation/gravity-forms-unique-id/
  */
-// Replace 3 with the minimum length
- add_filter( 'gpui_numeric_minimum_length', function() { 
-	return 3; 
+add_filter( 'gpui_numeric_minimum_length', function() { 
+	return 3;
 } );
