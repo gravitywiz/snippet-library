2923407401b051312bcf1bd5af92685f7e699c0d
Samuel Bassah <sbassah@gmail.com>
2022-08-01 15:41:08 +0000

    Change Max Delta

---
+++ gp-date-time-calculator/gpdtc-change-max-delta.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Gravity Perks // Date Time Calculator // Change Max Delta
+ * https://gravitywiz.com/documentation/gravity-forms-date-time-calculator/
+ *
+ * Control the max delta that’s allowable between the frontend calculation result
+ * and the backend calculation result before triggering a warning e-mail and/or note.
+ */
+add_filter( 'gpdtc_warning_max_delta', function( $delta, $result, $formula, $field, $form, $entry ) {
+	// Update "4" to the ID of the Number Field with the Date Time Calculations.
+	if ( $field->id != 4 ) {
+		return $delta;
+	}
+	// Update "0.0002" to your preferred max delta value.
+	return 0.0002;
+}, 10, 6 );
