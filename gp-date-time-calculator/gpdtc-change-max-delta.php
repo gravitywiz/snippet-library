273b373f9213e65ad6e4a3410f9f642e3c5c16be
David Smith <david@gravitywiz.com>
2022-08-02 09:18:46 -0400

    Update gpdtc-change-max-delta.php

--- gp-date-time-calculator/gpdtc-change-max-delta.php
+++ gp-date-time-calculator/gpdtc-change-max-delta.php
@@ -3,11 +3,11 @@
  * Gravity Perks // Date Time Calculator // Change Max Delta
  * https://gravitywiz.com/documentation/gravity-forms-date-time-calculator/
  *
- * Control the max delta that’s allowable between the frontend calculation result
- * and the backend calculation result before triggering a warning e-mail and/or note.
+ * Control the max delta that’s allowable between the frontend calculation result and the backend calculation
+ * result before triggering a warning email and/or note.
  */
 add_filter( 'gpdtc_warning_max_delta', function( $delta, $result, $formula, $field, $form, $entry ) {
-	// Update "123" to the Form ID and "4" to the ID of the Number Field with the Date Time Calculations.
+	// Update "123" to the form ID and "4" to the ID of the Number field with the Date Time calculations.
 	if ( $field->formId !== 123 || $field->id != 4 ) {
 		return $delta;
 	}
