8685ebeec3ca36b71e633bb5a72fb3abc6a436bf
dariospace <30393370+dariospace@users.noreply.github.com>
2022-08-02 12:27:40 -0300

    Create gppa-add-a-custom-choice-template-to-choices.php
    added hook

---
+++ gp-populate-anything/gppa-add-a-custom-choice-template-to-choices.php
@@ -0,0 +1,21 @@
+/**
+ * Gravity Perks // Populate Anything // Add A Custom Choice Template To Choices
+ * https://gravitywiz.com/documentation/gravity-forms-populate-anything/
+ */
+add_action( 'admin_print_footer_scripts', function () {
+	if ( ! is_callable( 'GFCommon::is_form_editor' ) || ! GFCommon::is_form_editor() ) {
+		return;
+	}
+	?>
+    <script>
+        window.gform.addFilter('gppa_template_rows', function (templateRows, field, populate) {
+            if (populate !== 'choices') return templateRows;
+				templateRows.push({
+					id: 'image',
+					label: 'Image',
+				})
+            return templateRows;
+        });
+    </script>
+	<?php
+} );
