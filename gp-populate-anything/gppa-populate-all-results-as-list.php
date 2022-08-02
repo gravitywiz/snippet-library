757307b0a8fb2ec44314e13a1121361939ca47c8
David Smith <david@gravitywiz.com>
2022-08-02 09:24:12 -0400

    Update and rename gppa-include-all-results-as-list.php to gppa-populate-all-results-as-list.php

--- gp-populate-anything/gppa-populate-all-results-as-list.php
+++ gp-populate-anything/gppa-populate-all-results-as-list.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * Gravity Perks // Populate Anything // Include All Query Results In Value As Comma-delimited List
+ * Gravity Perks // Populate Anything // Populate All Query Results As Comma-delimited List
  * https://gravitywiz.com/documentation/gravity-forms-populate-anything/
  */
-// Replace "123" with your form ID and "4" with your field ID
+// Replace "123" with your form ID and "4" with your field ID.
 add_filter( 'gppa_get_input_values_123_4', function ( $value, $field, $template, $objects ) {
 	$processed_values = array();
 	foreach ( $objects as $object ) {
