2d408d9dc14f18b11b27a29746cd2c89646e4951
David Smith <david@gravitywiz.com>
2022-08-02 09:23:02 -0400

    Update gpb-enable-user-agent-checking.php

--- gp-blocklist/gpb-enable-user-agent-checking.php
+++ gp-blocklist/gpb-enable-user-agent-checking.php
@@ -3,5 +3,5 @@
  * Gravity Perks // Blocklist // Enable User-Agent Checking
  * https://gravitywiz.com/documentation/gravity-forms-blocklist/
  */
-// Update "123" to the Form ID.
+// Update "123" to the form ID.
 add_filter( 'gpcb_validate_user_agent_123', '__return_true' );
