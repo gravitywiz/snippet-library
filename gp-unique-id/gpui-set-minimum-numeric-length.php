30bd2e48450ec2228df224250a42d2215d418247
dariospace <30393370+dariospace@users.noreply.github.com>
2022-08-02 12:02:09 -0300

    Update gpui-set-minimum-numeric-length.php

--- gp-unique-id/gpui-set-minimum-numeric-length.php
+++ gp-unique-id/gpui-set-minimum-numeric-length.php
@@ -3,6 +3,6 @@
  * Gravity Perks // Unique ID // Set Minimum Numeric Unique ID Length
  * https://gravitywiz.com/documentation/gravity-forms-unique-id/
  */
-add_filter( 'gpui_numeric_minimum_length', function() { 
+add_filter( 'gpui_numeric_minimum_length', function() {
 	return 3;
 } );
