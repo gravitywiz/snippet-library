17569bee9622d487ad3c58b786a84cca57eecaea
David Smith <david@gravitywiz.com>
2022-08-02 09:11:48 -0400

    Update gpns-send-notification-at-the-beginning-of-next-work-week.php

--- gp-notification-scheduler/gpns-send-notification-at-the-beginning-of-next-work-week.php
+++ gp-notification-scheduler/gpns-send-notification-at-the-beginning-of-next-work-week.php
@@ -4,17 +4,21 @@
  * https://gravitywiz.com/documentation/gravity-forms-notification-scheduler/
  */
 add_filter( 'gpns_schedule_timestamp', function ( $timestamp, $notification, $entry, $is_recurring, $current_time ) {
-	$desired_time = 'next monday 12:00 PM'; // See https://www.php.net/manual/en/datetime.formats.php for supported date/time formats.
-	// Update "123" to the Form ID.
+	
+	// See https://www.php.net/manual/en/datetime.formats.php for supported date/time formats.
+	$desired_time = 'next monday 12:00 PM'; 
+	
+	// Update "123" to the form ID.
 	$form_id = 123;
-	// Update "61f31e80d0299" to the Notificaiton ID.
-	$notification_id = '61f31e80d0299';
+	
+	// Update "1a23bc456def7" to the notificaiton ID.
+	$notification_id = '1a23bc456def7';
 
 	if ( (int) $entry['form_id'] !== (int) $form_id || $notification['id'] !== $notification_id ) {
 		return $timestamp;
 	}
 
-	$local_timestamp = date( 'Y-m-d H:i:s', strtotime( $desired_time ) );
+	$local_timestamp = gmdate( 'Y-m-d H:i:s', strtotime( $desired_time ) );
 	$utc_timestamp   = strtotime( get_gmt_from_date( $local_timestamp ) );
 
 	return $utc_timestamp;
