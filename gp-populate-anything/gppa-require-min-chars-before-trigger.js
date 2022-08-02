335220ba922038f9ab76cb869e8c784fd5a23693
Samuel Bassah <sbassah@gmail.com>
2022-08-02 15:38:04 +0000

    Require at Least Four Characters Before Triggering a Change
    * Require Input to Have at Least Four Characters Before Triggering a Change
    
    * Update gppa-require-atleast-four-characters-before-triggering-a-change.js
    
    * Rename gppa-require-atleast-four-characters-before-triggering-a-change.js to gppa-require-at-least-four-characters-before-triggering-a-change.js
    
    * Update and rename gppa-require-at-least-four-characters-before-triggering-a-change.js to gppa-require-min-chars-before-trigger.js
    
    Co-authored-by: David Smith <david@gravitywiz.com>

---
+++ gp-populate-anything/gppa-require-min-chars-before-trigger.js
@@ -0,0 +1,18 @@
+/**
+ * Gravity Perks // Populate Anything // Require a Minimum Character Count Before Triggering a Change
+ * https://gravitywiz.comhttps://gravitywiz.com/documentation/gravity-forms-populate-anything/
+ *
+ * 1. Install this snippet with our free Custom JavaScript plugin.
+ *    https://gravitywiz.com/gravity-forms-custom-javascript/
+ */
+window.gform.addFilter('gppa_should_trigger_change', function( triggerChange, formId, inputId, $el, event ) {
+	// Update "1" to the field ID.
+	if ( inputId == 1 ) {
+		// Require length for input 1 to be at least 4.
+		if ( event.currentTarget.value.length < 4 ) {
+			return false;
+		}
+	}
+	
+	return triggerChange;
+} );
