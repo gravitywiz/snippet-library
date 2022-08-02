982c3fdad441ae86ed36798f9108b7492f4879c6
David Smith <david@gravitywiz.com>
2022-08-02 09:22:37 -0400

    Update and rename gppa-add-a-custom-choice-template-to-choices.php to gppa-add-a-custom-choice-template.php

--- gp-populate-anything/gppa-add-a-custom-choice-template-to-choices.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Gravity Perks // Populate Anything // Add A Custom Choice Template To Choices
- * https://gravitywiz.com/documentation/gravity-forms-populate-anything/
- */
-add_filter( 'gppa_input_choice', function( $choice, $field, $object, $objects ) {
-	$choice['image'] = gp_populate_anything()->process_template( $field, 'image', $object, 'choices', $objects );
-	return $choice;
-}, 10, 4 );
