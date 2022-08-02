92c01b396501c3152b6194941d7951d308e74531
David Smith <david@gravitywiz.com>
2022-08-02 10:40:07 -0400

    Update gppa-require-atleast-four-characters-before-triggering-a-change.js

--- gp-populate-anything/gppa-require-atleast-four-characters-before-triggering-a-change.js
+++ gp-populate-anything/gppa-require-atleast-four-characters-before-triggering-a-change.js
@@ -5,14 +5,14 @@
  * 1. Install this snippet with our free Custom JavaScript plugin.
  *    https://gravitywiz.com/gravity-forms-custom-javascript/
  */
-window.gform.addFilter('gppa_should_trigger_change', function(triggerChange, formId, inputId, $el, event) {
-	// Update "1" to the Field ID.
-	if (inputId === '1') {
+window.gform.addFilter('gppa_should_trigger_change', function( triggerChange, formId, inputId, $el, event ) {
+	// Update "1" to the field ID.
+	if ( inputId == 1 ) {
 		// Require length for input 1 to be at least 4.
-		if (event.currentTarget.value.length < 4) {
+		if ( event.currentTarget.value.length < 4 ) {
 			return false;
 		}
 	}
 	
 	return triggerChange;
-});
+} );
