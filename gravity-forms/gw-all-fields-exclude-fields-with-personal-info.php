cba3962c8692c7906aef5a6b3ea7bd76a5521891
dariospace <30393370+dariospace@users.noreply.github.com>
2022-08-02 14:56:09 -0300

    Update gw-all-fields-exclude-fields-with-personal-info.php

--- gravity-forms/gw-all-fields-exclude-fields-with-personal-info.php
+++ gravity-forms/gw-all-fields-exclude-fields-with-personal-info.php
@@ -2,7 +2,7 @@
 /**
  * Gravity Wiz // Gravity Forms // Exclude Fields With Personal Information
  *
- * This snippet shows you how to exclude fields with personal information from instances 
+ * This snippet shows you how to exclude fields with personal information from instances
  * of the {all_fields} merge tag that include the custom modifier: {all_fields:exclude[persInfoFields]}.
  */
  add_filter( 'gwaft_modifier_value_persInfoFields', function() {
