b320808bf3cf5219bdb7ec4eef58d95c4b090398
David Smith <david@gravitywiz.com>
2022-08-03 09:37:59 -0400

    Update gpnf-disable-loading-child-form-html-after-delete.js

--- gp-nested-forms/gpnf-disable-loading-child-form-html-after-delete.js
+++ gp-nested-forms/gpnf-disable-loading-child-form-html-after-delete.js
@@ -2,9 +2,11 @@
  * Gravity Perks // Nested Forms // Disable automatically loading child form HTML after child entries are deleted
  * https://gravitywiz.com/documentation/gravity-forms-nested-forms/
  *
- * We recommend installing this snippet with our free Custom Javascript plugin:
- * https://gravitywiz.com/gravity-forms-custom-javascript/
+ * Instructions:
+ * 
+ * 1. Install this snippet with our free Custom JavaScript plugin.
+ *    https://gravitywiz.com/gravity-forms-custom-javascript/
  */
- window.gform.addFilter('gpnf_fetch_form_html_after_delete', function () {
+window.gform.addFilter( 'gpnf_fetch_form_html_after_delete', function() {
 	return false;
-});
+} );
