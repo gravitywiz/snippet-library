040eb533bd91b489abdf0c6a88c6687b52a17e04
David Smith <david@gravitywiz.com>
2022-08-03 09:45:40 -0400

    Update gpqr-add-qr-scanner-to-email-field.js

--- gp-qr-code/gpqr-add-qr-scanner-to-email-field.js
+++ gp-qr-code/gpqr-add-qr-scanner-to-email-field.js
@@ -2,8 +2,10 @@
  * Gravity Perks // GP QR Code // Add QR Code Scanner Checkbox To Email Fields
  * https://gravitywiz.com/documentation/gravity-forms-qr-code/
  *
- * We recommend installing this snippet with our free Custom Javascript plugin:
- * https://gravitywiz.com/gravity-forms-custom-javascript/
+ * Instructions:
+ * 
+ * 1. Install this snippet with our free Custom JavaScript plugin.
+ *    https://gravitywiz.com/gravity-forms-custom-javascript/
  */
 window.gform.addFilter( 'gpqr_is_supported_field', function( isSupported, field ) {
 	return isSupported || (field.type === 'email' || field.inputType === 'email');
