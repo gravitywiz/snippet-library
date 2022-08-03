f43ab70b0554a1c18223ce73560129bfb3c18dcb
Samuel Bassah <sbassah@gmail.com>
2022-08-03 16:07:51 +0000

    Repopulate Parent Merge Tags When Editing Entries
    * Repopulate Parent Merge Tags When Editing Entries
    
    * Update gpnf-repopulate-parent-merge-tags-when-editing-entries.js
    
    Co-authored-by: David Smith <david@gravitywiz.com>

---
+++ gp-nested-forms/gpnf-repopulate-parent-merge-tags-when-editing-entries.js
@@ -0,0 +1,14 @@
+/**
+ * Gravity Perks // Nested Forms // Repopulate Parent Merge Tags When Editing Entries
+ * https://gravitywiz.com/documentation/gravity-forms-nested-forms/
+ *
+ * Use this snippet to repopulate {Parent} merge tag when editing entries, replacing any previously saved values.
+ *
+ * Instructions:
+ * 
+ * 1. Install this snippet with our free Custom JavaScript plugin.
+ *    https://gravitywiz.com/gravity-forms-custom-javascript/
+ */
+window.gform.addFilter( 'gpnf_replace_parent_merge_tag_on_edit', function () {
+	 return true;
+} );
