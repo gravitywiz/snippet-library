857af847cf2b43344fb0cc1caa6900a5f256719e
David Smith <david@gravitywiz.com>
2022-08-03 09:37:11 -0400

    Update gpnf-disable-automatically-loading-child-form-html.js

--- gp-nested-forms/gpnf-disable-automatically-loading-child-form-html.js
+++ gp-nested-forms/gpnf-disable-automatically-loading-child-form-html.js
@@ -2,9 +2,11 @@
  * Gravity Perks // Nested Forms // Disable Automatically Loading Child Form HTML
  * https://gravitywiz.com/documentation/gravity-forms-nested-forms/
  *
- * We recommend installing this snippet with our free Custom Javascript plugin:
- * https://gravitywiz.com/gravity-forms-custom-javascript/
+ * Instructions:
+ *
+ * 1. Install this snippet with our free Custom JavaScript plugin.
+ *    https://gravitywiz.com/gravity-forms-custom-javascript/
  */
-window.gform.addFilter('gpnf_fetch_form_html_on_load', function () {
+window.gform.addFilter( 'gpnf_fetch_form_html_on_load', function() {
         return false;
-});
+} );
