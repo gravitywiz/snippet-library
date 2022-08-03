1cc1afec008437f84982cc1858efa35ea1b1af65
David Smith <david@gravitywiz.com>
2022-08-03 09:46:38 -0400

    Update gpui-set-minimum-numeric-length.php

--- gp-unique-id/gpui-set-minimum-numeric-length.php
+++ gp-unique-id/gpui-set-minimum-numeric-length.php
@@ -1,8 +1,9 @@
 <?php
 /**
- * Gravity Perks // Unique ID // Set Minimum Numeric Unique ID Length
+ * Gravity Perks // Unique ID // Set Minimum Length for Numeric Unique ID
  * https://gravitywiz.com/documentation/gravity-forms-unique-id/
  */
 add_filter( 'gpui_numeric_minimum_length', function() {
+	// Update "3" to your desired minimum length.
 	return 3;
 } );
