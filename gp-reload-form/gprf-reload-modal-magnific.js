a0a842aaa33bf4418c573f74af9cbeb831e34a84
David Smith <david@gravitywiz.com>
2022-08-04 10:07:39 -0400

    Update gprf-reload-modal-magnific.js

--- gp-reload-form/gprf-reload-modal-magnific.js
+++ gp-reload-form/gprf-reload-modal-magnific.js
@@ -1,24 +1,37 @@
-// Magnific Popup example using a link to trigger the modal
-// Replace ".open-popup" with your modal trigger
-// Replace "82" with the ID of your form
-$('.open-popup').magnificPopup({
-  type:'inline',
-  callbacks: {
-    close: function() {
-      var gwrf = window.gwrf_82;
-      if( typeof gwrf != 'undefined' ) {
-        gwrf.reloadForm();
-      }
-    }
-  }
-});
+/**
+ * Gravity Perks // Reload Form // Reload Form in Magnific Modal
+ * https://gravitywiz.com/automatically-reload-gravity-form-modal-closed/
+ * 
+ * Describe this snippet's purpose and usage. Be generous when describing both. There
+ * is no maximum description length.
+ * 
+ * Instructions:
+ * 
+ * 1. Install this snippet with our free Custom JavaScript plugin.
+ *    https://gravitywiz.com/gravity-forms-custom-javascript/
+ * 2. Two examples are included below. Remove the example that does not match your use case.
+ */
+// Example #1 – Initialize a new Magnific Popup that will automatically reload the form when the modal is closed.
+// Update ".open-popup" to the selector for the element that will open the modal.
+$( '.open-popup' ).magnificPopup( {
+	type:'inline',
+	callbacks: {
+		close: function() {
+			// Update "82" to the ID of the form within the modal.
+			var gwrf = window.gwrf_82;
+			if( typeof gwrf != 'undefined' ) {
+				gwrf.reloadForm();
+			}
+		}
+	}
+} );
 
-// Example of how to bind to an already initialized Magnific Popup
-// Replace ".open-popup" with your modal trigger
-// Replace "82" with the ID of your form
-$('.open-popup').on('mfpClose', function() {
-  var gwrf = window.gwrf_82;
-  if( typeof gwrf != 'undefined' ) {
-    gwrf.reloadForm();
-  } 
-});
+// Example #2 – Automatically reload a form when a previously configured Magnific popup is closed.
+// Update ".open-popup" to the selector for the element that has been previoulsy been initialized with Magnific.
+$( '.open-popup' ).on( 'mfpClose', function() {
+	// Update "82" to the ID of the form within the modal.
+	var gwrf = window.gwrf_82;
+	if ( typeof gwrf != 'undefined' ) {
+		gwrf.reloadForm();
+	}
+} );
