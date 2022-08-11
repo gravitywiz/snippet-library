019f964adfb8dabf5343ae001a074f0934ef2072
dariospace <30393370+dariospace@users.noreply.github.com>
2022-08-11 14:39:08 -0300

    Update gppa-lmt-add-line-break.php
    added instruction video

--- gp-populate-anything/gppa-lmt-add-line-break.php
+++ gp-populate-anything/gppa-lmt-add-line-break.php
@@ -2,6 +2,8 @@
 /**
  * Gravity Perks // Populate Anything // Add Line Break Between Live Merge Tag Checkbox Values
  * https://gravitywiz.com/documentation/gravity-forms-populate-anything/
+ *
+ * Instruction Video: https://www.loom.com/share/0cfd96b62c264b5faf48fa0498b49124
  */
 // Update "123" with your form ID and "4" with your Checkbox field ID.
 add_filter( 'gppa_live_merge_tag_value_123_4', function( $value, $merge_tag, $form, $field_id, $entry_values ) {
