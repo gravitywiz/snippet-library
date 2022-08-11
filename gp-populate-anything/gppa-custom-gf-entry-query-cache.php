7d6fe2510d14239ee5712da46c439078f7325f0b
dariospace <30393370+dariospace@users.noreply.github.com>
2022-08-11 15:55:56 -0300

    Update gppa-custom-gf-entry-query-cache.php

--- gp-populate-anything/gppa-custom-gf-entry-query-cache.php
+++ gp-populate-anything/gppa-custom-gf-entry-query-cache.php
@@ -8,11 +8,11 @@
 add_filter( 'gppa_query_cache_hash', function( $query_cache_hash, $object_type, $args ) {
 	if ( $object_type === 'gf_entry' ) {
 		return sha1( sprintf( '%s-%s-%s-%s-%s',
-		$args['field']->formId,
-		json_encode( $args['filter_groups'] ),
-		json_encode( $args['ordering'] ),
-		json_encode( $args['primary_property_value'] ),
-		json_encode( $args['unique'] )
+			$args['field']->formId,
+			json_encode( $args['filter_groups'] ),
+			json_encode( $args['ordering'] ),
+			json_encode( $args['primary_property_value'] ),
+			json_encode( $args['unique'] )
 		) );
 	}
 	return $query_cache_hash;
