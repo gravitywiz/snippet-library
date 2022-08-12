d415b5f003c4cd543dbf0caaafa06d1cdd56bf4b
David Smith <david@gravitywiz.com>
2022-08-12 16:27:26 -0400

    Update gppa-change-query-limit-for-post-object.php

--- gp-populate-anything/gppa-change-query-limit-for-post-object.php
+++ gp-populate-anything/gppa-change-query-limit-for-post-object.php
@@ -2,7 +2,7 @@
 /**
  * Gravity Perks // Populate Anything // Change Query Limit for a Specific Object Type
  * https://gravitywiz.com/documentation/gravity-forms-populate-anything/
- *
+ */
 add_filter( 'gppa_query_limit', function( $query_limit, $object_type ) {
 	// Update "post" to your the object for which you would like to increase the limit.
 	if ( $object_type->id === 'post' ) {
