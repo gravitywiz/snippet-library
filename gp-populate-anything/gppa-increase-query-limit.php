5a97903c4c624aa5cf35e206c1edee52bfd2d62d
Scott Buchmann <scott@gravitywiz.com>
2022-08-15 09:36:59 -0400

    Fix gppa-increase-query-limit.php to be correct snippet.

--- gp-populate-anything/gppa-increase-query-limit.php
+++ gp-populate-anything/gppa-increase-query-limit.php
@@ -2,9 +2,8 @@
 /**
  * Gravity Perks // GP Populate Anything // Change The Query Limit
  * https://gravitywiz.com/documentation/gravity-forms-populate-anything/
- *
-// Replace "123" with your form ID and "4" with your field ID.
-add_filter( 'gppa_query_limit_123_4', function() {
-	// Update "1000" to whatever you would like the query limit to be.
-	return 1000;
+ */
+add_filter( 'gppa_query_limit', function() {
+	// Update "750" to whatever you would like the query limit to be.
+	return 750;
 } );
