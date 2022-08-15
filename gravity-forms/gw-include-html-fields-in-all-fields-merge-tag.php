8d2bff82d06172a5afef72a6c33d1f157d228430
Samuel Bassah <sbassah@gmail.com>
2022-08-15 21:36:04 +0000

    Update gw-include-html-fields-in-all-fields-merge-tag.php

--- gravity-forms/gw-include-html-fields-in-all-fields-merge-tag.php
+++ gravity-forms/gw-include-html-fields-in-all-fields-merge-tag.php
@@ -4,7 +4,7 @@
  * https://gravitywiz.com/
  */
 add_action( 'gform_merge_tag_filter', function( $value, $tag, $modifiers, $field ) {
-	if ( $field->type == 'html' && $tag !== 'all_fields' ) {
+	if ( $field->type == 'html' && $tag == 'all_fields' ) {
 		$value = $field->content;
 	}
 	return $value;
