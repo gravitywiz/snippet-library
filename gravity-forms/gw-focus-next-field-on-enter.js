cd7248d91cd9428ff99532d73246c8e0c61abcd9
David Smith <david@gravitywiz.com>
2022-08-16 16:12:57 -0400

    Create gw-focus-next-field-on-enter.js

---
+++ gravity-forms/gw-focus-next-field-on-enter.js
@@ -0,0 +1,23 @@
+/**
+ * Gravity Wiz // Gravity Forms // Focus Next Field on Enter
+ * https://gravitywiz.com/
+ *
+ * Instructions:
+ * 
+ * 1. Install this snippet with our free Custom JavaScript plugin.
+ *    https://gravitywiz.com/gravity-forms-custom-javascript/
+ */
+$( document ).on( 'keypress', '.gform_wrapper', function (e) {
+    var code = e.keyCode || e.which;
+    if ( code == 13 && ! $( e.target ).is( 'textarea,input[type="submit"],input[type="button"]' ) ) {
+        e.preventDefault();
+		$( e.target )
+			.parents( '.gfield' )
+			.next( '.gfield' )
+			.find( 'input, textarea, select' )
+			.filter( ':visible' )
+			.first()
+			.focus();
+        return false;
+    }
+} );
