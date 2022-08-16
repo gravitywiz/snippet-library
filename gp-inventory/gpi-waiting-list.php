3c1c3756a568b20d3cd21fb08604658888bd253f
Clay Griffiths <clay@claygriffiths.com>
2022-08-16 16:06:51 -0500

    `gpi-waiting-list.php`: Refactor to class and add support for Single Products

--- gp-inventory/gpi-waiting-list.php
+++ gp-inventory/gpi-waiting-list.php
@@ -8,98 +8,192 @@
  * Replace the default available inventory message with a waiting list message when a choice's inventory is exhausted.
  * The choice will be selectable and submittable.
  */
-add_filter( 'gpi_disable_choices', '__return_false' );
-add_filter( 'gpi_remove_choices', '__return_false' );
+class GPI_Waiting_List {
+	public $waitlist_message = '(waiting list)';
 
-function gpiw_apply_waitlist_message( $choice, $field, $form, $how_many_left = false ) {
-	$message         = '(waiting list)';
-	$default_message = gp_inventory_type_choices()->replace_choice_available_inventory_merge_tags( gp_inventory_type_choices()->get_inventory_available_message( $field ), $field, $form, $choice, $how_many_left );
-	if ( strpos( $choice['text'], $default_message ) === false ) {
-		$choice['text'] .= ' ' . $message;
-	} else {
-		$choice['text'] = str_replace( $default_message, $message, $choice['text'] );
-	}
-	return $choice;
-}
-
-add_filter( 'gpi_pre_render_choice', function( $choice, $exceeded_limit, $field, $form, $count ) {
-
-	$limit         = (int) rgar( $choice, 'inventory_limit' );
-	$how_many_left = max( $limit - $count, 0 );
-
-	if ( $how_many_left <= 0 ) {
-		$choice                 = gpiw_apply_waitlist_message( $choice, $field, $form, $how_many_left );
-		$choice['isWaitlisted'] = true;
+	public function __construct() {
+		add_action( 'init', array( $this, 'add_hooks' ), 20 ); // Wait for GPI to be instantiated
 	}
 
-	return $choice;
-}, 10, 5 );
+	public function add_hooks() {
+		add_filter( 'gform_entry_post_save', array( $this, 'add_entry_meta' ), 10, 2 );
 
-// Add support for showing waiting list message in confirmations and notifications.
-add_filter( 'gform_pre_submission_filter', function( $form ) {
-	foreach ( $form['fields'] as &$field ) {
-		if ( ! gp_inventory_type_choices()->is_applicable_field( $field ) ) {
-			continue;
-		}
-		$choice_counts = gp_inventory_type_choices()->get_choice_counts( $form['id'], $field );
-		$choices       = $field['choices'];
-		foreach ( $choices as &$choice ) {
-			$value        = $field->sanitize_entry_value( $choice['value'], $form['id'] );
-			$choice_count = intval( rgar( $choice_counts, $value ) );
-			$choice       = gf_apply_filters( array( 'gpi_pre_render_choice', $form['id'], $field->id ), $choice, null, $field, $form, $choice_count );
-		}
-		$field['choices'] = $choices;
+		/*
+		 * Choice-based hooks
+		 */
+		add_filter( 'gpi_disable_choices', '__return_false' );
+		add_filter( 'gpi_remove_choices', '__return_false' );
+
+		add_filter( 'gpi_pre_render_choice', array( $this, 'pre_render_choice' ), 10, 5 );
+
+		// Add support for showing waiting list message in confirmations and notifications.
+		add_filter( 'gform_pre_submission_filter', array( $this, 'add_waitlist_message_to_choices_on_submission' ), 10, 2 );
+
+		add_filter( 'gform_entries_field_value', array( $this, 'entries_field_value_with_waitlist_message' ), 10, 4 );
+		add_filter( 'gform_entry_field_value', array( $this, 'add_waitlist_message_to_entry_value' ), 10, 4 );
+
+		/**
+		 * Single products
+		 */
+		// Mark item as waitlisted during submission for other processes such as saving meta
+		add_filter( 'gform_pre_submission_filter', array( $this, 'add_waiting_list_to_single_product' ) );
+
+		// Prevent validation of inventory
+		remove_filter( 'gform_validation', array( gp_inventory_type_advanced(), 'validation' ) );
+		remove_filter( 'gform_validation', array( gp_inventory_type_simple(), 'validation' ) );
+
+		// Remove locking out single products.
+		add_filter( 'gform_field_input', function ( $field_content, $field ) {
+			remove_filter( "gform_field_input_{$field->formId}_{$field->id}", array(
+				gp_inventory_type_simple(),
+				'hide_field',
+			) );
+
+			remove_filter( "gform_field_input_{$field->formId}_{$field->id}", array(
+				gp_inventory_type_advanced(),
+				'hide_field',
+			) );
+		}, 15, 2 );
+
+		add_filter( 'gform_pre_render', array( $this, 'add_waiting_list_to_single_product' ) );
 	}
-	return $form;
-} );
 
-add_filter( 'gform_entry_post_save', function( $entry, $form ) {
-	foreach ( $form['fields'] as $field ) {
-		if ( ! gp_inventory_type_choices()->is_applicable_field( $field ) ) {
-			continue;
-		}
-		foreach ( $field->choices as $choice ) {
-			if ( rgar( $choice, 'isWaitlisted' ) ) {
-				gform_add_meta( $entry['id'], sprintf( 'gpi_is_waitlisted_%d_%s', $field->id, sanitize_title( $choice['value'] ) ), true );
-			}
-		}
-	}
-	return $entry;
-}, 10, 2 );
+	public function entries_field_value_with_waitlist_message( $value, $form_id, $field_id, $entry ) {
+		$form  = GFAPI::get_form( $form_id );
+		$field = GFAPI::get_field( $form, $field_id );
 
-add_filter( 'gform_entries_field_value', function( $value, $form_id, $field_id, $entry ) {
+		$value = $this->add_waitlist_message_to_entry_value( $value, $field, $entry, $form );
 
-	$form  = GFAPI::get_form( $form_id );
-	$field = GFAPI::get_field( $form, $field_id );
-
-	$value = gpiw_add_waitlist_message_to_entry_value( $value, $entry, $field, $form );
-
-	return $value;
-}, 10, 4 );
-
-add_filter( 'gform_entry_field_value', function ( $value, $field, $entry, $form ) {
-	return gpiw_add_waitlist_message_to_entry_value( $value, $entry, $field, $form );
-}, 10, 4 );
-
-function gpiw_add_waitlist_message_to_entry_value( $value, $entry, $field, $form ) {
-
-	if ( ! gp_inventory_type_choices()->is_applicable_field( $field ) ) {
 		return $value;
 	}
 
-	foreach ( $field->choices as $choice ) {
-		if ( $choice['text'] != $value ) {
-			continue;
+	public function pre_render_choice( $choice, $exceeded_limit, $field, $form, $count ) {
+		$limit         = (int) rgar( $choice, 'inventory_limit' );
+		$how_many_left = max( $limit - $count, 0 );
+
+		if ( $how_many_left <= 0 ) {
+			$choice                 = $this->apply_waitlist_message_to_choice( $choice, $field, $form, $how_many_left );
+			$choice['isWaitlisted'] = true;
 		}
-		$is_waitlisted = gform_get_meta( $entry['id'], sprintf( 'gpi_is_waitlisted_%d_%s', $field->id, sanitize_title( $choice['value'] ) ) );
-		if ( $is_waitlisted ) {
-			$choice = gpiw_apply_waitlist_message( $choice, $field, $form );
-			$value  = $choice['text'];
-		}
+
+		return $choice;
 	}
 
-	return $value;
+	public function apply_waitlist_message_to_choice( $choice, $field, $form, $how_many_left = false ) {
+		$message         = $this->waitlist_message;
+		$default_message = gp_inventory_type_choices()->replace_choice_available_inventory_merge_tags( gp_inventory_type_choices()->get_inventory_available_message( $field ), $field, $form, $choice, $how_many_left );
+		if ( strpos( $choice['text'], $default_message ) === false ) {
+			$choice['text'] .= ' ' . $message;
+		} else {
+			$choice['text'] = str_replace( $default_message, $message, $choice['text'] );
+		}
+
+		return $choice;
+	}
+
+	public function add_waitlist_message_to_entry_value( $value, $field, $entry, $form ) {
+		if ( gp_inventory_type_choices()->is_applicable_field( $field ) ) {
+			foreach ( $field->choices as $choice ) {
+				if ( $choice['text'] != $value ) {
+					continue;
+				}
+
+				$is_waitlisted = gform_get_meta( $entry['id'], sprintf( 'gpi_is_waitlisted_%d_%s', $field->id, sanitize_title( $choice['value'] ) ) );
+
+				if ( $is_waitlisted ) {
+					$choice = $this->apply_waitlist_message_to_choice( $choice, $field, $form );
+					$value  = $choice['text'];
+				}
+			}
+		}
+
+		if ( gp_inventory_type_simple()->is_applicable_field( $field ) || gp_inventory_type_advanced()->is_applicable_field( $field ) ) {
+			$is_waitlisted = gform_get_meta( $entry['id'], sprintf( 'gpi_is_waitlisted_%d', $field->id ) );
+
+			if ( $is_waitlisted ) {
+				$value .= ' ' . $this->waitlist_message;
+			}
+		}
+
+		return $value;
+	}
+
+	public function add_entry_meta( $entry, $form ) {
+		foreach ( $form['fields'] as $field ) {
+			// Checks all inventory types
+			if ( ! gp_inventory_conditional_logic()->is_applicable_field( $field ) ) {
+				continue;
+			}
+
+			if ( gp_inventory_type_choices()->is_applicable_field( $field ) ) {
+				foreach ( $field->choices as $choice ) {
+					if ( rgar( $choice, 'isWaitlisted' ) ) {
+						gform_add_meta( $entry['id'], sprintf( 'gpi_is_waitlisted_%d_%s', $field->id, sanitize_title( $choice['value'] ) ), true );
+					}
+				}
+			}
+
+			if ( gp_inventory_type_simple()->is_applicable_field( $field ) || gp_inventory_type_advanced()->is_applicable_field( $field ) ) {
+				if ( rgar( $field, 'isWaitlisted' ) ) {
+					gform_add_meta( $entry['id'], sprintf( 'gpi_is_waitlisted_%d', $field->id ), true );
+				}
+			}
+		}
+
+		return $entry;
+	}
+
+	public function add_waitlist_message_to_choices_on_submission( $form ) {
+		foreach ( $form['fields'] as &$field ) {
+			// Checks all inventory types
+			if ( ! gp_inventory_conditional_logic()->is_applicable_field( $field ) ) {
+				continue;
+			}
+
+			if ( gp_inventory_type_choices()->is_applicable_field( $field ) ) {
+				$choice_counts = gp_inventory_type_choices()->get_choice_counts( $form['id'], $field );
+				$choices       = $field['choices'];
+
+				foreach ( $choices as &$choice ) {
+					$value        = $field->sanitize_entry_value( $choice['value'], $form['id'] );
+					$choice_count = intval( rgar( $choice_counts, $value ) );
+					$choice       = gf_apply_filters( array(
+						'gpi_pre_render_choice',
+						$form['id'],
+						$field->id,
+					), $choice, null, $field, $form, $choice_count );
+				}
+
+				$field['choices'] = $choices;
+			}
+		}
+
+		return $form;
+	}
+
+	public function add_waiting_list_to_single_product( $form ) {
+		foreach ( $form['fields'] as &$field ) {
+			if ( ! gp_inventory_type_simple()->is_applicable_field( $field ) && ! gp_inventory_type_advanced()->is_applicable_field( $field ) ) {
+				continue;
+			}
+
+			$gpi_instance = gp_inventory_type_simple()::$type === rgar( $field, 'gpiInventory' ) ? gp_inventory_type_simple() : gp_inventory_type_advanced();
+
+			$available = (int) $gpi_instance->get_available_stock( $field );
+
+			if ( $available <= 0 ) {
+				$message             = $this->waitlist_message;
+				$field->description  = '<div class="gpi-available-inventory-message" style="padding-bottom: 13px;">' . $message . '</div>' . $field->description;
+				$field->isWaitlisted = true;
+			}
+		}
+
+		return $form;
+	}
+
 }
+
+new GPI_Waiting_List();
 
 /**
  * @todo Add support for searching for entries that contain items from any waitlist or a product/choice-specific waitlist.
