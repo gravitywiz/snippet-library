7193dabd64595ad34768c7fc17f6fb1ae36a80a7
dariospace <30393370+dariospace@users.noreply.github.com>
2022-08-17 17:41:35 -0300

    Update gpfup-auto-submit-after-uploads-finish.js (#392)
    Added Instruction Video

--- gp-file-upload-pro/gpfup-auto-submit-after-uploads-finish.js
+++ gp-file-upload-pro/gpfup-auto-submit-after-uploads-finish.js
@@ -1,6 +1,8 @@
 /**
  * Gravity Perks // File Upload Pro // Auto Submit After Uploads Finish
  * https://gravitywiz.com/documentation/gravity-forms-file-upload-pro/
+ *
+ * Instruction Video: https://www.loom.com/share/6349c239eaa341998792ff6be9810e10
  */
 gform.addAction( 'gpfup_uploader_ready', function( gpfup ) {
 	gpfup.Uploader.bind( 'UploadComplete', function() {
