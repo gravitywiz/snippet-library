c7d284846b4ae4b19f859736669ad6cb3c198156
Samuel Bassah <sbassah@gmail.com>
2022-08-18 15:23:10 +0000

    Uppercase Entry Value (by Field)

---
+++ gravity-forms/gw-uppercase-entry-value.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Gravity Wiz // Gravity Forms // Uppercase Entry Value (by Field)
+ * https://gravitywiz.com/
+ */
+// Update "123" to your form ID and "4" to your field ID
+add_filter( 'gform_get_input_value_123_4', function( $value ) {
+	return strtoupper( $value );
+} );
