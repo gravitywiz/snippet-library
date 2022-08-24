1b416e54ab7d7e86f90b41fc5513fdd83a82094d
dariospace <30393370+dariospace@users.noreply.github.com>
2022-08-24 17:49:59 -0300

    Update gppa-explode-commas-as-choices.php
    added instruction video

--- gp-populate-anything/gppa-explode-commas-as-choices.php
+++ gp-populate-anything/gppa-explode-commas-as-choices.php
@@ -10,6 +10,8 @@
  * Version:      0.1
  * Author URI:   https://gravitywiz.com
  *
+ * Instruction Video: https://www.loom.com/share/66c75aec58924ed2907291caa37ca2ef
+ *
  * If you have an array-like value mapped to a choice-based field, Populate Anything does not yet know when it should be
  * converted to choices or imploded into a string so it defaults to the latter.
  *
