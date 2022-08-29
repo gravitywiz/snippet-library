3ecda097941f65f4d38b9bd277742f9ce47f2ad2
David Smith <david@gravitywiz.com>
2022-08-29 17:26:00 -0400

    Create gpi-copy-exhausted-inventory-to-another-field.js

---
+++ experimental/gpi-copy-exhausted-inventory-to-another-field.js
@@ -0,0 +1,17 @@
+/**
+ * Gravity Perks // Inventory // Copy Exhausted Choices to Another Field
+ * https://gravitywiz.com/documentation/gravity-forms-inventory/
+ * 
+ * Instructions:
+ * 
+ * 1. Install this snippet with our free Custom JavaScript plugin.
+ *    https://gravitywiz.com/gravity-forms-custom-javascript/
+ * 2. Configure the snippet per the inline instructions.
+ */
+// Update "1" to the ID of your Inventory-enabled field.
+var $disabled = $( '#input_GFFORMID_1 option:disabled' );
+
+// Update "2" to the ID of the field to which exhausted choices should be copied.
+$( '#input_GFFORMID_2' ).html( $disabled.clone().prop( 'disabled', false ) );
+
+$disabled.remove();
