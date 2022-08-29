38453850fe34a991fee90b8d7c090d22ae941e58
Samuel Bassah <sbassah@gmail.com>
2022-08-29 19:35:59 +0000

    Replace Field with Spinner Instead of Pulsing

---
+++ gp-populate-anything/gppa-replace-field-with-spinner-instead-of-pulsing.js
@@ -0,0 +1,23 @@
+/**
+ * Gravity Perks // Populate Anything // Replace Field with Spinner Instead of Pulsing
+ * https://gravitywiz.comhttps://gravitywiz.com/documentation/gravity-forms-populate-anything/
+ *
+ * 1. Install this snippet with our free Custom JavaScript plugin.
+ *    https://gravitywiz.com/gravity-forms-custom-javascript/
+ */
+gform.addFilter('gppa_loading_field_target_meta', function (targetMeta, $elem, context) {
+	/* Only modify behavior in loading context */
+	if (context !== 'loading') {
+		return targetMeta;
+	}
+
+	var spinner = document.createElement('div');
+
+	targetMeta[0].find('input, select, textarea').hide();
+
+	targetMeta[0].prepend(spinner);
+	targetMeta[0] = jQuery(spinner);
+	targetMeta[1] = 'gppa-spinner';
+
+	return targetMeta;
+});
