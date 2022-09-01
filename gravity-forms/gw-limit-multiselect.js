0068d03455ad1e813062d0e7ca83e2a21120b959
David Smith <david@gravitywiz.com>
2022-09-01 09:41:38 -0400

    Create gw-limit-multiselect.js

---
+++ gravity-forms/gw-limit-multiselect.js
@@ -0,0 +1,29 @@
+/**
+ * Gravity Wiz // Gravity Forms // Limit Multi Selects
+ * https://gravitywiz.com/
+ *
+ * Limit how many options may be selected in a Multi Select field. Works with
+ * regular Multi Select fields as well as fields with Enhanced UI enabled.
+ * 
+ * Instructions:
+ * 
+ * 1. Install this snippet with our free Custom JavaScript plugin.
+ *    https://gravitywiz.com/gravity-forms-custom-javascript/
+ * 2. Configure snippet per inline instructions.
+ */
+// Update "1" to the ID of your Multi Select field.
+$( '#input_GFFORMID_1' ).on( 'change', function () {
+	// Update "2" to the max number of options that should be selectable.
+	var maxSelected = 2;
+	// Alternate: Set max number by the value of another field.
+	// var maxSelected = parseInt( $( '#input_GFFORMID_4' ).val() );
+	limitMultiSelect( $( this ), maxSelected );
+} );
+
+function limitMultiSelect( $select, maxSelected ) {
+	var disable = $select.find( 'option:checked' ).length === maxSelected;
+	$select
+		.find( 'option:not(:checked)' )
+		.prop( 'disabled', disable )
+		.trigger( 'chosen:updated' );
+}
