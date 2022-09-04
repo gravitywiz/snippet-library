f82b2e90feb0122b8a794bff0b8131a4cdaf868f
David Smith <david@gravitywiz.com>
2022-09-04 15:19:46 -0400

    Update gpfup-require-camera.js

--- gp-file-upload-pro/gpfup-require-camera.js
+++ gp-file-upload-pro/gpfup-require-camera.js
@@ -11,22 +11,6 @@
  * 1. Install this snippet with our free Custom JavaScript plugin.
  *    https://gravitywiz.com/gravity-forms-custom-javascript/
  */
-var $addEntry;
-
-$( '#gform_GFFORMID' ).find( '.gpnf-add-entry' ).on( 'click', function() {
-	$addEntry = $( this );
-} );
-
-$( '#gform_GFFORMID' ).on( 'click', '.gpnf-row-actions .edit-button', function() {
-	$addEntry = null;
-} );
-
-$( document ).on( 'gform_confirmation_loaded', function( event, formId ) {
-	if ( $addEntry && formId == $addEntry.data( 'nestedformid' ) ) {
-		$addEntry.click();
-	}
-} );
- */
 gform.addAction( 'gpfup_uploader_ready', function( gpfup ) {
 	gpfup.Uploader.bind( 'PostInit', function() {
 		$( gpfup.$field ).find( 'input[type="file"]' ).attr( 'capture', 'camera' );
