bc188634f2bac4af734d8797295dfd74d4de1d3c
dariospace <30393370+dariospace@users.noreply.github.com>
2022-09-08 14:12:59 -0300

    Update gpapf-disable-validation.php
    added instruction video

--- gp-advanced-phone-field/gpapf-disable-validation.php
+++ gp-advanced-phone-field/gpapf-disable-validation.php
@@ -3,6 +3,8 @@
  * Gravity Perks // Advanced Phone Field // Disable Phone Validation
  * https://gravitywiz.com/documentation/gravity-forms-advanced-phone-field/
  *
+ * Instruction Video: https://www.loom.com/share/9d7666ed480b47c4847f76ebd415ddd8
+ *
  * Plugin Name:  GP Advanced Phone Field — Disable Phone Validation
  * Description:  Disable phone number validation provided by Advanced Phone Field.
  * Author:       Gravity Wiz
