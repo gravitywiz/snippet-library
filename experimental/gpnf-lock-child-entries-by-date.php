1968834374a3c62f2db8e4a888e1add1ab23f027
David Smith <david@gravitywiz.com>
2022-09-08 23:22:18 -0400

    gpnf-lock-child-entries-by-date.php: Fixed issue where locked entries could still be deleted.

--- experimental/gpnf-lock-child-entries-by-date.php
+++ experimental/gpnf-lock-child-entries-by-date.php
@@ -11,22 +11,31 @@
  * Clicking "Delete" will do nothing.
  */
 add_filter( 'gpnf_can_user_edit_entry', function( $can_user_edit_entry, $entry ) {
-	
+
 	// Update "2022-09-01" to your desired lockout date in YYYY-MM-DD format.
-	$locked_date  = new DateTime( '2022-09-01' );
-	
+	$locked_date  = new DateTime( '2022-09-05' );
+
 	// Update "123" to the parent form ID for which child entries should be locked.
-	$target_parent_form_id = 123;
-	
-	$parent_form_id = rgpost( 'gpnf_parent_form_id' );
+	$target_parent_form_id = 369;
+
+	$action = rgpost( 'action' );
+	switch ( $action ) {
+		case 'gpnf_edit_entry':
+			$parent_form_id = rgpost( 'gpnf_parent_form_id' );
+			break;
+		case 'gpnf_delete_entry';
+			$parent_form_id = gform_get_meta( rgpost( 'gpnf_entry_id' ), GPNF_Entry::ENTRY_PARENT_FORM_KEY );
+			break;
+	}
+
 	if ( ! $parent_form_id || $parent_form_id != $target_parent_form_id ) {
 		return $can_user_edit_entry;
 	}
-	
+
 	$date_created = new DateTime( $entry['date_created'] );
 	if ( $date_created < $locked_date ) {
 		$can_user_edit_entry = false;
 	}
-	
+
 	return $can_user_edit_entry;
 }, 10, 2 );
