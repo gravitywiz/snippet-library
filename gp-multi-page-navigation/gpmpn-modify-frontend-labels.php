58d0bef99af0b8cce7fe54b7575a644b24f32b7c
scyt <scott@gravitywiz.com>
2022-09-12 16:56:46 -0400

    Add gpmpn-modify-frontend-labels.php (#519)
    * Add gpmpn-modify-frontend-labels.php
    
    * Fixed PHPCS spacing issues.

---
+++ gp-multi-page-navigation/gpmpn-modify-frontend-labels.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Gravity Perks // Multi-page Navigation // Modify Frontend Labels
+ * http://gravitywiz.com/documentation/gravity-forms-multi-page-navigation/
+ */
+add_filter( 'gpmpn_frontend_labels', function( $labels ) {
+	$labels['backToLastPage']     = 'My Custom Button Label';
+	$labels['submit']             = 'Submit';
+	$labels['nextPageWithErrors'] = 'Next Page with Errors';
+	return $labels;
+} );
