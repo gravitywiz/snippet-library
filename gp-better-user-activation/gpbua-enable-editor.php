81d5b503a096f90c65c290005a3b0dba1dd08d2e
Scott Buchmann <scott@gravitywiz.com>
2022-09-15 14:52:39 -0400

    Updated gpbua-enable-editor.php to fix PHPCS errors.

--- gp-better-user-activation/gpbua-enable-editor.php
+++ gp-better-user-activation/gpbua-enable-editor.php
@@ -4,7 +4,7 @@
  * http://gravitywiz.com/documentionat/gravity-forms-better-user-activation/
  */
 add_action( 'init', function() {
-	if ( is_callable( 'gp_better_user_activation' ) ) {	
+	if ( is_callable( 'gp_better_user_activation' ) ) {
 		remove_action( 'admin_head', array( gp_better_user_activation(), 'remove_default_content_editor' ) );
 	}
 } );
