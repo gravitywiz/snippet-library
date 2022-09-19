21b36f295437003172e30cf7d767fc62720a38cc
dariospace <30393370+dariospace@users.noreply.github.com>
2022-09-19 15:40:17 -0300

    Update gpfup-disable-submit-during-upload.js (#489)
    * Update gpfup-disable-submit-during-upload.js
    
    * Update gpfup-disable-submit-during-upload.js
    
    New video added with the correct screen setup

--- gp-file-upload-pro/gpfup-disable-submit-during-upload.js
+++ gp-file-upload-pro/gpfup-disable-submit-during-upload.js
@@ -2,6 +2,8 @@
  * Gravity Perks // File Upload Pro // Disable Submit During Upload
  * https://gravitywiz.com/documentation/gravity-forms-file-upload-pro/
  *
+ * Instruction Video: https://www.loom.com/share/e35cf2da108b4064b3b33ea5d65ed021
+ *
  * Use this snippet alongside our free [Gravity Forms Custom JavaScript][1] plugin to disable 
  * the submit button while files are being uploaded.
  *
