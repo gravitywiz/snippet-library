c59948706947feacc7e788a6be3a5fae46a69736
dariospace <30393370+dariospace@users.noreply.github.com>
2022-09-19 15:40:36 -0300

    Update gpwc-disable-truncation.php (#457)
    * Update gpwc-disable-truncation.php
    
    added instruction video
    
    * Update gpwc-disable-truncation.php
    
    Updated Instruction Video with an improved sound version
    
    * Update gpwc-disable-truncation.php
    
    re did the video with the correct aspect ratio

--- gp-word-count/gpwc-disable-truncation.php
+++ gp-word-count/gpwc-disable-truncation.php
@@ -3,6 +3,8 @@
  * Gravity Perks // Word Count // Disable Truncation
  * https://gravitywiz.com/documentation/gravity-forms-word-count/
  *
+ * Instruction Video: https://www.loom.com/share/4f588b75b8bb46d7b2d1bdb9ce11f56e
+ *
  * This will prevent the words in a specific field from being truncated when the limit is reached.
  */
 add_filter( 'gpwc_script_args', function( $args, $field ) {
