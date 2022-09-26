b772963a0cd996efa34446f347370a81fb1d0739
Saif Sultan <saif@gravitywiz.com>
2022-09-27 01:40:51 +0530

    Updated "Require Unique Values for Different Fields" snippet with case sensitive option (#532)

--- gravity-forms/gw-require-unique-values-usage.php
+++ gravity-forms/gw-require-unique-values-usage.php
@@ -12,6 +12,14 @@ new GW_Require_Unique_Values( array(
 	'validation_message' => 'My custom validation message!',
 ) );
 
+# Use Case Senstive Comparison
+
+new GW_Require_Unique_Values( array(
+	'form_id'            => 12,
+	'field_ids'          => array( 14, 15 ),
+	'case_sensitive'     => true,
+) );
+
 # Create Multiple Unique "Groups" on the Same Form
 
 new GW_Require_Unique_Values( array(
