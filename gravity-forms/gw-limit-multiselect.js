bff60520883dfe79f951d7fc9817126d0c1d32ed
veryspry <ehlinger.matt@gmail.com>
2022-09-28 15:05:10 +0200

    `gravity-forms/gw-limit-multiselect`: Fixed issue where multi select limit did not work correctly when going backwards on multi-page forms.

--- gravity-forms/gw-limit-multiselect.js
+++ gravity-forms/gw-limit-multiselect.js
@@ -4,26 +4,35 @@
  *
  * Limit how many options may be selected in a Multi Select field. Works with
  * regular Multi Select fields as well as fields with Enhanced UI enabled.
- * 
+ *
  * Instructions:
- * 
+ *
  * 1. Install this snippet with our free Custom JavaScript plugin.
  *    https://gravitywiz.com/gravity-forms-custom-javascript/
  * 2. Configure snippet per inline instructions.
  */
-// Update "1" to the ID of your Multi Select field.
-$( '#input_GFFORMID_1' ).on( 'change', function () {
-	// Update "2" to the max number of options that should be selectable.
+(function() {
+	// Update "1" to the ID of your Multi Select field.
+	var multiSelectId = '#input_GFFORMID_1';
 	var maxSelected = 2;
-	// Alternate: Set max number by the value of another field.
-	// var maxSelected = parseInt( $( '#input_GFFORMID_4' ).val() );
-	limitMultiSelect( $( this ), maxSelected );
-} );
+	var $select = $( multiSelectId );
 
-function limitMultiSelect( $select, maxSelected ) {
-	var disable = $select.find( 'option:checked' ).length === maxSelected;
-	$select
-		.find( 'option:not(:checked)' )
-		.prop( 'disabled', disable )
-		.trigger( 'chosen:updated' );
-}
+	limitMultiSelect( $select, maxSelected );
+
+	$select.on( 'change', function () {
+		// Update "2" to the max number of options that should be selectable.
+
+		// Alternate: Set max number by the value of another field.
+		// var maxSelected = parseInt( $( '#input_GFFORMID_4' ).val() );
+		limitMultiSelect( $( this ), maxSelected );
+	} );
+
+	function limitMultiSelect( $select, maxSelected ) {
+		var disable = $select.find( 'option:checked' ).length === maxSelected;
+		$select
+			.find( 'option:not(:checked)' )
+			.prop( 'disabled', disable )
+			.trigger( 'chosen:updated' );
+	}
+
+})();
