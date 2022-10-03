cafd33989e4ede9e1b288985828430350a161026
David Smith <david@gravitywiz.com>
2022-10-03 14:43:20 -0400

    Update gpi-set-requested-quantity-by-field-value.php

--- gp-inventory/gpi-set-requested-quantity-by-field-value.php
+++ gp-inventory/gpi-set-requested-quantity-by-field-value.php
@@ -1,8 +1,10 @@
 <?php
 /**
- * Gravity Perks // Inventory // Set Requested Quantity by Field Value
+ * Gravity Perks // Inventory // Set Requested Inventory by Field Value
  * https://gravitywiz.com/documentation/gravity-forms-inventory/
  */
+// Update "123" to your form ID and "4" to your Inventory-enabled field.
 add_filter( 'gpi_requested_quantity_123_4', function( $requested_quantity ) {
-	return rgpost( 'input_3' );
+	// Update "5" to the ID of the field that should be used to specify the quantity.
+	return rgpost( 'input_5' );
 } );
