d32caef3b209717177a0c10d22c9631844387154
Saif Sultan <saif@gravitywiz.com>
2022-10-12 03:21:11 +0530

    `gppa-filter-by-relative-dates.php`: Updated snippet to allow new `{relative:valid strtotime value}` merge tag and also support all core Object Types.

--- gp-populate-anything/gppa-filter-by-relative-dates.php
+++ gp-populate-anything/gppa-filter-by-relative-dates.php
@@ -19,9 +19,11 @@
  *     Examples:
  *         {3 days ago}
  *         {2 hours ago}
- *           {1 month ago}
+ *         {1 month ago}
  *         {3 weeks ago}
  *         {1 year ago}
+ *
+ * Another possible way of passing the value is : {relative:valid_strtotime_value}
  *
  * Plugin Name: GP Populate Anything - Filter Results on Relative Dates
  * Plugin URI:  https://gravitywiz.com/documentation/gravity-forms-populate-anything/
@@ -30,11 +32,21 @@
  * Version: 0.1
  * Author URI: http://gravitywiz.com
  **/
-add_filter( 'gppa_replace_filter_value_variables_post', function ( $value ) {
+// {INTEGER UNIT ago}
+add_filter( 'gppa_replace_filter_value_variables_post', 'gppa_replace_int_unit_ago_merge_tags' );
+add_filter( 'gppa_replace_filter_value_variables_gf_entry', 'gppa_replace_int_unit_ago_merge_tags' );
+add_filter( 'gppa_replace_filter_value_variables_user', 'gppa_replace_int_unit_ago_merge_tags' );
+add_filter( 'gppa_replace_filter_value_variables_term', 'gppa_replace_int_unit_ago_merge_tags' );
+add_filter( 'gppa_replace_filter_value_variables_database', 'gppa_replace_int_unit_ago_merge_tags' );
+
+function gppa_replace_int_unit_ago_merge_tags( $value ) {
 	preg_match_all( '/{(\d+) ((week|day|month|year)s?) ago}/m', $value, $matches, PREG_SET_ORDER, 0 );
 
 	if ( ! $matches || ! count( $matches ) ) {
-		return $value;
+		$relative_time = explode( ':', substr( $value, 1, -1 ) );
+		if ( ! strtotime( $relative_time[1] ) ) {
+			return $value;
+		}
 	}
 
 	foreach ( $matches as $match ) {
@@ -43,4 +55,27 @@ add_filter( 'gppa_replace_filter_value_variables_post', function ( $value ) {
 	}
 
 	return $value;
-} );
+}
+
+// {relative:valid_strtotime_value}
+add_filter( 'gppa_replace_filter_value_variables_post', 'gppa_replace_relative_strtotime_merge_tags' );
+add_filter( 'gppa_replace_filter_value_variables_gf_entry', 'gppa_replace_relative_strtotime_merge_tags' );
+add_filter( 'gppa_replace_filter_value_variables_user', 'gppa_replace_relative_strtotime_merge_tags' );
+add_filter( 'gppa_replace_filter_value_variables_term', 'gppa_replace_relative_strtotime_merge_tags' );
+add_filter( 'gppa_replace_filter_value_variables_database', 'gppa_replace_relative_strtotime_merge_tags' );
+
+function gppa_replace_relative_strtotime_merge_tags( $value ) {
+	preg_match_all( '/{relative:(.*?)}/m', $value, $matches, PREG_SET_ORDER, 0 );
+
+	if ( ! $matches || ! count( $matches ) ) {
+		return $value;
+	}
+
+	foreach ( $matches as $match ) {
+		if ( strtotime( $match[1] ) ) {
+			$value = str_replace( $match[0], wp_date( 'Y-m-d H:i:s', strtotime( $match[1] ) ), $value );
+		}
+	}
+
+	return $value;
+}
