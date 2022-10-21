8aacf710d5ed44c586335ada20e9a38d63caa36f
David Smith <david@gravitywiz.com>
2022-10-21 12:45:52 -0400

    Create gpalf-select-first-option.js

---
+++ gp-auto-list-field/gpalf-select-first-option.js
@@ -0,0 +1,19 @@
+/**
+ * Gravity Perks // Auto List Field // Select First Option in List Field Selects
+ * https://gravitywiz.com/documentation/gravity-forms-auto-list-field/
+ * 
+ * By default, when using the [`gform_input_column`][1] filter to convert a List field column 
+ * from an input into a select, no option is selected after adding a new row. This differs from
+ * the behavior of the first row which defaults to the first option being selected. 
+ * 
+ * This snippet aligns the behavior for newly added rows to also select the first option.
+ * Additionally, this resolves an issue where Gravity Forms threw an error after adding a new
+ * row to a List field that contained a select when that List field was used in a calculation
+ * formula via Auto List Field. 
+ * 
+ *   [1]: https://docs.gravityforms.com/gform_column_input/#examples
+ */
+gform.addFilter( 'gform_list_item_pre_add', function( $clone ) {
+	$clone.find( 'select' ).find( 'option:first' ).prop( 'selected', true );
+	return $clone;
+} );
