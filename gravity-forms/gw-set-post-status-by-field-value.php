3976330f474b68e69e011e713b14a10b50b18cec
scyt <scott@gravitywiz.com>
2022-10-26 15:01:34 -0400

    `gw-set-post-status-by-field-value`: Migrated from GitHub Gists. (#555)
    `gw-scott/fix/set-post-status-by-field-value-advanced`: Fixed filename to match snippet.

--- gravity-forms/gw-set-post-status-by-field-value.php
+++ gravity-forms/gw-set-post-status-by-field-value.php
@@ -1,22 +1,16 @@
 <?php
 /**
-* Gravity Wiz // Gravity Forms // Set Post Status by Field Value (Advanced)
-* https://gravitywiz.com/set-post-status-by-field-value-advanced/
-*/
-// update "3" to the ID of your form
-add_filter( 'gform_post_data_3', 'gform_dynamic_post_status', 10, 3 );
+ * Gravity Wiz // Gravity Forms // Set Post Status by Field Value
+ * https://gravitywiz.com/set-post-status-by-field-value/
+ */
+// update "123" to the ID of your form
+add_filter( 'gform_post_data_123', 'gform_dynamic_post_status', 10, 3 );
 function gform_dynamic_post_status( $post_data, $form, $entry ) {
 
-	// update "5" to the ID of your custom post status field
-	if ( $entry[5] ) {
-		switch ( $entry[5] ) {
-			case 'Yes, please review my post.':
-				$post_data['post_status'] = 'pending';
-				break;
-			case 'No, please publish my post.':
-				$post_data['post_status'] = 'publish';
-				break;
-		}
+	// update "4" to the ID of your custom post status field
+	if ( $entry[4] ) {
+		$post_data['post_status'] = $entry[4];
 	}
+
 	return $post_data;
 }
