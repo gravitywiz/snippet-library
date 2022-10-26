7ce1854c29edd112defa1b00318a4249785c1c6e
Scott Buchmann <scott@gravitywiz.com>
2022-10-26 16:24:49 -0400

    `gw-set-post-status-by-product-field-value.php`: Migrated from GitHub Gists.

---
+++ gravity-forms/gw-set-post-status-by-product-field-value.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Gravity Wiz // Gravity Forms // Set Post Status by Product Field Value
+ * https://gravitywiz.com/set-post-status-by-field-value-advanced/
+ */
+// update "123" to the ID of your form
+add_filter( 'gform_post_data_123', 'gform_dynamic_post_status', 10, 3 );
+function gform_dynamic_post_status( $post_data, $form, $entry ) {
+
+	// update "4" to the ID of your custom post status field
+	if ( $entry[4] ) {
+		$values = explode( '|', $entry[6] );
+		switch ( $values[0] ) {
+			case 'Basic Package':
+				$post_data['post_status'] = 'draft';
+				break;
+			case 'Premium Package':
+				$post_data['post_status'] = 'publish';
+				break;
+		}
+	}
+
+	return $post_data;
+}
