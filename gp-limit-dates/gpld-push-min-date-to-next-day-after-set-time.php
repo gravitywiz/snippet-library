ae75d63b79fb06b4620eb9c1c95addeb109c1996
David Smith <david@gravitywiz.com>
2022-10-28 16:55:10 -0400

    Create gpld-push-min-date-to-next-day-after-set-time.php

---
+++ gp-limit-dates/gpld-push-min-date-to-next-day-after-set-time.php
@@ -0,0 +1,18 @@
+/**
+ * Gravity Perks // Limit Dates // Push Minimum Date to Next Day After Set Time
+ * https://gravitywiz.com/documentation/gravity-forms-limit-dates/
+ *
+ * If you've configured your Date field's minimum date to "Current Date", use this snippet to set the minimum date to
+ * the next day after a set time on the current date.
+ */
+// Update "123" to your form ID and "4" to your Date field ID.
+add_filter( 'gpld_limit_dates_options_123_4', function( $options ) {
+	// Update "17" to the hour (in 24-hour format) at which the minimum date should be pushed to tomorrow.
+	$cutoff_hour  = 17; // (e.g. 5pm)
+	$current_time = new DateTime( wp_timezone_string() );
+	$cutoff_time  = ( new DateTime( wp_timezone_string() ) )->setTime( $cutoff_hour, 0 );
+	if ( $current_time > $cutoff_time ) {
+		$options['minDate'] = date( 'm/d/Y', strtotime( 'midnight tomorrow', $current_time->getTimestamp() ) );
+	}
+	return $options;
+} );
