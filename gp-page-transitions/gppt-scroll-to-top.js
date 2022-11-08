72cb76f093f747c811398463aab7d888b67676f6
dariospace <30393370+dariospace@users.noreply.github.com>
2022-11-08 10:55:12 -0300

    Update gppt-scroll-to-top.js
    Added Instruction Video

--- gp-page-transitions/gppt-scroll-to-top.js
+++ gp-page-transitions/gppt-scroll-to-top.js
@@ -1,6 +1,8 @@
 /**
  * Gravity Perks // Page Transitions // Scroll to Top for Long Pages
  * https://gravitywiz.com/
+ * 
+ * Instruction Video: https://www.loom.com/share/74b3371a4feb4d89b87740af76e202f2
  */
 gform.addAction( 'gppt_before_transition', function() {
   window.scroll( {
