41c446e9c5cbfe7e1af097d7294c20b73e04a4b0
David Smith <david@gravitywiz.com>
2022-11-10 13:58:27 -0500

    Update gw-gfapc-map-multiple-fields-to-taxonomy.php

--- experimental/gw-gfapc-map-multiple-fields-to-taxonomy.php
+++ experimental/gw-gfapc-map-multiple-fields-to-taxonomy.php
@@ -3,7 +3,7 @@
  * Gravity Wiz // Gravity Forms // Map Multiple Fields w/ Advanced Post Creation
  * https://gravitywiz.com/
  *
- * By default, the Advanced Post Creation add-on does not allow you to map multiple fields to a taxonomy nor does it 
+ * By default, the Advanced Post Creation add-on does not allow you to map multiple fields to a taxonomy nor does it
  * allow you to set terms by ID.
  * 
  * This snippet allows to specify multiple fields on a form that have been populated with term IDs (we recommend 
@@ -15,12 +15,13 @@ add_action( 'gform_advancedpostcreation_post_after_creation', function ( $post_i
 	
 	// Update "1", "2", "3" to the field IDs that have been populated with terms. Add additional IDs as needed.
 	$term_field_ids = array( 1, 2, 3 );
+
 	// Update "categories" to the name of the taxonomy to which the populated terms belong.
-	$taxonomy       = 'categories';
+	$taxonomy = 'categories';
 	
 	$term_ids = array();
 	foreach ( $term_field_ids as $term_field_id ) {
-		$term_ids[] = $entry[ $term_field_id ];
+		$term_ids[] = (int) $entry[ $term_field_id ];
 	}
 	
 	wp_set_object_terms( $post_id, $term_ids, $taxonomy );
