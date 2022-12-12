211e6806eb737400bb157e13bbc42b78a6706f92
David Smith <david@gravitywiz.com>
2022-12-12 12:18:18 -0500

    `gpcc-copy-multiple-checkboxes-to-list-field.js`: Created new snippet.

---
+++ gp-copy-cat/gpcc-copy-multiple-checkboxes-to-list-field.js
@@ -0,0 +1,23 @@
+/**
+ * Gravity Perks // Copy Cat // Copy Multiple Checkbox Fields to a List Field
+ * https://gravitywiz.com/documentation/gravity-forms-copy-cat/
+ * 
+ * Copy checked checkbox values from multiple Checkbox fields to a single List field. 
+ * [Screenshot](https://gwiz.io/3hhPeXH).
+ * 
+ * Instructions:
+ * 
+ * 1. Install this snippet with our free Custom JavaScript plugin.
+ *    https://gravitywiz.com/gravity-forms-custom-javascript/
+ * 
+ * 2. Update snippet based on inline instructions.
+ */
+gform.addFilter( 'gpcc_field_group', function( $group, field, groupType, $field ) {
+	// Update "3.1" to your List field ID and column number.
+	if ( groupType !== 'source' || field.target !== '3.1' ) {
+		return $group;
+	}
+	// Update "1" to your first Checkbox field ID and "2" to your second Checkbox field ID.
+	$group = $( '#field_605_1, #field_605_2' ).find( 'input:checked' );
+	return $group;
+} );
