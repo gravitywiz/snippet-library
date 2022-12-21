1f23f0d10aec2be55e10b463501776a39fc4a12f
saifsultanc <saifraikkonen@gmail.com>
2022-12-21 07:11:11 +0530

    `gppa-strip-slashes-from-result-value.php`: Added snippet to strip slashes from GPPA populated value.

--- gp-populate-anything/gppa-strip-slashes-from-result-value.php
+++ gp-populate-anything/gppa-strip-slashes-from-result-value.php
@@ -3,7 +3,7 @@
  * Gravity Perks // Populate Anything // Strip slashes from result values
  * https://gravitywiz.com/documentation/gravity-forms-populate-anything/
  */
-// Replace "123" with your form ID and "4" with your field ID.
-add_filter( 'gppa_get_input_values_123_4', function ( $value, $field, $template, $objects ) {
+// Replace "123" with your form ID and "5" with your field ID.
+add_filter( 'gppa_get_input_values_123_5', function ( $value, $field, $template, $objects ) {
 	return stripslashes( $value );
 }, 10, 4 );
