5509795923e8d0b08619191ebd2a334baf677e46
saifsultanc <saifraikkonen@gmail.com>
2022-12-30 17:25:06 +0530

    `gppa-force-gppa-on-save-and-continue.php`: Added snippet to force GPPA value over a Save & Continue Value.

--- gp-populate-anything/gppa-force-gppa-on-save-and-continue.php
+++ gp-populate-anything/gppa-force-gppa-on-save-and-continue.php
@@ -9,13 +9,15 @@ add_filter( 'gform_incomplete_submission_post_get', function( $submission_json, 
 	$target_field_id = 1;
 	static $_gppa_forcing_hydration;
 	if ( $form['id'] == $target_form_id && ! $_gppa_forcing_hydration ) {
-		$_gppa_forcing_hydration                          = true;
-		$submission                                       = json_decode( $submission_json, ARRAY_A );
-		$field                                            = GFAPI::get_field( $form, $target_field_id );
-		$hydrated_field                                   = gp_populate_anything()->hydrate_field( $field, $form, array(), null, false );
-		$submission['submitted_values'][$target_field_id] = $hydrated_field['field_value'];
-		$submission_json                                  = json_encode( $submission );
-		$_gppa_forcing_hydration                          = false;
+		$_gppa_forcing_hydration = true;
+		$submission              = json_decode( $submission_json, ARRAY_A );
+		$field                   = GFAPI::get_field( $form, $target_field_id );
+		$hydrated_field          = gp_populate_anything()->hydrate_field( $field, $form, array(), null, false );
+
+		$submission['submitted_values'][ $target_field_id ] = $hydrated_field['field_value'];
+
+		$submission_json         = json_encode( $submission );
+		$_gppa_forcing_hydration = false;
 	}
 	return $submission_json;
 }, 10, 3 );
