af87d47b18822055ae63121ebbd651cfa0e87383
David Smith <david@gravitywiz.com>
2023-01-12 13:28:15 -0500

    `gpls-collective-limits-for-roles.php`: Added new snippet.

---
+++ gp-limit-submissions/gpls-collective-limits-for-roles.php
@@ -0,0 +1,11 @@
+/**
+ * Gravity Perks // Limit Submissions // Collective Limits for Roles
+ * https://gravitywiz.com/documentation/gravity-forms-limit-submissions/
+ *
+ * By default, role-based limits are applied per user. This means each user of the specified
+ * role can submit the form up to the specified limit.
+ * 
+ * Use this snippet to change this behavior so that the limit is applied collectively to all
+ * users of the specified role.
+ */
+ add_filter( 'gpls_apply_role_limit_per_user', '__return_false' );
