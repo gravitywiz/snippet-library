2187666c500449a295ee9f1417ddc32e486805ea
David Smith <david@gravitywiz.com>
2023-01-16 16:15:32 -0500

    `gw-shortcodes-as-merge-tags.php`: Updated to support using in shortcode merge tags in default values.

--- gravity-forms/gw-shortcodes-as-merge-tags.php
+++ gravity-forms/gw-shortcodes-as-merge-tags.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Gravity Wiz // Gravity Forms // Shortcodes as Merge Tags for Gravity Forms Conditional Shortcode
+ * Gravity Wiz // Gravity Forms // Shortcodes as Merge Tags
  * https://gravitywiz.com/
  *
  * Requires Gravity Forms 2.4.17.2+
@@ -12,12 +12,47 @@
  * Shortcode: [gravityforms action="remaining" id="123" input_id="4.3" limit="100"]
  * Merge Tag: {shortcode:gravityforms&action=remaining&id=123&input_id=4.3&limit=100}
  *
- * 2. Use your merge tag in the GF conditional shortcode `merge_tag` parameter.
+ * 2a. Use your merge tag in as the default value in a field.
+ *
+ * 2b. Use your merge tag in the GF conditional shortcode `merge_tag` parameter.
  *
  * [gravityforms action="conditional" merge_tag="{shortcode:gravityforms&action=remaining&id=123&input_id=4.3&limit=100}" condition="less_than" value="50"]
  *     Hurry! Only [gravityforms action="remaining" id="661" input_id="1.3" limit="100"] tickets left!
  * [/gravityforms]
  */
+add_filter( 'gform_replace_merge_tags', function( $text ) {
+
+	// Update to a list of shortcodes that should be allowed as merge tags.
+	$white_listed_shortcodes = array( 'gravityforms', 'gpls' );
+
+	if ( strpos( $text, '{shortcode:' ) === false ) {
+		return $text;
+	}
+
+	preg_match( '/{shortcode:(.+?)}/', htmlspecialchars_decode( $text ), $matches );
+	if ( empty( $matches ) ) {
+		return $text;
+	}
+
+	parse_str( $matches[1], $atts );
+	$shortcode = array( array_key_first( $atts ) );
+	if ( ! in_array( $shortcode, $white_listed_shortcodes ) ) {
+		return $text;
+	}
+
+	array_shift( $atts );
+
+	foreach ( $atts as $prop => $value ) {
+		$shortcode[] = sprintf( '%s="%s"', $prop, $value );
+	}
+
+	$shortcode = sprintf( '[%s]', implode( ' ', $shortcode ) );
+	$text      = str_replace( $matches[0], do_shortcode( $shortcode ), $text );
+
+	return $text;
+} );
+
+// @todo: Refactor to remove duplicated code.
 add_filter( 'shortcode_atts_gravityforms_conditional', function( $out, $pairs, $atts ) {
 
 	if ( isset( $out['merge_tag'] ) && strpos( $out['merge_tag'], '{shortcode:' ) !== false ) {
