105d3a6cbcc558fc1476700df38a0aca48164256
Samuel Bassah <sbassah@gmail.com>
2023-01-19 23:56:40 +0000

    `gw-update-posts.php`: Added support to update post date and time. (#607)
    * `gw-update-posts.php`: Added support to update post date and time.
    
    * `gw-update-posts.php`: Added support to update post date gmt.
    
    * `gw-update-posts.php`: Made requested changes.

--- gravity-forms/gw-update-posts.php
+++ gravity-forms/gw-update-posts.php
@@ -92,6 +92,29 @@ class GW_Update_Posts {
 			$post->post_name = rgar( $entry, $this->_args['slug'] );
 		}
 
+		if ( $this->_args['date_time'] ) {
+			$date = $this->_args['date_time']['date'];
+			$time = $this->_args['date_time']['time'];
+
+			$date = rgar( $entry, $date );
+			$time = rgar( $entry, $time, '00:00 am' );
+
+			if ( empty( $date ) ) {
+				$date = explode( ' ', $post->post_date )[0];
+			}
+
+			if ( $time ) {
+				list( $hour, $min, $am_pm ) = array_pad( preg_split( '/[: ]/', $time ), 3, false );
+				if ( strtolower( $am_pm ) == 'pm' ) {
+					$hour += 12;
+				}
+			}
+
+			$new_date_time       = gmdate( 'Y-m-d H:i:s', strtotime( sprintf( '%s %s:%s:00', $date, $hour, $min ) ) );
+			$post->post_date     = $new_date_time;
+			$post->post_date_gmt = get_gmt_from_date( $new_date_time );
+		}
+
 		if ( $this->_args['featured_image'] && is_callable( 'gp_media_library' ) ) {
 			if ( rgar( $entry, $this->_args['featured_image'] ) ) {
 				$image_id = gp_media_library()->get_file_ids( $entry['id'], $this->_args['featured_image'], 0 );
