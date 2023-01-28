859e55fdfdac7e0369fcfd99ea0e89926557c1a3
saifsultanc <saifraikkonen@gmail.com>
2023-01-28 07:38:33 +0530

    `gpui-sequential-ids-as-per-date-field.php`: Added snippet to generate Unique IDs as per selected date field.

--- gp-unique-id/gpui-sequential-ids-as-per-date-field.php
+++ gp-unique-id/gpui-sequential-ids-as-per-date-field.php
@@ -7,13 +7,13 @@
  * generate Unique IDs for a particular date.
  */
 // Update "123" to your form ID and "4" to your Unique ID field.
-add_filter( 'gpui_unique_id_attributes_123_4', 'gw_generate_unique_id_as_per_date_field', 10, 4);
+add_filter( 'gpui_unique_id_attributes_123_4', 'gw_generate_unique_id_as_per_date_field', 10, 4 );
 function gw_generate_unique_id_as_per_date_field( $atts, $form_id, $field_id, $entry ) {
 	// Replace 5 with the Date field ID used for targetting Unique ID generation
 	$date_field_id = 5;
 
 	// Remove any special character like / - . which can be in the date field value.
-	$date_string = str_replace( array( '/', '-', '.' ), '', $entry[$date_field_id] );
+	$date_string = str_replace( array( '/', '-', '.' ), '', $entry[ $date_field_id ] );
 
 	// Update UID Attributes with the date string.
 	$atts['form_id'] = (int) $date_string;
