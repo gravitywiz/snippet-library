d826b0853ab357dc5b886d39c1f9d9ea5e2ec880
Clay Griffiths <clay@claygriffiths.com>
2023-01-30 17:45:35 -0600

    `gppa-google-sheets-filter-by-date.php`: Added new snippet for changing the date format when searching for values in a Google Sheet.

---
+++ experimental/gppa-google-sheets-filter-by-date.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Gravity Perks // Populate Anything + Google Sheets // Convert Date Format in Date Field for Search to m/d/y
+ * https://gravitywiz.com/documentation/gravity-forms-populate-anything/
+ * https://gravitywiz.com/populate-gravity-forms-with-google-sheets/
+ */
+add_filter( 'gppa_replace_filter_value_variables_google_sheet', function ( $filter_value ) {
+	if ( is_array( $filter_value ) && rgar( $filter_value, 'year' ) && rgar( $filter_value, 'month' ) && rgar( $filter_value, 'day' ) ) {
+		$filter_value = sprintf( '%s/%s/%s', $filter_value['month'], $filter_value['day'], $filter_value['year'] );
+	}
+
+	return $filter_value;
+}, 11 );
