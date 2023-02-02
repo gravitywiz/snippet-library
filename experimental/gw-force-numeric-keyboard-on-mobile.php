a0a79055798649a3b6d0f01b0b0b6856a108a838
David Smith <david@gravitywiz.com>
2023-02-02 09:21:01 -0500

    `gw-force-numeric-keyboard-on-mobile.php`: Added support for Number fields using the decimal-comma and currency formats.

--- experimental/gw-force-numeric-keyboard-on-mobile.php
+++ experimental/gw-force-numeric-keyboard-on-mobile.php
@@ -5,10 +5,14 @@
  *
  * This snippet forces the numeric keyboard for Number fields on mobile devices by updating the "type" attribute of the
  * input to "tel" instead of "number". If you find there are any drawbacks to this approach, let us know!
+ *
+ * Known Limitations
+ *
+ * 1. This will not work if you're collecting decimal numbers as mobile numeric keyboards do not support decimals or commas.
  */
 add_filter( 'gform_field_content', function( $content, $field ) {
 	if ( $field->get_input_type() === 'number' ) {
-		$content = str_replace( 'type=\'number\'', 'type=\'tel\'', $content );
+		$content = preg_replace( '/type=\'(number|text)\'/', 'type=\'tel\'', $content );
 	}
 	return $content;
 }, 10, 2 );
