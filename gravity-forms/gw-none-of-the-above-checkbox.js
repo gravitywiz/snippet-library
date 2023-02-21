6b5087f7ee97f370736874f7619e4614842f5a74
David Smith <david@gravitywiz.com>
2023-02-21 16:07:48 -0500

    `gw-none-of-the-above-checkbox.js`: Refactored to make applying to multiple fields much easier.

--- gravity-forms/gw-none-of-the-above-checkbox.js
+++ gravity-forms/gw-none-of-the-above-checkbox.js
@@ -2,32 +2,46 @@
  * Gravity Wiz // Gravity Forms // "None of the Above" Checkbox
  * https://gravitywiz.com/
  *
- * Instruction Video: https://www.loom.com/share/0f8de708790b4afd879bf0632efd7eae
+ * Instruction Video: https://www.loom.com/share/0f8de708790b4afd879bf0632efd7eae (Out of date)
  *
  * Use this snippet to enable a proper "None of the Above" option in your Checkbox fields. If any
  * other option is checked, the "None of the Above" option will be disabled. If the "None of the Above"
  * option is checked, all other options will be disabled.
+ *
+ * Instructions:
+ *
+ * 1. Install this snippet with our free Custom JavaScript plugin.
+ *    https://gravitywiz.com/gravity-forms-custom-javascript/
+ *
+ * 2. Add a new choice to your Checkbox field to act as the "None of the Above" choice.
+ *    This must be the last choice in the field.
+ *
+ * 3. Add "gw-none-of-the-above" to the Custom CSS Class field setting under the Appearance tab for
+ *    any Checkbox field to which this should be applied.
  */
-// Update "1" to your Checkbox field ID.
-var $field  = $( '#field_GFFORMID_1' );
-var $last   = $field.find( '.gchoice:last-child input' );
-var $others = $field.find( 'input' ).not( $last );
+$( '.gw-none-of-the-above' ).each( function() {
+	
+	var $field  = $( this );
+	var $last   = $field.find( '.gchoice:last-child input' );
+	var $others = $field.find( 'input' ).not( $last );
 
-$last.on( 'click', function() {
-	var $checkboxes = $field.find( 'input' ).not( $( this ) )
-	if ( $( this ).is( ':checked' ) ) {
-		$checkboxes
-			.prop( 'checked', false )
-			.prop( 'disabled', true );
-	} else {
-		$checkboxes.prop( 'disabled', false );
-	}
-} );
+	$last.on( 'click', function() {
+		var $checkboxes = $field.find( 'input' ).not( $( this ) )
+		if ( $( this ).is( ':checked' ) ) {
+			$checkboxes
+				.prop( 'checked', false )
+				.prop( 'disabled', true );
+		} else {
+			$checkboxes.prop( 'disabled', false );
+		}
+	} );
 
-$others.on( 'click', function() {
-	if ( $others.filter( ':checked' ).length ) {
-		$last.prop( 'disabled', true );
-	} else {
-		$last.prop( 'disabled', false );
-	}
+	$others.on( 'click', function() {
+		if ( $others.filter( ':checked' ).length ) {
+			$last.prop( 'disabled', true );
+		} else {
+			$last.prop( 'disabled', false );
+		}
+	} );
+
 } );
