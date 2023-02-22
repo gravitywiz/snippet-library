de756d677a957b38116bd5d7489ad8d7ee45c33e
David Smith <david@gravitywiz.com>
2023-02-22 16:00:45 -0500

    `gpfr-gf-salesforce-compatibility.php`: Renamed to move to correct folder.

--- gp-file-renamer/gpfr-gf-salesforce-compatibility.php
+++ gp-file-renamer/gpfr-gf-salesforce-compatibility.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Gravity Perks // File Renamer // Gravity Forms Salesforce Plugin Compatibility
  *
@@ -11,7 +10,6 @@
  * Compatibility snippet to ensure that files are renamed by GP File Renamer before being
  * uploaded to the Salesforce API by the Gravity Forms Salesforce plugin.
  */
-
 add_filter( 'plugins_loaded', function() {
 	global $vxg_salesforce;
 
@@ -20,13 +18,11 @@ add_filter( 'plugins_loaded', function() {
 	}
 
 	remove_action( 'gform_entry_created', array( $vxg_salesforce, 'gf_entry_created_before' ), 99 );
-}, 11 ); // 11 so that this happens after vxg salesforce
-
-
+}, 11 ); // Priority 11 so that this happens after GF Salesforce.
 
 add_filter( 'gform_entry_post_save', function( $entry, $form ) {
 	global $vxg_salesforce;
 	$vxg_salesforce->gf_entry_created_before( $entry, $form );
 
 	return $entry;
-}, 10, 2 ); // 10 so that this happens after GP-File-Renamer
+}, 10, 2 ); // Priority 10 so that this happens after GP File Renamer.
