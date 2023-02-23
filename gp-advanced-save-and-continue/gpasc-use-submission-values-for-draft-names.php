095f7617bb196f7a1d2735d79fe4e19304e1aaec
sbassah <sbassah@gmail.com>
2023-02-23 06:05:21 +0000

    `gpasc-use-submission-values-for-draft-names.php`: Added support to append the Name field value to the draft name.

--- gp-advanced-save-and-continue/gpasc-use-submission-values-for-draft-names.php
+++ gp-advanced-save-and-continue/gpasc-use-submission-values-for-draft-names.php
@@ -7,13 +7,18 @@
  *
  * Use draft submission values to populate the draft name.
  */
-
-add_filter( 'gpasc_draft_display_name', function( $display_name, $form_id, $resume_token_data ) {
+// Update "123" to your form ID.
+add_filter( 'gpasc_draft_display_name_123', function( $display_name, $form_id, $resume_token_data ) {
+	// Update "4" to the ID of the field to be used to set the draft name.
+	$field_id          = 4;
 	$submission_data   = GFFormsModel::get_draft_submission_values( $resume_token_data['token'] );
 	$submission_values = json_decode( $submission_data['submission'], true );
-
-	// submission values has a bunch of data on it including the values from each form field.
-
-	// TODO return some concatenation of the submission values to use as the draft name.
+	$submission_value  = rgar( $submission_values['submitted_values'], $field_id );
+	if ( ! is_array( $submission_value ) ) {
+		empty( $submission_value ) ? $display_name : $display_name .= ' - ' . $submission_value;
+	} else {
+		$full_name = $submission_value[ $field_id . '.3' ] . ' ' . $submission_value[ $field_id . '.6' ];
+		empty( trim( $full_name ) ) ? $display_name : $display_name .= ' - ' . $full_name;
+	}
 	return $display_name;
 }, 10, 3 );
