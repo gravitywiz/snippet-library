f4a72ddd149e7b47b7c2e8a094305eca93230f7e
David Smith <david@gravitywiz.com>
2023-03-20 23:04:17 -0400

    `gpcc-copy-comma-delimited-value-to-checkbox.js`: Created a new snippet.

---
+++ gp-copy-cat/gpcc-copy-comma-delimited-value-to-checkbox.js
@@ -0,0 +1,19 @@
+/**
+ * Gravity Perks // Copy Cat // Copy Comma-delimited Value to Checkbox Field
+ * https://gravitywiz.com/documentation/gravity-forms-copy-cat/
+ *
+ * Use Copy Cat to check the checkboxes in a Checkbox field based on a comma-delimited value
+ * from a single-value field (like a Single Line Text field).
+ *
+ * Instructions:
+ *
+ * 1. Install this snippet with our free Custom JavaScript plugin.
+ *    https://gravitywiz.com/gravity-forms-custom-javascript/
+ */
+gform.addFilter( 'gpcc_copied_value', function( sourceValues, $targetElem, field ) {
+  // Update "3" to your Checkbox field ID.
+	if ( field.target == 3 ) {
+		sourceValues = sourceValues[0].split( ', ' );
+	}
+	return sourceValues;
+} );
