e3a80e32ecc3597612546359c53c48f6d5dec82f
David Smith <david@gravitywiz.com>
2023-03-24 17:41:30 -0400

    `gpasc-draft-groups.php`: Created a new snippet that implements the concept of drafts groups, specified by a target query parameter.

---
+++ gp-advanced-save-and-continue/gpasc-draft-groups.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Gravity Perks // Advanced Save & Continue // Draft Groups by Query Parameter
+ * https://gravitywiz.com/documentation/gravity-forms-advanced-save-continue/
+ *
+ * Instruction Video: Incoming...
+ * 
+ * Create draft groups where only drafts saved from a URL with the same group ID 
+ * (specified by a query parameter) will be displayed when that query parameter is
+ * present.
+ */
+add_filter( 'gpasc_form_resume_tokens', function( $tokens, $form_id ) {
+	// Update "order_id" to your desired query parameter.
+	$target_parameter = 'order_id';
+	$target_value = rgget( $target_parameter );
+	if ( ! $target_value ) {
+		return $tokens;
+	}
+	foreach ( $tokens as &$token ) {
+		// If token is specifically passed via parameter, don't interfere.
+		$query_token = rgget( 'gf_token' );
+		if ( $query_token && $query_token === $token['token'] ) {
+			continue;
+		}
+		$path = parse_url( $token['form_path'], PHP_URL_QUERY );
+		parse_str( $path, $params );
+		if ( ! isset( $params[ $target_parameter ] ) || $params[ $target_parameter ] != $target_value ) {
+			$token = null;
+		}
+		unset( $token );
+	}
+	return array_filter( $tokens );
+}, 10, 2 );
