fc582bf4f90bee3ba7fd25e7bfcbdb82b96683d8
David Smith <david@gravitywiz.com>
2023-04-03 17:29:42 -0400

    `gpeb-restart-workflow-on-edit.php`: Updated as `gform_entry_created` doesn't have a form-specific version. 🤯

--- gp-entry-blocks/gpeb-restart-workflow-on-edit.php
+++ gp-entry-blocks/gpeb-restart-workflow-on-edit.php
@@ -3,9 +3,10 @@
  * Gravity Perks // Entry Blocks // Restart Workflow on Entry Edit
  * https://gravitywiz.com/documentation/gravity-forms-entry-blocks/
  */
-// Update "123" with your form ID.
-add_filter( 'gform_entry_created_123', function( $entry, $form ) {
-	if ( class_exists( 'Gravity_Flow_API' )
+add_filter( 'gform_entry_created', function( $entry, $form ) {
+	// Update "123" with your form ID.
+	if ( $form['id'] == 123
+		 && class_exists( 'Gravity_Flow_API' )
 	     && is_callable( 'gp_entry_blocks' )
 	     && gp_entry_blocks()->block_edit_form->has_submitted_edited_entry()
 	) {
