8115e1b1c8c0e032e382e42a4da255a16f3b2d41
David Smith <david@gravitywiz.com>
2023-04-03 17:19:08 -0400

    `gpeb-restart-workflow-on-edit.php`: PHPCS

--- gp-entry-blocks/gpeb-restart-workflow-on-edit.php
+++ gp-entry-blocks/gpeb-restart-workflow-on-edit.php
@@ -4,12 +4,12 @@
  * https://gravitywiz.com/documentation/gravity-forms-entry-blocks/
  */
 // Update "123" with your form ID.
-add_filter( 'gform_entry_created_123', function( $entry, $form) {
+add_filter( 'gform_entry_created_123', function( $entry, $form ) {
 	if ( class_exists( 'Gravity_Flow_API' )
 	     && is_callable( 'gp_entry_blocks' )
 	     && gp_entry_blocks()->block_edit_form->has_submitted_edited_entry()
 	) {
-		$api = new Gravity_Flow_API($form['id'] );
+		$api = new Gravity_Flow_API( $form['id'] );
 		$api->restart_workflow( $entry );
 	}
 }, 10, 2 );
