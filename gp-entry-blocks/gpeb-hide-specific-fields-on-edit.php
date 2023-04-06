3f62f26076ec65559eae1d03bc7c4f2eae8dbf7e
David Smith <david@gravitywiz.com>
2023-04-06 14:55:10 -0400

    `gpeb-hide-specific-fields-on-edit.php`: Deprecated.

--- gp-entry-blocks/gpeb-hide-specific-fields-on-edit.php
+++ gp-entry-blocks/gpeb-hide-specific-fields-on-edit.php
@@ -1,21 +1,6 @@
 <?php
 /**
- * Gravity Perks // Entry Blocks // Hide Specific Fields on Edit
- * https://gravitywiz.com/documentation/gravity-forms-entry-blocks/
+ * This snippet has evolved! 🦄
+ * Find the latest version of this snippet here: 
+ * https://github.com/gravitywiz/snippet-library/blob/master/gp-entry-blocks/gpeb-remove-fields-on-edit.php
  */
-// Update "123" to your form ID.
-add_filter( 'gform_pre_render_123', function ( $form ) {
-	// Update "4", "5", and "6" to the IDs of the fields you'd like to hide.
-	$fields_to_hide = array( 4, 5, 6 );
-	if ( ! class_exists( 'WP_Block_Supports' ) || rgar( WP_Block_Supports::$block_to_render, 'blockName' ) !== 'gp-entry-blocks/edit-form' ) {
-		return $form;
-	}
-	$filtered_fields = array();
-	foreach ( $form['fields'] as &$field ) {
-		if ( ! in_array( $field->id, $fields_to_hide ) ) {
-			$filtered_fields[] = $field;
-		}
-	}
-	$form['fields'] = $filtered_fields;
-	return $form;
-} );
