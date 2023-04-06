89bf46fac5438df9e077abb7e8e9f4708fadc29c
saifsultanc <saifraikkonen@gmail.com>
2023-04-06 23:08:29 +0530

    `gf-openai-post-save-result-to-field.php`: Added snippet to populate fields with Open AI generated values.

--- gravityforms-openai/gf-openai-post-save-result-to-field.php
+++ gravityforms-openai/gf-openai-post-save-result-to-field.php
@@ -1,16 +1,16 @@
 <?php
 /**
- * Gravity Perks // GravityForms-OpenAI // Add support to populate fields with Open AI generated values.
- *
+ * Gravity Perks // GravityForms-OpenAI // Add support to re-populate fields with Open AI generated values.
  * https://gravitywiz.com/
- *
+ * 
  * Requires 1.0-beta-1.2; or newer of Gravity Forms OpenAI.
+ * 
+ * Instruction Video: https://www.loom.com/share/341ab02ca790461fa852eda2f8e95fbc
  *
- * Instructions:
- *   - Install using https://gravitywiz.com/gravity-forms-custom-javascript/
+ * Instructions: https://gravitywiz.com/documentation/how-do-i-install-a-snippet/
  */
 // Replace 342 with the form id, and 3 with the targetted field id.
-add_action( 'gf_openai_post_save_result_to_field_342', 'gw_openai_post_save_result' ); 
+add_action( 'gf_openai_post_save_result_to_field_342', 'gw_openai_post_save_result' );
 function gw_openai_post_save_result( $result ) {
 	$_POST['input_3'] = $result;
 }
