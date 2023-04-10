1b0e04df808e652c28f88dcaae00399161d6d6d9
saifsultanc <saifraikkonen@gmail.com>
2023-04-10 18:21:12 +0530

    `gpnf-limit-entry-min-max-from-field.php`: Fixed an issue with the snippet stopping with error.

--- gp-nested-forms/gpnf-limit-entry-min-max-from-field.php
+++ gp-nested-forms/gpnf-limit-entry-min-max-from-field.php
@@ -131,7 +131,7 @@ class GP_Nested_Forms_Dynamic_Entry_Min_Max {
 
 					self.init = function () {
 
-						var attrReadyMaxFieldId = self.maxFieldId.replace( '.', '_' );
+						var attrReadyMaxFieldId = self.maxFieldId;
 						var maxFieldId = 'input_' + self.parentFormId + '_' + attrReadyMaxFieldId;
 
 						// GF uses "1" for the input index in the HTML id attribute for Single Product fields. Weird.
