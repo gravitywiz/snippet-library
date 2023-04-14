a768abbca7c0b43b2533b4b2b9ffa240f2ed1bf2
David Smith <david@gravitywiz.com>
2023-04-14 17:13:32 -0400

    `gpasc-draft-groups-by-page.php`: Added a new snippet to group drafts by the page they were submitted from.

---
+++ gp-advanced-save-and-continue/gpasc-draft-groups-by-page.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Gravity Perks // Advanced Save & Continue // Draft Groups by Page
+ * https://gravitywiz.com/documentation/gravity-forms-advanced-save-continue/
+ *
+ * Create draft groups where only drafts saved from the current URL will be displayed. For example, if you're using the
+ * same form on multiple pages, you can use this snippet to display drafts saved from the current page only.
+ */
+add_filter( 'gpasc_form_resume_tokens', function( $tokens, $form_id ) {
+
+	// Set ignore parameters to false if you would like parameters to be included in identifying unique pages.
+	$ignore_parameters = true;
+
+	if ( is_ajax() ) {
+		return $tokens;
+	}
+
+	$target_value = $_SERVER['REQUEST_URI'];
+	if ( $ignore_parameters ) {
+		$target_value = strtok( $target_value, '?' );
+	}
+
+	foreach ( $tokens as $index => $token ) {
+		// If token is specifically passed via parameter, don't interfere.
+		$query_token = rgget( 'gf_token' );
+		if ( $query_token && $query_token === $token['token'] ) {
+			continue;
+		}
+		$token_path = $token['form_path'];
+		if ( $ignore_parameters ) {
+			$token_path = strtok( $token['form_path'], '?' );
+		}
+		if ( $token_path !== $target_value ) {
+			unset( $tokens[ $index ] );
+		}
+	}
+
+	return array_filter( $tokens );
+}, 10, 2 );
