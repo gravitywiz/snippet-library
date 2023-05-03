3c46b629ba9800e98c9f7f602fcf41a5d8e58850
Coulter Peterson <29346967+coulterpeterson@users.noreply.github.com>
2023-05-03 09:55:30 -0400

    Added additional clarity and instruction for various uses

--- gravity-forms/gw-update-user-password-after-submission.php
+++ gravity-forms/gw-update-user-password-after-submission.php
@@ -7,8 +7,15 @@
  * 
  */
 
-// Update "123" to your form ID, "4" to your "Password" field ID, and "5" to your "New Password" field ID.
+// Update "123" to your form ID, "4" to your "Password" field ID, and "5" to your "Confirm Password" field ID.
+// If you're not using a "Confirm Password" field, change "$double_password_confirmation" from "true" to "false".
 add_filter( 'gform_field_validation_123_5', function( $result, $value, $form ) {
+
+    $double_password_confirmation = true;
+    
+    if( ! $double_password_confirmation ) {
+    	return;
+    }
 
     $master = rgpost( 'input_4' );
     if ( $result['is_valid'] && $value != $master ) {
@@ -20,6 +27,8 @@ add_filter( 'gform_field_validation_123_5', function( $result, $value, $form ) {
 }, 10, 4 );
 
 // Update "123" to your form ID, "4" to your "Email" field ID, and "5) to your "Password" field ID.
+// If you're using GP Nested Forms, set these to the Form and Field IDs of the child form that contains the email and password fields.
+// If you're using GP Populate Anything, make sure the "value" for the user email field is set to "User Email".
 add_action( 'gform_after_submission_123', function ( $entry, $form ) {
 
     $user = get_user_by( 'email', rgar( $entry, 4 ) );
