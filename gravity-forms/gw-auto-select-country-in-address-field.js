d062096f6b48eead456da199e607977d69cd3d8a
David Smith <david@gravitywiz.com>
2023-05-11 22:00:49 -0400

    `gw-auto-select-country-in-address-field.js`: Updated to trigger change event after country is auto-selected.

--- gravity-forms/gw-auto-select-country-in-address-field.js
+++ gravity-forms/gw-auto-select-country-in-address-field.js
@@ -8,8 +8,9 @@
  * 
  * 1. Install this snippet with our free Custom JavaScript plugin.
  *    https://gravitywiz.com/gravity-forms-custom-javascript/
- * 2. Customize the selector in the .always() callback accordingly.
+ * 2. Customize the selector for the $country variable below.
  */
+var $country = jQuery( '#input_GFFORMID_1_6' );
 jQuery
 	.get( {
 		url: 'https://ipinfo.io',
@@ -17,9 +18,14 @@ jQuery
 	} )
 	.always( function ( resp ) {
 		const countryCode = resp && resp.country ? resp.country : null;
-
-		// Customize selector
-		jQuery( '#input_GFFORMID_1_6' ).val( countryCode in isoAlpha2ToCountry ? isoAlpha2ToCountry[countryCode] : '' );
+	
+		var oldValue = $country.val();
+		var newValue = countryCode in isoAlpha2ToCountry ? isoAlpha2ToCountry[countryCode] : '';
+	
+		$country.val( newValue );
+		if ( newValue != oldValue ) {
+			$country.trigger( 'change' );
+		}
 	} );
 
 const isoAlpha2ToCountry = {
