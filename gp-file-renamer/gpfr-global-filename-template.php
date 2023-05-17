4208430ede4c20281e19effb4700088ce5734280
David Smith <david@gravitywiz.com>
2023-05-16 20:20:56 -0400

    `gpfr-global-filename-template.php`: Updated to use new filter parameters and correct function for processing the naming template.

--- gp-file-renamer/gpfr-global-filename-template.php
+++ gp-file-renamer/gpfr-global-filename-template.php
@@ -1,11 +1,13 @@
 <?php
 /**
- * Gravity Perks // GP File Renamer // Global Filename Template
+ * Gravity Perks // File Renamer // Global Filename Template
  * https://gravitywiz.com/documentation/gravity-forms-file-renamer/
  *
  * This snippet puts all files in an entry-specific subfolder and increments the filename with its count.
+ *
+ * Requires GP File Renamer v1.0.5+.
  */
-add_filter( 'gpfr_filename', function( $renamed_file, $file, $entry ) {
-	$renamed_file = gp_file_renamer()->rename_file( $file, '{entry:id}/{filename}-{i}', $entry );
+add_filter( 'gpfr_filename', function( $renamed_file, $file, $entry, $form, $field ) {
+	$renamed_file = gp_file_renamer()->get_template_value( '{entry:id}/{filename}-{i}', $file, $entry, $form, $field );
 	return $renamed_file;
-}, 10, 3 );
+}, 10, 5 );
