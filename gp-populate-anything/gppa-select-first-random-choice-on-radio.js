1c0f038fd6aefd7f014987475915d31ef4cb73a9
Haneef <298208+haneef95@users.noreply.github.com>
2023-05-23 17:06:12 +0100

    Clarified comment.

--- gp-populate-anything/gppa-select-first-random-choice-on-radio.js
+++ gp-populate-anything/gppa-select-first-random-choice-on-radio.js
@@ -6,7 +6,7 @@
  *    https://gravitywiz.com/gravity-forms-custom-javascript/
  */
 
-// This has also been tested to work with GP Populate Anything.
+// click vs checked: Click triggers other GPPA Fields to be populated appropriately as well, whereas checked does not trigger further reloads.
 // Update "3" to the the ID of your Radio Button field, and keep it as "0" for for the first choice.
 // Rename "SelectDefaultRoomType" to something unique.
 const SelectDefaultRoomType = document.getElementById('choice_' + GFFORMID + '_3_0'); SelectDefaultRoomType.click();
