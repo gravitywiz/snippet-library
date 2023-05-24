4f57ac55b0208fbbdeb4cc79666547d9e02ef83a
David Smith <david@gravitywiz.com>
2023-05-24 13:40:39 -0400

    Updated snippets for compatiblity with GPPA 2.0.

--- gp-populate-anything/gppa-populate-all-results.php
+++ gp-populate-anything/gppa-populate-all-results.php
@@ -5,7 +5,7 @@
  *
  * Instruction Video: https://www.loom.com/share/f324b8477a1140d7808b49b6b553258f
  *
- * By default, when populating a field's value, Populate Anything will only populated the first found result. This
+ * By default, when populating a field's value, Populate Anything will only populate the first found result. This
  * snippet adds support for populating all found results. Just add the "gppa-populate-all" CSS class to the field's
  * CSS Class Name setting.
  */
@@ -22,3 +22,11 @@ add_filter( 'gppa_process_template_value', function( $template_value, $field, $t
 	}
 	return $template_value;
 }, 10, 8 );
+
+// Required by GPPA 2.0+ to ensure that all results are returned.
+add_filter( 'gppa_query_all_value_objects', function( $should_query_all, $field ) {
+	if ( strpos( $field->cssClass, 'gppa-populate-all' ) !== false ) {
+		$should_query_all = true;
+	}
+	return $should_query_all;
+}, 10, 2 );
--- gp-populate-anything/gppa-populate-unique-results-count.php
+++ gp-populate-anything/gppa-populate-unique-results-count.php
@@ -7,3 +7,6 @@
 add_filter( 'gppa_get_input_values_123_4', function( $values, $field, $template, $objects ) {
 	return count( $objects );
 }, 10, 4 );
+
+// Update "123" and "4" to match the same form ID and field ID as above.
+add_filter( 'gppa_query_all_value_objects_123_4', '__return_true' );
