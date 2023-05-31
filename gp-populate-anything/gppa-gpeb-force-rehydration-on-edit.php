ff0120a57f134652e6b34ff35b68e457fb5f4844
David Smith <david@gravitywiz.com>
2023-05-31 15:47:22 -0400

    `gppa-gpeb-force-rehydration-on-edit.php`: Fixed fatal error with Populate Anythign 2.0+.

--- gp-populate-anything/gppa-gpeb-force-rehydration-on-edit.php
+++ gp-populate-anything/gppa-gpeb-force-rehydration-on-edit.php
@@ -5,6 +5,8 @@
  *
  * Use this snippet to force fields to be rehydrated via Populate Anything when they are rendered in
  * the Entry Blocks edit form.
+ *
+ * Note: This snippet requires Populate Anything 2.0+.
  */
 add_filter( 'gpeb_edit_form_entry', function( $entry ) {
 
@@ -14,14 +16,14 @@ add_filter( 'gpeb_edit_form_entry', function( $entry ) {
 	// Update (or remove) "4", "5", and "6" to the field IDs that should be rehydrated.
 	$field_ids = array( 4, 5, 6 );
 
-	if ( ! isset( $GLOBALS['gppa-field-values'] ) || $entry['form_id'] != $form_id ) {
+	if ( ! is_callable( array( 'GP_Populate_Anything', 'populate_field' ) ) || ! isset( $GLOBALS['gppa-field-values'] ) || $entry['form_id'] != $form_id ) {
 		return $entry;
 	}
 
 	$form = GFAPI::get_form( $form_id );
 	foreach ( $form['fields'] as &$field ) {
 		if ( in_array( $field->id, $field_ids ) ) {
-			$hydrated_field      = gp_populate_anything()->hydrate_field( $field, $form, array(), null );
+			$hydrated_field      = gp_populate_anything()->populate_field( $field, $form, array(), $entry );
 			$entry[ $field->id ] = $hydrated_field['field_value'];
 		}
 	}
