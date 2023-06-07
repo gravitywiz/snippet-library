6fa231f1a0924f948ca8a94966b5f696b3ed99b7
veryspry <ehlinger.matt@gmail.com>
2023-06-07 13:39:24 +0200

    `gpfr-gf-salesforce-compatibility.php`: Added usage instructions.

--- gp-advanced-save-and-continue/gpasc-use-dropdown-to-toggle-enabled.js
+++ gp-advanced-save-and-continue/gpasc-use-dropdown-to-toggle-enabled.js
@@ -3,12 +3,23 @@
  * https://gravitywiz.com/documentation/gravity-forms-advanced-save-continue/
  *
  * This snippet allows you to toggle GPASC client side with a dropdown.
+ * 
+ * Instructions:
+ *  1. Add snippet to form using https://gravitywiz.com/gravity-forms-custom-javascript/
+ *  2. Customize formId to match the form you want to use this with.
+ *  3. Customize the dropdownFieldId to match the DropdownField you want to use this with.
+ *  4. Customize dropdownEnableValue to the HTML "value" attribute which you want GPASC to
+ *     be enabled with. For example, if you want GPASC to be enabled when a dropdown
+ *     </option> with HTML value of "enabled" is selected, set this to "enabled".
  */
 
 // change this to match your form ID
 var formId = 1;
 // change this to match the dropdown field you'd like to use to control this
 var dropdownFieldId = 1;
+// change this to match the desired "value" (e.g. HTML value attribute) of the Dropdown Field
+// that, when selected, should enable GPASC.
+var dropdownEnableValue = '1';
 
 var gpascInstanceKey = 'GPASC_' + formId;
 var dropdownSelector = '#input_' + formId + '_' + dropdownFieldId;
@@ -30,7 +41,7 @@ function getDropdownValue() {
 }
 
 function shouldEnable() {
-	return getDropdownValue() === '1';
+	return getDropdownValue() === dropdownEnableValue;
 }
 
 window.gform.addAction('gpasc_js_init', function (formId) {
@@ -46,7 +57,8 @@ window.gform.addAction('gpasc_js_init', function (formId) {
 });
 
 $(dropdownSelector).on('change', function(event) {
-	if (event.target.value === '1') {
+	console.log({ event })
+	if (event.target.value === dropdownEnableValue) {
 		enableGPASC();
 	} else {
 		disableGPASC();
