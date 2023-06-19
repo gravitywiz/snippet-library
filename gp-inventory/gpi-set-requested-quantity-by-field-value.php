a044bb90300990acb97a76c2d9924bb13bba018f
Clay Griffiths <clay@claygriffiths.com>
2023-06-19 13:31:59 -0500

    `gpi-set-requested-quantity-by-field-value.php`: Updated snippet to use new `gpi_quantity_input_ids` filter hook.

--- gp-inventory/gpi-set-requested-quantity-by-field-value.php
+++ gp-inventory/gpi-set-requested-quantity-by-field-value.php
@@ -4,7 +4,7 @@
  * https://gravitywiz.com/documentation/gravity-forms-inventory/
  */
 // Update "123" to your form ID and "4" to your Inventory-enabled field.
-add_filter( 'gpi_requested_quantity_123_4', function( $requested_quantity ) {
+add_filter( 'gpi_quantity_input_ids_123_4', function( $input_ids, $field ) {
 	// Update "5" to the ID of the field that should be used to specify the quantity.
-	return rgpost( 'input_5' );
-} );
+	return array( 5 );
+}, 10, 2 );
