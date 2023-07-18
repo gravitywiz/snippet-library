09efdbc078a6dbba8a283e281cd286bf50b21191
saifsultanc <saifraikkonen@gmail.com>
2023-07-18 23:37:58 +0530

    `gw-prevent-duplicate-drop-down-selections.js`: Fixed an issue where options disabled by inventory could get re-enabled even if inventory was at zero.

--- experimental/gw-prevent-duplicate-drop-down-selections.js
+++ experimental/gw-prevent-duplicate-drop-down-selections.js
@@ -23,6 +23,10 @@ jQuery(".gfield_ddselect").change(function () {
 	jQuery(".gfield_ddselect").each(function (a) {
 		var parent_el = jQuery(this);
 		jQuery("option", jQuery(this)).each(function (b) {
+			// With GP Inventory, any Inventory Exhausted choice should remain disabled.
+			if ($(this).attr('class') != 'gpi-disabled') {
+				jQuery(this).prop("disabled", false);
+			}
 			if (b != 0) {
 				for (c = 0; c < select_num; c++) {
 					if (a != c) {
