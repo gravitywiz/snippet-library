b970d7a9841b2ab6fa12c915ee7ed300a854f3cf
David Smith <david@gravitywiz.com>
2023-08-28 21:32:07 -0400

    `gw-prevent-duplicate-selections.js`: Added support for Drop Down and Enhanced-UI-enabled Multi Select fields.

--- gravity-forms/gw-prevent-duplicate-selections.js
+++ gravity-forms/gw-prevent-duplicate-selections.js
@@ -2,11 +2,8 @@
  * Gravity Wiz // Gravity Forms // Prevent Duplicate Selections
  * https://gravitywiz.com/
  *
- * Prevent duplicate selections in choice-based fields. Currently works with Checkbox and
- * Radio Button fields with plans to support all choice-based fields (e.g. Drop Downs & Multi Selects).
- *
- * For Drop Down field support, use this snippet:
- * https://github.com/gravitywiz/snippet-library/blob/master/experimental/gw-prevent-duplicate-drop-down-selections.js
+ * Prevent duplicate selections in choice-based fields. Currently works with Checkbox, Radio Button, Drop Down and
+ * Enhanced-UI-enabled Multi Select fields.
  *
  * Instructions:
  *
@@ -17,34 +14,51 @@
  *    should be prevented.
  */
 window.gform.addFilter( 'gplc_excluded_input_selectors', function( selectors ) {
-	selectors.push('.gw-disable-duplicates-disabled');
+	selectors.push( '.gw-disable-duplicates-disabled' );
 	return selectors;
 });
 
-$inputs = $( '.gw-prevent-duplicates' ).find( 'input' );
+$inputs = $( '.gw-prevent-duplicates' ).find( 'input, select' );
 
-$inputs.change( function() {
+$inputs.change( function( event, selected ) {
+	gwDisableDuplicates( $( this ), $inputs, selected );
+} );
+
+$inputs.each( function( event ) {
 	gwDisableDuplicates( $( this ), $inputs );
 } );
 
-$inputs.each( function() {
-	gwDisableDuplicates( $( this ), $inputs );
-} );
+function gwDisableDuplicates( $elem, $group, selected ) {
 
-function gwDisableDuplicates( $elem, $group ) {
+	// Some elements have a parent element (e.g. a <select>) that contains the actual elements (e.g. <option>) we want enable/disable.
+	let $parent = $elem;
+
+	if ( $elem.is( 'select' ) ) {
+		// Multi Selects fields require Chosen to be enabled. It provides the `selected` variable which indicates which
+		// option was selected/deselected.
+		if ( selected ) {
+			let value = selected.selected ? selected.selected : selected.deselected;
+			$elem     = $elem.find( '[value="' + value + '"]' );
+		} else {
+			$elem = $elem.find( 'option:selected' );
+		}
+		// Note: This prevents selects from working with other field types.
+		$group = $group.find( 'option' );
+	}
 
 	let value     = $elem.val();
-	let $targets  = $group.not( $elem ).not( '.gplc-disabled' );
+	let $targets  = $group.not( $elem ).not( '.gplc-disabled' ).not( '.gpi-disabled' );
 	let isChecked = $elem.is( ':checked' );
+
 	// We use this to instruct Gravity Forms not to re-enable disabled duplicate options when
 	// that option is revealed by conditional logic.
 	let disabledClass = 'gf-default-disabled gw-disable-duplicates-disabled';
 	let previousValue;
 
-	// Only one choice can be selected in a Radio Button field while multiple choices
-	// can be selected in a Checkbox field. This logic handles saving/retrieving the previous
-	// value and re-enabling inputs with the previous value.
-	if ( $elem.is( ':radio' ) ) {
+	// Only one choice can be selected in a Radio Button or Drop Down field while multiple choices
+	// can be selected in a Checkbox or Multi Select field. This logic handles saving/retrieving the
+	// previous value and re-enabling inputs/options with the previous value.
+	if ( $elem.is( ':radio, option' ) && ! $parent.prop( 'multiple' ) ) {
 		previousValue = $elem.parents( '.gfield' ).data( 'previous-value' );
 		$elem.parents( '.gfield' ).data( 'previous-value', $elem.val() );
 		if ( previousValue ) {
@@ -57,7 +71,19 @@ function gwDisableDuplicates( $elem, $group ) {
 
 	let $filteredTargets = $targets
 		.filter( '[value="{0}"]'.format( value ) )
-		.prop( 'disabled', $elem.is( ':checked' ) );
+		.prop( 'disabled', isChecked );
+
+	// For Drop Down and Multi Selects, we need to loop through each field and select the first available option - and -
+	// trigger Chosen to update the select box so newly disabled options are displayed as disabled.
+	if ( $elem.is( 'option' ) ) {
+		$filteredTargets.parents( 'select' ).each( function() {
+			let $options = $( this ).find( 'option' );
+			if ( $options.filter( ':selected:disabled' ).length ) {
+				$options.not( ':disabled' ).first().prop( 'selected', true );
+			}
+			$( this ).trigger( 'chosen:updated' );
+		} );
+	}
 
 	if ( isChecked ) {
 		$filteredTargets.addClass( disabledClass );
