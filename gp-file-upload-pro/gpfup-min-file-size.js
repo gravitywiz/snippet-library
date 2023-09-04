ca03d89836e27e41e7fd35dc8d9c319a9d75d09b
David Smith <david@gravitywiz.com>
2023-09-04 16:19:03 -0400

    `gpfup-min-file-size.js`: Added new snippet for setting a minimum file size.

---
+++ gp-file-upload-pro/gpfup-min-file-size.js
@@ -0,0 +1,27 @@
+/**
+ * Gravity Perks // File Upload Pro // Set Minimum File Size
+ * https://gravitywiz.com/documentation/gravity-forms-file-upload-pro/
+ *
+ * Specify a minimum size per file.
+ *
+ * Instructions:
+ *
+ * 1. Install this snippet with our free Custom JavaScript plugin.
+ *    https://gravitywiz.com/gravity-forms-custom-javascript/
+ */
+window.gform.addAction( 'gpfup_before_upload', (formId, fieldId, file, up, gpfupInstance) => {
+	// Specify min size per file in megabytes.
+	var maxTotalSize = 1;
+		
+	if ( file.size < maxTotalSize * 1000000 ) {
+		file.type = 'application/octet-stream'; // Prevent image processing
+		
+		gpfupInstance.handleFileError( up, file, {
+			code: 'too_little_file',
+			message: 'File size must be at least ' + maxTotalSize + 'MB.',
+		} );
+		
+		up.stop();
+		up.start();
+	}
+} );
