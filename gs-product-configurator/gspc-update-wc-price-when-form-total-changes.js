4a9cd6f9a737a36ed108a2ed41a482df5f177281
Clay Griffiths <clay@claygriffiths.com>
2023-09-05 17:51:02 -0500

    `gspc-update-wc-price-when-form-total-changes.js`: Added snippet to update WooCommerce product prices on page when the form total changes.

---
+++ gs-product-configurator/gspc-update-wc-price-when-form-total-changes.js
@@ -0,0 +1,16 @@
+/**
+ * Gravity Shop // Product Configurator // Update product price when form total changes
+ * https://gravitywiz.com/documentation/gravity-shop-product-configurator/
+ * 
+ * Instructions:
+ *  1. Add to form using https://gravitywiz.com/gravity-forms-custom-javascript/
+ */
+jQuery('.ginput_total_GFFORMID').on('change', function() {
+  var total = jQuery(this).val();
+  var formatted = gformFormatMoney(total, true);
+
+  // Find any character outside of 0-9, period, or comma and wrap it with span.woocommerce-Price-currencySymbol
+  formatted = formatted.replace(/[^0-9\.,]/g, '<span class="woocommerce-Price-currencySymbol">$&</span>');
+
+  jQuery('p.price .woocommerce-Price-amount bdi').html(formatted);
+});
