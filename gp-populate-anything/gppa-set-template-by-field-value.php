5d74d75ebbebc24ac3885ce70b7403ce4155613a
David Smith <david@gravitywiz.com>
2023-09-04 22:15:23 -0400

    `gppa-set-template-by-field-value.php`: Added video.

--- gp-populate-anything/gppa-set-template-by-field-value.php
+++ gp-populate-anything/gppa-set-template-by-field-value.php
@@ -3,7 +3,7 @@
  * Gravity Perks // Populate Anything // Set Template by Field Value
  * https://gravitywiz.com/documentation/gravity-forms-populate-anything/
  * 
- * Instruction Video: Coming soon... 
+ * Instruction Video: https://www.loom.com/share/02cb5511b5ba4cf6a051ee7038f55f16
  *
  * I'll take another stab at explaining this another day. For now, watch the instructional video above. 😅
  */
