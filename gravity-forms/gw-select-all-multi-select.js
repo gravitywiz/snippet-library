a7d31cb10d851cf5232fadd5666d1d5bbe492a87
David Smith <david@gravitywiz.com>
2023-09-11 22:57:56 -0400

    `gw-select-all-multi-select.js`: Added "Clear All" option.

--- gravity-forms/gw-select-all-multi-select.js
+++ gravity-forms/gw-select-all-multi-select.js
@@ -14,13 +14,23 @@
 // Update "4" to your Multi Select field ID.
 let targetFieldId = 4;
 
-let $button = $( '<button type="button" style="margin-top: 1rem;">Select All</button>' );
-let $target = $( '#input_GFFORMID_' + targetFieldId );
+let $selectAll = $( '<button type="button" style="margin-top: 1rem;">Select All</button>' );
+let $clearAll  = $( '<button type="button" style="margin: 1rem 0 0 0.5rem;">Clear All</button>' );
+let $target    = $( '#input_GFFORMID_' + targetFieldId );
 
-$target.parents( '.ginput_container' ).append( $button );
+$target.parents( '.ginput_container' ).append( $selectAll, $clearAll );
 
-$button.on( 'click', function() {
-	$target.find( 'option' ).prop( 'selected', true );
-	window[ 'GPAdvancedSelect_GFFORMID_' + targetFieldId ].sync();
-	$target.trigger( 'change' );
+$selectAll.on( 'click', function() {
+	selectAllOptions( $target);
 } );
+
+$clearAll.on( 'click', function() {
+	selectAllOptions( $target, false );
+} );
+
+function selectAllOptions( $select, selected ) {
+	selected = typeof selected !== 'undefined' ? selected : true;
+	$select.find( 'option' ).prop( 'selected', selected );
+	window[ 'GPAdvancedSelect_GFFORMID_' + targetFieldId ].sync();
+	$select.trigger( 'change' );
+}
