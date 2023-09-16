27c023dd47bbeb06770fc79bf8c5c44273cd159e
David Smith <david@gravitywiz.com>
2023-09-16 08:38:41 -0400

    `gpaa-gf-address-enhanced.js`: Added more details in the description.

--- experimental/gpaa-gf-address-enhanced.js
+++ experimental/gpaa-gf-address-enhanced.js
@@ -1,13 +1,16 @@
 /**
- * Gravity Perks // GP Address Autocomplete // Populate Province when GF Address Enhanced is enabled
+ * Gravity Perks // GP Address Autocomplete // Populate State/Province Drop Down for GF Address Enhanced
  * https://gravitywiz.com/documentation/gravity-forms-address-autocomplete
+ *
+ * Add support for automatically selecting the state/province when using the Gravity Forms Address Enhanced plugin.
+ * GF Address Enhanced does not preserve the populated value in the input when the field is converted
+ * into a select after the country is automatically populated by GPAA.
  *
  * Instructions:
  *     1. Install our free Custom JavaScript for Gravity Forms plugin.
  *         Download the plugin here: https://gravitywiz.com/gravity-forms-custom-javascript/
  *     2. Copy and paste the snippet into the editor of the Custom Javascript for Gravity Forms plugin.
  */
-
 gform.addAction('gpaa_fields_filled', function (place, instance, formId, fieldId) {
 	place.address_components.forEach( function( component ) { 
 		if ( component.types.indexOf( 'administrative_area_level_1' ) !== -1 ) {
