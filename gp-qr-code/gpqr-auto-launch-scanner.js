5fa1526f80537640ac1cb378a20c557c331340c2
David Smith <david@gravitywiz.com>
2023-09-20 15:13:45 -0400

    `gpqr-auto-launch-scanner.js`: Added new snippet.

---
+++ gp-qr-code/gpqr-auto-launch-scanner.js
@@ -0,0 +1,14 @@
+/**
+ * Gravity Perks // GP QR Code // Automatically Launch Scanner on Form Render
+ * https://gravitywiz.com/documentation/gravity-forms-qr-code/
+ *
+ * When using the QR scanner, automatically launch the scanner after the form has rendered.
+ *
+ * Instructions:
+ * 
+ * 1. Install this snippet with our free Custom JavaScript plugin.
+ *    https://gravitywiz.com/gravity-forms-custom-javascript/
+ */
+setTimeout( function() {
+	$( '.gpqr-scanner-button' ).click();
+} );
