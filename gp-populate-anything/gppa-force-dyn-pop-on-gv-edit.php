a32f10dbd918eabadd5da4a15f3edf59a15c8797
saifsultanc <saifraikkonen@gmail.com>
2023-09-28 16:42:59 +0530

    `gppa-force-dyn-pop-on-gv-edit.php`: Fixed an issue with GPPA GV Edit on GPPA 2.0

--- gp-populate-anything/gppa-force-dyn-pop-on-gv-edit.php
+++ gp-populate-anything/gppa-force-dyn-pop-on-gv-edit.php
@@ -14,7 +14,7 @@ add_filter( 'gravityview/edit_entry/field_value', function( $field_value, $field
 	}
 
 	// Update "123" to your form ID and "4" to your field ID.
-	if ( $field->formId == 268 && $field->id == 1 ) {
+	if ( $field->formId == 123 && $field->id == 4 ) {
 		if ( isset( $GLOBALS['gppa-field-values'] ) ) {
 			// Based on GPPA version, we will need a different method call.
 			$hydrated_field = version_compare( $gppa_version, '2.0', '>=' ) ?
