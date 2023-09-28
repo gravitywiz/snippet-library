a5977b410503eea677087fed5a7af5546176e2fe
saifsultanc <saifraikkonen@gmail.com>
2023-09-28 20:24:11 +0530

    `gppa-force-dyn-pop-on-gv-edit.php`: Fixed an issue with GPPA GV Edit on GPPA 2.0.

--- gp-populate-anything/gppa-force-dyn-pop-on-gv-edit.php
+++ gp-populate-anything/gppa-force-dyn-pop-on-gv-edit.php
@@ -7,19 +7,10 @@
  * https://github.com/gravitywiz/snippet-library/blob/master/gp-populate-anything/gppa-gpnf-force-dyn-pop-on-gv-edit.php
  */
 add_filter( 'gravityview/edit_entry/field_value', function( $field_value, $field ) {
-	$gppa_version = defined( 'GPPA_VERSION' ) ? GPPA_VERSION : 'NA';
-	// If GPPA is not active, return.
-	if ( $gppa_version === 'NA' ) {
-		return $field_value;
-	}
-
 	// Update "123" to your form ID and "4" to your field ID.
 	if ( $field->formId == 123 && $field->id == 4 ) {
 		if ( isset( $GLOBALS['gppa-field-values'] ) ) {
-			// Based on GPPA version, we will need a different method call.
-			$hydrated_field = version_compare( $gppa_version, '2.0', '>=' ) ?
-				gp_populate_anything()->populate_field( $field, GFAPI::get_form( $field->formId ), array(), null, false ) :
-				gp_populate_anything()->hydrate_field( $field, GFAPI::get_form( $field->formId ), array(), null, false );
+			$hydrated_field = gp_populate_anything()->populate_field( $field, GFAPI::get_form( $field->formId ), array(), null, false );
 			$field_value    = $hydrated_field['field_value'];
 		}
 	}
