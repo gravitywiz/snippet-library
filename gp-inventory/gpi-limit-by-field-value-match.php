83924f87249fdb1ad665a25171e7c444459dffb6
David Smith <david@gravitywiz.com>
2023-10-02 12:25:42 -0400

    `gpi-limit-by-field-value-match.php`: Fixed issue with static DB prefix.

--- gp-inventory/gpi-limit-by-field-value-match.php
+++ gp-inventory/gpi-limit-by-field-value-match.php
@@ -7,10 +7,11 @@
  */
 // Update "123" to your form ID and "4" to your Inventory-enabled field ID.
 add_filter( 'gpi_query_123_4', function ( $query ) {
+	global $wpdb;
 
 	// Update "5" to the ID of the field you would like to match and "xyz" to the value you would like to match.
 	$query['where'] .= ' AND em_by_field.meta_key = "5" AND em_by_field.meta_value = "xyx" ';
-	$query['join']  .= ' INNER JOIN wp_gf_entry_meta em_by_field ON em_by_field.entry_id = em.entry_id ';
+	$query['join']  .= " INNER JOIN {$wpdb->prefix}gf_entry_meta em_by_field ON em_by_field.entry_id = em.entry_id ";
 
 	return $query;
 } );
