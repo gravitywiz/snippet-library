a7e5f0be3657f867cb3a887f70ab7d72515c4c61
David Smith <david@gravitywiz.com>
2023-10-14 12:32:18 -0400

    `gw-get-total-paid-by-email.php`: Added new snippet.

---
+++ gravity-forms/gw-get-total-paid-by-email.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Gravity Wiz // Gravity Forms // Get Total Paid by Email
+ * https://gravitywiz.com/
+ *
+ * Use this helper function to get the total paid across all forms for a email address.
+ * 
+ * Example:
+ *
+ * $total = gw_get_total_by_email( 'dave@smiff.com' );
+ */
+function gw_get_total_by_email( $email, $form_ids = null ) {
+	$entries = gfapi::get_entries( $form_ids, array(
+		'field_filters' => array(
+			array( 'value' => $email ),
+			array( 'key' => 'payment_status', 'value' => 'paid' )
+		)
+	) );
+	$total   = 0;
+	foreach ( $entries as $entry ) {
+		$total += floatval( $entry['payment_amount'] );
+	}
+	return $total;
+}
