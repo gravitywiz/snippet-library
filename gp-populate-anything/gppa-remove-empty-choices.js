aba07fccb7b0e412bdd25c36b959dca9974bc2c5
David Smith <david@gravitywiz.com>
2023-10-20 17:26:22 -0400

    `gppa-remove-empty-checkbox-choices.js`: Updated snippet name to `gppa-remove-empty-choices.js` and added support for Radio Buttons.

--- gp-populate-anything/gppa-remove-empty-choices.js
+++ gp-populate-anything/gppa-remove-empty-choices.js
@@ -1,25 +1,25 @@
 /**
- * Gravity Perks // Populate Anything // Remove Empty Checkbox Choices
+ * Gravity Perks // Populate Anything // Remove Empty Choices
  * https://gravitywiz.com/documentation/gravity-forms-populate-anything/
  *
  * 1. Install this snippet with our free Custom JavaScript plugin.
  *    https://gravitywiz.com/gravity-forms-custom-javascript/
  */
 
- var $containers = $( '.gfield_checkbox' );
+ var $containers = $( '.gfield--type-choice' );
  
 // On page load.
 $containers.each( function() {
-	gwizRemoveEmptyCheckboxes( $( this ) );
+	gwizRemoveEmptyChoices( $( this ) );
 } );
 
 // Show/hide checkboxes when the field is manipulated.
 $containers.bind( 'DOMNodeInserted DOMNodeRemoved', function() {
-	gwizRemoveEmptyCheckboxes( $( this ) );
+	gwizRemoveEmptyChoices( $( this ) );
 } );
 
-function gwizRemoveEmptyCheckboxes( $elem ) {
-	$elem.find( 'input[type="checkbox"]' ).each( function() { 
+function gwizRemoveEmptyChoices( $elem ) {
+	$elem.find( 'input[type="checkbox"], input[type="radio"]' ).each( function() { 
 		if( ! $( this ).val() ) {
 			$( this ).parent().hide();
 		}
