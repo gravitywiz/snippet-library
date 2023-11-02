9635651f335539eddc48831cf80435b1baddac2f
Clay Griffiths <clay@claygriffiths.com>
2023-11-02 14:25:42 -0500

    `gppa-force-dyn-pop-on-gv-edit.php`: Updated snippet to work with GPPA 2.0+.

--- gp-populate-anything/gppa-force-dyn-pop-on-gv-edit.php
+++ gp-populate-anything/gppa-force-dyn-pop-on-gv-edit.php
@@ -6,13 +6,23 @@
  * If you need to force repopulation when editing a child entry via GravityView, see:
  * https://github.com/gravitywiz/snippet-library/blob/master/gp-populate-anything/gppa-gpnf-force-dyn-pop-on-gv-edit.php
  */
-add_filter( 'gravityview/edit_entry/field_value', function( $field_value, $field ) {
-	// Update "123" to your form ID and "4" to your field ID.
-	if ( $field->formId == 123 && $field->id == 4 ) {
-		if ( isset( $GLOBALS['gppa-field-values'] ) ) {
-			$hydrated_field = gp_populate_anything()->hydrate_field( $field, GFAPI::get_form( $field->formId ), array(), null, false );
-			$field_value    = $hydrated_field['field_value'];
+add_filter( 'gravityview/edit_entry/field_value', function( $field_value, $field, $_this ) {
+	// Start customizing
+	$form_id               = 1;
+	$field_ids_to_populate = array( 9, 10 );
+	// End customizing
+
+	if ( $field->formId == $form_id && in_array( $field->id, $field_ids_to_populate ) ) {
+		$populated_field = gp_populate_anything()->populate_field( $field, $_this->form, gp_populate_anything()->get_posted_field_values( $_this->form ), $_this->entry );
+
+		if ( $field->storageType === 'json' ) {
+			$field_value = json_encode( $populated_field['field_value'] );
+		} else {
+			$field_value = $populated_field['field_value'];
 		}
+
+		$GLOBALS['gppa-field-values'][ $field->formId ][ $field->id ] = $field_value;
 	}
+
 	return $field_value;
-}, 10, 2 );
+}, 10, 3 );
