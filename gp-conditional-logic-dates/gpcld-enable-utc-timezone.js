6302c5d383602f5f41aa42e495feb2a4ec703729
scyt <scott@gravitywiz.com>
2023-11-02 10:59:58 -0400

    `gpcld-enable-utc-timezone.js`: Removed due to deprecated filter. (#726)
    * `gpcld-enable-utc-timezone.js`: Removed due to deprecated filter.
    
    * `gpcld-enable-utc-timezone.js`: Deprecated.
    
    * `gpcld-enable-utc-timezone.js`: Deprecated.
    
    * `gpcld-enable-utc-timezone.js`: Added link to docs.

--- gp-conditional-logic-dates/gpcld-enable-utc-timezone.js
+++ gp-conditional-logic-dates/gpcld-enable-utc-timezone.js
@@ -1,4 +1,9 @@
 /**
+ * WARNING! THIS SNIPPET IS DEPRECATED. 🚧
+ * This snippet has been replaced by the `gpcld_use_visitor_timezone` filter.
+ * https://gravitywiz.com/documentation/gpcld_use_visitor_timezone/
+ */
+/**
  * Gravity Perks // Conditional Logic Dates // Adjust User's Local Time to UTC
  * https://gravitywiz.com/documentation/gravity-forms-conditional-logic-dates/
  *
