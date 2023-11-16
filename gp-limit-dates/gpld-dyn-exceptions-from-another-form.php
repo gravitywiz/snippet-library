2287e97c94760dcc68f606fe56b0a5a71c3546b5
David Smith <david@gravitywiz.com>
2023-11-16 15:17:10 -0500

    `gpld-dynamic-exceptions-from-another-form.php`: Added new snippet.

---
+++ gp-limit-dates/gpld-dyn-exceptions-from-another-form.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Gravity Perks // Limit Dates // Dynamically Set Exceptions from Another Form
+ * https://gravitywiz.com/documentation/gravity-forms-limit-dates/
+ *
+ * This snippet will dynamically populate the Limit Dates exception dates for a Date field on Form A from the entry data
+ * for a Date field on Form B.
+ */
+// Update "123" to your target form ID and "4" to your target field ID.
+add_filter( 'gpld_limit_dates_options_123_4', function( $field_options, $form, $field ) {
+
+	if ( ! isset( $field_options['exceptions'] ) || ! is_array( $field_options['exceptions'] ) ) {
+		$field_options['exceptions'] = array();
+	}
+
+  // Update "124" to the ID of the form from which the exceptions should be pulled.
+	$source_form_id  = 124;
+
+  // Update "5" to the ID of the field on the source form from which the exceptions should be pulled.
+	$source_field_id = 5;
+
+	$field_options['exceptionMode'] = 'disable';
+
+	$query_args = array(
+		'field_filters' => array(
+			array(
+				'key' => $source_field_id,
+				'value' => gmdate( 'Y-m-d' ),
+				'operator' => '>=',
+			),
+		),
+	);
+
+	$paging = array( 'offset' => 0, 'page_size' => 50 );
+
+	$entries  = GFAPI::get_entries( $source_form_id, $query_args, null, $paging );
+
+	foreach ( $entries as $entry ) {
+		$field_options['exceptions'][] = date( 'm/d/Y', strtotime( rgar( $entry, $source_field_id ) ) );
+	}
+
+	return $field_options;
+}, 10, 3 );
