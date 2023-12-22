dd748b7e5d3eb3b11ecd7d908513a0e7f7682cd4
Clay Griffiths <clay@claygriffiths.com>
2023-12-21 20:13:22 -0600

    `gpadvs-enable-add-new-option.js`: Updated path to new snippet.

--- gp-advanced-select/gpadvs-enable-add-new-option.js
+++ gp-advanced-select/gpadvs-enable-add-new-option.js
@@ -1,5 +1,5 @@
 /**
  * This snippet has evolved! 🦄
  * Find the new version of this snippet here:
- * https://github.com/gravitywiz/snippet-library/gp-advanced-select/gpadvs-enable-add-new-option.php
+ * https://github.com/gravitywiz/snippet-library/blob/master/gp-advanced-select/gpadvs-enable-add-new-option.php
  */
