f67f43aed39c3778597ac7065eac3a11eef04f33
Clay Griffiths <clay@claygriffiths.com>
2024-01-14 22:38:52 -0600

    `gpml-acf-user-image-field.php`: Improved compatibility with GF User Registration.

--- gp-media-library/gpml-acf-user-image-field.php
+++ gp-media-library/gpml-acf-user-image-field.php
@@ -7,10 +7,12 @@
  * Plugin URI:   https://gravitywiz.com/documentation/gravity-forms-media-library/
  * Description:  Set the user profile image using image uploaded via Gravity Forms.
  * Author:       Gravity Wiz
- * Version:      1.2
+ * Version:      1.3
  * Author URI:   https://gravitywiz.com/
  */
 class GPML_ACF_User_Image_Field {
+
+    protected $_args;
 
 	public function __construct( $args ) {
 
@@ -28,6 +30,7 @@ class GPML_ACF_User_Image_Field {
 
 		add_action( 'gform_user_registered', array( $this, 'update_user_image_field' ), 10, 3 );
 		add_action( 'gform_user_updated', array( $this, 'update_user_image_field' ), 10, 3 );
+        add_filter( "gform_gravityformsuserregistration_pre_process_feeds_" . $this->_args['form_id'], array( $this, 'disable_gfur_for_metakey' ), 10, 3 );
 
 	}
 
@@ -50,6 +53,36 @@ class GPML_ACF_User_Image_Field {
 
 		}
 	}
+
+    /**
+     * If using this snippet with GF User Registration, we need to remove the mapping for the metakey to prevent GFUR
+     * from clearing out the value if an existing file is already set.
+     */
+    function disable_gfur_for_metakey( $feeds, $entry, $form ) {
+        foreach ( $feeds as &$feed ) {
+            // If the feed is not set to update, skip it.
+            if ( rgars( $feed, 'meta/feedType' ) !== 'update' ) {
+                continue;
+            }
+
+            $dynamic_field_map_fields = rgars( $feed, 'meta/userMeta' );
+
+            if ( empty( $dynamic_field_map_fields ) || ! is_array( $dynamic_field_map_fields ) ) {
+                continue;
+            }
+
+            // Remove the metakey mapping.
+            foreach ( $dynamic_field_map_fields as $difm_index => $dynamic_field_map_field ) {
+                if ( $dynamic_field_map_field['key'] === $this->_args['meta_key'] ) {
+                    unset( $dynamic_field_map_fields[$difm_index] );
+                }
+            }
+
+            $feed['meta']['userMeta'] = $dynamic_field_map_fields;
+        }
+
+        return $feeds;
+    }
 
 }
 
