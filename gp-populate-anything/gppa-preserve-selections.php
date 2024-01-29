3eb6184f515d5c85660aa7099b47297f04478565
saifsultanc <saifraikkonen@gmail.com>
2024-01-29 15:31:53 +0530

    `gppa-preserve-selections.php`: Fixed an issue with checkboxes.

--- gp-populate-anything/gppa-preserve-selections.php
+++ gp-populate-anything/gppa-preserve-selections.php
@@ -7,9 +7,7 @@
 add_filter( 'gppa_input_choices_123_4', function( $choices, $field, $objects ) {
 	$selected_values = (array) rgar( gp_populate_anything()->get_field_values_from_request(), $field->id );
 	foreach ( $choices as &$choice ) {
-		if ( in_array( $choice['value'], $selected_values, true ) ) {
-			$choice['isSelected'] = true;
-		}
+		$choice['isSelected'] = true;
 	}
 	return $choices;
 }, 10, 3 );
