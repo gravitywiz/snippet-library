724699f8d764375540b1654cfdccd7a14c1675e2
David Smith <david@gravitywiz.com>
2024-02-14 16:12:34 -0500

    `gpfr-custom-file-paths-in-dropbox.php`: Added support for Multi-file Upload fields and fixed an issue where path included filename.

--- gp-file-renamer/gpfr-custom-file-paths-in-dropbox.php
+++ gp-file-renamer/gpfr-custom-file-paths-in-dropbox.php
@@ -8,18 +8,22 @@
  * Activate this snippet to honor custom file paths (defined in GPFR's Filename Template setting) are honored when a
  * file is uploaded to Dropbox (via the GF Drobox add-on).
  */
-add_filter( 'gform_dropbox_folder_path', function( $folder_path, $form, $field_id, $entry, $feed ) {
+add_filter( 'gform_dropbox_folder_path', function ( $folder_path, $form, $field_id, $entry, $feed ) {
 	$field = GFAPI::get_field( $form, $field_id );
 	if ( $field->gpfrTemplate ) {
-		$file_url     = $entry[ $field_id ];
+		$file_url = $entry[ $field_id ];
 		if ( $file_url ) {
-      // Note: Using this method means this will only consistently work on initial submission. If Dropbox feed is processed at
-      // some later date (either when an entry is edited or as part of a workflow), this may not work as default upload paths
-      // are date-sensitive.
+			if ( $field->multipleFiles ) {
+				$file_url = json_decode( $file_url )[0];
+			}
+			// Note: Using this method means this will only consistently work on initial submission. If Dropbox feed is processed at
+			// some later date (either when an entry is edited or as part of a workflow), this may not work as default upload paths
+			// are date-sensitive.
 			$upload_roots = GF_Field_FileUpload::get_default_upload_roots( $form['id'] );
-			$path         = str_replace( $upload_roots['url'], '', $file_url );
-			$folder_path .= "/{$path}";
+			$path         = dirname( str_replace( $upload_roots['url'], '', $file_url ) );
+			$folder_path  .= "/{$path}";
 		}
 	}
+
 	return $folder_path;
 }, 10, 5 );
