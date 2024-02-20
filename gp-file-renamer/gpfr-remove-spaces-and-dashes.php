cedfbeff05bbb44ee113e00803b65e9b83ac9d43
David Smith <david@gravitywiz.com>
2024-02-20 16:32:19 -0500

    `gpfr-remove-spaces-and-dashes.php`: Added new snippet.

---
+++ gp-file-renamer/gpfr-remove-spaces-and-dashes.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Gravity Perks // File Renamer // Remove Spaces & Dashes in Filenames
+ * https://gravitywiz.com/documentation/gravity-forms-file-renamer/
+ */
+add_filter( 'gpfr_filename', function( $renamed_file, $file, $entry, $form, $field ) {
+
+	$directory_path    = dirname( $renamed_file );
+	$filename          = basename( $renamed_file );
+	$modified_filename = str_replace( '-', '', $filename);
+
+	return $directory_path . '/' . $modified_filename;
+}, 10, 5 );
