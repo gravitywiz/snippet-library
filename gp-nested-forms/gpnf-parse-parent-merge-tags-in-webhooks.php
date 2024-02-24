c8830dfd86bec437ac361d6ba5c657e91847bc9f
David Smith <david@gravitywiz.com>
2024-02-24 08:06:20 -0500

    `gpnf-parse-parent-merge-tags-in-webhooks.php`: Added note about using "complicated" merge tag format.

--- gp-nested-forms/gpnf-parse-parent-merge-tags-in-webhooks.php
+++ gp-nested-forms/gpnf-parse-parent-merge-tags-in-webhooks.php
@@ -2,6 +2,9 @@
 /**
  * Gravity Perks // Nested Forms // Parse Parent Merge Tags for Webhooks Add-on
  * https://gravitywiz.com/documentation/gravity-forms-nested-forms/
+ * 
+ * Note: Merge tags must be specified in their "complicated" format to avoid being processed prematurely in the child form context.
+ * Example: {Parent:2} → {%GPNF:Parent:2%}
  */
 add_filter( 'gform_webhooks_request_data', function( $request_data, $feed, $entry, $form ) {
 
