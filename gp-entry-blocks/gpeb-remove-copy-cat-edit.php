2f66760da83f173792b46ad7322f71ee89a4827b
Saif Sultan <saif@gravitywiz.com>
2024-03-08 08:25:29 +0530

    `gpeb-remove-copy-cat-edit.php`: Added support to remove Copy Cat on Entry Blocks Edit Page.

--- gp-entry-blocks/gpeb-remove-copy-cat-edit.php
+++ gp-entry-blocks/gpeb-remove-copy-cat-edit.php
@@ -5,19 +5,7 @@
  *
  * Removes copy cat functionality on Entry Blocks' Edit Page.
  */
-add_filter( 'gform_pre_render', 'gpeb_remove_gpcc_on_edit' );
-add_filter( 'gform_pre_process', 'gpeb_remove_gpcc_on_edit' );
-
-function gpeb_remove_gpcc_on_edit( $form ) {
-
-	$is_block = (bool) rgpost( 'gpeb_entry_id' );
-	if ( ! $is_block ) {
-		$is_block = class_exists( 'WP_Block_Supports' ) && rgar( WP_Block_Supports::$block_to_render, 'blockName' ) === 'gp-entry-blocks/edit-form';
-		if ( ! $is_block ) {
-			return $form;
-		}
-	}
-
+add_filter( 'gpeb_edit_form', function ( $form ) {
 	foreach ( $form['fields'] as &$field ) {
 		// remove any of the copy cat classes from the field.
 		preg_match_all( '/copy-([0-9]+(?:.[0-9]+)*)-to-([0-9]+(?:.[0-9]+)*)(?:-if-([0-9]+(?:.[0-9]+)*))?/', $field['cssClass'], $matches, PREG_SET_ORDER );
@@ -28,4 +16,4 @@ function gpeb_remove_gpcc_on_edit( $form ) {
 	}
 
 	return $form;
-}
+} );
