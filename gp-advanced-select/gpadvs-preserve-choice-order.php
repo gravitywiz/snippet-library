a1d350d9f81cca96c84e8214bd8295cab57ed0e0
saifsultanc <saifraikkonen@gmail.com>
2024-03-14 17:45:55 +0530

    `gpadvs-preserve-choice-order.php`: Fixed PHP warnings.

--- gp-advanced-select/gpadvs-preserve-choice-order.php
+++ gp-advanced-select/gpadvs-preserve-choice-order.php
@@ -22,10 +22,17 @@ add_filter( 'gform_save_field_value', function ( $value, $lead, $field, $form ) 
 	$value_array = json_decode( $value, true );
 
 	// Create a map of choice values to their respective index, and sort with that.
-	$value_indices = array_flip( array_column( $field->choices, 'value' ) );
-	usort( $value_array, function ( $x, $y ) use ( $value_indices ) {
-		return $value_indices[ $x ] - $value_indices[ $y ];
-	});
+	if ( is_array( $field->choices ) && ! empty( $field->choices ) && $value_array ) {
+		$valid_values = array_filter( array_column( $field->choices, 'value' ), function ( $value ) {
+			return is_string( $value ) || is_int( $value );
+		});
+		$value_indices = array_flip( $valid_values );
+		if ( is_array( $value_indices ) ) {
+			usort( $value_array, function ( $x, $y ) use ( $value_indices ) {
+				return $value_indices[ $x ] - $value_indices[ $y ];
+			});
+		}
+	}
 
 	// Encode back to JSON string.
 	$value = json_encode( $value_array );
