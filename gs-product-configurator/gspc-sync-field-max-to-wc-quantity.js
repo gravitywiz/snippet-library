cd9b4308e4bd76e9043afbbddf6e2d20e7341cb1
Clay Griffiths <clay@claygriffiths.com>
2024-03-13 22:33:23 -0400

    `gspc-sync-field-max-to-wc-quantity.js`: Updated instructions.

--- gs-product-configurator/gspc-sync-field-max-to-wc-quantity.js
+++ gs-product-configurator/gspc-sync-field-max-to-wc-quantity.js
@@ -7,6 +7,7 @@
  *     1. Install our free Gravity Forms Code Chest plugin.
  *         Download the plugin here: https://gravitywiz.com/gravity-forms-code-chest/
  *     2. Copy and paste the snippet into the "JavaScript" setting under the Code Chest form settings subview.
+ *     3. Add "sync-max-to-quantity" to the Custom CSS Class setting of the fields you want to sync the max.
  */
 var $form = jQuery('#gform_GFFORMID');
 var $quantity = jQuery('.input-text.qty');
