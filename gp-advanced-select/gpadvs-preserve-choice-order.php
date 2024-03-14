5a427f48df2184687fcd83f964d3502338e89e41
Saif Sultan <saif@gravitywiz.com>
2024-03-14 06:28:28 +0530

    `gpadvs-preserve-choice-order.php`: Added support to preserve choice order with Multi Select fields using GP Advanced Select.

---
+++ gp-advanced-select/gpadvs-preserve-choice-order.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Gravity Perks // Advanced Select // Preserve Choice Order
+ * https://gravitywiz.com/documentation/gravity-forms-advanced-select/
+ *
+ * Preserve choice order with Multi Select fields using GP Advanced Select.
+ *
+ * Instruction Video: https://www.loom.com/share/d2ad3fea02234f02871ac9b1efe20b53
+ *
+ * Instructions:
+ *
+ * 1. Install this snippet by following the instructions here:
+ *    https://gravitywiz.com/documentation/how-do-i-install-a-snippet/
+ */
+add_filter( 'gform_save_field_value', function ( $value, $lead, $field, $form ) {
+	// For non GPAS values, return.
+	if ( $field->type != 'multiselect' || ! gp_advanced_select()->is_advanced_select_field( $field ) ) {
+		return $value;
+	}
+
+	// Decode JSON "value" string to an array.
+	$value_array = json_decode( $value, true );
+
+	// Create a map of choice values to their respective index, and sort with that.
+	$value_indices = array_flip( array_column( $field->choices, 'value' ) );
+	usort( $value_array, function ( $x, $y ) use ( $value_indices ) {
+		return $value_indices[ $x ] - $value_indices[ $y ];
+	});
+
+	// Encode back to JSON string.
+	$value = json_encode( $value_array );
+
+	return $value;
+}, 10, 4 );
