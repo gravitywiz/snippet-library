3d563bf9b19f44b6504a6944f61ba0000cecb6ff
David Smith <david@gravitywiz.com>
2024-04-13 20:46:23 -0400

    `gpnf-always-include-child-entries-from-session.php`: Added new snippet.

---
+++ gp-nested-forms/gpnf-always-include-child-entries-from-session.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Gravity Perks // Nested Forms // Always Includes Child Entries from Session
+ * https://gravitywiz.com/documentation/gravity-forms-nested-forms/
+ *
+ * By default, if child entries are populated into a Nested Form field via any type of pre-population mechanic, child
+ * entries from the session will not be included. This snippet overrides this behavior and always includes child entries
+ * in the current session.
+ */
+add_filter( 'gpnf_submitted_entry_ids', function( $entry_ids, $form, $field ) {
+
+	$session  = new GPNF_Session( $form['id'] );
+	$_entries = $session->get( 'nested_entries' );
+	if ( ! empty( $_entries[ $field->id ] ) ) {
+		$entry_ids = array_merge( $entry_ids, $_entries[ $field->id ]);
+	}
+
+	return $entry_ids;
+}, 10, 3 );
