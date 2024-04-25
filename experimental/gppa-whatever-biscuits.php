f47da1e1c1a6c848121192646bbf3be97572c49b
David Smith <david@gravitywiz.com>
2024-04-25 10:02:41 -0400

    `gppa-whatever-biscuits.php`: Added new snippet.

---
+++ experimental/gppa-whatever-biscuits.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Category // Perk or Plugin Name // Snippet Title
+ * https://gravitywiz.com/path/to/article/
+ * 
+ * Describe this snippet's purpose and usage. Be generous when describing both. There
+ * is no maximum description length.
+ * 
+ * Instructions:
+ * 
+ * 1. Install this snippet with our free Custom JavaScript plugin.
+ *    https://gravitywiz.com/gravity-forms-custom-javascript/
+ */
+new GW_Redirect_Email_Exists( array(
+	'form_id'        => 123,
+	'email_field_id' => 4,
+) );
