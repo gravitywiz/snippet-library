f18324c01b527630a3c5e376cd2ded71e3b20985
David Smith <david@gravitywiz.com>
2024-05-13 17:28:55 -0400

    `display-number-of-entries-left.php`: Updated header.

--- gravity-forms/display-number-of-entries-left.php
+++ gravity-forms/display-number-of-entries-left.php
@@ -5,7 +5,10 @@
  *
  * Instruction Video: https://www.loom.com/share/b6c46aebf0ff483496faf9994e36083e
  *
- * Installation instructions: https://gravitywiz.com/documentation/how-do-i-install-a-snippet/
+ * Instructions: 
+ * 
+ * 1. Install the snippet.
+ *    https://gravitywiz.com/documentation/how-do-i-install-a-snippet/
  */
 add_filter( 'gform_shortcode_entries_left', 'gwiz_entries_left_shortcode', 10, 2 );
 function gwiz_entries_left_shortcode( $output, $atts ) {
