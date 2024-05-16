212d8953f7b06e4eb7f3bd21a5ff7bd53bbb3554
David Smith <david@gravitywiz.com>
2024-05-16 12:18:30 -0400

    `gppa-custom-database.php`: Updated header (again).

--- gp-populate-anything/gppa-custom-database.php
+++ gp-populate-anything/gppa-custom-database.php
@@ -3,6 +3,8 @@
  * Gravity Perks // Populate Anything // Custom Database
  * https://gravitywiz.com/documentation/gravity-forms-populate-anything/
  *
+ * Instruction Video: https://www.loom.com/share/f9bf62e358dd4c54ab346998c5f3b516
+ *
  * By default, Populate Anything will only show the database that the current WordPress
  * installation is on.
  *
@@ -10,8 +12,6 @@
  *
  * You can add multiple databases by adjusting the class names and adding additional
  * `gp_populate_anything()->register_object_type()` calls.
- *
- * Instruction Video: https://www.loom.com/share/f9bf62e358dd4c54ab346998c5f3b516
  */
 class GPPA_Object_Type_Database_Testing extends GPPA_Object_Type_Database {
 	const DB_NAME = 'testing';
