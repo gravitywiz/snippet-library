ee44b1d6ba0b074c7c51a9bb751012b9c6e34262
David Smith <david@gravitywiz.com>
2024-05-16 12:17:27 -0400

    `gppa-custom-database.php`: Updated header.

--- gp-populate-anything/gppa-custom-database.php
+++ gp-populate-anything/gppa-custom-database.php
@@ -9,11 +9,9 @@
  * The following snippet allows registering additional an additional database with custom credentials.
  *
  * You can add multiple databases by adjusting the class names and adding additional
- * gp_populate_anything()->register_object_type() calls.
+ * `gp_populate_anything()->register_object_type()` calls.
  *
- *
- * Video
- * https://www.loom.com/share/f9bf62e358dd4c54ab346998c5f3b516
+ * Instruction Video: https://www.loom.com/share/f9bf62e358dd4c54ab346998c5f3b516
  */
 class GPPA_Object_Type_Database_Testing extends GPPA_Object_Type_Database {
 	const DB_NAME = 'testing';
