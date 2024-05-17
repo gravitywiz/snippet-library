3d818a6c98178c19fd809cc54579092e2edeb4f7
Clay Griffiths <clay@claygriffiths.com>
2024-05-17 10:19:22 -0500

    `gw-custom-validation.php`: Added header.

--- gravity-forms/gw-custom-validation.php
+++ gravity-forms/gw-custom-validation.php
@@ -1,13 +1,14 @@
 <?php
 /**
- * Gravity Wiz // Gravity Forms // Custom Validation
+ * Gravity Wizards // Gravity Forms // Custom Validation
  *
- * This snippet is an example of how to add custom validation to a Gravity Form field that is activated by adding
+ * This snippet is an example of how to add custom validation to a Gravity Forms field that is activated by adding
  * a class to the "Custom CSS Class" field setting under the "Appearance" panel of the field settings.
  *
  * Instructions:
- *   1. Add to snippet to site. See https://gravitywiz.com/documentation/how-do-i-install-a-snippet/.
- *   2. Add `validation-no-period` to the "Custom CSS Class" field setting of the field(s) you want to validate.
+ *
+ * 1. Add to snippet to site. See https://gravitywiz.com/documentation/how-do-i-install-a-snippet/.
+ * 2. Add `validation-no-period` to the "Custom CSS Class" field setting of the field(s) you want to validate.
  */
 add_filter( 'gform_field_validation', function ( $result, $value, $form, $field ) {
 	// Only validate fields that contain the class 'no-period'
