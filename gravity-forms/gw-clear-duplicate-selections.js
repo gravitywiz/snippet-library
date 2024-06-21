590a1b428e672b8bef86c20424ba5f4696225acd
David Smith <david@gravitywiz.com>
2024-06-20 22:15:13 -0400

    Update and rename gw-limit-radio-button-selections-across-fields.js to gw-clear-duplicate-selections.js

--- gravity-forms/gw-clear-duplicate-selections.js
+++ gravity-forms/gw-clear-duplicate-selections.js
@@ -1,18 +1,22 @@
 /**
- * Gravity Wiz // Gravity Forms // Limit Radio Button Selections across Multiple Fields.
+ * Gravity Wiz // Gravity Forms // Clear Duplicate Selections
  * https://gravitywiz.com/
+ *
+ * Clear the selection in one Radio Button field when the same value is selected in another.
  *
  * Instruction Video: https://www.loom.com/share/3d42f3c88ab14f23ad63d491bb77bac4
  *
- * * Instructions:
- *     1. Install our free Custom Javascript for Gravity Forms plugin.
- *        Download the plugin here: https://gravitywiz.com/gravity-forms-code-chest/
- *     2. Copy and paste the snippet into the editor of the Custom Javascript for Gravity Forms plugin.
+ * Instructions:
+ * 
+ * 1. Install this snippet with our free Code Chest plugin.
+ *    https://gravitywiz.com/gravity-forms-code-chest/
+ *
+ * 2. Configure the snippet based on inline instructions.
  */
-// Update radio button field ids to target
+// Update radio button field ids to target.
 var fieldIds = [1, 3, 4];
 
-// Function to clear the same value in other fields
+// Function to clear the same value in other fields.
 function clearSameValue( changedFieldId, selectedValue ) {
 	$.each( fieldIds, function( index, fieldId ) {
 		if ( fieldId !== changedFieldId ) {
@@ -24,7 +28,7 @@ function clearSameValue( changedFieldId, selectedValue ) {
 	} );
 }
 
-// Attach change event listeners to the radio buttons
+// Attach change event listeners to the radio buttons.
 $.each( fieldIds, function( index, fieldId ) {
 	$( 'input[name="input_' + fieldId + '"]' ).on( 'change', function() {
 		var selectedValue = $( this ).val();
