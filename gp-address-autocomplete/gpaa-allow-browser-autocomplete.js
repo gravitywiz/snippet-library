10cc201973a3f2df703e4e6bb41c514336691871
Saif Sultan <saifsultanc@gmail.com>
2024-07-09 23:06:50 +0530

    `gpaa-allow-browser-autocomplete.js`: Added snippet to allow browser autocomplete with GPAA.

--- gp-address-autocomplete/gpaa-allow-browser-autocomplete.js
+++ gp-address-autocomplete/gpaa-allow-browser-autocomplete.js
@@ -3,9 +3,8 @@
  * https://gravitywiz.com/documentation/gravity-forms-address-autocomplete
  *
  * Instructions:
- *     1. Install our free Custom JavaScript for Gravity Forms plugin.
- *        Download the plugin here: https://gravitywiz.com/gravity-forms-code-chest/
- *     2. Copy and paste the snippet into the editor of the Custom Javascript for Gravity Forms plugin.
+ *     1. Install our free [Gravity Forms Code Chest](https://gravitywiz.com/gravity-forms-code-chest/) plugin.
+ *     2. Copy and paste the snippet into the JavaScript section of Code Chest for the form you wish to apply this snippet to.
  */
 window.gform.addFilter('gpaa_prevent_browser_autocomplete', function( preventBrowserAutocomplete, instance, formId, fieldId ) {
 	return false;
