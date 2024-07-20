fcc8ebf2ae69082c782c070935a1d028fc58a979
David Smith <david@gravitywiz.com>
2024-07-20 07:54:10 -0400

    Fixed issue where Firefox no longer honored the "off" value for the autocomplete attribute.

--- gravity-forms/gw-disable-autocomplete.php
+++ gravity-forms/gw-disable-autocomplete.php
@@ -10,13 +10,13 @@
  */
 // Disable auto-complete on form.
 add_filter( 'gform_form_tag', function( $form_tag ) {
-	$autocomplete = gw_get_browser_name( $_SERVER['HTTP_USER_AGENT'] ) === 'Chrome' ? 'password' : 'off';
+	$autocomplete = 'off';
 	return str_replace( '>', ' autocomplete="' . $autocomplete . '">', $form_tag );
 }, 11 );
 
 // Diable auto-complete on each field.
 add_filter( 'gform_field_content', function( $input ) {
-	$autocomplete = gw_get_browser_name( $_SERVER['HTTP_USER_AGENT'] ) === 'Chrome' ? 'password' : 'off';
+	$autocomplete = in_array( gw_get_browser_name( $_SERVER['HTTP_USER_AGENT'] ), array( 'Chrome', 'Firefox' ) ) ? 'new-password' : 'off';
 	return preg_replace( '/<(input|textarea)/', '<${1} autocomplete="' . $autocomplete . '" ', $input );
 }, 11 );
 
