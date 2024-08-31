916973bbc81e04be54b018aaba1aa9144d2a5b71
David Smith <david@gravitywiz.com>
2024-08-31 13:47:41 -0400

    `gcoai-stream-to-paragraph-field.js`: Added new snippet.

---
+++ gc-openai/gcoai-stream-to-paragraph-field.js
@@ -0,0 +1,35 @@
+/**
+ * Gravity Connect // OpenAI // Stream to Paragraph Field
+ * https://gravitywiz.com/documentation/gravity-connect-openai/
+ *
+ * Stream OpenAI responses from the OpenAI Stream field to a Paragraph (or Single Line Text)
+ * field instead. Useful when wanting to provide a starting point for users while allowing them
+ * to edit the final result.
+ * 
+ * Instruction Video: https://www.loom.com/share/f793319da7e449a8b01e5a8c077e24c7
+ *
+ * Instructions:
+ *
+ * 1. Install this snippet with our free Custom JavaScript plugin.
+ *    https://gravitywiz.com/gravity-forms-custom-javascript/
+ * 
+ * 2. Update the variables to match your own field IDs.
+ */
+var streamFieldId = 1;
+var promptFieldId = 2;
+var responseFieldId = 3;
+
+var $streamFieldInput = $( `#input_GFFORMID_${streamFieldId}` );
+var $streamButton     = $streamFieldInput.parents( '.gfield' ).find( '.gcoai-trigger' );
+
+$streamFieldInput.on( 'change', function() {
+    $( `#input_GFFORMID_${responseFieldId}` ).val( this.value );
+} );
+
+$streamButton
+	.clone()
+	.attr( 'style', 'margin-top: var(--gf-label-space-primary, 8px);' )
+	.on( 'click', function() {
+		$streamButton.trigger( 'click' )
+	} )
+	.insertAfter( $( `#input_GFFORMID_${promptFieldId}` ) )
