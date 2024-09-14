c4b0da4227ac475540a8b69f7b4d6c070686ca44
saifsultanc <saifsultanc@gmail.com>
2024-09-14 10:53:37 +0530

    `gspc-remove-zero-dollar-woocommerce-products-from-summary.php`: Added snippet for removing zero dollar woocommerce products from summary.

---
+++ gs-product-configurator/gspc-remove-zero-dollar-woocommerce-products-from-summary.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Gravity Shop // GS Product Configurator // Remove Zero Dollar WooCommerce Products from Summary.
+ *
+ * Instructions: https://www.loom.com/share/384805955a184bada7034bea6d9de857
+ */
+if ( is_callable( 'gs_product_configurator' ) ) :
+	add_filter( 'gform_product_info', function ( $product_info, $form, $entry ) {
+		if ( isset( $product_info['products'][\GS_Product_Configurator\WC_Product_Form_Display::BASE_PRICE_PRODUCT_FIELD_ID] ) ) {
+			$price         = $product_info['products'][\GS_Product_Configurator\WC_Product_Form_Display::BASE_PRICE_PRODUCT_FIELD_ID]['price'];
+			$numeric_price = (float) preg_replace( '/[^\d.]/', '', $price );
+
+			if ( $numeric_price == 0.0 ) {
+				unset( $product_info['products'][\GS_Product_Configurator\WC_Product_Form_Display::BASE_PRICE_PRODUCT_FIELD_ID] );
+			}
+		}
+
+		return $product_info;
+	}, 16, 3 );
+endif;
