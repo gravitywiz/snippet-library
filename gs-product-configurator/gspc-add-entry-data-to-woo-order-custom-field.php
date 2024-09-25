da70c04ded88a59ceaeb00c835db30223f6e4e86
David Smith <david@gravitywiz.com>
2024-09-25 12:46:54 -0400

    `gspc-add-entry-data-to-woo-order-custom-field.php`: Fixed PHPCS issues.

--- gs-product-configurator/gspc-add-entry-data-to-woo-order-custom-field.php
+++ gs-product-configurator/gspc-add-entry-data-to-woo-order-custom-field.php
@@ -10,20 +10,20 @@
  *   1. Install per https://gravitywiz.com/documentation/how-do-i-install-a-snippet/
  *   2. Configure the snippet based on inline instructions.
  */
- add_action( 'woocommerce_checkout_create_order_line_item', function ( $item, $cart_item_key, $values, $order ) {
-    
+add_action( 'woocommerce_checkout_create_order_line_item', function ( $item, $cart_item_key, $values, $order ) {
+
 	$entry_ids = $item->get_meta( 'gspc_gf_entry_ids', true );
-		
-	if( $entry_ids ){
-		
+
+	if ( $entry_ids ) {
+
 		$entry_id = $entry_ids[0];
-		
+
 		$entry = GFAPI::get_entry( $entry_id );
 
 		$data = rgar( $entry, '4' ); // Replace 4 with the field ID you need
-		
+
 		$order->update_meta_data( '_my_custom_key', $data ); // Replace _my_custom_key with your custom meta key
-		
+
 	}
 
 }, 10, 4 );
