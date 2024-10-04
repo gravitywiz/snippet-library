870ddfd80243f30920ea1397c55bcdbc2ecdf9f4
barthc <mackintosh02@gmail.com>
2024-10-04 14:44:51 +0100

    Feedback fixes.

--- gp-entry-blocks/gpeb-process-feeds-on-edit.php
+++ gp-entry-blocks/gpeb-process-feeds-on-edit.php
@@ -9,6 +9,9 @@
  * Note, delayed feeds will still not be processed.
  */
 add_filter( 'gform_entry_post_save', function( $entry, $form ) {
+	// Array of feed ids to exclude for processing, if you want all feeds to be process use an empty array.
+	$excluded_feed_ids = array( 103, 104 );
+
 	if ( ! function_exists( 'gp_entry_blocks' ) ) {
 		return $entry;
 	}
@@ -25,10 +28,8 @@ add_filter( 'gform_entry_post_save', function( $entry, $form ) {
 	$filter_priority = rand( 100000, 999999 );
 	add_filter( 'gform_is_feed_asynchronous', '__return_false', $filter_priority );
 
-	add_filter( 'gform_addon_pre_process_feeds', function( $feeds, $entry, $form ) {
-		// Array of feed ids to exclude
-		$excluded_feed_ids = array( 103, 104 );
-		$feeds             = array_filter( $feeds, function( $feed ) use ( $excluded_feed_ids ) {
+	add_filter( 'gform_addon_pre_process_feeds', function( $feeds, $entry, $form ) use ( $excluded_feed_ids ) {
+		$feeds = array_filter( $feeds, function( $feed ) use ( $excluded_feed_ids ) {
 			return ! in_array( $feed['id'], $excluded_feed_ids );
 		} );
 
