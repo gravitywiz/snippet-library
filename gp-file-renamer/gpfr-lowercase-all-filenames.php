a7938434e7fda913082a58c403a267790afb3e03
Clay Griffiths <clay@claygriffiths.com>
2024-10-11 14:44:11 -0500

    `gpfr-lowercase-all-filenames.php`: Fixed issue with snippet where the entire path would be lowercased which could create issues down the road with deleting files.

--- gp-file-renamer/gpfr-lowercase-all-filenames.php
+++ gp-file-renamer/gpfr-lowercase-all-filenames.php
@@ -6,5 +6,8 @@
  * This snippet will automatically lowercase all filenames.
  */
 add_filter( 'gpfr_filename', function( $renamed_file, $file, $entry ) {
-	return strtolower( $renamed_file );
+	// Important note, $renamed_file is the entire path.
+	$lowercase_filename = strtolower( wp_basename( $renamed_file ) );
+
+	return str_replace( wp_basename( $renamed_file ), $lowercase_filename, $renamed_file );
 }, 10, 3 );
