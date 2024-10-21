bc17c96a80f0e0ab9f8ed2ce883a4a42878b8408
David Smith <david@gravitywiz.com>
2024-10-21 16:33:52 -0400

    `gpnf-subscribe-to-child-entry-updates.js`: Updated to wrap in `gfcc_deferred`.

--- gp-nested-forms/gpnf-subscribe-to-child-entry-updates.js
+++ gp-nested-forms/gpnf-subscribe-to-child-entry-updates.js
@@ -5,10 +5,14 @@
  * If you're having issues getting this to work, try using our [Gravity Forms Custom Javascript](https://gravitywiz.com/gravity-forms-code-chest/)
  * plugin. It handles ensuring the Nested Forms has fully initialized so you don't have to.
  */
-// Get your Nested Forms JavaScript instance where "123" is the form ID and "4" is the Nested Form field ID.
-var gpnf = window.GPNestedForms_123_4;
+gform.addAction( 'gfcc_deferred', function() {
+	
+	// Get your Nested Forms JavaScript instance where "123" is the form ID and "4" is the Nested Form field ID.
+	var gpnf = window.GPNestedForms_123_4;
+	
+	gpnf.viewModel.entries.subscribe( function( entries ) {
+	  // Do whatever you want any time the child entries for your Nested Form field are updated.
+	  console.log( entries );
+	} );
 
-gpnf.viewModel.entries.subscribe( function( entries ) {
-  // Do whatever you want any time the child entries for your Nested Form field are updated.
-  console.log( entries );
 } );
