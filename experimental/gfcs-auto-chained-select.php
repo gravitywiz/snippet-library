e694a02df60f5e684410b41c98a7b0eb0142ce5c
David Smith <david@gravitywiz.com>
2024-12-03 21:20:03 -0500

    `gfcs-auto-chained-select.php`: Migrated.

--- experimental/gfcs-auto-chained-select.php
+++ experimental/gfcs-auto-chained-select.php
@@ -1,24 +1,5 @@
-<?php
 /**
- * Gravity Wiz // Gravity Forms Chained Selects // Auto Select Only Option
- * https://gravitywiz.com/
- *
- * The Gravity Forms Chained Selects field requires you to manually select a value in each Drop Down
- * even if there is only a single option available in that Drop Down. This snippet will automatically
- * selected an option when it is the only option available.
+ * We're no longer using the experimental folder for experimental snippets. 🚧
+ * You can now find the snippet here:
+ * https://github.com/gravitywiz/snippet-library/blob/master/gravity-forms/gfcs-auto-chained-select.php.php
  */
-add_filter( 'gform_chained_selects_input_choices', function( $choices ) {
-	$choices = gfcs_auto_select_only_choice( $choices );
-	return $choices;
-} );
-
-function gfcs_auto_select_only_choice( $choices ) {
-
-	$choices[0]['isSelected'] = $choices[0]['isSelected'] || count( $choices ) <= 1;
-
-	if ( ! empty( $choices['choices'] ) ) {
-		$choices['choices'] = gfcs_auto_select_only_choice( $choices['choices'] );
-	}
-
-	return $choices;
-}
