a4ef12df10f7db9275290bf5a29cc78177bf00b9
Samuel Bassah <sbassah@gmail.com>
2024-12-13 10:08:43 +0000

    `gp-hide-perks-from-plugins-page.php`: Migrated from `experimental` folder.

---
+++ gravity-forms/gp-hide-perks-from-plugins-page.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Gravity Perks // Hide Perks from Plugins Page
+ * https://gravitywiz.com/documentation/
+ *
+ * Experimental Snippet 🧪
+ */
+add_filter( 'all_plugins', function( $plugins ) {
+
+	if ( ! is_callable( 'get_current_screen' ) || get_current_screen()->id !== 'plugins' ) {
+		return $plugins;
+	}
+
+	$filtered_plugins = array();
+
+	foreach ( $plugins as $slug => $plugin ) {
+		if ( ! wp_validate_boolean( rgar( $plugin, 'Perk' ) ) ) {
+			$filtered_plugins[ $slug ] = $plugin;
+		}
+	}
+
+	return $filtered_plugins;
+} );
