ee6d8a058957b823ff7224787546109270b10b1b
Samuel Bassah <sbassah@gmail.com>
2024-12-19 16:17:55 +0000

    `gpfup-count-files.js`: Migrated.

--- experimental/gpfup-count-files.js
+++ experimental/gpfup-count-files.js
@@ -1,17 +1,5 @@
-jQuery(function() {
-  // Update "123" to your form ID and "4" to your File Upload field ID.
-	if (typeof window['GPFUP_123_4'] === 'undefined') {
-		return;
-	}
-	
-  // Update "123" to your form ID and "4" to your File Upload field ID.
-	var store = window.GPFUP_123_4.$store;
-	
-	store.subscribe(function(mutation, state) {
-		if (mutation.type !== 'SET_FILES') {
-			return;
-		}
-		// Update "5" to your Single Product field ID - or - update `#input_123_5_1` to your desired input's HTML ID.
-		jQuery( '#input_123_5_1' ).val( state.files.length ).change();
-	});
-});
+/**
+ * We're no longer using the experimental folder for experimental snippets. 🚧
+ * You can now find the snippet here:
+ * https://github.com/gravitywiz/snippet-library/blob/master/gp-file-upload-pro/gpfup-count-files.js
+ */
