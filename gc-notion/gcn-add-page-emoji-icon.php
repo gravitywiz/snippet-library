278e69342ad5b96dc5275f9bd9593f4762e8af64
David Smith <david@gravitywiz.com>
2025-01-02 10:35:03 -0500

    `gcn-add-page-emoji-icon.php`: Fixed issue in header for HSL.
    `gcn-add-page-emoji-icon.php`: Updated to improve HSL output.
    
    `gcn-add-page-emoji-icon.php`: Updated again for HSL.
    
    ~ Update.

--- gc-notion/gcn-add-page-emoji-icon.php
+++ gc-notion/gcn-add-page-emoji-icon.php
@@ -6,20 +6,20 @@
  * when a form entry is being added to or edited in Notion.
  *
  * Instructions:
- *   1. Modify the filter name to scope as needed.
- *     * see filter reference for full list of variants: https://gravitywiz.com/documentation/gcn_notion_page_data/)
- *   2. Update the $emoji variable to change the icon.
  *
- * Installation:
- *   1. Install per https://gravitywiz.com/documentation/how-do-i-install-a-snippet/
+ * 1. Install per https://gravitywiz.com/documentation/how-do-i-install-a-snippet/
+ *
+ * 2. Modify the filter name to scope as needed.<br>See filter reference for full list of variants: https://gravitywiz.com/documentation/gcn_notion_page_data/
+ *
+ * 3. Update the $emoji variable to change the icon.
  *
  * References:
- *   * GC Notion: https://gravitywiz.com/documentation/gravity-connect-notion/
- *   * Notion page POST reference: https://developers.notion.com/reference/post-page
+ *
+ * - GC Notion: https://gravitywiz.com/documentation/gravity-connect-notion/
+ * - Notion page POST reference: https://developers.notion.com/reference/post-page
  *
  * @since 1.0-beta-1.9
  */
-
 add_filter( 'gcn_notion_page_data', function( $page_data, $form, $entry, $feed ) {
 	/**
 	 * Modify the $emoji variable to change the icon.
