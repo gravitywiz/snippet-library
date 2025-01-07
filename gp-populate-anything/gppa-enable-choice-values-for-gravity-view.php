002488d2c89c61d1f93af4cccd00fed66ce8bbce
Clay Griffiths <clay@claygriffiths.com>
2025-01-07 10:38:51 -0600

    `gppa-enable-choice-values-for-gravity-view.php`: Migrated from experimental folder.

--- gp-populate-anything/gppa-enable-choice-values-for-gravity-view.php
+++ gp-populate-anything/gppa-enable-choice-values-for-gravity-view.php
@@ -4,6 +4,8 @@
  * will present the option to show the value or label when including this field in a view.
  *
  * See: https://secure.helpscout.net/conversation/1511770443/24411/#thread-4407403575
+ *
+ * Experimental Snippet 🧪
  */
 add_filter( 'gform_form_post_get_meta', function( $form ) {
 
