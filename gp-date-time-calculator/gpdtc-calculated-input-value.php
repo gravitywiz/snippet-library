4f9adfbdec42ce140b475324ee7e4bd420cef05c
Clay Griffiths <clay@claygriffiths.com>
2025-01-07 10:38:47 -0600

    `gpdtc-calculated-input-value.php`: Migrated from experimental folder.

--- gp-date-time-calculator/gpdtc-calculated-input-value.php
+++ gp-date-time-calculator/gpdtc-calculated-input-value.php
@@ -3,4 +3,6 @@
  * This snippet has evolved! 🦄
  * Find the new version of this snippet here:
  * https://github.com/gravitywiz/snippet-library/blob/master/experimental/gpdtc-recalc.php
+ *
+ * Experimental Snippet 🧪
  */
