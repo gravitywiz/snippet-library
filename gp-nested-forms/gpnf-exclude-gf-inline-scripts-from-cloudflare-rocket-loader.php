448650dc5771ea566187a54bc91dccf7b18e2582
Clay Griffiths <clay@claygriffiths.com>
2025-01-07 10:38:49 -0600

    `gpnf-exclude-gf-inline-scripts-from-cloudflare-rocket-loader.php`: Migrated from experimental folder.

--- gp-nested-forms/gpnf-exclude-gf-inline-scripts-from-cloudflare-rocket-loader.php
+++ gp-nested-forms/gpnf-exclude-gf-inline-scripts-from-cloudflare-rocket-loader.php
@@ -2,6 +2,8 @@
 /**
  * Gravity Perks // Nested Forms // Exclude Gravity Forms Inline Scripts from CloudFlare's Rocket Loader™.
  * https://gravitywiz.com/documentation/gravity-forms-nested-forms/
+ *
+ * Experimental Snippet 🧪
  */
 add_filter( 'wp_inline_script_attributes', function ( $attributes, $javascript ) {
 	if ( strpos( $javascript, 'gform.' ) ) {
