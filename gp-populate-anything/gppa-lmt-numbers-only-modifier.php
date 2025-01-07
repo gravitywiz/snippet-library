c2cef5252471805323429065b39d5c3b3334a06e
Clay Griffiths <clay@claygriffiths.com>
2025-01-07 10:38:52 -0600

    `gppa-lmt-numbers-only-modifier.php`: Migrated from experimental folder.

--- gp-populate-anything/gppa-lmt-numbers-only-modifier.php
+++ gp-populate-anything/gppa-lmt-numbers-only-modifier.php
@@ -2,6 +2,8 @@
 /**
  * Gravity Perks // Populate Anything // Live Merge Tags: Numbers Only Modifier
  * https://gravitywiz.com/documentation/gravity-forms-populate-anything/
+ *
+ * Experimental Snippet 🧪
  */
 add_filter( 'gppa_live_merge_tag_value', function( $value, $merge_tag, $form, $field_id, $entry_values ) {
 	if ( strpos( $merge_tag, ':numbersonly' ) !== false && preg_match( '/([0-9]+)/', $value, $matches ) ) {
