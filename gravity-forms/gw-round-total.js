4f18d7a51b05bb06128744c9c245addebd84b861
Clay Griffiths <clay@claygriffiths.com>
2025-01-07 10:38:59 -0600

    `gw-round-total.js`: Migrated from experimental folder.

--- gravity-forms/gw-round-total.js
+++ gravity-forms/gw-round-total.js
@@ -1,3 +1,6 @@
+/**
+ * Experimental Snippet 🧪
+ */
 // Round the total field
 gform.addFilter( 'gform_product_total', function(total, formId){
 	var interval = 1; // Change this to the nearest percision (e.g. 10 rounds to the nearest 10,20,30... etc.)
