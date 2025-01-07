38b648351dc03f270649521765f2184ac2863d99
Clay Griffiths <clay@claygriffiths.com>
2025-01-07 10:38:56 -0600

    `gw-add-custom-footer-button.php`: Migrated from experimental folder.

--- gravity-forms/gw-add-custom-footer-button.php
+++ gravity-forms/gw-add-custom-footer-button.php
@@ -2,6 +2,8 @@
 /**
  * Gravity Wiz // Gravity Forms // Add Custom Footer Button after Save & Continue
  * https://gravitywiz.com/
+ *
+ * Experimental Snippet 🧪
  */
 // Update "123" to your form ID.
 add_filter( 'gform_savecontinue_link_123', function( $markup, $form ) {
