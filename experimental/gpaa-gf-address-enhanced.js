4531958ae3e87f930b1c33a21facb3b9f53cc16a
Clay Griffiths <clay@claygriffiths.com>
2025-01-07 09:34:29 -0600

    Formatting: Added placeholder files to `experimental` for snippets that were migrated out.

---
+++ experimental/gpaa-gf-address-enhanced.js
@@ -0,0 +1,5 @@
+/**
+ * We're no longer using the experimental folder for experimental snippets. 🚧
+ * You can now find the snippet here:
+ * https://github.com/gravitywiz/snippet-library/blob/master/gp-address-autocomplete/gpaa-gf-address-enhanced.js
+ */
---
+++ experimental/gpld-default-to-min-date.js
@@ -0,0 +1,5 @@
+/**
+ * We're no longer using the experimental folder for experimental snippets. 🚧
+ * You can now find the snippet here:
+ * https://github.com/gravitywiz/snippet-library/blob/master/gp-limit-dates/gpld-default-to-min-date.js
+ */
