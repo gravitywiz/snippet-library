37a27d2199cd4d771f5f0a1121e11f41bef5b006
Clay Griffiths <clay@claygriffiths.com>
2025-01-07 10:39:52 -0600

    Formatting: Added placeholder files to `experimental` for snippets that were migrated out.

---
+++ experimental/gspc-remove-wc-product-from-entry-order-summary.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * We're no longer using the experimental folder for experimental snippets. 🚧
+ * You can now find the snippet here:
+ * https://github.com/gravitywiz/snippet-library/blob/master/gs-product-configurator/gspc-remove-wc-product-from-entry-order-summary.php
+ */
---
+++ experimental/gw-rich-text-html-fields.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * We're no longer using the experimental folder for experimental snippets. 🚧
+ * You can now find the snippet here:
+ * https://github.com/gravitywiz/snippet-library/blob/master/gravity-forms/gw-rich-text-html-fields.php
+ */
