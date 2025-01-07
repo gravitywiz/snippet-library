c21520bf6b22122e79caa27a073fafff32498eec
Clay Griffiths <clay@claygriffiths.com>
2025-01-07 10:38:59 -0600

    `gw-prevent-duplicate-drop-down-selections.js`: Migrated from experimental folder.

--- gravity-forms/gw-prevent-duplicate-drop-down-selections.js
+++ gravity-forms/gw-prevent-duplicate-drop-down-selections.js
@@ -2,4 +2,6 @@
  * This snippet has evolved! 🦄
  * Find the new version of this snippet here: 
  * https://github.com/gravitywiz/snippet-library/blob/master/gravity-forms/gw-prevent-duplicate-selections.js
+ *
+ * Experimental Snippet 🧪
  */
