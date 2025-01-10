fc98116cb4ec15064c2f0300d5c05a154fefe433
Samuel Bassah <sbassah@gmail.com>
2025-01-10 11:44:27 +0000

    `gppa-acf-datepicker-comparison.php`: Updated snippet URL in header.

--- gp-populate-anything/gppa-acf-datepicker-comparison.php
+++ gp-populate-anything/gppa-acf-datepicker-comparison.php
@@ -2,5 +2,5 @@
 /**
  * This snippet has evolved! 🦄
  * Find the new version of this snippet here:
- * https://github.com/gravitywiz/snippet-library/gp-populate-anything/gppa-post-meta-datepicker-comparison.php
+ * https://github.com/gravitywiz/snippet-library/blob/master/gp-populate-anything/gppa-post-meta-datepicker-comparison.php
  */
