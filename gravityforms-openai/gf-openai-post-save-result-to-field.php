76a5e0f96130a8286a132b405ea61ba3a2944da9
Saif Sultan <saifsultanc@gmail.com>
2025-01-10 17:26:57 +0530

    `gf-openai-post-save-result-to-field.php`: Added snippet to populate fields with Open AI generated values.

--- gravityforms-openai/gf-openai-post-save-result-to-field.php
+++ gravityforms-openai/gf-openai-post-save-result-to-field.php
@@ -12,8 +12,7 @@
  *
  * Instructions: https://gravitywiz.com/documentation/how-do-i-install-a-snippet/
  */
-// Replace 342 with the form id, and 3 with the targetted field id.
-add_action( 'gf_openai_post_save_result_to_field_342', 'gw_openai_post_save_result' );
-function gw_openai_post_save_result( $result ) {
+// Replace 1 with the form id, and 3 with the targetted field id.
+add_action( 'gf_openai_post_save_result_to_field_1', function ( $result ) {
 	$_POST['input_3'] = $result;
-}
+} );
