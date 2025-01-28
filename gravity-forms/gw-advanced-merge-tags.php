35cdededb5e4c8919690025df3f572a56bdf9632
saifsultanc <saifsultanc@gmail.com>
2025-01-28 14:54:47 +0530

    `gw-advanced-merge-tags.php`: Added support for `{all_fields}` modifier to show data for all entries.

--- gravity-forms/gw-advanced-merge-tags.php
+++ gravity-forms/gw-advanced-merge-tags.php
@@ -507,25 +507,6 @@ class GW_Advanced_Merge_Tags {
 					}
 					break;
 				case 'all_fields':
-					// {all_fields} merge tag support needs to get the entry IDs from the raw value.
-					$entries = json_decode( $raw_value, true );
-
-					// loop through each entry ID and get the {all_fields} output.
-					$output  = '';
-					foreach ( $entries as $entry_id ) {
-						$entry = GFAPI::get_entry( $entry_id );
-						$form  = GFAPI::get_form( $entry['form_id'] );
-
-						// Get the {all_fields} output for each entry.
-						$all_fields_output = GFCommon::replace_variables( '{all_fields}', $form, $entry, false, false, false );
-
-						$output .= $all_fields_output;
-					}
-
-					// if no entries were found, return the original value.
-					$value = $output ? $output : $value;
-					break;
-				case 'all_fields':
 					// Only to be applied for GPPA scenarios, where we may using dynamically populated entry ID data.
 					if ( ! is_callable( 'gp_populate_anything' ) || ! gp_populate_anything()->is_field_dynamically_populated( $field ) ) {
 						break;
