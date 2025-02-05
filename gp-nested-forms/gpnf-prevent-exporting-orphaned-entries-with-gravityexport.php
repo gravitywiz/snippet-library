37691cbefc1313b3f610e390779b14cf398121e1
David Smith <david@gravitywiz.com>
2025-02-05 14:01:44 -0500

    `gpnf-prevent-exporting-orphaned-entries-with-gravityexport.php`: Fixed issue where non-child-entries would not be exported.

--- gp-nested-forms/gpnf-prevent-exporting-orphaned-entries-with-gravityexport.php
+++ gp-nested-forms/gpnf-prevent-exporting-orphaned-entries-with-gravityexport.php
@@ -14,7 +14,7 @@
 add_filter( 'gfexcel_output_rows', function( $rows ) {
 	for ( $i = count( $rows ) - 1; $i >= 0; $i-- ) {
 		foreach ( $rows[ $i ] as $row_value ) {
-			if ( $row_value->getField()->id == 'gpnf_entry_parent' && ! is_numeric( $row_value->getValue() ) ) {
+			if ( $row_value && $row_value->getField()->id == 'gpnf_entry_parent' && $row_value->getValue() && ! is_numeric( $row_value->getValue() ) ) {
 				unset( $rows[ $i ] );
 			}
 		}
