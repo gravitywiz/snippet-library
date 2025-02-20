56d6e53baac8a70823c02ade3a2dce88c95a79cf
David Smith <david@gravitywiz.com>
2025-02-20 17:30:25 -0500

    `gppa-set-template-by-field-value.php`: Updated header.

--- gp-populate-anything/gppa-set-template-by-field-value.php
+++ gp-populate-anything/gppa-set-template-by-field-value.php
@@ -5,7 +5,7 @@
  *
  * Instruction Video: https://www.loom.com/share/02cb5511b5ba4cf6a051ee7038f55f16
  *
- * I'll take another stab at explaining this another day. For now, watch the instructional video above. 😅
+ * I'll take another stab at explaining this another day. For now, watch the instructional video. 😅
  */
 // Update "123" to your form ID and "4" to the ID of the field being populated.
 add_filter( 'gppa_pre_populate_field_123_4', function( $return, $field, $form, $field_values, $entry, $force_use_field_value, $include_html, $run_pre_render ) {
