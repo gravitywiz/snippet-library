cee80095c88d901e7400aafd091a47433707b59d
saifsultanc <saifsultanc@gmail.com>
2025-02-26 23:37:48 +0530

    `gw-all-fields-template.php`: Added filter to customize template content.

--- gravity-forms/gw-all-fields-template.php
+++ gravity-forms/gw-all-fields-template.php
@@ -591,7 +591,20 @@ class GW_All_Fields_Template {
 			include( $template );
 		}
 		$content = ob_get_clean();
-		return ! $template ? false : $content;
+
+		/**
+		 * Filters the content loaded by the template.
+		 *
+		 * @since 0.12
+		 *
+		 * @param string $content  The content loaded by the template.
+		 * @param string $slug     The slug name for the generic template.
+		 * @param string $name     The name of the specialized template.
+		 * @param array  $data     An array of data extracted for use in the template.
+		 * @param array  $suffixes An array of suffixes used to locate the template.
+		 */
+		$content = apply_filters( 'gwaft_template_content', $content, $slug, $name, $data, $suffixes );
+		return ! $template && ! $content ? false : $content;
 	}
 
 	public function get_template_part( $slug, $name = null, $load = true, $suffixes = array() ) {
