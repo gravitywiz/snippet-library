b14477ee588b930dfb0a8e04daa3c4682171c17a
David Smith <david@gravitywiz.com>
2025-02-28 13:44:07 -0500

    Update gw-all-fields-template.php

--- gravity-forms/gw-all-fields-template.php
+++ gravity-forms/gw-all-fields-template.php
@@ -8,7 +8,7 @@
  * Plugin URI:   https://gravitywiz.com/gravity-forms-all-fields-template/
  * Description:  Modify the {all_fields} merge tag output via a template file.
  * Author:       Gravity Wiz
- * Version:      0.11
+ * Version:      0.12
  * Author URI:   http://gravitywiz.com
  *
  * Usage:
