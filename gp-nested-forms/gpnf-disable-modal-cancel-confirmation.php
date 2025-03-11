56fe961c389d87efdfd62f5e1fadc61367dcc408
David Smith <david@gravitywiz.com>
2025-03-11 17:18:42 -0400

    `gpnf-disable-modal-cancel-confirmation.php`: Added new snippet.

---
+++ gp-nested-forms/gpnf-disable-modal-cancel-confirmation.php
@@ -0,0 +1,17 @@
+/**
+ * Gravity Perks // Nested Forms // Disable Modal Cancel Confirmation
+ * https://gravitywiz.com/documentation/gravity-forms-nested-forms/
+ *
+ * This snippet removes the confirmation prompt when canceling a Nested Form modal. 
+ * Normally, when a user attempts to close the modal without saving, they are asked 
+ * to confirm their action. With this snippet, the modal closes immediately.
+ *
+ * Instructions:
+ *
+ * 1. Install this snippet.
+ *    https://gravitywiz.com/documentation/managing-snippets/#where-do-i-put-snippets
+ */
+add_filter( 'gpnf_init_script_args', function( $args, $field, $form ) {
+	$args['modalLabels']['confirmAction'] = false;
+	return $args;
+}, 10, 3 );
