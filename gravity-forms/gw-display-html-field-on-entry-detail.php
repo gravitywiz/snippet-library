50ff4df38d6c0aa91aeefc3496251603d6434684
Saif Sultan <saifsultanc@gmail.com>
2025-04-14 22:52:16 +0530

    `gw-display-html-field-on-entry-detail.php`: Added a snippet to display HTML field content on Entry Details page.

--- gravity-forms/gw-display-html-field-on-entry-detail.php
+++ gravity-forms/gw-display-html-field-on-entry-detail.php
@@ -31,25 +31,38 @@ class GW_Display_HTML_Field_Entry_Detail {
 	}
 
 	public function init() {
-
 		add_filter( 'gform_entry_post_save', array( $this, 'save_html_field_content' ), 10, 1 );
-		add_action( 'gform_entry_detail', array( $this, 'display_html_field_content' ), 10, 2 );
+		add_action( 'gform_entry_detail', array( $this, 'display_html_field_content_entry_detail' ), 10, 2 );
+		add_filter( 'gform_get_input_value', array( $this, 'display_html_field_content_entry_list' ), 10, 4 );
 	}
 
-	public function is_applicable_form( $form ) {
+	private function is_applicable_form( $form ) {
 		$form_id = is_array( $form ) && isset( $form['id'] ) ? $form['id'] : (int) $form;
-
 		return empty( $this->_args['form_id'] ) || (int) $form_id === (int) $this->_args['form_id'];
 	}
 
-	/**
-	 * Save HTML field content to entry meta.
-	 *
-	 * @param array $entry The entry object.
-	 * @return array
-	 */
-	public function save_html_field_content( $entry ) {
+	private function is_applicable_field( $field ) {
+		return $field->get_input_type() === 'html' &&
+			( empty( $this->_args['field_id'] ) || (int) $field->id === (int) $this->_args['field_id'] );
+	}
 
+	private function process_html_content( $content, $form, $entry ) {
+		// Process Live Merge Tags.
+		if (
+			method_exists( 'GP_Populate_Anything_Live_Merge_Tags', 'has_live_merge_tag' ) &&
+			GP_Populate_Anything_Live_Merge_Tags::get_instance()->has_live_merge_tag( $content )
+		) {
+			$content = gp_populate_anything()->live_merge_tags->replace_live_merge_tags_static( $content, $form, $entry );
+		}
+
+		// Replace merge tags and shortcodes.
+		$content = GFCommon::replace_variables( $content, $form, $entry );
+		$content = do_shortcode( $content );
+
+		return ! empty( $content ) ? wp_kses_post( $content ) : '';
+	}
+
+	public function save_html_field_content( $entry ) {
 		$form = GFAPI::get_form( rgar( $entry, 'form_id' ) );
 
 		if ( ! $this->is_applicable_form( $form ) ) {
@@ -57,13 +70,7 @@ class GW_Display_HTML_Field_Entry_Detail {
 		}
 
 		foreach ( $form['fields'] as $field ) {
-			if ( $field->get_input_type() === 'html' ) {
-
-				// Only process matching field_id if defined.
-				if ( ! empty( $this->_args['field_id'] ) && (int) $field->id !== (int) $this->_args['field_id'] ) {
-					continue;
-				}
-
+			if ( $this->is_applicable_field( $field ) ) {
 				gform_update_meta( $entry['id'], 'html_field_' . $field->id, $field->content );
 			}
 		}
@@ -71,53 +78,46 @@ class GW_Display_HTML_Field_Entry_Detail {
 		return $entry;
 	}
 
-	/**
-	 * Display HTML content on entry detail page.
-	 *
-	 * @param array $form  The form object.
-	 * @param array $entry The entry object.
-	 */
-	public function display_html_field_content( $form, $entry ) {
-
+	public function display_html_field_content_entry_detail( $form, $entry ) {
 		if ( ! $this->is_applicable_form( $form ) ) {
 			return;
 		}
 
 		foreach ( $form['fields'] as $field ) {
+			if ( $this->is_applicable_field( $field ) ) {
+				$content = gform_get_meta( $entry['id'], 'html_field_' . $field->id );
+				$content = $this->process_html_content( $content, $form, $entry );
 
-			if ( $field->get_input_type() !== 'html' ) {
-				continue;
-			}
-
-			// Only process matching field_id if defined.
-			if ( ! empty( $this->_args['field_id'] ) && (int) $field->id !== (int) $this->_args['field_id'] ) {
-				continue;
-			}
-
-			$content = gform_get_meta( $entry['id'], 'html_field_' . $field->id );
-
-			// Process GPPA Live Merge Tags if present.
-			if (
-				method_exists( 'GP_Populate_Anything_Live_Merge_Tags', 'has_live_merge_tag' )
-				&& GP_Populate_Anything_Live_Merge_Tags::get_instance()->has_live_merge_tag( $content )
-			) {
-				$content = gp_populate_anything()->live_merge_tags->replace_live_merge_tags_static( $content, $form, $entry );
-			}
-
-			// Process static merge tags.
-			$content = GFCommon::replace_variables( $content, $form, $entry );
-
-			// Process shortcodes.
-			$content = do_shortcode( $content );
-
-			if ( ! empty( $content ) ) {
-				printf(
-					'<h4>%s</h4><div>%s</div><hr>',
-					esc_html( $field->label ),
-					wp_kses_post( $content )
-				);
+				if ( $content ) {
+					printf(
+						'<h4>%s</h4><div>%s</div><hr>',
+						esc_html( $field->label ),
+						$content
+					);
+				}
 			}
 		}
+	}
+
+	public function display_html_field_content_entry_list( $value, $entry, $field, $input_id ) {
+		static $is_running = false;
+
+		if ( $is_running || rgget( 'page' ) !== 'gf_entries' || ! $this->is_applicable_field( $field ) ) {
+			return $value;
+		}
+
+		$form = GFAPI::get_form( $field->formId );
+		if ( ! $this->is_applicable_form( $form ) ) {
+			return $value;
+		}
+
+		$is_running = true;
+		$entry      = GFAPI::get_entry( $entry['id'] );
+		$content    = gform_get_meta( $entry['id'], 'html_field_' . $field->id );
+		$content    = $this->process_html_content( $content, $form, $entry );
+		$is_running = false;
+
+		return $content ?: $value;
 	}
 }
 
