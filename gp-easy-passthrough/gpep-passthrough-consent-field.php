9308a6b0535b4729dcaa15798cdee910b6bcac61
Saif Sultan <saifsultanc@gmail.com>
2025-04-17 08:03:38 +0530

    `gpep-passthrough-consent-field.php`: Added snippet for easy passthrough on Consent Field.

--- gp-easy-passthrough/gpep-passthrough-consent-field.php
+++ gp-easy-passthrough/gpep-passthrough-consent-field.php
@@ -8,7 +8,7 @@
  * Passes through the value of a consent field from the source form to the target form.
  */
 add_filter( 'gform_pre_render', function( $form ) {
-    // Update to the source and target consent field IDs and target form ID.
+	// Update to the source and target consent field IDs and target form ID.
 	$source_consent_field_id = '4';
 	$target_consent_field_id = '4';
 	$target_form_id          = '80';
@@ -18,14 +18,19 @@ add_filter( 'gform_pre_render', function( $form ) {
 	}
 
 	if ( rgget( 'ep_token' ) && is_callable( 'gp_easy_passthrough' ) ) {
-		$entry = gp_easy_passthrough()->get_entry_for_token( rgget( 'ep_token' ) );
+		$token = rgget( 'ep_token' );
+		$entry = $token ? gp_easy_passthrough()->get_entry_for_token( $token ) : array();
+
+		if ( empty( $entry ) ) {
+			return $form;
+		}
 
 		foreach ( $form['fields'] as $field ) {
-			if ( $field->type == 'consent' && $field->id == $target_consent_field_id && $entry[ $source_consent_field_id . '.1' ] == '1' ) {
+			if ( $field->type == 'consent' && $field->id == $target_consent_field_id && rgar( $entry, $source_consent_field_id . '.1' ) == '1' ) {
 				?>
 				<script type="text/javascript">
 					document.addEventListener( 'DOMContentLoaded', function() {
-						var consentCheckbox = document.querySelector( 'input[name="input_<?php echo $field->id; ?>.1"] ');
+						var consentCheckbox = document.getElementById( 'input_<?php echo $form['id']; ?>_<?php echo $field->id; ?>_1' );
 
 						if ( consentCheckbox ) {
 							consentCheckbox.checked = true;
