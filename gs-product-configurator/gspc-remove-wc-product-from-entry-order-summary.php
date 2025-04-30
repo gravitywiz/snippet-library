3705c5ff7c5da94849852f738b0a26fc6c6f5949
Malay Ladu <malayladu@gmail.com>
2025-04-30 22:46:33 +0530

    `gspc-remove-wc-product-from-entry-order-summary.php`: Fixed snippet removes only product name from order summary but retains product price and quantity.

--- gs-product-configurator/gspc-remove-wc-product-from-entry-order-summary.php
+++ gs-product-configurator/gspc-remove-wc-product-from-entry-order-summary.php
@@ -9,5 +9,6 @@ add_filter( 'init', function() {
 	if ( is_callable( 'gs_product_configurator' ) ) {
 		remove_filter( 'gppa_ajax_form_pre_render', array( gs_product_configurator()->wc_product_form_display, 'inject_base_price_product_field_gppa_ajax' ) );
 		remove_filter( 'gform_product_info', array( gs_product_configurator()->wc_product_form_display, 'inject_base_price_into_product_info' ) );
+		remove_filter( 'gform_pre_render', array( gs_product_configurator()->wc_product_form_display, 'inject_base_price_product_field' ), 5 );
 	}
 }, 16 );
