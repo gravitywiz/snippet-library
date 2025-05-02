bb603dafa08c250d95902797ade432c0696aacad
David Smith <david@gravitywiz.com>
2025-05-02 14:01:27 -0400

    `gpep-delete-token-after-use.php`: Updated header to disambiguate from another snippet.

--- gp-easy-passthrough/gpep-delete-token-after-use.php
+++ gp-easy-passthrough/gpep-delete-token-after-use.php
@@ -4,6 +4,11 @@
  * https://gravitywiz.com/documentation/gravity-forms-easy-passthrough/
  *
  * Delete the EP token that was used to populate a given form after that form is submitted.
+ *
+ * **Note:** This differs from the [Delete Passthrough Entry After Submission][1] snippet in that it only deletes the token, preserving
+ * the entry for your records.
+ *
+ * [1]: https://gravitywiz.com/snippet-library/gpep-delete-passthrough-entry-after-submission/
  */
 add_filter( 'gform_after_submission', function() {
 	if ( rgget( 'ep_token' ) && is_callable( 'gp_easy_passthrough' ) ) {
