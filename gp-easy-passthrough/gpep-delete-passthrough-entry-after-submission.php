01c2bb7b76e6cea6e0fe7c4e11d06b446413527d
David Smith <david@gravitywiz.com>
2025-05-02 14:03:30 -0400

    `gpep-delete-passthrough-entry-after-submission.php`: Updated description to disambiguate from a similar snippet.

--- gp-easy-passthrough/gpep-delete-passthrough-entry-after-submission.php
+++ gp-easy-passthrough/gpep-delete-passthrough-entry-after-submission.php
@@ -6,6 +6,11 @@
  * Instruction Video: https://www.loom.com/share/91866484b97248d5bf6a2db576d11957
  *
  * Delete the entry passed through via the EP token after the target form has been submitted.
+ *
+ * **Note:** This differs from the [Delete Token After Use][1] snippet in that it fully deletes the passed through
+ * entry rather than only deleting the token and preserving the entry.
+ *
+ * [1]: https://gravitywiz.com/snippet-library/gpep-delete-token-after-use/
  */
 add_filter( 'gform_after_submission', function() {
 	if ( rgget( 'ep_token' ) && is_callable( 'gp_easy_passthrough' ) ) {
