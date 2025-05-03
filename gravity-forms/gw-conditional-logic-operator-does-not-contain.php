073c80c38c97da93b05a64267de5ff357a978c08
Saif Sultan <saifsultanc@gmail.com>
2025-05-03 15:00:24 +0530

    `gw-conditional-logic-operator-does-not-contain.php`: Fixed rule value for `does not contain` rule.

--- gravity-forms/gw-conditional-logic-operator-does-not-contain.php
+++ gravity-forms/gw-conditional-logic-operator-does-not-contain.php
@@ -45,14 +45,11 @@ class GF_CLO_Does_Not_Contain {
 				operators.does_not_contain = 'does not contain';
 				return operators;
 			} );
-			gform.addFilter( 'gform_conditional_logic_values_input', function( str, objectType, ruleIndex, selectedFieldId, value ) {
-				const operator = jQuery( '#field_rule_operator_' + ruleIndex ).val();
-				if ( operator !== 'does_not_contain' ) {
-					return str;
-				}
-				str = '<input type="text" placeholder="Enter a value" data-js-rule-input="value" class="gfield_rule_select gfield_rule_input active" id="field_rule_value_' + ruleIndex + '" name="field_rule_value_' + ruleIndex + '" value="' + value + '">';
-				return str;
-			} );
+
+			// Override the default GF function to add our custom operator.
+			function ruleNeedsTextValue( rule ) {
+				return ['does_not_contain','contains', 'starts_with', 'ends_with', '<', '>' ].indexOf ( rule.operator ) !== -1;
+			}
 		</script>
 		<?php
 	}
