7e5500fc844f64a44b788532078c55175160d47f
Saif Sultan <saifsultanc@gmail.com>
2025-05-09 16:43:31 +0530

    `gw-disable-submission-on-enter.js`: Fixed an issue with disable enter snippet not working with Conversational Forms.

--- gravity-forms/gw-disable-submission-on-enter.js
+++ gravity-forms/gw-disable-submission-on-enter.js
@@ -7,10 +7,11 @@
  * Download the plugin here: https://gravitywiz.com/gravity-forms-code-chest/
  * 2. Copy and paste the snippet into the editor of the Custom Javascript for Gravity Forms plugin.
  */
-jQuery(document).on( 'keypress', '.gform_wrapper', function (e) {
-    var code = e.keyCode || e.which;
-    if ( code == 13 && ! jQuery( e.target ).is( 'textarea,input[type="submit"],input[type="button"]' ) ) {
-        e.preventDefault();
-        return false;
-    }
-} );
+document.addEventListener( 'keydown', function(e) {
+	var isGravityForm = e.target.closest( '.gform_wrapper' ) !== null;
+	var code = e.keyCode || e.which;
+	if ( code == 13 && ! jQuery( e.target ).is( 'textarea,input[type="submit"],input[type="button"]' )  && isGravityForm ) {
+		e.stopImmediatePropagation();
+		e.preventDefault();
+	}
+}, true );
