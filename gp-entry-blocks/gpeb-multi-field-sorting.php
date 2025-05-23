b8c309d5bde5827e92fe9d48feb30e61459769af
Saif Sultan <saifsultanc@gmail.com>
2025-05-24 00:38:06 +0530

    `gpeb-multi-field-sorting.php`: Added snippet for multi field sorting on Entry Blocks.

--- gp-entry-blocks/gpeb-multi-field-sorting.php
+++ gp-entry-blocks/gpeb-multi-field-sorting.php
@@ -3,39 +3,63 @@
  * Gravity Perks // Entry Blocks // Multi Field Sorting
  * https://gravitywiz.com/documentation/gravity-forms-entry-blocks/
  *
- * Use confirmation message from Form Settings when editing an entry in GPEB.
+ * Mutli Field Sorting for GP Entry Blocks.
  * 
  * Instruction Video: https://www.loom.com/share/b9867d2735d44519bf563961e9b30bd2
  */
-add_filter( 'gpeb_queryer_entries', function( $entries, $gf_queryer ) {
+class GPEB_Multi_Field_Sorting {
 
-	// Update the form ID to match your form.
-	if ( $gf_queryer->form_id != 933 ) {
+	private $form_id;
+	private $primary_sorting_field_id;
+	private $secondary_sorting_field_id;
+	private $sorting_direction;
+
+	public function __construct( $config = array() ) {
+		$this->form_id                    = rgar( $config, 'form_id' );
+		$this->primary_sorting_field_id   = rgar( $config, 'primary_sorting_field_id' );
+		$this->secondary_sorting_field_id = rgar( $config, 'secondary_sorting_field_id' );
+		$this->sorting_direction          = strtoupper( rgar( $config, 'sorting_direction', 'ASC' ) );
+
+		add_action( 'init', array( $this, 'init' ) );
+	}
+
+	public function init() {
+		add_filter( 'gpeb_queryer_entries', array( $this, 'sort_entries' ), 10, 2 );
+	}
+
+	private function is_applicable_form( $current_form_id ) {
+		return $current_form_id == $this->form_id;
+	}
+
+	public function sort_entries( $entries, $gf_queryer ) {
+		if ( ! $this->is_applicable_form( $gf_queryer->form_id ) ) {
+			return $entries;
+		}
+
+		usort( $entries, array( $this, 'sort_callback' ) );
+
 		return $entries;
 	}
 
-	// Update the field IDs to match your form.
-	$primary_sorting_field_id   = '1.6';
-	$secondary_sorting_field_id = '1.3';
-	$sorting_direction = 'ASC'; // 'ASC' or 'DESC'
-
-	usort( $entries, function( $a, $b ) use (
-		$primary_sorting_field_id,
-		$secondary_sorting_field_id,
-		$sorting_direction
-	) {
-		$a_primary = isset( $a[ $primary_sorting_field_id ] ) ? $a[ $primary_sorting_field_id ] : '';
-		$b_primary = isset( $b[ $primary_sorting_field_id ] ) ? $b[ $primary_sorting_field_id ] : '';
+	private function sort_callback( $a, $b ) {
+		$a_primary = rgar( $a, $this->primary_sorting_field_id, '' );
+		$b_primary = rgar( $b, $this->primary_sorting_field_id, '' );
 
 		$cmp = strcasecmp( $a_primary, $b_primary );
-		if ( $cmp === 0 ) {
-			$a_secondary = isset( $a[ $secondary_sorting_field_id ] ) ? $a[ $secondary_sorting_field_id ] : '';
-			$b_secondary = isset( $b[ $secondary_sorting_field_id ] ) ? $b[ $secondary_sorting_field_id ] : '';
+
+		if ( $cmp === 0 && $this->secondary_sorting_field_id ) {
+			$a_secondary = rgar( $a, $this->secondary_sorting_field_id, '' );
+			$b_secondary = rgar( $b, $this->secondary_sorting_field_id, '' );
 			$cmp = strcasecmp( $a_secondary, $b_secondary );
 		}
 
-		return ( $sorting_direction === 'DESC' ) ? -$cmp : $cmp;
-	});
+		return ( $this->sorting_direction === 'DESC' ) ? -$cmp : $cmp;
+	}
+}
 
-	return $entries;
-}, 10, 2 );
+new GPEB_Multi_Field_Sorting( array(
+	'form_id'                    => 933,
+	'primary_sorting_field_id'   => '1.6',
+	'secondary_sorting_field_id' => '1.3',
+	'sorting_direction'          => 'ASC', // or 'DESC'
+) );
