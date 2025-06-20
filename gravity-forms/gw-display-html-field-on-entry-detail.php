bddad13344826ffcb4ebc8c8c7d9d5099986edcc
Saif Sultan <saifsultanc@gmail.com>
2025-06-20 18:09:32 +0530

    `gw-display-html-field-on-entry-detail.php`: Added a snippet to display HTML field content on Entry Details page.

--- gravity-forms/gw-display-html-field-on-entry-detail.php
+++ gravity-forms/gw-display-html-field-on-entry-detail.php
@@ -2,14 +2,11 @@
 /**
  * Gravity Wiz // Gravity Forms // Display HTML Field on Entry Details
  *
- * Instruction Video: https://www.loom.com/share/fa4b49240b6447c7839392af69476bf2
- *
- * Save and display HTML field content (including Live Merge Tags and shortcodes) in the entry detail view.
- * Useful for retaining dynamic HTML field output as it appeared when the entry was submitted.
+ * Displays dynamically rendered HTML field content (with merge tags and shortcodes) on the entry detail and entry list view.
  *
  * Plugin Name:  Display HTML Field on Entry Details
  * Plugin URI:   http://gravitywiz.com/
- * Description:  Save and display HTML field content (including Live Merge Tags and shortcodes) in the entry detail view.
+ * Description:  Display HTML field content (with Live Merge Tags and shortcodes) dynamically in the entry detail view.
  * Author:       Gravity Wiz
  * Version:      0.1
  * Author URI:   http://gravitywiz.com
@@ -31,7 +28,6 @@ class GW_Display_HTML_Field_Entry_Detail {
 	}
 
 	public function init() {
-		add_filter( 'gform_entry_post_save', array( $this, 'save_html_field_content' ), 10, 1 );
 		add_action( 'gform_entry_detail', array( $this, 'display_html_field_content_entry_detail' ), 10, 2 );
 		add_filter( 'gform_get_input_value', array( $this, 'display_html_field_content_entry_list' ), 10, 4 );
 	}
@@ -47,7 +43,7 @@ class GW_Display_HTML_Field_Entry_Detail {
 	}
 
 	private function process_html_content( $content, $form, $entry ) {
-		// Process Live Merge Tags.
+		// Process Live Merge Tags if available.
 		if (
 			method_exists( 'GP_Populate_Anything_Live_Merge_Tags', 'has_live_merge_tag' ) &&
 			GP_Populate_Anything_Live_Merge_Tags::get_instance()->has_live_merge_tag( $content )
@@ -62,22 +58,6 @@ class GW_Display_HTML_Field_Entry_Detail {
 		return ! empty( $content ) ? wp_kses_post( $content ) : '';
 	}
 
-	public function save_html_field_content( $entry ) {
-		$form = GFAPI::get_form( rgar( $entry, 'form_id' ) );
-
-		if ( ! $this->is_applicable_form( $form ) ) {
-			return $entry;
-		}
-
-		foreach ( $form['fields'] as $field ) {
-			if ( $this->is_applicable_field( $field ) ) {
-				gform_update_meta( $entry['id'], 'html_field_' . $field->id, $field->content );
-			}
-		}
-
-		return $entry;
-	}
-
 	public function display_html_field_content_entry_detail( $form, $entry ) {
 		if ( ! $this->is_applicable_form( $form ) ) {
 			return;
@@ -85,8 +65,7 @@ class GW_Display_HTML_Field_Entry_Detail {
 
 		foreach ( $form['fields'] as $field ) {
 			if ( $this->is_applicable_field( $field ) ) {
-				$content = gform_get_meta( $entry['id'], 'html_field_' . $field->id );
-				$content = $this->process_html_content( $content, $form, $entry );
+				$content = $this->process_html_content( $field->content, $form, $entry );
 
 				if ( $content ) {
 					printf(
@@ -112,9 +91,7 @@ class GW_Display_HTML_Field_Entry_Detail {
 		}
 
 		$is_running = true;
-		$entry      = GFAPI::get_entry( $entry['id'] );
-		$content    = gform_get_meta( $entry['id'], 'html_field_' . $field->id );
-		$content    = $this->process_html_content( $content, $form, $entry );
+		$content    = $this->process_html_content( $field->content, $form, $entry );
 		$is_running = false;
 
 		return $content ?: $value;
