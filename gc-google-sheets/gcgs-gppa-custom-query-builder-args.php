6c471e6e4380c5200ca8680404cd82abfe9d2bba
Saif Sultan <saif@gravitywiz.com>
2025-06-27 16:56:03 +0530

    `gcgs-gppa-custom-query-builder-args.php`: Added instruction video and scoping to specific field-form IDs.

--- gc-google-sheets/gcgs-gppa-custom-query-builder-args.php
+++ gc-google-sheets/gcgs-gppa-custom-query-builder-args.php
@@ -2,12 +2,15 @@
 /**
  * Gravity Connect // Google Sheets // Custom Query Build Arguments
  *
+ * Instruction Video: https://www.loom.com/share/a7bf1139baef48fc8b9b31209827cd17
+ *
  * This snippet customizes the generation of query builder arguments for Google Sheets queries.
  *
  * Installation:
  *   1. Install per https://gravitywiz.com/documentation/how-do-i-install-a-snippet/
  */
 add_filter( 'gcgs_gppa_query_builder_args', function( $query_builder_args, $args, $object ) {
+
 	/** @var string|string[] */
 	$filter_value = null;
 
@@ -20,8 +23,20 @@ add_filter( 'gcgs_gppa_query_builder_args', function( $query_builder_args, $args
 	/** @var string */
 	$property_id = null;
 
+	/** @var object */
+	$field = null;
+
 	// phpcs:ignore WordPress.PHP.DontExtract.extract_extract
 	extract( $args );
+
+	// UPDATE 1 to your targeted field ID.
+	$target_field_id = '1';
+	// UPDATE 2 to your targeted form ID.
+	$target_form_id = '2';
+
+	if ( $field->id != $target_field_id || $field->formId != $target_form_id || $filter['operator'] != 'is' ) {
+		return $query_builder_args;
+	}
 
 	$column_letter = $object->get_column_letter( $args['primary_property_value'], $property_id );
 
@@ -32,7 +47,7 @@ add_filter( 'gcgs_gppa_query_builder_args', function( $query_builder_args, $args
 			if ( ! empty( $v ) ) {
 				$conditions[] = sprintf( "lower(%s) = '%s'", $column_letter, strtolower( $v ) );
 			}
-	}
+		}
 
 		// Implode the conditions array with ' OR ' to form the $clause.
 		$query_builder_args['where'][ $filter_group_index ][ $filter_group_index ] = implode( ' OR ', $conditions );
