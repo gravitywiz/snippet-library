4eafadb4aff2214a487ce4e43f74c326fe67b6b3
Sebastian Pedersen <sebastian@gravitywiz.com>
2025-08-09 23:27:00 -0700

    `gpeb-display-stars-for-rating-fields.php`: Added customizable SVG star with empty star display option for rating fields.

--- gp-entry-blocks/gpeb-display-stars-for-rating-fields.php
+++ gp-entry-blocks/gpeb-display-stars-for-rating-fields.php
@@ -7,20 +7,24 @@ add_filter( 'gpeb_entry', function( $entry, $form ) {
 
 	// Configure star settings
 	$config = [
-		'star_size'        => '24',      // Width/height of stars in pixels
+		'star_size'        => 24,        // Width/height of stars in pixels
 		'star_color'       => '#FFAC33', // Color of the stars
-		'stroke_width'     => '1.5',     // Thickness of star outline
+		'stroke_width'     => 1.5,       // Thickness of star outline
 		'show_empty_stars' => false,     // Whether to show empty stars
 	];
 
-	$star_svg = '<svg xmlns="http://www.w3.org/2000/svg" width="' . $config['star_size'] . '" height="' . $config['star_size'] . '" viewBox="0 0 24 24" stroke="' . $config['star_color'] . '" stroke-width="' . $config['stroke_width'] . '" stroke-linecap="round" stroke-linejoin="round"><polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"/></svg>';
+	$star_size    = max( 1, (int) $config['star_size'] );
+	$stroke_width = (float) $config['stroke_width'];
+	$star_color   = sanitize_hex_color( $config['star_color'] ) ?: '#FFAC33';
+
+	$star_svg = '<svg xmlns="http://www.w3.org/2000/svg" width="' . esc_attr( $star_size ) . '" height="' . esc_attr( $star_size ) . '" viewBox="0 0 24 24" stroke="' . esc_attr( $star_color ) . '" stroke-width="' . esc_attr( $stroke_width ) . '" stroke-linecap="round" stroke-linejoin="round"><polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"/></svg>';
 
 	foreach ( $form['fields'] as $field ) {
 		if ( $field->get_input_type() === 'rating' ) {
 			$selected_value = $entry[ $field->id ];
 			foreach ( $field->choices as $index => $choice ) {
 				if ( $choice['value'] === $selected_value ) {
-					$filled_stars = str_repeat(str_replace('<svg ', '<svg fill="' . $config['star_color'] . '" class="gpeb-filled-star" ', $star_svg), $index + 1);
+					$filled_stars = str_repeat(str_replace('<svg ', '<svg fill="' . esc_attr( $star_color ) . '" class="gpeb-filled-star" ', $star_svg), $index + 1);
 					$empty_stars = $config['show_empty_stars'] ? str_repeat(str_replace('<svg ', '<svg fill="none" class="gpeb-outline-star" ', $star_svg), 5 - ($index + 1)) : '';
 
 					$entry[$field->id] = $filled_stars . $empty_stars;
