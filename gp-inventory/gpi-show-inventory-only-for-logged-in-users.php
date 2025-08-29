57b61dd4beca5e15ecaf1b9ea7d03ec2a5f16a06
Saif Sultan <saif@gravitywiz.com>
2025-08-29 11:46:22 +0530

    `gpi-show-inventory-only-for-logged-in-users.php`: Added snippet to show available inventory message only for logged in user.

---
+++ gp-inventory/gpi-show-inventory-only-for-logged-in-users.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Gravity Perks // Inventory // Show Inventory Only For Logged In users
+ * https://gravitywiz.com/documentation/gravity-forms-inventory/
+ *
+ * Instruction Video: https://www.loom.com/share/2d9843302a54456987ed2c66a83612ba
+ *
+ * By default the inventory available is shown for everyone, if enabled.
+ * Use this filter to show the inventory available only for logged in users.
+ */
+add_filter( 'gpi_inventory_available_message', function( $message ) {
+	if ( is_user_logged_in() ) {
+		return $message;
+	}
+	return '';
+} );
