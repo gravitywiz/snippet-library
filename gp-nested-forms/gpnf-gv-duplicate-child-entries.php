2c9ef1fb6cae81831bbb6497436c7107adba8f6f
David Smith <david@gravitywiz.com>
2025-10-29 17:24:07 -0400

    `gpnf-gv-duplicate-child-entries.php`: Removed emoji.

--- gp-nested-forms/gpnf-gv-duplicate-child-entries.php
+++ gp-nested-forms/gpnf-gv-duplicate-child-entries.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * This snippet has graduated from the experimental folder. 🎓
+ * This snippet has graduated from the experimental folder.
  * You can now find the snippet here:
  * https://gravitywiz.com/snippet-library/gpnf-duplicate-child-entries-on-parent-duplication/
  */
