999404a37bce8770df8533db6119029cbc509bee
matty0501 <17788098+matty0501@users.noreply.github.com>
2025-12-09 13:25:12 +0000

    `gcgs-disable-async.php`: Added new snippet to disable async.

---
+++ gc-google-sheets/gcgs-disable-async.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Gravity Connect // Google Sheets // Disable Async on Google Sheets Feeds
+ *
+ * By default, GC Google Sheets feeds run asynchronously. This snippet will disable that, so they run in the same request as the form submission.
+ *
+ * This is useful for debugging whether there is a wider issue with asynchronous feeds on a site.
+ *
+ * Installation:
+ *   1. Install per https://gravitywiz.com/documentation/how-do-i-install-a-snippet/
+ */
+add_filter( 'gform_is_feed_asynchronous', function( $is_async, $feed, $entry, $form ) {
+	if ( $feed['addon_slug'] === 'gp-google-sheets' ) {
+		return false;
+	}
+
+	return $is_async;
+}, 50, 4 );
