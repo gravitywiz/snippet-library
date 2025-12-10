b59c63f5e598a20ba39bf7e0f99078c4755170aa
veryspry <ehlinger.matt@gmail.com>
2025-12-10 14:44:44 +0100

    wip

--- gravity-forms/gw-rich-text-html-fields.php
+++ gravity-forms/gw-rich-text-html-fields.php
@@ -25,22 +25,6 @@ add_action( 'gform_field_standard_settings', function( $position ) {
 	}
 	?>
 
-	<li class="content_setting rich_content_setting field_setting">
-		<label for="field_content" class="section_label">
-			<?php esc_html_e( 'Content', 'gravityforms' ); ?>
-			<?php gform_tooltip( 'form_field_content' ); ?>
-		</label>
-		<?php
-		$id       = 'field_rich_content';
-		$settings = array(
-			'tinymce'       => true,
-			'textarea_name' => $id,
-			'editor_height' => 250,
-		);
-		wp_editor( '', $id, $settings );
-		?>
-	</li>
-
 	<style>
 		.rich_content_setting label {
 			/* Display the label above the editor without overlapping the Visual/Code tabs. */
@@ -60,63 +44,94 @@ add_action( 'gform_field_standard_settings', function( $position ) {
 	</style>
 
 	<script>
+		window.allEditors = [];
+
 		jQuery( document ).on( 'gform_load_field_settings', function( event, field ) {
+			if (field.type !== 'html') {
+				return;
+			}
+
+			console.log('gform_load_field_settings', { event, field, content: field.content, type: field.type });
+
 			var id = 'field_rich_content';
 			wp.editor.remove( id );
+
 			jQuery( '#' + id ).val( field.content );
+
 			wp.editor.initialize( id, {
 				tinymce: {
+					height: 250,
 					setup: function( editor ) {
+
+
+						window.allEditors.push(editor);
+						console.log({ editor })
+
+
 						editor.on( 'Paste Change input Undo Redo', function () {
 							SetFieldProperty( 'content', editor.getContent() );
 						} );
+
+						editor.settings.toolbar1 = 'bold,italic,underline,bullist,numlist,alignleft,aligncenter,alignright,link,image';
+
+						editor.setContent(field.content);
+
+						// Handle image insertion from media library
+						editor.addButton( 'image', {
+							icon: 'image',
+							tooltip: 'Insert Image',
+							onclick: function() {
+								var frame = wp.media({
+									title: 'Insert Media',
+									button: { text: 'Insert into HTML Field' },
+									multiple: false,
+									library: { type: 'image' }
+								} );
+
+								frame.on('select', function() {
+									var selection = frame.state().get('selection').first();
+									if (!selection) {
+										return;
+									}
+
+									var attachment = selection.toJSON();
+									var url = attachment.url.replace(/"/g, '&quot;');
+									var alt = (attachment.alt || '').replace(/"/g, '&quot;');
+									editor.insertContent('<img src="' + url + '" alt="' + alt + '" />');
+								} );
+
+								frame.open();
+							}
+						} );
+
+						// Switch to visual/text mode.
+						jQuery(`#wp-${id}-wrap .switch-tmce, #wp-${id}-wrap .switch-html`).on('click', function() {
+							var mode = jQuery(this).hasClass('switch-tmce') ? 'tmce' : 'html';
+
+							window.switchEditors.go(id, mode);
+						});
 					}
 				},
 				quicktags: true
 			} );
 		} );
-		jQuery( document).on( 'tinymce-editor-setup', function ( event, editor ) {
-			var editorId = 'field_rich_content';
-			if ( editor.id === editorId ) {
-				editor.settings.toolbar1 = 'bold,italic,underline,bullist,numlist,alignleft,aligncenter,alignright,link,image';
-
-				// Handle image insertion from media library
-				editor.addButton( 'image', {
-					icon: 'image',
-					tooltip: 'Insert Image',
-					onclick: function() {
-						var frame = wp.media({
-							title: 'Insert Media',
-							button: { text: 'Insert into HTML Field' },
-							multiple: false,
-							library: { type: 'image' }
-						} );
-
-						frame.on('select', function() {
-							var selection = frame.state().get('selection').first();
-							if (!selection) {
-								return;
-							}
-
-							var attachment = selection.toJSON();
-							var url = attachment.url.replace(/"/g, '&quot;');
-							var alt = (attachment.alt || '').replace(/"/g, '&quot;');
-							editor.insertContent('<img src="' + url + '" alt="' + alt + '" />');
-						} );
-
-						frame.open();
-					}
-				} );
-
-				// Switch to visual/text mode.
-				jQuery(`#wp-${editorId}-wrap .switch-tmce, #wp-${editorId}-wrap .switch-html`).on('click', function() {
-					var mode = jQuery(this).hasClass('switch-tmce') ? 'tmce' : 'html';
-
-					window.switchEditors.go(editorId, mode);
-				});
-			}
-		} );
 	</script>
+
+	<li class="content_setting rich_content_setting field_setting">
+		<label for="field_content" class="section_label">
+			<?php esc_html_e( 'Content', 'gravityforms' ); ?>
+			<?php gform_tooltip( 'form_field_content' ); ?>
+		</label>
+		<?php
+		$id       = 'field_rich_content';
+		$settings = array(
+			'tinymce'       => true,
+			'textarea_name' => $id,
+			'editor_height' => 250,
+		);
+		wp_editor( '', $id, $settings );
+		?>
+	</li>
 
 	<?php
 } );
