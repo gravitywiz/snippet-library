eaca5d646313548942a255e8679bd5e5cc2b917d
veryspry <ehlinger.matt@gmail.com>
2025-12-11 10:24:46 +0100

    wip2

--- gravity-forms/gw-rich-text-html-fields.php
+++ gravity-forms/gw-rich-text-html-fields.php
@@ -44,77 +44,104 @@ add_action( 'gform_field_standard_settings', function( $position ) {
 	</style>
 
 	<script>
-		window.allEditors = [];
 
-		jQuery( document ).on( 'gform_load_field_settings', function( event, field ) {
-			if (field.type !== 'html') {
-				return;
+		function gwRichTextHTMLFields() {
+			function isReady() {
+				return document.readyState === 'complete';
 			}
 
-			console.log('gform_load_field_settings', { event, field, content: field.content, type: field.type });
+			async function waitForDOMReady() {
+				console.log('running wait function')
+				return new Promise((resolve) => {
+					const interval = setInterval(() => {
+						if (isReady()) {
+							console.log('detected ready')
+							clearInterval(interval)	;
+							resolve();
+						}
+					}, 50)
+				});
+			}
 
-			var id = 'field_rich_content';
-			wp.editor.remove( id );
+			jQuery( document ).on( 'gform_load_field_settings', async function( event, field ) {
+				if (field.type !== 'html') {
+					return;
+				}
 
-			jQuery( '#' + id ).val( field.content );
+				// Set textarea value BEFORE initializing the editor so it has content to load
+				jQuery( '#' + id ).val( field.content );
 
-			wp.editor.initialize( id, {
-				tinymce: {
-					height: 250,
-					setup: function( editor ) {
+				console.log('gform_load_field_settings', { tineymce: window.tinymce, event, field, content: field.content, type: field.type });
+
+				if (!isReady()) {
+					console.log('waiting for dom to be ready')
+					await waitForDOMReady();
+				}
+
+				console.log('continue init')
+
+				var id = 'field_rich_content';
+				wp.editor.remove( id );
 
 
-						window.allEditors.push(editor);
-						console.log({ editor })
+				wp.editor.initialize( id, {
+					tinymce: {
+						height: 250,
+						setup: function( editor ) {
 
+							editor.on( 'Paste Change input Undo Redo', function () {
+								SetFieldProperty( 'content', editor.getContent() );
+							} );
 
-						editor.on( 'Paste Change input Undo Redo', function () {
-							SetFieldProperty( 'content', editor.getContent() );
-						} );
+							editor.settings.toolbar1 = 'bold,italic,underline,bullist,numlist,alignleft,aligncenter,alignright,link,image';
 
-						editor.settings.toolbar1 = 'bold,italic,underline,bullist,numlist,alignleft,aligncenter,alignright,link,image';
+							editor.on('init', function () {
+								editor.setContent(field.content);
+				    		    console.log('WP TinyMCE editor ready 1:', editor.id, editor.getContent());
+				    		});
 
-						editor.setContent(field.content);
+							// Handle image insertion from media library
+							editor.addButton( 'image', {
+								icon: 'image',
+								tooltip: 'Insert Image',
+								onclick: function() {
+									var frame = wp.media({
+										title: 'Insert Media',
+										button: { text: 'Insert into HTML Field' },
+										multiple: false,
+										library: { type: 'image' }
+									} );
 
-						// Handle image insertion from media library
-						editor.addButton( 'image', {
-							icon: 'image',
-							tooltip: 'Insert Image',
-							onclick: function() {
-								var frame = wp.media({
-									title: 'Insert Media',
-									button: { text: 'Insert into HTML Field' },
-									multiple: false,
-									library: { type: 'image' }
-								} );
+									frame.on('select', function() {
+										var selection = frame.state().get('selection').first();
+										if (!selection) {
+											return;
+										}
 
-								frame.on('select', function() {
-									var selection = frame.state().get('selection').first();
-									if (!selection) {
-										return;
-									}
+										var attachment = selection.toJSON();
+										var url = attachment.url.replace(/"/g, '&quot;');
+										var alt = (attachment.alt || '').replace(/"/g, '&quot;');
+										editor.insertContent('<img src="' + url + '" alt="' + alt + '" />');
+									} );
 
-									var attachment = selection.toJSON();
-									var url = attachment.url.replace(/"/g, '&quot;');
-									var alt = (attachment.alt || '').replace(/"/g, '&quot;');
-									editor.insertContent('<img src="' + url + '" alt="' + alt + '" />');
-								} );
+									frame.open();
+								}
+							} );
 
-								frame.open();
-							}
-						} );
+							// Switch to visual/text mode.
+							jQuery(`#wp-${id}-wrap .switch-tmce, #wp-${id}-wrap .switch-html`).on('click', function() {
+								var mode = jQuery(this).hasClass('switch-tmce') ? 'tmce' : 'html';
 
-						// Switch to visual/text mode.
-						jQuery(`#wp-${id}-wrap .switch-tmce, #wp-${id}-wrap .switch-html`).on('click', function() {
-							var mode = jQuery(this).hasClass('switch-tmce') ? 'tmce' : 'html';
-
-							window.switchEditors.go(id, mode);
-						});
-					}
-				},
-				quicktags: true
+								window.switchEditors.go(id, mode);
+							});
+						}
+					},
+					quicktags: true
+				} );
 			} );
-		} );
+		}
+
+		gwRichTextHTMLFields();
 	</script>
 
 	<li class="content_setting rich_content_setting field_setting">
