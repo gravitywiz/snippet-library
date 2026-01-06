da1b1ae146ba655c15245eb73e2a9c223a6a61cb
SebastianWiz <165194375+SebastianWiz@users.noreply.github.com>
2026-01-05 23:54:21 -0800

    `gpev-allow-invalid-submissions.php`: Added snippet to allow form submissions regardless of email validation status.

--- gp-email-validator/gpev-allow-invalid-submissions.php
+++ gp-email-validator/gpev-allow-invalid-submissions.php
@@ -6,12 +6,39 @@
  * Allow form submissions to go through regardless of email validation status, while still preserving
  * the original validation results (status, reasons, and technical details) in entry meta.
  */
-add_filter( 'gpev_validation_result', function( $validation_result, $value, $field, $form, $validator ) {
+$gpev_failures = [];
+
+// Update "123" to your form ID and "4" to your Email field ID.
+add_filter( 'gpev_validation_result_123_4', function( $validation_result, $value, $field, $form, $validator ) use ( &$gpev_failures ) {
 	if ( ! $validation_result ) {
 		return $validation_result;
 	}
 
-	$validation_result->mark_valid();
+	if ( ! $validation_result->is_valid() ) {
+		$form_id  = rgar( $form, 'id' );
+		$field_id = $field->id ?? null;
+
+		if ( $form_id && $field_id ) {
+			$gpev_failures[ $form_id ][ $field_id ] = true;
+		}
+	}
 
 	return $validation_result;
 }, 10, 5 );
+
+// Update "123" to your form ID and "4" to your Email field ID.
+add_filter( 'gform_field_validation_123_4', function( $result, $value, $form, $field ) use ( &$gpev_failures ) {
+	if ( ! gp_email_validator()->is_email_validator_field( $field ) ) {
+		return $result;
+	}
+
+	$form_id  = rgar( $form, 'id' );
+	$field_id = $field->id ?? null;
+
+	if ( $form_id && $field_id && ! empty( $gpev_failures[ $form_id ][ $field_id ] ) ) {
+		$result['is_valid'] = true;
+		$result['message']  = '';
+	}
+
+	return $result;
+}, 11, 4 );
