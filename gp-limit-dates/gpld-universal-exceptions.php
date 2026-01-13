2da16dadb12234410bfb17f08c4f918169c02cc7
SebastianWiz <165194375+SebastianWiz@users.noreply.github.com>
2026-01-12 18:28:46 -0800

    `gpld-universal-exceptions.php`: Added option to exclude date fields from universal exceptions.

--- gp-limit-dates/gpld-universal-exceptions.php
+++ gp-limit-dates/gpld-universal-exceptions.php
@@ -6,15 +6,47 @@
  * Except dates across all Date fields on all forms.
  */
 add_filter( 'gpld_limit_dates_options', function( $field_options, $form, $field ) {
+	// Optional exclusions: add IDs to skip applying exceptions.
+	$exclude = array();
+	/*
+	$exclude = array(
+		array(
+			'form_id' => 123, // Exclude all date fields on form 123.
+		),
+		array(
+			'form_id'  => 124,
+			'field_id' => 5, // Exclude field 5 on form 124.
+		),
+	);
+	*/
 
-	foreach ( $field_options as &$_field_options ) {
+	$form_id           = (int) $form['id'];
+	$excluded_field_ids = array();
+
+	foreach ( $exclude as $rule ) {
+		if ( empty( $rule['form_id'] ) || (int) $rule['form_id'] !== $form_id ) {
+			continue;
+		}
+
+		if ( empty( $rule['field_id'] ) ) {
+			return $field_options;
+		}
+
+		$excluded_field_ids[] = (int) $rule['field_id'];
+	}
+
+	foreach ( $field_options as $field_id => &$_field_options ) {
+		if ( in_array( (int) $field_id, $excluded_field_ids, true ) ) {
+			continue;
+		}
+
 		if ( ! isset( $_field_options['exceptions'] ) || ! is_array( $_field_options['exceptions'] ) ) {
 			$_field_options['exceptions'] = array();
 		}
 		// Add as many exceptions as you need here.
-		$_field_options['exceptions'][] = '04/01/2023';
-		$_field_options['exceptions'][] = '07/04/2023';
-		$_field_options['exceptions'][] = '12/25/2023';
+		$_field_options['exceptions'][] = '04/01/2026';
+		$_field_options['exceptions'][] = '07/04/2026';
+		$_field_options['exceptions'][] = '12/25/2026';
 	}
 
 	return $field_options;
