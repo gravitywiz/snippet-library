c72ae4fb90fc01db6fc40feaed15e7f3fb9c2ae3
veryspry <ehlinger.matt@gmail.com>
2026-02-11 13:52:21 +0100

    `gravity-connect/gc-disable-async-feed-processing.php`: Added new snippet to disable async for all GC plugins.

---
+++ gravity-connect/gc-disable-async-feed-processing.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Gravity Connect // Disable Async Feed Processing for all Gravity Connect Plugins
+ *
+ * By default, Gravity Connect feeds run asynchronously. This snippet will disable that, so they run in the same request as the form submission.
+ *
+ * This is useful for debugging whether there is a wider issue with asynchronous feeds on a site.
+ *
+ * Installation:
+ *   1. Install per https://gravitywiz.com/documentation/how-do-i-install-a-snippet/
+ */
+add_filter( 'gform_is_feed_asynchronous', function( $is_async, $feed, $entry, $form ) {
+	if ( ! class_exists( 'GFFeedAddOn' ) ) {
+		return $is_async;
+	}
+
+	$addon = GFFeedAddOn::get_addon_by_slug( $feed['addon_slug'] );
+
+	if ( ! $addon ) {
+		return $is_async;
+	}
+
+	if ( $addon instanceof \GC_Plugin ) {
+		return false;
+	}
+
+	return $is_async;
+}, 50, 4 );
