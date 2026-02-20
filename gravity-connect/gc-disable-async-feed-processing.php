faeedbefa19ce040abc90615c10bb85aef64164d
David Smith <david@gravitywiz.com>
2026-02-20 17:29:26 -0500

    `gc-disable-async-feed-processing.php`: Updated header for HSL compat.

--- gravity-connect/gc-disable-async-feed-processing.php
+++ gravity-connect/gc-disable-async-feed-processing.php
@@ -1,13 +1,15 @@
 <?php
 /**
- * Gravity Connect // Disable Async Feed Processing for all Gravity Connect Plugins
+ * Gravity Wiz // Gravity Connect // Disable Async Feed Processing for all Gravity Connect Plugins
  *
  * By default, Gravity Connect feeds run asynchronously. This snippet will disable that, so they run in the same request as the form submission.
  *
  * This is useful for debugging whether there is a wider issue with asynchronous feeds on a site.
  *
  * Installation:
- *   1. Install per https://gravitywiz.com/documentation/how-do-i-install-a-snippet/
+ * 
+ * 1. Install per https://gravitywiz.com/documentation/how-do-i-install-a-snippet/
+ * 2. Profit.  
  */
 add_filter( 'gform_is_feed_asynchronous', function( $is_async, $feed, $entry, $form ) {
 	if ( ! class_exists( 'GFFeedAddOn' ) ) {
