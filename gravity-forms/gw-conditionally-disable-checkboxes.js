f88e07321bd94e15a87b91cb968f91b15607d187
Saif Sultan <saif@gravitywiz.com>
2026-02-21 07:20:17 +0530

    `gw-conditionally-disable-checkboxes.js`: Updated snippet to work with multiple checkbox groupings.

--- gravity-forms/gw-conditionally-disable-checkboxes.js
+++ gravity-forms/gw-conditionally-disable-checkboxes.js
@@ -12,12 +12,15 @@
  * 2. Install this snippet with our free Custom JavaScript plugin.
  *    https://gravitywiz.com/gravity-forms-code-chest/
  */
-// Update "1" to your first Checkbox field's ID.
-var $cbs1 = $( '#input_GFFORMID_1' );
-// Update "2" to your second Checkbox field's ID.
-var $cbs2 = $( '#input_GFFORMID_2' );
-// See video above for instructions on editing exclusions.
-// Note: This uses the choice value not the choice label. When targeting Product Option fields configured as a checkbox, you must include the price (e.g. 'First Choice B|15').
+// Add all checkbox field IDs here
+var checkboxGroups = [
+	'#input_GFFORMID_1',
+	'#input_GFFORMID_2',
+	'#input_GFFORMID_3',
+	'#input_GFFORMID_4'
+];
+
+// Exclusions map: key = value of checked checkbox, value = array of checkbox values to disable
 var exclusions = {
 	// First Checkbox field exclusions.
 	'First Choice A': [ 'First Choice B' ],
@@ -29,34 +32,44 @@ var exclusions = {
 	'Third Choice B': [ 'Third Choice A' ],
 	'Fourth Choice B': [ 'Third Choice A' ],
 	'Fifth Choice B': [ 'Third Choice A' ],
+	// Third Checkbox field exclusions.
+	'First Choice C': [ 'First Choice D', 'Second Choice D' ],
+	// Fourth Checkbox field exclusions.
+	'First Choice D': [ 'First Choice C' ],
+	'Second Choice D': [ 'First Choice C' ]
 };
 
-$cbs1.on( 'change', function() {
-	gwDisableCheckboxes( $cbs1, $cbs2, exclusions )
-} );
+var $groups = $( checkboxGroups.join(',') );
+$groups.on('change', 'input[type="checkbox"]', function() {
+	updateAllCheckboxes();
+});
 
-$cbs2.on( 'change', function() {
-	gwDisableCheckboxes( $cbs2, $cbs1, exclusions )
-} );
+updateAllCheckboxes();
 
-function gwDisableCheckboxes( $triggerField, $targetField, exclusions ) {
+function updateAllCheckboxes() {
 
+	// Collect all checked values across all groups
 	var checkedValues = [];
-	$.each( $triggerField.find( 'input:checked:not( .gplc-disabled, .gwlc-disabled, .gpi-disabled )' ), function() {
-		checkedValues.push( $(this).val() );
-	} );
 
-	var $targetCheckboxes = $targetField.find( 'input[type="checkbox"]:not( .gplc-disabled, .gwlc-disabled, .gpi-disabled )' );
-	$targetCheckboxes.prop( 'disabled', false );
+	$groups.each(function() {
+		$(this).find('input:checked:not(.gplc-disabled, .gwlc-disabled, .gpi-disabled)')
+			.each(function() {
+				checkedValues.push($(this).val());
+			});
+	});
 
-	for ( const [ key, value ] of Object.entries( exclusions ) ) {
-		if ( $.inArray( key, checkedValues ) !== -1 ) {
-			for ( const targetValue of value ) {
-				$targetCheckboxes.filter( '[value="' + targetValue + '"]' )
-				                 .prop( 'checked', false )
-				                 .prop( 'disabled', true );
-			}
+	// First reset everything
+	$groups.find('input[type="checkbox"]:not(.gplc-disabled, .gwlc-disabled, .gpi-disabled)')
+		.prop('disabled', false);
+
+	// Apply exclusions globally
+	$.each(exclusions, function(key, valuesToDisable) {
+		if ($.inArray(key, checkedValues) !== -1) {
+			$.each(valuesToDisable, function(index, targetValue) {
+				$groups.find('input[value="' + targetValue + '"]')
+					.prop('checked', false)
+					.prop('disabled', true);
+			});
 		}
-	}
-
+	});
 }
