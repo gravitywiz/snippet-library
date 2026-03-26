9c6bb1364081ca20c4802a8f5785cde015427211
David Smith <david@gravitywiz.com>
2026-03-26 17:11:38 -0400

    `gw-form-list-active-by-default.php`: Deprecated in favor of GF Dev Tools.

--- gravity-forms/gw-form-list-active-by-default.php
+++ gravity-forms/gw-form-list-active-by-default.php
@@ -1,47 +1,6 @@
 <?php
 /**
- * Gravity Wiz // Gravity Forms // Default Form List to Active Forms
- *
- * Sets Form List view to Active Forms by Default.
- *
- * @version 0.1
- * @author  David Smith <david@gravitywiz.com>
- * @license GPL-2.0+
- * @link    https://gravitywiz.com
- *
- * Plugin Name: Gravity Forms Default Form List to Active
- * Plugin URI: http://gravitywiz.com
- * Description: Sets Form List view to Active Forms by Default.
- * Author: Gravity Wiz
- * Version: 0.1
- * Author URI: http://gravitywiz.com
- *
+ * This snippet is now available as a free plugin with Spellbook. ✨
+ * Instructions for installing this plugin can be found here:
+ * https://gravitywiz.com/gravity-forms-dev-tools/#form-list-default-filter
  */
-add_action( 'init', function() {
-	if ( ! class_exists( 'GFForms' ) ) {
-		return;
-	}
-	if ( GFForms::get_page() === 'form_list' ) {
-
-		$params = array();
-
-		if ( ! isset( $_GET['sort'] ) ) {
-			$params = array(
-				'sort'    => 'id',
-				'dir'     => 'desc',
-				'orderby' => 'id',
-				'order'   => 'desc',
-			);
-		}
-
-		if ( ! isset( $_GET['filter'] ) ) {
-			$params['filter'] = 'active';
-		}
-
-		if ( ! empty( $params ) ) {
-			wp_redirect( add_query_arg( $params ) );
-			exit;
-		}
-	}
-
-} );
