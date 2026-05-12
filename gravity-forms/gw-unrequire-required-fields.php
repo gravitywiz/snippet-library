3ed26f0506a0c664027c18811e2d7bf664fd8b70
Saif Sultan <saifsultanc@gmail.com>
2026-05-12 21:33:36 +0530

    `gw-unrequire-required-fields.php`: Added deprecation notice to the snippet.

--- gravity-forms/gw-unrequire-required-fields.php
+++ gravity-forms/gw-unrequire-required-fields.php
@@ -1,6 +1,6 @@
 <?php
 /**
  * WARNING! THIS SNIPPET IS DEPRECATED. 🚧
- * This functionality is now available as a free plugin via Spellbook.
+ * This functionality is now available as a free plugin called Dev Tools via Spellbook.
  * You can read more about it here: https://gravitywiz.com/gravity-forms-dev-tools/
  */
