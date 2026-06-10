7f1d837bcbba2fb244a490f0c11fde909dfd2e25
Saif Sultan <saifsultanc@gmail.com>
2026-06-10 11:34:38 +0530

    `gpld-limit-max-date-to-current-year.php`: Added new snippet.

--- gp-limit-dates/gpld-limit-max-date-to-current-year.php
+++ gp-limit-dates/gpld-limit-max-date-to-current-year.php
@@ -6,7 +6,7 @@
 // Update "123" to your form ID and "4" to your Date field ID.
 add_filter( 'gpld_limit_dates_options_123_4', function( $field_options ) {
 
-	$field_options['maxDate'] = date( '12/31/Y' );
+	$field_options['maxDate'] = gmdate( '12/31/Y' );
 
 	return $field_options;
 } );
