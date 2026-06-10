f59a1cf59f977306b18b99dda6ca6e379bb646c0
Saif Sultan <saif@gravitywiz.com>
2026-06-10 11:46:49 +0530

    `gw-day-count`: Added evolving snippet message.

--- gravity-forms/gw-day-count.php
+++ gravity-forms/gw-day-count.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * WARNING! THIS SNIPPET IS DEPRECATED. 🚧
- * This functionality is now available as part of our Perk called GP Advanced Calculations.
- * You can read more about it here: https://gravitywiz.com/documentation/gravity-forms-advanced-calculations/
+ * This snippet has evolved! 🦄
+ * This functionality is now available as part of our Perk called GP Advanced Calculations. Learn how to use it here:
+ * https://gravitywiz.com/calculate-the-number-of-days-between-two-dates-with-gravity-forms/
  */
