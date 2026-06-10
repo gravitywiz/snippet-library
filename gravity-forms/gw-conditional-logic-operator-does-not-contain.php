e1fd755d24c436dba91ec4a601958194588f6ce0
Saif Sultan <saifsultanc@gmail.com>
2026-06-10 11:48:44 +0530

    `gw-conditional-logic-operator-does-not-contain.php`: Added evolving snippet message.

--- gravity-forms/gw-conditional-logic-operator-does-not-contain.php
+++ gravity-forms/gw-conditional-logic-operator-does-not-contain.php
@@ -1,6 +1,6 @@
 <?php
 /**
  * This snippet has evolved! 🦄
- * Find the new version of this snippet here: 
+ * This functionality is now available as a perk called GP Advanced Conditional Logic.
  * https://gravitywiz.com/documentation/gravity-forms-advanced-conditional-logic/
  */
