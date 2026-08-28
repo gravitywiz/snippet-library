5216a8b477fb3467e12cbe79e1a2ca8de1ec7db8
Saif Sultan <saifsultanc@gmail.com>
2026-08-28 22:19:39 +0530

    `gpro-readonly-on-edit.php`: Updated snippet to support read only edit on GF Entries.

--- gp-read-only/gpro-readonly-on-edit.php
+++ gp-read-only/gpro-readonly-on-edit.php
@@ -3,7 +3,7 @@
  * Gravity Perks // GP Read Only // Set Fields as Readonly On Edit
  * https://gravitywiz.com/documentation/gravity-forms-read-only/
  *
- * Configure fields to be readonly when editing via Entry Blocks or GravityView or Gravity Flow User Input Step.
+ * Configure fields to be readonly when editing via Gravity Forms Entries Edit, Entry Blocks, GravityView or Gravity Flow User Input Step.
  *
  * Usage:
  *
@@ -14,10 +14,10 @@
  * Plugin URI:   https://gravitywiz.com/documentation/gravity-forms-read-only/
  * Description:  This snippet allows you to set read only for fields when editing.
  * Author:       Gravity Wiz
- * Version:      0.1
+ * Version:      0.2
  * Author URI:   https://gravitywiz.com/
  */
-
+add_filter( 'gform_admin_pre_render', 'gpeb_set_readonly_on_edit' );
 add_filter( 'gform_pre_render', 'gpeb_set_readonly_on_edit' );
 add_filter( 'gform_pre_process', 'gpeb_set_readonly_on_edit' );
 
@@ -30,9 +30,10 @@ function gpeb_set_readonly_on_edit( $form ) {
 
 	$is_gravityview  = function_exists( 'gravityview' ) && gravityview()->request->is_edit_entry();
 	$is_gravity_flow = rgget( 'lid' ) && rgget( 'page' ) == 'gravityflow-inbox';
+	$is_entry_detail = GFCommon::is_entry_detail();
 
 	// disable the target field for GPEB, GravityView and Gravity Flow User Input step.
-	if ( $is_block || $is_gravityview || $is_gravity_flow ) {
+	if ( $is_block || $is_gravityview || $is_gravity_flow || $is_entry_detail ) {
 		foreach ( $form['fields'] as &$field ) {
 			if ( strpos( $field->cssClass, 'gpro-readonly-on-edit' ) !== false ) {
 				$field->gwreadonly_enable = true;
@@ -42,3 +43,17 @@ function gpeb_set_readonly_on_edit( $form ) {
 
 	return $form;
 }
+
+add_filter( 'gform_field_input', function( $input_html, $field ) {
+	if ( GFCommon::is_entry_detail_edit() && rgar( $field, 'gwreadonly_enable' ) ) {
+		add_filter( 'gform_is_entry_detail', '__return_false' );
+	}
+
+	return $input_html;
+}, 9, 2 );
+
+add_filter( 'gform_field_input', function( $input_html ) {
+	remove_filter( 'gform_is_entry_detail', '__return_false' );
+
+	return $input_html;
+}, 12 );
