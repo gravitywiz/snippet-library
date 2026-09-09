ad22fd07921cb862fd675ddeb5e9da1922036a43
Saif Sultan <saif@gravitywiz.com>
2026-09-09 22:26:17 +0530

    `gpnf-listener-field.php`: Fixed an issue with parent value not populating live from the child field.

--- gp-nested-forms/gpnf-listener-field.php
+++ gp-nested-forms/gpnf-listener-field.php
@@ -8,9 +8,8 @@
  * # Current Limitations
  *
  * 1. The value will only be retrieved from the last submitted child entry in the designed Nested Form field.
- * 2. The Listener field's value will only update on the first submission.
  *
- * @version  0.1
+ * @version  0.2
  * @author   David Smith <david@gravitywiz.com>
  * @license  GPL-2.0+
  * @link     http://gravitywiz.com/documentation/gravity-forms-nested-forms/
@@ -19,7 +18,7 @@
  * Plugin URI:   http://gravitywiz.com/documentation/gravity-forms-nested-forms/
  * Description:  Add a form field that will listen for changes in a Nested Form field and update to pull the value of a designated child form field.
  * Author:       Gravity Wiz
- * Version:      0.1
+ * Version:      0.2
  * Author URI:   http://gravitywiz.com
  */
 class GPNF_Listener_Field {
@@ -43,27 +42,82 @@ class GPNF_Listener_Field {
 
 	public function init() {
 
-		// carry on
-		add_filter( "gform_save_field_value_{$this->_args['form_id']}_{$this->_args['nested_form_field_id']}", array( $this, 'refresh_value_on_save' ), 10, 5 );
+		add_filter( "gform_entry_post_save_{$this->_args['form_id']}", array( $this, 'refresh_value_on_save' ), 10, 2 );
+
+		add_action( 'gform_register_init_scripts', array( $this, 'register_init_script' ) );
 
 	}
 
-	public function refresh_value_on_save( $value, $entry, $field, $form, $input_id ) {
+	public function register_init_script( $form ) {
 
-		$child_entry_ids = explode( ',', $value );
+		if ( ! $this->is_applicable_form( $form ) ) {
+			return;
+		}
+
+		$script = "
+			( function( $ ) {
+
+				var formId        = {$form['id']},
+					nestedFieldId = {$this->_args['nested_form_field_id']},
+					targetFieldId = '{$this->_args['target_field_id']}',
+					sourceFieldId = '{$this->_args['source_field_id']}',
+					bound         = false;
+
+				// GPNF's instance is only guaranteed to exist once the child form initializes, which always happens
+				// before an entry can be added.
+				gform.addAction( 'gpnf_init_nested_form', function( nestedFormId, gpnf ) {
+
+					if ( bound || gpnf.formId != formId || gpnf.fieldId != nestedFieldId || ! gpnf.viewModel ) {
+						return;
+					}
+
+					bound = true;
+
+					gpnf.viewModel.entries.subscribe( function( entries ) {
+
+						var value = '';
+
+						if ( entries.length ) {
+							value = entries[ entries.length - 1 ][ 'f' + sourceFieldId ];
+							value = value && typeof value === 'object' ? value.value : value;
+						}
+
+						$( '#input_' + formId + '_' + targetFieldId.split( '.' ).join( '_' ) )
+							.val( value == null ? '' : value )
+							.trigger( 'change' );
+
+					} );
+
+				} );
+
+			} )( jQuery );
+		";
+
+		GFFormDisplay::add_init_script( $form['id'], 'gpnf_listener_field_' . $this->_args['target_field_id'], GFFormDisplay::ON_PAGE_RENDER, $script );
+
+	}
+
+	public function refresh_value_on_save( $entry, $form ) {
+
+		$value           = rgar( $entry, (string) $this->_args['nested_form_field_id'], rgpost( 'input_' . $this->_args['nested_form_field_id'] ) );
+		$child_entry_ids = array_filter( array_map( 'trim', explode( ',', (string) $value ) ) );
+
 		if ( empty( $child_entry_ids ) ) {
-			return $value;
+			return $entry;
 		}
 
 		$target_child_entry = GFAPI::get_entry( array_pop( $child_entry_ids ) );
 		if ( is_wp_error( $target_child_entry ) ) {
-			return $value;
+			return $entry;
 		}
 
-		$target_value = rgar( $target_child_entry, $this->_args['source_field_id'] );
-		GFAPI::update_entry_field( $entry['id'], $this->_args['target_field_id'], $target_value );
+		$target_value = rgar( $target_child_entry, (string) $this->_args['source_field_id'] );
 
-		return $value;
+		GFAPI::update_entry_field( $entry['id'], (string) $this->_args['target_field_id'], $target_value );
+
+		$entry[ (string) $this->_args['target_field_id'] ] = $target_value;
+
+		return $entry;
 	}
 
 	public function is_applicable_form( $form ) {
@@ -74,3 +128,10 @@ class GPNF_Listener_Field {
 	}
 
 }
+
+new GPNF_Listener_Field( array(
+	'form_id'              => 53,
+	'nested_form_field_id' => 1,
+	'target_field_id'      => 2,
+	'source_field_id'      => 2,
+) );
